59768cbe6b3c894a1f4b3ead301ee195a2862119
Adrian Webb <adrianwebb.78@gmail.com>
2018-08-18 20:57:03 +0000

    Using shared library functions in the rotate-keys script.

--- scripts/rotate-keys.py
+++ scripts/rotate-keys.py
@@ -2,47 +2,10 @@
 
 from pathlib import Path
 
+from shared import ssh_key, load_inventory, command, update_keys
+
 import argparse
 import os
-import stat
-import yaml
-import subprocess
-import shutil
-
-
-ssh_key = "keys/id_rsa"
-
-
-def command(command_str, stdin=None):
-    process = subprocess.Popen(command_str,
-                               shell=True,
-                               stdin=stdin,
-                               stdout=subprocess.PIPE,
-                               stderr=subprocess.PIPE)
-    
-    stdoutdata, stderrdata = process.communicate()
-        
-    return {
-        'stdout': stdoutdata, 
-        'stderr': stderrdata, 
-        'status': process.returncode
-    }
-
-def ssh_command(host, command_str, stdin=None):
-    return command("ssh {} {}".format(host, command_str))
-    
-
-def load_inventory():
-    inventory = None
-    
-    with open("inventory.yml", 'r') as stream:
-        try:
-            inventory = yaml.load(stream)
-
-        except yaml.YAMLError as error:
-            print(error)
-            
-    return inventory
 
 
 def generate_key():
@@ -75,17 +38,6 @@ def rotate_keys(inventory, ssh_pub_key):
         
         if info['status'] != 0:
             raise Exception("Error copying SSH key to host {}".format(host))
-
-
-def update_keys():
-    home = str(Path.home())
-    home_ssh_key = "{}/.ssh/id_rsa".format(home)
-    home_ssh_pub_key = "{}/.ssh/id_rsa.pub".format(home)
-    
-    shutil.copyfile(ssh_key, home_ssh_key)
-    os.chmod(home_ssh_key, stat.S_IRUSR | stat.S_IWUSR)
-    
-    shutil.copyfile("{}.pub".format(ssh_key), home_ssh_pub_key)
 
 
 def main():
