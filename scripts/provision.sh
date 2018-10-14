f4e38afa9f06c806f32cdf96a876c016644460b6
Adrian Webb <adrian@webb.sh>
2018-10-14 16:45:50 -0400

    Updating the provision script to support prompted password and environment selection.

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -6,7 +6,12 @@ set -e
 SCRIPT_DIR="$(cd "$(dirname "$([ `readlink "$0"` ] && echo "`readlink "$0"`" || echo "$0")")"; pwd -P)"
 cd "$SCRIPT_DIR/.."
 
-PASSWORD="${1}" # Required!!
+ENVIRONMENT="${1:-dev}"
+
+# Get sudo password
+echo -n "Password: " 
+read -s PASSWORD
+echo
 
 #-------------------------------------------------------------------------------
 
@@ -14,7 +19,7 @@ PASSWORD="${1}" # Required!!
 ./scripts/update-keys.py
 
 # Generate fresh host configuration
-./scripts/gen-host-config.py
+./scripts/gen-host-config.py "$ENVIRONMENT"
 
 # Provision cluster nodes
 ansible-playbook --become update-cluster.yml --extra-vars "ansible_become_pass=$PASSWORD"
