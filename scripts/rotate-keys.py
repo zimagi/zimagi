5acb85ff07379f8d9383df0586fd801f2e1b2f57
Adrian Webb <adrian@webb.sh>
2018-10-15 01:35:39 +0000

    Updates to the rotate-keys script to support different environments.

--- scripts/rotate-keys.py
+++ scripts/rotate-keys.py
@@ -1,29 +1,42 @@
 #!/usr/bin/env python3
 
-from shared import ssh_key, load_inventory, command, update_keys
+from shared import command, load_inventory, update_keys
 
 import argparse
 import os
 
 
-def generate_key():
-    info = command("ssh-keygen -y -q -t rsa -b 4096 -N '' -f {}".format(ssh_key))
+def generate_key(environment):
+    ssh_key = 'keys/id_rsa'
+    home_ssh_key = "{}/.ssh/id_rsa".format(os.getenv("HOME"))
+    
     ssh_prv_key = None
     ssh_pub_key = None
     
-    with open(ssh_key, 'r') as file:
-        ssh_prv_key = file.read()
+    if environment == 'dev':
+        with open(home_ssh_key, 'r') as file:
+            ssh_prv_key = file.read()
     
-    with open("{}.pub".format(ssh_key), 'r') as file:
-        ssh_pub_key = file.read()
+        with open("{}.pub".format(home_ssh_key), 'r') as file:
+            ssh_pub_key = file.read()
+    else:
+        info = command("ssh-keygen -y -q -t rsa -b 4096 -N '' -f {}".format(ssh_key))
     
-    if info['status'] != 0:
-        raise Exception("Error generating SSH key")
+        with open(ssh_key, 'r') as file:
+            ssh_prv_key = file.read()
+    
+        with open("{}.pub".format(ssh_key), 'r') as file:
+            ssh_pub_key = file.read()
+    
+        if info['status'] != 0:
+            raise Exception("Error generating SSH key")
 
     return (ssh_prv_key, ssh_pub_key)
 
 
 def rotate_keys(inventory, ssh_pub_key):
+    home_ssh_key = "{}/.ssh/id_rsa".format(os.getenv("HOME"))
+    
     nodes = inventory['masters']
     nodes.extend(inventory['nodes'])
     
@@ -32,19 +45,25 @@ def rotate_keys(inventory, ssh_pub_key):
         host = "{}@{}".format(server['user'], server['ip'])
         
         print("Configuring host: {}".format(host))
-        info = command('ssh {} "mkdir -p ~/.ssh" && scp {}.pub {}:~/.ssh/authorized_keys'.format(host, ssh_key, host))
+        info = command('ssh {} "mkdir -p ~/.ssh" && scp {}.pub {}:~/.ssh/authorized_keys'.format(host, home_ssh_key, host))
         
         if info['status'] != 0:
             raise Exception("Error copying SSH key to host {}".format(host))
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Initialize or rotate Kubernetes node SSH keys')    
+    parser = argparse.ArgumentParser(description='Initialize or rotate Kubernetes node SSH keys')
+    parser.add_argument('environment', nargs ='?', action = 'store', default = 'dev')
+       
     args = parser.parse_args()
-    inventory = load_inventory()
+    inventory = load_inventory(args.environment)
+    
+    if args.environment == 'dev':
+        # Ensure Vagrant keys are transferred / generated
+        update_keys(args.environment)
     
     # Generate new SSH keypair
-    ssh_prv_key, ssh_pub_key = generate_key()
+    ssh_prv_key, ssh_pub_key = generate_key(args.environment)
     
     # Print private key for backup storage
     print(ssh_prv_key)
@@ -54,7 +73,7 @@ def main():
     rotate_keys(inventory, ssh_pub_key)
     
     # Copy generated keys to the .ssh directory
-    update_keys()
+    update_keys(args.environment)
 
 
 if __name__ == "__main__":
