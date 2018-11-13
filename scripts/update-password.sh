96cdf26b4942049c6678c697e01ab66d10d0268e
Adrian Webb <adrian@webb.sh>
2018-11-13 06:23:31 -0500

    Adding an initial update-password script.

---
+++ scripts/update-password.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+# Update host user password on all ansible nodes
+
+set -e
+
+SCRIPT_DIR="$(cd "$(dirname "$([ `readlink "$0"` ] && echo "`readlink "$0"`" || echo "$0")")"; pwd -P)"
+cd "$SCRIPT_DIR/.."
+
+ENVIRONMENT="${1:-dev}"
+PLAYBOOK_CONFIG="playbooks/password.yml"
+
+# Get sudo password
+echo -n "Current password: " 
+read -s PASSWORD
+
+echo -n "New password: " 
+read -s NEW_PASSWORD
+
+echo -n "Retype new password: " 
+read -s CONFIRM_PASSWORD
+echo
+
+if [ "$NEW_PASSWORD" != "$CONFIRM_PASSWORD" ]
+then
+  echo "New password does not match!"
+  exit 1   
+fi
+
+#-------------------------------------------------------------------------------
+
+# Generate fresh host configuration
+./scripts/gen-host-config.py "$ENVIRONMENT"
+
+# Ensure the SSH keys are situated properly
+./scripts/update-keys.py "$ENVIRONMENT"
+
+# Provision cluster nodes
+ansible-playbook --become "$PLAYBOOK_CONFIG" --extra-vars "ansible_become_pass=$PASSWORD updated_password=$NEW_PASSWORD cluster_environment=$ENVIRONMENT"
