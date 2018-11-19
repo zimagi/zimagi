eea65a1b76cb666909920970cf41f40c468137cd
Adrian Webb <adrian@webb.sh>
2018-11-19 03:30:13 +0000

    Adding an initial hcp script.

---
+++ scripts/hcp.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Run a manage.py command from the app directory.
+
+SCRIPT_DIR="$(cd "$(dirname "$([ `readlink "$0"` ] && echo "`readlink "$0"`" || echo "$0")")"; pwd -P)"
+cd "$SCRIPT_DIR/../app"
+
+# Run manage.py from the app directory, regardless of where this run command was called
+./manage.py cli "$@"
