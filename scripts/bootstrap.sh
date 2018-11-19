f96bc4854be5284565fc480f958adbc1221df553
Adrian Webb <adrian@webb.sh>
2018-11-19 03:26:11 +0000

    Adding installation of Sqlite3 in the bootstrap script.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -14,15 +14,14 @@ cd "$PROJ_DIR"
 echo "> Updating OS package repositories"
 sudo apt-get update >/dev/null 2>&1
 
-#install basic dependencies
-if ! which git >/dev/null
-then
-  echo "> Installing Git version control"
-  sudo apt-get install -y git >/dev/null 2>&1
-fi
-
-echo "> Installing package management utilities"
+echo "> Ensuring package management utilities"
 sudo apt-get install -y software-properties-common >/dev/null 2>&1
+
+echo "> Ensuring Git version control"
+sudo apt-get install -y git >/dev/null 2>&1
+
+echo "> Ensuring Sqlite3 local database support"
+sudo apt-get install -y sqlite3 libsqlite3-dev >/dev/null 2>&1
 
 #install Python
 ./scripts/setup-python.sh
