97bad878008dc39433a9fbbffc3ec126389e4100
Adrian Webb <adrian@webb.sh>
2018-11-19 08:53:18 +0000

    Updating the app executable location in the hcp script.

--- scripts/hcp.py
+++ scripts/hcp.py
@@ -5,4 +5,4 @@ SCRIPT_DIR="$(cd "$(dirname "$([ `readlink "$0"` ] && echo "`readlink "$0"`" || 
 cd "$SCRIPT_DIR/../app"
 
 # Run manage.py from the app directory, regardless of where this run command was called
-./manage.py cli "$@"
+./hcp cli "$@"
