f215638ea6eb153f67f9c328b5ab03894e0d21f7
Adrian Webb <adrian@webb.sh>
2018-11-21 11:03:27 +0000

    First full implementation of the app CLI interface gateway.

--- app/systems/command/cli.py
+++ app/systems/command/cli.py
@@ -1,5 +1,7 @@
 from collections import OrderedDict, defaultdict
+from difflib import get_close_matches
 
+from django.conf import settings
 from django.core import management
 from django.core.management import ManagementUtility, find_commands, load_command_class
 from django.core.management.color import color_style
@@ -7,33 +9,17 @@ from django.core.management.base import (
     BaseCommand, CommandError, CommandParser, handle_default_options,
 )
 
+from utility.text import wrap
+from .base import ComplexCommand
+
+import django
 import os
+import sys
 import functools
 
 
 @functools.lru_cache(maxsize=None)
 def get_commands():
-    """
-    Return a dictionary mapping command names to their callback applications.
-
-    Look for a management.commands package in django.core, and in each
-    installed application -- if a commands package exists, register all
-    commands in that package.
-
-    Core commands are always included. If a settings module has been
-    specified, also include user-defined commands.
-
-    The dictionary is in the format {command_name: app_name}. Key-value
-    pairs from this dictionary can then be used in calls to
-    load_command_class(app_name, command_name)
-
-    If a specific version of a command must be loaded (e.g., with the
-    startapp command), the instantiated module can be placed in the
-    dictionary in place of the application name.
-
-    The dictionary is cached on the first call and reused on subsequent
-    calls.
-    """
     init_commands = management.get_commands()
     include_commands = {
         'django.core': [
@@ -56,54 +42,63 @@ def get_commands():
 
 
 class AppManagementUtility(ManagementUtility):
-    
-    def __init__(self, argv=None):
-        super(AppManagementUtility, self).__init__(argv)
-        self.prog_name = 'hcp'
 
+    def main_help_text(self):
+        style = color_style()
 
-    def main_help_text(self, commands_only=False):
-        if commands_only:
-            usage = sorted(get_commands())
-        else:
-            usage = [
-                "",
-                "Type '%s help <subcommand>' for help on a specific subcommand." % self.prog_name,
-                "",
-                "Available subcommands:",
-            ]
-            commands_dict = defaultdict(lambda: [])
-            for name, app in get_commands().items():
-                if app == 'django.core':
-                    app = 'django'
-                else:
-                    app = app.rpartition('.')[-1]
- 
-                commands_dict[app].append(name)
- 
-            style = color_style()
-            for app in sorted(commands_dict):
-                usage.append("")
-                usage.append(style.NOTICE("[%s]" % app))
- 
-                for name in sorted(commands_dict[app]):
-                    usage.append("    %s" % name)
- 
-            if self.settings_exception is not None:
-                usage.append(style.NOTICE(
-                    "Note that only Django core commands are listed "
-                    "as settings are not properly configured (error: %s)."
-                    % self.settings_exception))
- 
+        commands = {}
+        usage = [
+            "",
+            "Type '{}' for help on a specific subcommand.".format(
+                style.SUCCESS("{} help <subcommand> ...".format(settings.APP_NAME))
+            ),
+            "",
+            "available subcommands:",
+            ""
+        ]
+
+        def process_subcommands(name, command, usage, width, init_indent, indent):
+            if isinstance(command, ComplexCommand):
+                for info in command.get_subcommands():
+                    full_name = "{} {}".format(name, info[0])
+                    subcommand = command.subcommands[info[0]]
+                    usage.extend(wrap(
+                        subcommand.get_description(True), width,
+                        init_indent = "{:{width}}{}  -  ".format(' ', style.SUCCESS(full_name), width = init_indent),
+                        init_style = style.WARNING,
+                        indent      = "".ljust(indent)
+                    ))                    
+                    process_subcommands(full_name, subcommand, usage, width - 5, init_indent + 5, indent + 5)
+
+        for name, app in get_commands().items():
+            if app != 'django.core':
+                command = self.fetch_command(name)
+                get_priority = getattr(command, 'get_priority', None)
+
+                if callable(get_priority):
+                    priority = get_priority()
+
+                    if priority not in commands:
+                        commands[priority] = {}
+
+                    command_help = wrap(
+                        command.get_description(True), settings.DISPLAY_WIDTH,
+                        init_indent = " {}  -  ".format(style.SUCCESS(name)),
+                        init_style = style.WARNING,
+                        indent      = " {:5}".format(' ')
+                    )
+                    process_subcommands(name, command, command_help, settings.DISPLAY_WIDTH - 5, 6, 11)
+                    commands[priority][name] = command_help
+
+        for priority in sorted(commands.keys(), reverse=True):
+            for name, command_help in commands[priority].items():
+                usage.extend(command_help)
+        
         return '\n'.join(usage)
 
 
     def fetch_command(self, subcommand):
-        """
-        Try to fetch the given subcommand, printing a message with the
-        appropriate command called from the command line (usually
-        "django-admin" or "manage.py") if it can't be found.
-        """
+        style = color_style()
         commands = get_commands()
         try:
             app_name = commands[subcommand]
@@ -114,12 +109,12 @@ class AppManagementUtility(ManagementUtility):
                 sys.stderr.write("No Django settings specified.\n")
              
             possible_matches = get_close_matches(subcommand, commands)
-            sys.stderr.write('Unknown command: %r' % subcommand)
+            sys.stderr.write(style.ERROR("Unknown command: {}".format(subcommand)))
              
             if possible_matches:
-                sys.stderr.write('. Did you mean %s?' % possible_matches[0])
+                sys.stderr.write(". Did you mean {}?".format(possible_matches[0]))
              
-            sys.stderr.write("\nType '%s help' for usage.\n" % self.prog_name)
+            sys.stderr.write("\nType '{}' for usage.\n".format(style.SUCCESS("{} help".format(settings.APP_NAME))))
             sys.exit(1)
          
         if isinstance(app_name, BaseCommand):
@@ -130,70 +125,44 @@ class AppManagementUtility(ManagementUtility):
         return klass
 
 
-    def autocomplete(self):
-        """
-        Output completion suggestions for BASH.
-
-        The output of this function is passed to BASH's `COMREPLY` variable and
-        treated as completion suggestions. `COMREPLY` expects a space
-        separated string as the result.
-
-        The `COMP_WORDS` and `COMP_CWORD` BASH environment variables are used
-        to get information about the cli input. Please refer to the BASH
-        man-page for more information about this variables.
-
-        Subcommand options are saved as pairs. A pair consists of
-        the long option string (e.g. '--exclude') and a boolean
-        value indicating if the option requires arguments. When printing to
-        stdout, an equal sign is appended to options which require arguments.
-
-        Note: If debugging this function, it is recommended to write the debug
-        output in a separate file. Otherwise the debug output will be treated
-        and formatted as potential completion suggestions.
-        """
-        if 'DJANGO_AUTO_COMPLETE' not in os.environ:
-            return
- 
-        cwords = os.environ['COMP_WORDS'].split()[1:]
-        cword = int(os.environ['COMP_CWORD'])
- 
+    def execute(self):
         try:
-            curr = cwords[cword - 1]
+            subcommand = self.argv[1]
         except IndexError:
-            curr = ''
- 
-        subcommands = [*get_commands(), 'help']
-        options = [('--help', False)]
- 
-        if cword == 1:
-            print(' '.join(sorted(filter(lambda x: x.startswith(curr), subcommands))))
-         
-        elif cwords[0] in subcommands and cwords[0] != 'help':
-            subcommand_cls = self.fetch_command(cwords[0])
- 
-            if cwords[0] in ('dumpdata', 'sqlmigrate', 'sqlsequencereset', 'test'):
-                try:
-                    app_configs = apps.get_app_configs()
-                    options.extend((app_config.label, 0) for app_config in app_configs)
-                except ImportError:
-                    pass
- 
-            parser = subcommand_cls.create_parser('', cwords[0])
-            options.extend(
-                (min(s_opt.option_strings), s_opt.nargs != 0)
-                for s_opt in parser._actions if s_opt.option_strings
-            )
-            prev_opts = {x.split('=')[0] for x in cwords[1:cword - 1]}
-            options = (opt for opt in options if opt[0] not in prev_opts)
- 
-            options = sorted((k, v) for k, v in options if k.startswith(curr))
-            for opt_label, require_arg in options:
-                if require_arg:
-                    opt_label += '='
- 
-                print(opt_label)
-         
-        sys.exit(0)
+            subcommand = 'help'
+
+        parser = CommandParser(usage='%(prog)s subcommand [options] [args]', add_help=False, allow_abbrev=False)
+        parser.add_argument('--settings')
+        parser.add_argument('--pythonpath')
+        parser.add_argument('args', nargs='*')
+        try:
+            options, args = parser.parse_known_args(self.argv[2:])
+            handle_default_options(options)
+        except CommandError:
+            pass
+
+        try:
+            settings.INSTALLED_APPS
+        except ImproperlyConfigured as exc:
+            self.settings_exception = exc
+        except ImportError as exc:
+            self.settings_exception = exc
+
+        if settings.configured:
+            django.setup()
+
+        if subcommand == 'help':
+            if not options.args:
+                sys.stdout.write(self.main_help_text() + '\n')
+            else:
+                self.fetch_command(options.args[0]).print_help(self.prog_name, options.args)
+        
+        elif subcommand == 'version' or self.argv[1:] == ['--version']:
+            sys.stdout.write(django.get_version() + '\n')
+        elif self.argv[1:] in (['--help'], ['-h']):
+            sys.stdout.write(self.main_help_text() + '\n')
+        else:
+            self.fetch_command(subcommand).run_from_argv(self.argv)
 
 
 def execute_from_command_line(argv=None):
