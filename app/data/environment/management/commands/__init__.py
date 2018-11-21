2df0b2f7fae99308023c5a6e47a50ec2115a2b7e
Adrian Webb <adrian@webb.sh>
2018-11-21 08:17:13 -0500

    Including the environment get and clear commands in the environment command __init__ file.

--- app/data/environment/management/commands/__init__.py
+++ app/data/environment/management/commands/__init__.py
@@ -2,6 +2,8 @@
 from ._list import *
 from ._add import *
 from ._rm import *
+from ._clear import *
+from ._get import *
 from ._set import *
 
 
@@ -9,5 +11,7 @@ __all__ = [
     '_list',
     '_add',
     '_rm',
+    '_clear',
+    '_get',
     '_set'
 ]
