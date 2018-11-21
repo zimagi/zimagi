25846fa8968cc1b9a59b16a94a1d3f3decfb9f71
Adrian Webb <adrian@webb.sh>
2018-11-21 11:07:02 +0000

    Refactoring the base env command to align with new CLI / base command architecture.

--- app/data/environment/management/commands/env.py
+++ app/data/environment/management/commands/env.py
@@ -2,13 +2,45 @@
 from django.core.management.base import CommandError
 
 from systems.command import ComplexCommand
+from data.environment.management import commands as env
 
 
 class Command(ComplexCommand):
+
+    def get_priority(self):
+        return 5
+
+    def get_command_name(self):
+        return 'env'
+
+    def get_description(self, overview):
+        if overview:
+            return """manage cluster environments
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam 
+pulvinar nisl ac magna ultricies dignissim. Praesent eu feugiat 
+elit. Cras porta magna vel blandit euismod.
+"""
+        else:
+            return """manage cluster environments
+                      
+Etiam mattis iaculis felis eu pharetra. Nulla facilisi. 
+Duis placerat pulvinar urna et elementum. Mauris enim risus, 
+mattis vel risus quis, imperdiet convallis felis. Donec iaculis 
+tristique diam eget rutrum.
+
+Etiam sit amet mollis lacus. Nulla pretium, neque id porta feugiat, 
+erat sapien sollicitudin tellus, vel fermentum quam purus non sem. 
+Mauris venenatis eleifend nulla, ac facilisis nulla efficitur sed. 
+Etiam a ipsum odio. Curabitur magna mi, ornare sit amet nulla at, 
+scelerisque tristique leo. Curabitur ut faucibus leo, non tincidunt 
+velit. Aenean sit amet consequat mauris.
+"""
+
     
-    def add_arguments(self, parser):
-        super(Command, self).add_arguments(parser)
-
-
-    def handle(self, *args, **options):
-        super(Command, self).handle(*args, **options)
+    def get_subcommands(self):
+        return (
+            ('add', env.AddCommand),
+            ('rm', env.RemoveCommand),
+            ('set', env.SetCommand)
+        )
