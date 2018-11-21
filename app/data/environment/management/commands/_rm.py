c5209665b2e7175ef2a785d7c2fcb3479d926bf6
Adrian Webb <adrian@webb.sh>
2018-11-21 11:08:20 +0000

    Adding a scaffolding app env rm command.

---
+++ app/data/environment/management/commands/_rm.py
@@ -0,0 +1,38 @@
+
+from django.core.management.base import CommandError
+
+from systems.command import SimpleCommand
+
+
+class RemoveCommand(SimpleCommand):
+
+    def get_description(self, overview):
+        if overview:
+            return """remove an existing cluster environment
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam 
+pulvinar nisl ac magna ultricies dignissim. Praesent eu feugiat 
+elit. Cras porta magna vel blandit euismod.
+"""
+        else:
+            return """remove an existing cluster environment
+                      
+Etiam mattis iaculis felis eu pharetra. Nulla facilisi. 
+Duis placerat pulvinar urna et elementum. Mauris enim risus, 
+mattis vel risus quis, imperdiet convallis felis. Donec iaculis 
+tristique diam eget rutrum.
+
+Etiam sit amet mollis lacus. Nulla pretium, neque id porta feugiat, 
+erat sapien sollicitudin tellus, vel fermentum quam purus non sem. 
+Mauris venenatis eleifend nulla, ac facilisis nulla efficitur sed. 
+Etiam a ipsum odio. Curabitur magna mi, ornare sit amet nulla at, 
+scelerisque tristique leo. Curabitur ut faucibus leo, non tincidunt 
+velit. Aenean sit amet consequat mauris.
+"""
+
+    def add_arguments(self, parser):
+        pass
+
+
+    def handle(self, *args, **options):
+        print("Hello from rm!")
