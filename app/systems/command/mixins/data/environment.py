a0be68d08389190cb59157fab24578e4d6292b08
Adrian Webb <adrian@webb.sh>
2018-11-24 12:38:30 -0500

    Adding get_env and set_env methods, and updating the facade locations of the environment state calls in the environment command data mixin.

--- app/systems/command/mixins/data/environment.py
+++ app/systems/command/mixins/data/environment.py
@@ -26,8 +26,22 @@ class EnvironmentMixin(object):
         return models.Environment.facade
 
 
+    def get_env(self):
+        return self._env.get_curr()
+
+    def set_env(self, name):
+        if not self._env.retrieve(name):
+            self.error("Environment does not exist")
+
+        state, created = self._env.set_curr(name)
+
+        if created:
+            self.success(" > Successfully created environment state")
+        else:
+            self.success(" > Successfully updated environment state")
+
     def delete_env(self):
-        if self._state.delete_env():
+        if self._env.delete_curr():
             self.success(" > Successfully deleted environment state")
         else:
             self.error("Environment state deletion failed")
