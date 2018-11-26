b7d5df662c694f529d2fc611a8b54e083b1c5386
Adrian Webb <adrian@webb.sh>
2018-11-26 10:00:22 -0500

    Adding a plaintext API renderer for command output.

---
+++ app/systems/api/renderers.py
@@ -0,0 +1,11 @@
+
+from django.utils.encoding import smart_unicode
+from rest_framework import renderers
+
+
+class PlainTextRenderer(renderers.BaseRenderer):
+    media_type = 'text/plain'
+    format = 'txt'
+
+    def render(self, data, media_type=None, renderer_context=None):
+        return data.encode(self.charset)
