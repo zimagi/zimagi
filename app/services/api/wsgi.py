f31ddce1c2db9006c5fcb8500a6014052b16a0b0
Adrian Webb <adrian@webb.sh>
2018-11-26 10:03:42 -0500

    Adding an WSGI gateway in the API service.

---
+++ app/services/api/wsgi.py
@@ -0,0 +1,14 @@
+"""
+API WSGI config.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+from django.core.wsgi import get_wsgi_application
+
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "services.api.settings")
+application = get_wsgi_application()
