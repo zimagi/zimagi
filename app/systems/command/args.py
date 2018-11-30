49f86dc1e1ae0649a0145f188ca8a17626a34aa8
Adrian Webb <adrian@webb.sh>
2018-11-30 17:52:14 -0500

    Various updates to the command args functions.

--- app/systems/command/args.py
+++ app/systems/command/args.py
@@ -3,35 +3,57 @@ import argparse
 
 
 class SingleValue(argparse.Action):
-     def __call__(self, parser, namespace, values, option_string=None):
-         setattr(namespace, self.dest, values[0])
+    def __call__(self, parser, namespace, values, option_string = None):
+        values = values[0] if isinstance(values, (list, tuple)) else values
+        setattr(namespace, self.dest, values)
 
+class MultiValue(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string = None):
+        if not values:
+            values = []
+        setattr(namespace, self.dest, values)    
 
 class KeyValues(argparse.Action):
-     def __call__(self, parser, namespace, values, option_string=None):
-         options = {}
-         
-         for key_value in values:
-             key, value = key_value.split("=")
-             options[key] = value
+    def __call__(self, parser, namespace, values, option_string = None):
+        options = {}
+
+        if values: 
+            for key_value in values:
+                key, value = key_value.split("=")
+                options[key] = value
+        else:
+            options = {}
         
-         setattr(namespace, self.dest, options)
+        setattr(namespace, self.dest, options)
 
 
-def parse_var(parser, name, type, help):
+def parse_var(parser, name, type, help, optional = False):
+    nargs = '?' if optional else 1
     parser.add_argument(
             name,
             action = SingleValue,
-            nargs = 1, 
+            nargs = nargs, 
             type = type, 
             help = help
         )
 
-def parse_key_values(parser, name, value_label, help):
+def parse_vars(parser, name, value_label, type, help, optional = False):
+    nargs = '*' if optional else '+'
+    parser.add_argument(
+            name,
+            action = MultiValue,
+            nargs = nargs,
+            metavar = value_label, 
+            type = type, 
+            help = help
+        )
+
+def parse_key_values(parser, name, value_label, help, optional = False):
+    nargs = '*' if optional else '+'
     parser.add_argument(
         name, 
         action = KeyValues, 
-        nargs = '+', 
+        nargs = nargs, 
         metavar = value_label,
         help = help
     )
