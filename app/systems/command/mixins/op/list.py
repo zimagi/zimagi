e52bc5bf46c3c87a254926281c7a4bb34bdeda59
Adrian Webb <adrian@webb.sh>
2018-11-30 23:41:33 -0500

    Adding exec_processed_list method in the list op command mixin.

--- app/systems/command/mixins/op/list.py
+++ app/systems/command/mixins/op/list.py
@@ -6,6 +6,20 @@ class ListMixin(object):
             self.print_table(facade.render_values(*fields, **filters))
 
 
+    def exec_processed_list(self, facade, process_func, *fields, **filters):
+        if facade.count():
+            data = facade.render(facade.values(*fields, **filters))
+            key_index = data[0].index(facade.key())
+
+            for index, info in enumerate(data):
+                if index == 0:
+                    process_func('label', info, key_index)
+                else:
+                    process_func('data', info, key_index)
+
+            self.print_table(data)
+
+
     def exec_list_related(self, facade, key, relation, *fields, **filters):
         queryset = facade.related(key, relation, **filters)
         
