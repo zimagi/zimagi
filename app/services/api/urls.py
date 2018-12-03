9a9a47445c64210a51a071bcd87d85a65295f81e
Adrian Webb <adrian@webb.sh>
2018-12-02 20:52:00 -0500

    Adding an API schema url endpoint in the api service urls definition.

--- app/services/api/urls.py
+++ app/services/api/urls.py
@@ -1,8 +1,18 @@
+
 from django.conf.urls import include, url
 
+from rest_framework import renderers
+from rest_framework.schemas import get_schema_view
+
 from systems.api import routers
+from systems.api.schema import generator
 
 
 urlpatterns = [
-    url(r'^', include(routers.CommandAPIRouter().urls))
+    url(r'^', include(routers.CommandAPIRouter().urls)),
+    url('^schema$', get_schema_view(
+        title = "CE API",
+        generator_class = generator.CommandSchemaGenerator,
+        renderer_classes = [ renderers.CoreJSONRenderer ]
+    ))
 ]
