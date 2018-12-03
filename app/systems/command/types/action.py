06ba32de6b479cd3bf0c790623589abb6167fb6e
Adrian Webb <adrian@webb.sh>
2018-12-03 01:51:42 -0500

    Quite a few updates to the base action command to support API calls.

--- app/systems/command/types/action.py
+++ app/systems/command/types/action.py
@@ -6,7 +6,8 @@ from rest_framework.compat import coreapi, coreschema
 from rest_framework.schemas.inspectors import field_to_schema
 
 from systems.command import base
-from systems.command.mixins.data import environment as mixins
+from systems.command.mixins.core import CoreMixin
+from systems.command.mixins.data.environment import EnvironmentMixin
 from systems.api.schema import command
 from systems.api import client
 from utility.display import print_table
@@ -17,18 +18,20 @@ import re
 
 
 class ActionCommand(
-    mixins.EnvironmentMixin, 
+    CoreMixin,
+    EnvironmentMixin, 
     base.AppBaseCommand
 ):
-    def __init__(self, stdout=None, stderr=None, no_color=False):
+    def __init__(self, stdout = None, stderr = None, no_color = False):
         super().__init__(stdout, stderr, no_color)
         self.sh = sh
 
-        self.options = {}
         self.schema = {}
-
+        self.options = {}
         self.parser = None
-        self.parse()
+        self.parse_base()
+
+        self.api_exec = False
 
 
     def add_schema_field(self, name, field, optional = True):
@@ -47,21 +50,16 @@ class ActionCommand(
         # Override in subclass
         pass
 
+    def parse_base(self):
+        self.parse_verbosity()
+        self.parse_color()
+        self.parse()
+
     def add_arguments(self, parser):
         super().add_arguments(parser)
 
         self.parser = parser
-        self.parse()
-
-    def get_options(self, input):
-        schema = self.get_schema()
-        options = {}
-
-        for name, value in input.items():
-            if name in schema:
-                options[name] = getattr(self, "_render_{}".format(schema[name]))(value)
-
-        return options
+        self.parse_base()
 
 
     def exec(self):
@@ -70,26 +68,15 @@ class ActionCommand(
 
     def handle(self, *args, **options):
         env = self.get_env()
-
-        self.options = options
-        self.exec()
+        
+        if not self.api_exec and env and env.host and self.server_enabled():
+            api = client.API(env.host, env.port, env.token)
+            self.data("Environment", env.name)
+            self.info(api.execute(self.get_full_name(), options))
+        else:
+            self.options = options
+            self.exec()
 
 
     def print_table(self, data):
         print_table(data)
-
-    
-    def _render_str(self, value):
-        if isinstance(value, (tuple, list)):
-            return str(value[0])
-        return str(value)
-
-    def _render_list(self, value):
-        if not isinstance(value, (tuple, list)):
-            value = [value]
-        return list(value)
-
-    def _render_dict(self, value):
-        if isinstance(value, (tuple, list)):
-            value = value[0]
-        return json.loads(value)
