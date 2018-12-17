24d8cfce4e3a9c410f08266a12003fecacfacb81
Adrian Webb <adrian@webb.sh>
2018-12-17 11:44:16 +0000

    Adding prefix display in the print_table display utility function.

--- app/utility/display.py
+++ app/utility/display.py
@@ -1,5 +1,14 @@
 from terminaltables import AsciiTable
 
 
-def print_table(data):
-    print(AsciiTable(data).table)
+def print_table(data, prefix = None):
+    table_rows = AsciiTable(data).table.splitlines()
+    prefixed_rows = []
+
+    if prefix:
+        for row in table_rows:
+            prefixed_rows.append("{}{}".format(prefix, row))
+    else:
+        prefixed_rows = table_rows
+    
+    print("\n".join(prefixed_rows))
