35c6eb11d1947eb1a07a5a6f171dfa6e80aafcc7
Adrian Webb <adrian@webb.sh>
2018-12-20 13:59:10 +0000

    Initial implementation of server add command.

--- app/data/server/management/commands/_server/add.py
+++ app/data/server/management/commands/_server/add.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 
 from systems.command.types import action
 from systems.command import mixins
@@ -31,9 +32,65 @@ Etiam a ipsum odio. Curabitur magna mi, ornare sit amet nulla at,
 scelerisque tristique leo. Curabitur ut faucibus leo, non tincidunt 
 velit. Aenean sit amet consequat mauris.
 """
+    def server_field_help(self):
+        help = ["server fields as key value pairs (by provider)", ' ']
+
+        def render(messages = '', prefix = ''):
+            if not isinstance(messages, (tuple, list)):
+                messages = [messages]
+
+            for message in messages:
+                help.append("{}{}".format(prefix, message))
+
+        for name, provider in settings.CLOUD_PROVIDERS.items():
+            cloud = self.cloud(name)
+            schema = cloud.server_schema()
+
+            render(("provider_name: {} ({})".format(self.success_color(name), cloud.name), ' '))
+
+            if schema['requirements']:
+                render('requirements:', '  ')
+                for require in schema['requirements']:
+                    render("{} - {}".format(self.warning_color(require['name']), require['help']), '    ')
+                render()
+
+            if schema['options']:
+                render('options:', '  ')
+                for option in schema['options']:
+                    render("{} ({}) - {}".format(self.warning_color(option['name']), self.success_color(str(option['default'])), option['help']), '    ')
+
+            render()
+
+        return help
+
+
     def parse(self):
-        self.parse_server()
-        self.parse_server_fields()
+        self.parse_provider_name()
+        self.parse_server_groups()
+        self.parse_server_fields(optional = True, help_callback = self.server_field_help)
 
     def exec(self):
-        self.exec_add(self._server, self.server_name, self.server_fields)
+        def complete_callback(server):
+            self.success(server)
+
+            instance = self.exec_add(self._server, server.name, {
+                'config': server.config,
+                'type': server.type,
+                'region': server.region,
+                'ip': server.ip,
+                'user': server.user,
+                'password': server.password,
+                'private_key': server.private_key,
+                'data_device': server.data_device
+            })
+            self.exec_add_related(
+                self._server_group, 
+                instance, 'groups', 
+                self.server_groups
+            )
+
+        self.provider.create_servers(
+            self.server_fields, 
+            self.server_groups, 
+            complete_callback
+        )        
