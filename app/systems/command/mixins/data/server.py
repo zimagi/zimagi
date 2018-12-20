05a0f93ecd43f72de7b7a4c5e5b2ae3827cab5c5
Adrian Webb <adrian@webb.sh>
2018-12-20 13:58:32 +0000

    Adding cloud provider support and some general refactoring in the server command system data mixin.

--- app/systems/command/mixins/data/server.py
+++ app/systems/command/mixins/data/server.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 
 from systems.command import args
 from data.server import models
@@ -6,9 +7,30 @@ from utility import text
 
 class ServerMixin(object):
 
-    def parse_server(self, optional = False):
-        name = 'server'
-        help_text = 'server name'
+    def parse_provider_name(self, optional = False):
+        name = 'provider_name'
+        help_text = 'resource provider'
+
+        self._provider = None
+        self.add_schema_field(name,
+            args.parse_var(self.parser, name, str, help_text, optional),
+            optional
+        )
+
+    @property
+    def provider_name(self):
+        return self.options.get('provider_name', None)
+
+    @property
+    def provider(self):
+        if not self._provider:
+            self._provider = self.cloud(self.provider_name)
+        return self._provider
+
+
+    def parse_server_name(self, optional = False):
+        name = 'server_name'
+        help_text = 'unique environment server name'
 
         self._data_server = None
         self.add_schema_field(name,
@@ -18,7 +40,7 @@ class ServerMixin(object):
 
     @property
     def server_name(self):
-        return self.options['server']
+        return self.options.get('server_name', None)
 
     @property
     def server(self):
@@ -31,13 +53,16 @@ class ServerMixin(object):
         return self._data_server
 
 
-    def parse_server_fields(self, optional = False):
+    def parse_server_fields(self, optional = False, help_callback = None):
         name = 'server_fields'
 
-        excluded_fields = ('created', 'updated', 'environment')
-        required = [x for x in self._server.required if x not in list(excluded_fields) + ['ssh_ip']]
-        optional = [x for x in self._server.optional if x not in excluded_fields]
-        help_text = "\n".join(text.wrap("server fields as key value pairs\n\ncreate required: {}\n\nupdate available: {}".format(", ".join(required), ", ".join(optional)), 60))
+        if help_callback and callable(help_callback):
+            help_text = "\n".join(help_callback())
+        else:
+            excluded_fields = ('created', 'updated', 'environment')
+            required = [x for x in self._server.required if x not in list(excluded_fields)]
+            optional = [x for x in self._server.optional if x not in excluded_fields]
+            help_text = "\n".join(text.wrap("provider fields as key value pairs\n\nrequirements: {}\n\noptions: {}".format(", ".join(required), ", ".join(optional)), 60))
 
         self.add_schema_field(name,
             args.parse_key_values(self.parser, name, 'field=value', help_text, optional),
@@ -46,12 +71,26 @@ class ServerMixin(object):
 
     @property
     def server_fields(self):
-        return self.options['server_fields']
+        return self.options.get('server_fields', {})
+
+
+    def parse_server_groups(self):
+        name = 'server_groups'
+        help_text = 'one or more server group names (comma separated)'
+
+        self.add_schema_field(name,
+            args.parse_csv_option(self.parser, name, '--groups', help_text, None),
+            True
+        )
+
+    @property
+    def server_groups(self):
+        return self.options.get('server_groups', [])
 
 
     @property
-    def _group(self):
-        return models.Group.facade
+    def _server_group(self):
+        return models.ServerGroup.facade
     
     @property
     def _server(self):
