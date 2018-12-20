a98e9bfa0046957619785f726323938b7c1d1af2
Adrian Webb <adrian@webb.sh>
2018-12-20 13:46:42 +0000

    Fixing the parameter processing and normalization in the command API system view.

--- app/systems/api/views.py
+++ app/systems/api/views.py
@@ -4,6 +4,7 @@ from django.http import StreamingHttpResponse
 from rest_framework.views import APIView
 
 import sys
+import json
 
 
 class ExecuteCommand(APIView):
@@ -25,10 +26,27 @@ class ExecuteCommand(APIView):
         return self._request(request, request.POST, format)
 
 
-    def _request(self, request, options, format = None):
+    def _request(self, request, params, format = None):
+        params = self._format_params(params)
         response = StreamingHttpResponse(
-            streaming_content = self.command.handle_api(options),
+            streaming_content = self.command.handle_api(params),
             content_type = 'application/json'
         )
         response['Cache-Control'] = 'no-cache'
         return response
+
+    def _format_params(self, data):
+        fields = self.schema.get_fields()
+        params = {}
+
+        for key, value in data.items():
+            type = fields[key].type
+            
+            if type == 'dictfield':
+                params[key] = json.loads(value)
+            elif type == 'listfield':
+                params[key] = json.loads(value)    
+            else:
+                params[key] = value
+        
+        return params
