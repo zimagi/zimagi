c77a59be9157b49afbf7143428b1567ec4e05bab
Adrian Webb <adrian@webb.sh>
2018-12-23 11:24:41 -0500

    Fixing gunicorn parameters.

--- app/scripts/start-ce.sh
+++ app/scripts/start-ce.sh
@@ -14,16 +14,15 @@ echo "> Clearing outdated locks" | tee -a "$LOG_FILE"
 ce clear_locks >>"$LOG_FILE" 2>&1
 
 echo "> Starting application" | tee -a "$LOG_FILE"
-gunicorn \
-  --cert-reqs 2 \
-  --ssl-version 2 \
-  --do-handshake-on-connect True \
-  --ca-certs /etc/ssl/certs/cenv-ca.crt \
-  --certfile /etc/ssl/certs/cenv.crt \
-  --keyfile /etc/ssl/private/cenv.key \
-  -k gevent \
-  -w 4 \
-  --threads 12 \
-  --worker-connections 100 \
-  -b 0.0.0.0:5123 \
-  services.api.wsgi:application
+gunicorn services.api.wsgi:application \
+  --cert-reqs=2 \
+  --ssl-version=2 \
+  --do-handshake-on-connect \
+  --ca-certs=/etc/ssl/certs/cenv-ca.crt \
+  --certfile=/etc/ssl/certs/cenv.crt \
+  --keyfile=/etc/ssl/private/cenv.key \
+  --worker-class=gevent \
+  --workers=4 \
+  --threads=12 \
+  --worker-connections=100 \
+  --bind=0.0.0.0:5123
