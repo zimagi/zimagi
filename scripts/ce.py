71cb8074fd353a8c1d24d7d74387ca717f92d5d5
Adrian Webb <adrian@webb.sh>
2018-12-23 05:14:52 +0000

    Renaming and refactoring the ce gateway CLI script.

--- scripts/ce.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Run a command from the app directory.
-
-SCRIPT_DIR="$(cd "$(dirname "$([ `readlink "$0"` ] && echo "`readlink "$0"`" || echo "$0")")"; pwd -P)"
-cd "$SCRIPT_DIR/../app"
-
-# Run manage.py from the app directory, regardless of where this run command was called
-./ce "$@"
