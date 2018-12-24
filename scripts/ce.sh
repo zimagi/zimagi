1dbc78d61ba0054e9b922010305b600e79b75fa0
Adrian Webb <adrian@webb.sh>
2018-12-24 06:03:27 -0500

    Parameter updates to the ce Dockerized client.

--- scripts/ce.sh
+++ scripts/ce.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #-------------------------------------------------------------------------------
-docker run \
-    -v /opt/cenv/app:/usr/local/share/cenv \
-    -v /var/local/cenv:/var/local/cenv \
-    -it cenv "$@"
+docker run --interactive --tty \
+    --network host \
+    --volume /opt/cenv/app:/usr/local/share/cenv \
+    --volume /var/local/cenv:/var/local/cenv \
+    cenv "$@"
