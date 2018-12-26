f7284b4555a608d6f1760f8dde798d70125acff5
Adrian Webb <adrian@webb.sh>
2018-12-26 18:56:51 +0000

    Removing the migrate command call in the API service url definition.

--- app/services/api/urls.py
+++ app/services/api/urls.py
@@ -1,17 +1,11 @@
 
 from django.conf.urls import include, url
-from django.core.management import call_command
 
 from rest_framework import renderers
 from rest_framework.schemas import get_schema_view
 
 from systems.api import routers
 from systems.api.schema import generator
-from utility.display import suppress_stdout
-
-
-with suppress_stdout():
-    call_command('migrate', interactive = False)
 
 
 urlpatterns = [
