802a92c9a3332d7a8acddfadb64c5d1656a76e6f
Adrian Webb <adrian@webb.sh>
2018-12-26 19:04:06 +0000

    Adding an environment file reference to the core config in the Dockerized ce script.

--- scripts/ce.sh
+++ scripts/ce.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #-------------------------------------------------------------------------------
 docker run --interactive --tty \
+    --env-file /opt/cenv/config/core \
     --network host \
     --volume /opt/cenv/app:/usr/local/share/cenv \
     --volume /var/local/cenv:/var/local/cenv \
