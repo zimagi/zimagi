f0cfb046b1b030742b1d09b808cc319db58cddc6
Adrian Webb <adrian@webb.sh>
2018-12-26 18:59:59 +0000

    Adding an initial database system backend mixin library.

---
+++ app/systems/db/backends/mixins.py
@@ -0,0 +1,21 @@
+from django.core.management import call_command
+
+from systems.db import manager
+
+
+class DatabaseRecoveryMixin(object):
+    
+    def load(self, data, encrypted = True):
+        call_command('migrate', interactive = False, verbosity = 0)
+        manager.DatabaseManager(self.alias).load(data, encrypted)
+    
+    def load_file(self, file_path = None, encrypted = True):
+        call_command('migrate', interactive = False, verbosity = 0)
+        manager.DatabaseManager(self.alias).load_file(file_path, encrypted)
+
+
+    def save(self, package, encrypted = True):
+        manager.DatabaseManager(self.alias).save(package, encrypted)
+        
+    def save_file(self, package, file_path = None, encrypted = True):
+        manager.DatabaseManager(self.alias).save_file(package, file_path, encrypted)
