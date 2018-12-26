f447d8ea22b28f73c0555d84b245ac5b5b96c9f0
Adrian Webb <adrian@webb.sh>
2018-12-26 19:00:48 +0000

    Adding an override DatabaseWrapper for the SQLite3 database.

---
+++ app/systems/db/backends/sqlite3/base.py
@@ -0,0 +1,14 @@
+from django.db.backends.sqlite3 import base as sqlite3
+
+from systems.db.backends import mixins
+
+
+class DatabaseWrapper(mixins.DatabaseRecoveryMixin, sqlite3.DatabaseWrapper):
+    
+    def connect(self):
+        super().connect()
+        self.load_file()
+    
+    def close(self):
+        self.save_file('all')
+        super().close()
