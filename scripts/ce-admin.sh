6e098fa6cda5bde648e13bdd126aff4febfaaadc
Adrian Webb <adrian@webb.sh>
2018-12-27 16:42:37 +0000

    Removing the non postgresql Dockerized client in the ca-admin script.

--- scripts/ce-admin.sh
+++ scripts/ce-admin.sh
@@ -1,24 +1,12 @@
 #!/usr/bin/env bash
 #-------------------------------------------------------------------------------
-
-if [ ! -z "$POSTGRES_HOST" -a ! -z "$POSTGRES_PORT" ]
-then
-    docker run --interactive --tty \
-        --env LOGLEVEL \
-        --env "POSTGRES_HOST=$POSTGRES_HOST" \
-        --env "POSTGRES_PORT=$POSTGRES_PORT" \
-        --env-file /opt/cenv/config/core \
-        --env-file /opt/cenv/config/pg.credentials \
-        --network host \
-        --volume /opt/cenv/app:/usr/local/share/cenv \
-        --volume /var/local/cenv:/var/local/cenv \
-        cenv "$@"    
-else
-    docker run --interactive --tty \
-        --env LOGLEVEL \
-        --env-file /opt/cenv/config/core \
-        --network host \
-        --volume /opt/cenv/app:/usr/local/share/cenv \
-        --volume /var/local/cenv:/var/local/cenv \
-        cenv "$@"
-fi
+docker run --interactive --tty \
+    --env LOGLEVEL \
+    --env "POSTGRES_HOST=localhost" \
+    --env "POSTGRES_PORT=5432" \
+    --env-file /opt/cenv/config/core \
+    --env-file /opt/cenv/config/pg.credentials \
+    --network host \
+    --volume /opt/cenv/app:/usr/local/share/cenv \
+    --volume /var/local/cenv:/var/local/cenv \
+    cenv "$@"
