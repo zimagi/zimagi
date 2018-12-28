3a0296a8ff75333c137d8e906d88f5081fe0d920
Adrian Webb <adrian@webb.sh>
2018-12-28 11:17:26 +0000

    Adding __init__ method overrides in the system db sqlite3 and postgresql backends.

--- app/systems/db/backends/postgresql/base.py
+++ app/systems/db/backends/postgresql/base.py
@@ -1,7 +1,10 @@
+from django.db import DEFAULT_DB_ALIAS
 from django.db.backends.postgresql import base as postgresql
 
 from systems.db.backends import mixins
 
 
 class DatabaseWrapper(mixins.DatabaseRecoveryMixin, postgresql.DatabaseWrapper):
-    pass
+    
+    def __init__(self, settings_dict, alias = DEFAULT_DB_ALIAS, allow_thread_sharing = True):
+        super().__init__(settings_dict, alias, True)
--- app/systems/db/backends/sqlite3/base.py
+++ app/systems/db/backends/sqlite3/base.py
@@ -1,9 +1,15 @@
+from django.db import DEFAULT_DB_ALIAS
 from django.db.backends.sqlite3 import base as sqlite3
 
 from systems.db.backends import mixins
 
 
 class DatabaseWrapper(mixins.DatabaseRecoveryMixin, sqlite3.DatabaseWrapper):
+
+    def __init__(self, settings_dict, alias = DEFAULT_DB_ALIAS, allow_thread_sharing = True):
+        settings_dict['NAME'] = ':memory:'
+        super().__init__(settings_dict, alias, True)
+
     
     def connect(self):
         super().connect()
