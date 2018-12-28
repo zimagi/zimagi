2dc1f6c45da3d4250a65d4413cd4a8e2ea083fe3
Adrian Webb <adrian@webb.sh>
2018-12-28 19:43:36 +0000

    Including the db and config command types in the __init__ file of the system command types library.

--- app/systems/command/types/__init__.py
+++ app/systems/command/types/__init__.py
@@ -1,7 +1,9 @@
 
 from .router import *
 from .action import *
+from .db import *
 from .environment import *
+from .config import *
 from .user import *
 from .user_group import *
 from .user_token import *
@@ -11,7 +13,9 @@ from .server_group import *
 __all__ = [
     'router',
     'action',
+    'db',
     'environment',
+    'config',
     'user',
     'user_group',
     'user_token',
