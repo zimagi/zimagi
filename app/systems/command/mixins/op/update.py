1c3e5f2108a2434344d32a0e7e282cff413859b3
Adrian Webb <adrian@webb.sh>
2019-01-03 04:17:04 +0000

    Updating success and error messages and adding instance return value in the command update operation mixin.

--- app/systems/command/mixins/op/update.py
+++ app/systems/command/mixins/op/update.py
@@ -6,20 +6,22 @@ class UpdateMixin(OpMixin):
     def exec_update(self, facade, name, fields = {}):
         for field in fields.keys():
             if field not in facade.fields:
-                self.error("Given field {} is not in {}".format(field, facade.name))
+                self.error("Given field {} is not in {} (available: {})".format(field, facade.name, ", ".join(facade.fields)))
 
         instance, created = facade.store(name, **fields)
         
         if instance:
-            self.success("Successfully updated {}".format(facade.name))
+            self.success("Successfully updated {} {}".format(facade.name, name))
         else:
-            self.error("{} update failed".format(facade.name.title()))
+            self.error("{} {} update failed".format(facade.name.title(), name))
+
+        return instance
 
 
     def exec_update_related(self, facade, instance, relation, keys, **fields):
         for field in fields.keys():
             if field not in facade.fields:
-                self.error("Given field {} is not in {}".format(field, facade.name))
+                self.error("Given field {} is not in {} (available: {})".format(field, facade.name, ", ".join(facade.fields)))
 
         queryset = query.get_queryset(instance, relation)
         instance_name = type(instance).__name__.lower()
