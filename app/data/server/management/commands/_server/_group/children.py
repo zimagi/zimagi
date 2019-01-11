5db5aae162fbc9363e523ef1d73fd0e146a48aee
Adrian Webb <adrian@webb.sh>
2019-01-11 09:28:19 +0000

    Fixing command parameter issue with the server group children command.

--- app/data/server/management/commands/_server/_group/children.py
+++ app/data/server/management/commands/_server/_group/children.py
@@ -31,7 +31,7 @@ velit. Aenean sit amet consequat mauris.
 """
     def parse(self):
         self.parse_server_group(help_text = 'parent server group name')
-        self.parse_server_groups(help_text = 'one or more child server group names')
+        self.parse_server_groups(False, help_text = 'one or more child server group names')
 
     def exec(self):
         parent = self.exec_update(self._server_group, self.server_group_name)
