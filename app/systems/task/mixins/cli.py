b8e87b1f35fa5877dd4670309a59ff21d6e8a971
Adrian Webb <adrian@webb.sh>
2019-01-18 13:15:16 +0000

    Adding an initial task provider cli mixin.

---
+++ app/systems/task/mixins/cli.py
@@ -0,0 +1,28 @@
+
+class CLITaskMixin(object):
+    
+    def _parse_args(self, args, overrides = None):
+        if overrides:
+            overrides = overrides.split(',') if isinstance(overrides, str) else overrides
+            arg_count = len(args)
+
+            for index, value in enumerate(overrides):
+                if index >= arg_count:
+                    args.append(value)
+                else:
+                    args[index] = value
+
+    def _parse_options(self, options, overrides = None):
+        if overrides:
+            for key, value in overrides.items():
+                options[key] = value    
+
+    def _ssh_exec(self, server, command, args = [], options = {}, sudo = False, ssh = None):
+        if not ssh:
+            ssh = server.compute_provider.ssh()
+        
+        if sudo:
+            ssh.sudo(command, *args, **options)
+        else:
+            ssh.exec(command, *args, **options)
+
