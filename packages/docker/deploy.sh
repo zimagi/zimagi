2721c1940437a774edd4984a877833c194ffb545
Adrian Webb <adrian@webb.sh>
2019-01-22 14:23:30 -0500

    Test updates to the CircleCI configuration and docker deploy script.

--- packages/docker/deploy.sh
+++ packages/docker/deploy.sh
@@ -2,9 +2,13 @@
 #-------------------------------------------------------------------------------
 set -e
 
+echo '1'
+
 SCRIPT_DIR="$(cd "$(dirname "$([ `readlink "$0"` ] && echo "`readlink "$0"`" || echo "$0")")"; pwd -P)"
 cd "$SCRIPT_DIR/../../app"
 #-------------------------------------------------------------------------------
+
+echo '2'
 
 if [ -z "$DOCKER_USER" ]
 then
@@ -17,10 +21,12 @@ then
     exit 1
 fi
 
+echo '3'
+
 VERSION=$(cat "settings/version.py" | egrep -o '[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+' | head -n1)
 
-echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USER" --password-stdin
+#echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USER" --password-stdin
 
-docker build -f Dockerfile -t cenv/cenv:latest -t "cenv/cenv:${VERSION}" .
-docker push cenv/cenv:latest
-docker push "cenv/cenv:${VERSION}"
+#docker build -f Dockerfile -t cenv/cenv:latest -t "cenv/cenv:${VERSION}" .
+#docker push cenv/cenv:latest
+#docker push "cenv/cenv:${VERSION}"
