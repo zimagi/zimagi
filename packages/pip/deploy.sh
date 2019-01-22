49d18431eaf1c17b7c78c96e99f33ab5dd1696c1
Adrian Webb <adrian@webb.sh>
2019-01-22 14:49:17 -0500

    Updates for testing the pip package deploy script.

--- packages/pip/deploy.sh
+++ packages/pip/deploy.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 #-------------------------------------------------------------------------------
+set -e
+
 SCRIPT_DIR="$(cd "$(dirname "$([ `readlink "$0"` ] && echo "`readlink "$0"`" || echo "$0")")"; pwd -P)"
 cd "$SCRIPT_DIR"
 #-------------------------------------------------------------------------------
 
 STATUS=0
+
+echo '1'
 
 if [ -z "$PIP_USER" ]
 then
@@ -16,6 +20,8 @@ then
     echo "PIP_PASSWORD environment variable must be defined to deploy application"
     exit 1
 fi
+
+echo '2'
 
 if [ ! -f ~/.pypirc ]
 then
@@ -30,15 +36,8 @@ password: $PIP_PASSWORD
 " > ~/.pypirc
 fi
 
-python3 -m pip install --no-cache-dir setuptools wheel twine \
-    && python3 setup.py sdist bdist_wheel --owner=root --group=root \
-    && python3 -m twine upload dist/*
+echo '3'
 
-if [ $? -ne 0 ]
-then
-    STATUS=1
-fi
-rm -Rf build
-rm -Rf cenv.egg-info
-rm -Rf dist
-exit $STATUS
+python3 -m pip install --no-cache-dir setuptools wheel twine
+python3 setup.py sdist bdist_wheel --owner=root --group=root
+python3 -m twine upload dist/*
