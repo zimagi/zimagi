a2bf147ee3bfdbcea68b70007583b9b4d1d6328d
Adrian Webb <adrian@webb.sh>
2019-01-22 11:01:55 -0500

    Adding user/password environment variables in the pip deploy script.

--- packaging/pip/deploy.sh
+++ packaging/pip/deploy.sh
@@ -6,6 +6,30 @@ SCRIPT_DIR="$(cd "$(dirname "$([ `readlink "$0"` ] && echo "`readlink "$0"`" || 
 cd "$SCRIPT_DIR"
 #-------------------------------------------------------------------------------
 
+if [ -z "$PIP_USER" ]
+then
+    echo "PIP_USER environment variable must be defined to deploy application"
+    exit 1
+fi
+if [ -z "$PIP_PASSWORD" ]
+then
+    echo "PIP_PASSWORD environment variable must be defined to deploy application"
+    exit 1
+fi
+
+if [ ! -f ~/.pypirc ]
+then
+    echo "
+[distutils]
+index-servers=
+    pypi
+
+[pypi]
+username: $PIP_USER
+password: $PIP_PASSWORD
+" > ~/.pypirc
+fi
+
 sudo pip3 install --no-cache-dir --upgrade setuptools wheel twine
 sudo python3 setup.py sdist bdist_wheel --owner=root --group=root
 sudo python3 -m twine upload dist/*
