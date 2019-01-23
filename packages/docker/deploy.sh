c5709f3b7fe9c5b9b3433d883e2771d4a651af17
Adrian Webb <adrian@webb.sh>
2019-01-23 03:42:27 -0500

    Uncommenting the docker package deploy logic.

--- packages/docker/deploy.sh
+++ packages/docker/deploy.sh
@@ -17,20 +17,14 @@ then
     exit 1
 fi
 
-cat certs/cenv-ca.key
-echo "\n\n"
-cat certs/cenv-ca.crt
-#cat certs/cenv.key
-#cat certs/cenv.crt
+VERSION=$(cat "settings/version.py" | egrep -o '[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+' | head -n1)
 
-#VERSION=$(cat "settings/version.py" | egrep -o '[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+' | head -n1)
+echo "Logging into DockerHub"
+echo "$PKG_DOCKER_PASSWORD" | docker login --username "$PKG_DOCKER_USER" --password-stdin
 
-#echo "Logging into DockerHub"
-#echo "$PKG_DOCKER_PASSWORD" | docker login --username "$PKG_DOCKER_USER" --password-stdin
+echo "Building Docker image"
+docker build -f Dockerfile -t cenv/cenv:latest -t "cenv/cenv:${VERSION}" .
 
-#echo "Building Docker image"
-#docker build -f Dockerfile -t cenv/cenv:latest -t "cenv/cenv:${VERSION}" .
-
-#echo "Pushing Docker image with tags"
-#docker push cenv/cenv:latest
-#docker push "cenv/cenv:${VERSION}"
+echo "Pushing Docker image with tags"
+docker push cenv/cenv:latest
+docker push "cenv/cenv:${VERSION}"
