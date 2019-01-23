8301295e0304df0fac304c423fb1c03f478a21cd
Adrian Webb <adrian@webb.sh>
2019-01-23 02:41:35 -0500

    Updates to the CircleCI configuration and pip and docker deploy scripts.

--- packages/docker/deploy.sh
+++ packages/docker/deploy.sh
@@ -6,21 +6,21 @@ SCRIPT_DIR="$(cd "$(dirname "$([ `readlink "$0"` ] && echo "`readlink "$0"`" || 
 cd "$SCRIPT_DIR/../../app"
 #-------------------------------------------------------------------------------
 
-if [ -z "$DOCKER_USER" ]
+if [ -z "$PKG_DOCKER_USER" ]
 then
-    echo "DOCKER_USER environment variable must be defined to deploy application"
+    echo "PKG_DOCKER_USER environment variable must be defined to deploy application"
     exit 1
 fi
-if [ -z "$DOCKER_PASSWORD" ]
+if [ -z "$PKG_DOCKER_PASSWORD" ]
 then
-    echo "DOCKER_PASSWORD environment variable must be defined to deploy application"
+    echo "PKG_DOCKER_PASSWORD environment variable must be defined to deploy application"
     exit 1
 fi
 
 VERSION=$(cat "settings/version.py" | egrep -o '[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+' | head -n1)
 
 echo "Logging into DockerHub"
-echo "$DOCKER_PASSWORD" | docker login --username "$DOCKER_USER" --password-stdin
+echo "$PKG_DOCKER_PASSWORD" | docker login --username "$PKG_DOCKER_USER" --password-stdin
 
 echo "Building Dockerfile"
 docker build -f Dockerfile -t cenv/cenv:latest -t "cenv/cenv:${VERSION}" .
--- packages/pip/deploy.sh
+++ packages/pip/deploy.sh
@@ -6,31 +6,36 @@ SCRIPT_DIR="$(cd "$(dirname "$([ `readlink "$0"` ] && echo "`readlink "$0"`" || 
 cd "$SCRIPT_DIR"
 #-------------------------------------------------------------------------------
 
-if [ -z "$PIP_USER" ]
+if [ -z "$PKG_PIP_USER" ]
 then
-    echo "PIP_USER environment variable must be defined to deploy application"
+    echo "PKG_PIP_USER environment variable must be defined to deploy application"
     exit 1
 fi
-if [ -z "$PIP_PASSWORD" ]
+if [ -z "$PKG_PIP_PASSWORD" ]
 then
-    echo "PIP_PASSWORD environment variable must be defined to deploy application"
+    echo "PKG_PIP_PASSWORD environment variable must be defined to deploy application"
     exit 1
 fi
 
+echo "Creating PyPi configuration"
 if [ ! -f ~/.pypirc ]
 then
     echo "
 [distutils]
-index-servers:
-    pypi
+index-servers = pypi
 
 [pypi]
-username: $PIP_USER
-password: $PIP_PASSWORD
+username: $PKG_PIP_USER
+password: $PKG_PIP_PASSWORD
 " > ~/.pypirc
 fi
 chmod 600 ~/.pypirc
 
-pip3 install --no-cache-dir setuptools wheel twine
+echo "Installing pip build tools"
+python3 -m pip install --no-cache-dir --upgrade setuptools wheel twine
+
+echo "Building pip distribution"
 python3 setup.py sdist bdist_wheel --owner=root --group=root
-twine upload dist/*
+
+echo "Distributing to PyPi repository"
+python3 -m twine upload dist/*
