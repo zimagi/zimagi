f040bbc225b36f93ee713253816e670801191223
Adrian Webb <adrian@webb.sh>
2019-01-23 05:00:27 -0500

    Updating the docker build context in the docker package deploy script.

--- packages/docker/deploy.sh
+++ packages/docker/deploy.sh
@@ -3,7 +3,7 @@
 set -e
 
 SCRIPT_DIR="$(cd "$(dirname "$([ `readlink "$0"` ] && echo "`readlink "$0"`" || echo "$0")")"; pwd -P)"
-cd "$SCRIPT_DIR/../../app"
+cd "$SCRIPT_DIR/../.."
 #-------------------------------------------------------------------------------
 
 if [ -z "$PKG_DOCKER_USER" ]
@@ -17,13 +17,13 @@ then
     exit 1
 fi
 
-VERSION=$(cat "settings/version.py" | egrep -o '[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+' | head -n1)
+VERSION=$(cat "app/settings/version.py" | egrep -o '[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+' | head -n1)
 
 echo "Logging into DockerHub"
 echo "$PKG_DOCKER_PASSWORD" | docker login --username "$PKG_DOCKER_USER" --password-stdin
 
 echo "Building Docker image"
-docker build -f Dockerfile -t cenv/cenv:latest -t "cenv/cenv:${VERSION}" .
+docker build -f app/Dockerfile -t cenv/cenv:latest -t "cenv/cenv:${VERSION}" .
 
 echo "Pushing Docker image with tags"
 docker push cenv/cenv:latest
