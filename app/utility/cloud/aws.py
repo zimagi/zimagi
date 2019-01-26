f86cab427a78713d292f94fbe8e068d2c8c949e4
Adrian Webb <adrian@webb.sh>
2019-01-26 07:36:21 -0500

    Removing network and security group related methods from the aws cloud utility class.

--- app/utility/cloud/aws.py
+++ app/utility/cloud/aws.py
@@ -1,30 +1,8 @@
 
 import boto3
-import netaddr
 import requests
 import string
 import random
-
-
-class AWSIngressRule(object):
-
-    def __init__(self, from_port, to_port = None, ip_protocol = 'tcp', cidrs = '0.0.0.0/0'):
-        self.from_port = from_port
-        self.to_port = to_port if to_port is not None else from_port
-        self.ip_protocol = ip_protocol
-        self.cidrs = [cidrs] if not isinstance(cidrs, (list, tuple)) else cidrs
-
-    def export(self):
-        data = {
-            'FromPort': self.from_port,
-            'ToPort': self.to_port,
-            'IpProtocol': self.ip_protocol,
-            'IpRanges': []    
-        }
-        for cidr in self.cidrs:
-            data['IpRanges'].append({ 'CidrIp': cidr })
-
-        return data
 
 
 class AWSServiceMixin(object):
@@ -88,57 +66,6 @@ class AWSServiceMixin(object):
                 errors.append("Zone '{}' is not available, consider: {}".format(value, ", ".join(zones)))
         else:
             errors.append("Region is required to validate availability zone")            
-
-
-    def get_subnets(self, cidr, subnet_block_size):
-        ip_space = netaddr.IPNetwork(cidr)
-        return list(ip_space.subnet(subnet_block_size))
-
-    def get_block_size(self, cidr):
-        ip_space = netaddr.IPNetwork(cidr)
-        return ip_space.prefixlen
-
-
-    def _create_subnet(self, ec2, vpc, zone, subnet_cidr, public_ips = True):
-        response = ec2.create_subnet(
-            VpcId = vpc,
-            AvailabilityZone = zone,
-            CidrBlock = subnet_cidr
-        )
-        subnet_id = response['Subnet']['SubnetId']
-
-        if public_ips:
-            ec2.modify_subnet_attribute(
-                SubnetId = subnet_id,
-                MapPublicIpOnLaunch = {
-                    'Value': True
-                } 
-            )
-        
-        return subnet_id
-
-    def _create_security_group(self, ec2, vpc, name):
-        response = ec2.create_security_group(
-            VpcId = vpc,
-            GroupName = name,
-            Description = name
-        )
-        return response['GroupId']
-
-    def _add_ingress_rules(self, ec2, sgroup_id, rules):
-        permissions = []
-
-        for rule in rules:
-            permissions.append(rule.export())
-
-        ec2.authorize_security_group_ingress(
-            GroupId = sgroup_id,
-            IpPermissions = permissions
-        )
-
-    @property
-    def ingress_rule(self):
-        return AWSIngressRule
 
 
     def get_external_ip(self):
