18b2abb5e35b3ec108bb140f289f37cd3a4580b4
Adrian Webb <adrian@webb.sh>
2019-01-27 18:58:22 -0500

    Updating the config access methods in the AWS cloud utility mixin.

--- app/utility/cloud/aws.py
+++ app/utility/cloud/aws.py
@@ -10,8 +10,8 @@ class AWSServiceMixin(object):
     def _init_session(self):
         if not getattr(self, 'session', None):
             try:
-                access_key = self.command.required_config('aws_access_key').strip()
-                secret_key = self.command.required_config('aws_secret_key').strip()
+                access_key = self.command.get_config('aws_access_key', required = True).strip()
+                secret_key = self.command.get_config('aws_secret_key', required = True).strip()
             except Exception:
                 self.command.error("To use AWS provider you must have 'aws_access_key' and 'aws_secret_key' environment configurations; see: config set")
 
