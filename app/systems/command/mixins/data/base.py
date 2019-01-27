49b7ffbcdf210653834610beb39dab2251e489ef
Adrian Webb <adrian@webb.sh>
2019-01-27 18:43:36 -0500

    Adding check_exists, facade accessor, and updating value labels in the base command system data mixin.

--- app/systems/command/mixins/data/base.py
+++ app/systems/command/mixins/data/base.py
@@ -2,14 +2,18 @@ from systems.command import args
 from utility import text
 
 import re
+import copy
 
 
 class DataMixin(object):
     
-    def parse_variable(self, name, optional, type, help_text):
+    def parse_variable(self, name, optional, type, help_text, value_label = None):
         if optional and isinstance(optional, str):
+            if not value_label:
+                value_label = name
+            
             self.add_schema_field(name,
-                args.parse_option(self.parser, name, optional, type, help_text, None),
+                args.parse_option(self.parser, name, value_label.upper(), optional, type, help_text, None),
                 True
             )
         else:
@@ -18,16 +22,20 @@ class DataMixin(object):
                 optional
             )
 
-    def parse_variables(self, name, value_label, optional, type, help_text):
+    def parse_variables(self, name, optional, type, help_text, value_label = None):
         if optional and isinstance(optional, str):
             help_text = "{} (comma separated)".format(help_text)
+
+            if not value_label:
+                value_label = name
+
             self.add_schema_field(name,
-                args.parse_csv_option(self.parser, name, optional, help_text, None),
+                args.parse_csv_option(self.parser, name, value_label.upper(), optional, help_text, None),
                 True
             )
         else:
             self.add_schema_field(name,
-                args.parse_vars(self.parser, name, value_label, type, help_text, optional),
+                args.parse_vars(self.parser, name, type, help_text, optional),
                 optional
             )
 
@@ -40,13 +48,13 @@ class DataMixin(object):
             help_text = "\n".join(text.wrap("fields as key value pairs\n\nrequirements: {}\n\noptions: {}".format(", ".join(required), ", ".join(optional)), 60))
 
         self.add_schema_field(name,
-            args.parse_key_values(self.parser, name, 'field=value', help_text, optional),
+            args.parse_key_values(self.parser, name, 'field=VALUE', help_text, optional),
             optional
         )
 
 
     def check_available(self, facade, name):
-        instance = self.get_instance(facade, name, error_on_not_found = False)
+        instance = self.get_instance(facade, name, required = False)
         if instance:
             self.warning("{} {} already exists".format(
                 facade.name.title(),
@@ -55,14 +63,24 @@ class DataMixin(object):
             return False        
         return True
 
+    def check_exists(self, facade, name):
+        instance = self.get_instance(facade, name, required = False)
+        if not instance:
+            self.warning("{} {} does not exist".format(
+                facade.name.title(),
+                name
+            ))
+            return False        
+        return True
 
-    def get_instance(self, facade, name, error_on_not_found = True):
+
+    def get_instance(self, facade, name, required = True):
         instance = self._get_cache_instance(facade, name)
 
         if not instance:
             instance = facade.retrieve(name)
-
-            if not instance and error_on_not_found:
+            
+            if not instance and required:
                 self.error("{} {} does not exist".format(facade.name.title(), name))
             else:
                 if instance:
@@ -101,9 +119,11 @@ class DataMixin(object):
         if not search_items and not names and not objects and not states:
             search_items = facade.all()
 
-        def init_instance(instance, state):
-            if isinstance(instance, str):
-                instance = facade.retrieve(instance)
+        def init_instance(input, state):
+            if isinstance(input, str):
+                instance = facade.retrieve(input)
+            else:
+                instance = input
             
             if instance:
                 cached = self._get_cache_instance(facade, instance.name)
@@ -119,7 +139,7 @@ class DataMixin(object):
                 if instance and (not states or instance.state in states):
                     instances.append(instance)
             else:
-                self.error("Instance {} does not exist".format(instance))
+                self.error("{} instance {} does not exist".format(facade.name.title(), input))
 
         self.run_list(search_items, init_instance)
         return instances
@@ -167,6 +187,16 @@ class DataMixin(object):
                 self.warning("No {} instances were found".format(facade.name))
         
         return results
+
+
+    def facade(self, facade):
+        if getattr(self, '_facade_cache', None) is None:
+            self._facade_cache = {}
+        
+        if not self._facade_cache.get(facade.name, None):
+            self._facade_cache[facade.name] = copy.deepcopy(facade)
+
+        return self._facade_cache[facade.name]
 
 
     def _init_instance_cache(self, facade):
