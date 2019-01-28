edad660720d3e50615fe0ce94e1db5a9b9b5bcb7
Adrian Webb <adrian@webb.sh>
2019-01-28 12:34:21 -0500

    Adding field label capabilities in the command system list operation mixin.

--- app/systems/command/mixins/op/list.py
+++ app/systems/command/mixins/op/list.py
@@ -5,7 +5,12 @@ class ListMixin(OpMixin):
     
     def exec_list(self, facade, *fields, **filters):
         if facade.count():
-            self.table(facade.render_values(*fields, **filters), '{}_list'.format(facade.name))
+            field_names, field_labels = self._get_field_info(fields)
+            
+            data = facade.render_values(*field_names, **filters)
+            data[0] = field_labels
+            
+            self.table(data, '{}_list'.format(facade.name))
 
 
     def _exec_processed_list(self, facade, process_func, *fields, **filters):
@@ -24,13 +29,19 @@ class ListMixin(OpMixin):
         return data
 
     def exec_processed_list(self, facade, process_func, *fields, **filters):
-        data = self._exec_processed_list(facade, process_func, *fields, **filters)
+        field_names, field_labels = self._get_field_info(fields)
+
+        data = self._exec_processed_list(facade, process_func, *field_names, **filters)
         if len(data):
+            data[0] = field_labels + data[0][len(field_labels):]
             self.table(data, '{}_list'.format(facade.name))
 
     def exec_processed_sectioned_list(self, facade, process_func, *fields, **filters):
-        data = self._exec_processed_list(facade, process_func, *fields, **filters)
+        field_names, field_labels = self._get_field_info(fields)
+
+        data = self._exec_processed_list(facade, process_func, *field_names, **filters)
         if len(data):
+            data[0] = field_labels + data[0][len(field_labels):]
             for index in range(1, len(data)):
                 section_data = [data[0]]
                 section_data.append(data[index])
@@ -38,7 +49,25 @@ class ListMixin(OpMixin):
 
 
     def exec_list_related(self, facade, key, relation, relation_facade, *fields, **filters):
+        field_names, field_labels = self._get_field_info(fields)
         queryset = facade.related(key, relation, **filters)
         
         if queryset:
-            self.table(relation_facade.render(fields, queryset.values(*fields)), '{}_{}_list'.format(facade.name, relation))
+            data = relation_facade.render(fields, queryset.values(*field_names))
+            data[0] = field_labels
+            self.table(data, '{}_{}_list'.format(facade.name, relation))
+
+
+    def _get_field_info(self, fields):
+        field_names = []
+        field_labels = []
+
+        for field in fields:
+            if isinstance(field, (list, tuple)):
+                field_names.append(field[0])
+                field_labels.append(field[1])
+            else:
+                field_names.append(field)
+                field_labels.append(field)
+        
+        return (field_names, field_labels)
