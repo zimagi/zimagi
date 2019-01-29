36368a300d893caed6360f1b149b50ecb231015d
Adrian Webb <adrian@webb.sh>
2019-01-29 17:21:46 -0500

    Adding a create_token function in the data utility library.

--- app/utility/data.py
+++ app/utility/data.py
@@ -1,3 +1,11 @@
+import string
+import random
+
 
 def clean_dict(data):
     return {key: value for key, value in data.items() if value is not None}
+
+
+def create_token():
+    chars = string.ascii_uppercase + string.digits
+    return ''.join(random.SystemRandom().choice(chars) for _ in range(32))
