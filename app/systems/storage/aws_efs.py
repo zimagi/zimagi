972e16b64c0fc65128fb2f2f6aac4f03e4130224
Adrian Webb <adrian@webb.sh>
2019-01-29 17:29:51 -0500

    Refactoring the storage providers to support split storage source and mount types.

--- app/systems/storage/aws_efs.py
+++ app/systems/storage/aws_efs.py
@@ -1,5 +1,6 @@
 from utility import cloud
 from .base import BaseStorageProvider
+from utility import data
 
 import time
 import json
@@ -8,111 +9,70 @@ import json
 class AWSEFS(cloud.AWSServiceMixin, BaseStorageProvider):
 
     def provider_config(self, type = None):
-        self.requirement('vpc', help = 'AWS VPC identifier (ex: vpc-20810ee6)', config_name = 'aws_vpc_id')
-        self.option('cidr', None, help = 'AWS EFS subnet base network CIDR address (within VPC CIDR)', config_name = 'aws_efs_base_cidr')
+        if type == 'storage':
+            self.option('performance_mode', 'generalPurpose', help = 'AWS EFS performance mode (can also be: maxIO)', config_name = 'aws_efs_perf_mode')
+            self.option('throughput_mode', 'bursting', help = 'AWS EFS throughput mode (can also be: provisioned)', config_name = 'aws_efs_tp_mode')
+            self.option('provisioned_throughput', 125, help = 'AWS EFS throughput in MiB/s', config_name = 'aws_efs_prov_tp')
         
-        self.option('region', 'us-east-1', self.validate_region, self.efs, help = 'AWS region name', config_name = 'aws_region')
-        self.option('sgroups', None, help = 'One or more AWS security group ids', config_name = 'aws_efs_sgroups')
-
-        self.option('performance_mode', 'generalPurpose', help = 'AWS EFS performance mode (can also be: maxIO)', config_name = 'aws_efs_perf_mode')
-        self.option('throughput_mode', 'bursting', help = 'AWS EFS throughput mode (can also be: provisioned)', config_name = 'aws_efs_tp_mode')
-        self.option('provisioned_throughput', 125, help = 'AWS EFS throughput in MiB/s', config_name = 'aws_efs_prov_tp')
+        elif type == 'mount':
+            pass
 
 
-    def initialize_provider_filesystem(self):
-        ec2 = self.ec2(self.config['region'])
-        efs = self.efs(self.config['region'])
+    def create_provider_storage(self, storage):
+        if storage.network.type != 'aws_vpc':
+            self.command.error("AWS VPC network needed to create AWS EFS storage filesystems")
 
-        self.config['list'] = self.zones(self.ec2, self.config['region'])
+        efs = self.efs(storage.network.config['region'])
 
-        if not self.config['cidr']:
-            response = ec2.describe_vpcs(
-                VpcIds = [ self.config['vpc'] ]
-            )
-            # VPC CIDR (x.x.255.255 between 16 and 28)
-            self.config['cidr'] = response['Vpcs'][0]['CidrBlock']
-        
-        # Subnet CIDR (between 16 and 28)
-        subnets = self.get_subnets(self.config['cidr'], 28)
-
-        if len(subnets) < len(self.config['list']):
-            self.command.error("Length of available subnet CIDRs should be greater than {}".format(len(self.config['list'])))
-
-        self.config['subnet_cidrs'] = {}
-        for index, zone in enumerate(self.config['list']):
-            self.config['subnet_cidrs'][zone] = str(subnets[index])
-
-        self.config['creation_token'] = self.create_token()
+        storage.config['creation_token'] = data.create_token()
         options = {
-            'CreationToken': self.config['creation_token'],
-            'PerformanceMode': self.config['performance_mode'],
-            'ThroughputMode': self.config['throughput_mode']
+            'CreationToken': storage.config['creation_token'],
+            'PerformanceMode': storage.config['performance_mode'],
+            'ThroughputMode': storage.config['throughput_mode']
         }
-        if self.config['throughput_mode'] == 'provisioned':
-            options['ProvisionedThroughputInMibps'] = self.config['provisioned_throughput']
+        if storage.config['throughput_mode'] == 'provisioned':
+            options['ProvisionedThroughputInMibps'] = storage.config['provisioned_throughput']
         
         response = efs.create_file_system(**options)
-        self.config['filesystem_id'] = response['FileSystemId']
-        self._get_filesystem(efs, self.config['filesystem_id'])
+        storage.config['filesystem_id'] = response['FileSystemId']
 
 
-    def create_provider_storage_mount(self, zone, storage):
-        storage.region = storage.config['region']
-        storage.zone = zone
-
-        ec2 = self.ec2(storage.region)
-        efs = self.efs(storage.region)
-
-        storage.config['subnet_id'] = self._create_subnet(ec2, 
-            storage.config['vpc'], 
-            storage.zone, 
-            storage.config['subnet_cidrs'][storage.zone]
+    def destroy_provider_storage(self):
+        efs = self.efs(self.storage.network.config['region'])
+        efs.delete_file_system(
+            FileSystemId = self.storage.config['filesystem_id']
         )
+
+
+    def create_provider_mount(self, mount):
+        efs = self.efs(self.storage.network.config['region'])
+
         options = {
-            'FileSystemId': storage.config['filesystem_id'],
-            'SubnetId': storage.config['subnet_id']
+            'FileSystemId': self.storage.config['filesystem_id'],
+            'SubnetId': mount.subnet.config['subnet_id']
         }
-        if storage.config['sgroups']:
-            sgroups = storage.config['sgroups']
-            options['SecurityGroups'] = [sgroups] if isinstance(sgroups, str) else sgroups
+        if mount.firewalls:
+            sgroups = self.get_security_groups(mount.firewalls)
+            options['SecurityGroups'] = sgroups
 
         response = efs.create_mount_target(**options)
+        mount.config['target_id'] = response['MountTargetId']
+        
+        mount.remote_host = response['IpAddress']
+        mount.remote_path = '/'
+        mount.mount_options = 'nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 0 0'
 
-        storage.name = response['MountTargetId']
-        storage.fs_name = storage.config['filesystem_id']
-        storage.remote_host = response['IpAddress']
-        storage.remote_path = '/'
-        storage.mount_options = 'nfs4 nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 0 0'
+
+    def update_provider_mount_firewalls(self, mount, firewalls):
+        efs = self.efs(self.storage.network.config['region'])
+        efs.modify_mount_target_security_groups(
+            MountTargetId = mount.config['target_id'],
+            SecurityGroups = self.get_security_groups(firewalls)
+        )
 
 
-    def destroy_provider_storage_mount(self):
-        ec2 = self.ec2(self.storage.region)
-        efs = self.efs(self.storage.region)
-
+    def destroy_provider_mount(self, mount):
+        efs = self.efs(self.storage.network.config['region'])
         efs.delete_mount_target(
-            MountTargetId = self.storage.name
+            MountTargetId = mount.config['target_id']
         )
-        ec2.delete_subnet(
-            SubnetId = self.storage.config['subnet_id']
-        )
-
-
-    def _get_filesystem(self, efs, name, tries = 5, interval = 2):
-        while True:
-            try:
-                result = efs.describe_file_systems(FileSystemId = name)
-                filesystem = result['FileSystems'][0]
-            
-                if not filesystem['LifeCycleState'] in ('creating', 'updating', 'deleting'):
-                    break
-                
-                time.sleep(interval)
-            
-            except Exception as e:
-                if not tries:
-                    self.command.error(e)
-                
-                time.sleep(interval)
-                tries -= 1
-
-        return filesystem
--- app/systems/storage/base.py
+++ app/systems/storage/base.py
@@ -9,31 +9,40 @@ import copy
 
 class StorageResult(object):
 
-    def __init__(self, type, config,
+    def __init__(self, type, network, config,
+        name = None
+    ):
+        self.type = type
+        self.network = network
+        self.config = copy.deepcopy(config)
+        self.name = name
+
+    def __str__(self):
+        return "[{}:{}]> {}".format(
+            self.type,
+            self.network.name,
+            self.name         
+        )
+
+
+class StorageMountResult(object):
+
+    def __init__(self, subnet, firewalls, config,
         name = None,
-        fs_name = None,
-        region = None,
-        zone = None,
         remote_host = None,
         remote_path = None,
         mount_options = None
     ):
-        self.type = type
+        self.subnet = subnet
+        self.firewalls = firewalls
         self.config = copy.deepcopy(config)
         self.name = name
-        self.fs_name = fs_name
-        self.region = region
-        self.zone = zone
         self.remote_host = remote_host
         self.remote_path = remote_path
         self.mount_options = mount_options
 
     def __str__(self):
-        return "[{}:{}:{}]> {}/{} ({}:{})".format(
-            self.type,
-            self.region,
-            self.zone,
-            self.fs_name,
+        return "[{}]> {}{}".format(
             self.name,
             self.remote_host,
             self.remote_path         
@@ -42,54 +51,49 @@ class StorageResult(object):
 
 class BaseStorageProvider(providers.BaseCommandProvider):
 
-    def __init__(self, name, command, storage = None):
+    def __init__(self, name, command, filesystem = None):
         super().__init__(name, command)
 
-        self.storage = storage
+        self.filesystem = filesystem
         self.thread_lock = threading.Lock()
 
         self.provider_type = 'storage'
         self.provider_options = settings.STORAGE_PROVIDERS
 
 
-    def create_storage_mounts(self, config, complete_callback = None):
+    def create_storage_name(self):
+        state_variable = 'storage_name_index'
+        name_index = int(self.command.get_state(state_variable, 1))
+        
+        self.command.set_state(state_variable, name_index)
+        return "cs{}".format(name_index)
+
+
+    def create_storage(self, network, config):
         self.config = config
         
-        self.provider_config()
+        self.provider_config('storage')
         self.validate()
 
-        def storage_callback(arg):
-            storage = StorageResult(self.name, self.config)
+        storage = StorageResult(self.name, network, config)
+        storage.name = self.create_storage_name()
 
-            for key, value in self.config.items():
-                if hasattr(storage, key) and key not in ('type', 'config'):
-                    setattr(storage, key, value)
+        for key, value in self.config.items():
+            if hasattr(storage, key) and key not in ('type', 'config', 'network'):
+                setattr(storage, key, value)
 
-            return storage
+        self.create_provider_storage(storage)
+        return storage
 
-        self.initialize_provider_filesystem()
-
-        return self.command.run_list(
-            self.config.pop('list', [0]), 
-            self.create_provider_storage_mount,
-            state_callback = storage_callback,
-            complete_callback = complete_callback
-        )
-
-    def initialize_provider_filesystem(self):
+    def create_provider_storage(self, storage):
         # Override in subclass
         pass
 
-    def create_provider_storage_mount(self, index, storage):
-        # Override in subclass
-        pass
-
-
-    def destroy_storage_mount(self, abort = False):
+    def destroy_storage(self, abort = False):
         if not self.storage:
-            self.command.error("Destroying storage mount requires a valid storage instance given to provider on initialization")
+            self.command.error("Destroying storage requires a valid storage instance given to provider on initialization")
         try:
-            self.destroy_provider_storage_mount()
+            self.destroy_provider_storage()
         
         except Exception as e:
             if abort:
@@ -97,6 +101,58 @@ class BaseStorageProvider(providers.BaseCommandProvider):
             else:
                 self.command.warning(str(e))
 
-    def destroy_provider_storage_mount(self):
+    def destroy_provider_storage(self):
+        # Override in subclass
+        pass
+
+
+    def create_mount(self, subnet, config, firewalls = []):
+        self.config = config
+        
+        self.provider_config('mount')
+        self.validate()
+
+        mount = StorageMountResult(subnet, firewalls, config)
+
+        for key, value in self.config.items():
+            if hasattr(mount, key) and key not in ('config', 'subnet', 'firewalls'):
+                setattr(mount, key, value)
+
+        self.create_provider_storage_mount(mount)
+        return mount
+
+    def create_provider_mount(self, mount):
+        # Override in subclass
+        pass
+
+    def update_mount_firewalls(self, mount, firewalls):
+        if not self.storage:
+            self.command.error("Updating storage mount firewalls requires a valid storage instance given to provider on initialization")
+        try:
+            self.update_provider_mount_firewalls(mount, firewalls)
+        
+        except Exception as e:
+            if abort:
+                raise e
+            else:
+                self.command.warning(str(e))
+
+    def update_provider_mount_firewalls(self, mount, firewalls):
+        # Override in subclass
+        pass
+
+    def destroy_mount(self, mount, abort = False):
+        if not self.storage:
+            self.command.error("Destroying storage mount requires a valid storage instance given to provider on initialization")
+        try:
+            self.destroy_provider_storage_mount(mount)
+        
+        except Exception as e:
+            if abort:
+                raise e
+            else:
+                self.command.warning(str(e))
+
+    def destroy_provider_mount(self, mount):
         # Override in subclass
         pass
