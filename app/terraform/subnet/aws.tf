2e815cdff6e3f86a83e81fd2e7c6cb4bcbee7bbf
Adrian Webb <adrian@webb.sh>
2019-02-06 15:45:59 -0500

    Updating the terraform AWS subnet manifest variable names.

--- app/terraform/subnet/aws.tf
+++ app/terraform/subnet/aws.tf
@@ -2,12 +2,12 @@
 provider "aws" {
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
-  region = "${var.network["region"]}"
+  region = "${var.network.region}"
 }
 
 resource "aws_subnet" "network" {
-  vpc_id = "${var.network["vpc"]}"
-  availability_zone_id = "${var.zone}"
+  vpc_id = "${var.network.vpc}"
+  availability_zone = "${var.zone}"
   cidr_block = "${var.cidr}"
   map_public_ip_on_launch = "${var.public_ip}"
 
@@ -18,5 +18,5 @@ resource "aws_subnet" "network" {
 
 resource "aws_route_table_association" "network" {
   subnet_id      = "${aws_subnet.network.id}"
-  route_table_id = "${var.network["route_table"]}"
+  route_table_id = "${var.network.route_table}"
 }
