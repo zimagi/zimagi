750385c2360d69a188e9c8946a7d89cd1b913731
Adrian Webb <adrian@webb.sh>
2019-02-06 14:08:08 -0500

    Fixing initialize_instance parameters in the manual network and git project providers.

--- app/systems/network/manual.py
+++ app/systems/network/manual.py
@@ -10,7 +10,7 @@ class ManualNetworkProvider(NetworkProvider):
         self.option(str, 'cidr_base', '10/8', help = 'IPv4 root CIDR address (not used if "cidr" option specified)')
         self.option(int, 'cidr_prefix', 16, help = 'IPv4 CIDR address prefix size (not used if "cidr" option specified)')
 
-    def initialize_instance(self, instance, created):
+    def initialize_instance(self, instance, created, test):
         instance.cidr = self.get_cidr(instance.config, self.command.networks)
         if not instance.cidr:
             self.command.error("No available network cidr matches. Try another cidr")
@@ -22,7 +22,7 @@ class ManualSubnetProvider(SubnetProvider):
         self.option(str, 'cidr', None, help = 'IPv4 CIDR address (between /16 and /28)')
         self.option(int, 'cidr_prefix', 24, help = 'IPv4 CIDR address prefix size (not used if "cidr" option specified)')
 
-    def initialize_instance(self, instance, created):
+    def initialize_instance(self, instance, created, test):
         instance.config['cidr_base'] = instance.network.cidr
         instance.cidr = self.get_cidr(instance.config, self.command.subnets)
         if not instance.cidr:
