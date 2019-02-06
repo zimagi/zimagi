780b8f867af5f5d1a54136412b7b258a8c46cf7c
Adrian Webb <adrian@webb.sh>
2019-02-06 17:51:13 -0500

    Fixing variable names in the AWS firewall terraform manifest.

--- app/terraform/firewall/aws.tf
+++ app/terraform/firewall/aws.tf
@@ -2,12 +2,12 @@
 provider "aws" {
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
-  region = "${var.region}"
+  region = "${var.network.region}"
 }
 
 resource "aws_security_group" "firewall" {
   name = "${var.name}"
-  vpc_id = "${var.vpc_id}"
+  vpc_id = "${var.network.vpc}"
   
   tags = {
     Name = "cenv-network"
