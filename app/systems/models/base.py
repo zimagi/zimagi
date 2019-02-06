7312d3a51bc5cc3c05584f25a61d9be04dbf7524
Adrian Webb <adrian@webb.sh>
2019-02-06 01:48:02 -0500

    Adding a base provider model, a facade accessor, and a save override in the base model definition.

--- app/systems/models/base.py
+++ app/systems/models/base.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.db.models.base import ModelBase
+from django.utils.timezone import now
 
 from .facade import ModelFacade
 
 import inspect
 import json
+import copy
 
 
 models.options.DEFAULT_NAMES += ('facade_class',)
@@ -33,6 +35,19 @@ class AppModel(models.Model, metaclass = AppMetaModel):
     def initialize(self, command):
         return True
 
+    def save(self, *args, **kwargs):
+        if not getattr(self, 'created', None):
+            self.created = now()
+        else:
+            self.updated = now()
+        
+        super().save(*args, **kwargs)
+
+
+    @property
+    def facade(self):
+        return copy.deepcopy(self.__class__.facade)
+
 
 class ConfigModelFacade(ModelFacade):
 
@@ -43,15 +58,13 @@ class ConfigModelFacade(ModelFacade):
 
 class AppConfigModel(AppModel):
 
-    _config = models.TextField(db_column="config", null=True)
+    _config = models.TextField(db_column="config", default='{}')
     
     @property
     def config(self):
-        if self._config:
-            if getattr(self, '_cached_config', None) is None:        
-                self._cached_config = json.loads(self._config)
-            return self._cached_config
-        return {}
+        if getattr(self, '_cached_config', None) is None:        
+            self._cached_config = json.loads(self._config)
+        return self._cached_config
 
     @config.setter
     def config(self, data):
@@ -65,3 +78,51 @@ class AppConfigModel(AppModel):
     class Meta:
         abstract = True
         facade_class = ConfigModelFacade
+
+
+class ProviderModelFacade(ConfigModelFacade):
+
+    def __init__(self, cls):
+        super().__init__(cls)
+        self.fields.extend(['variables', 'state'])
+
+
+class AppProviderModel(AppConfigModel):
+
+    _variables = models.TextField(db_column="variables", default='{}')
+    _state_config = models.TextField(db_column="state", default='{}')
+
+
+    @property
+    def variables(self):
+        if getattr(self, '_cached_variables', None) is None:        
+            self._cached_variables = json.loads(self._variables)
+        return self._cached_variables
+
+    @variables.setter
+    def variables(self, data):
+        if not isinstance(data, str):
+            data = json.dumps(data)
+        
+        self._variables = data
+        self._cached_variables = None
+
+    
+    @property
+    def state(self):
+        if getattr(self, '_cached_state', None) is None:        
+            self._cached_state = json.loads(self._state_config)
+        return self._cached_state
+
+    @state.setter
+    def state(self, data):
+        if not isinstance(data, str):
+            data = json.dumps(data)
+        
+        self._state_config = data
+        self._cached_state = None
+
+
+    class Meta:
+        abstract = True
+        facade_class = ProviderModelFacade
