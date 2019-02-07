25477bec292ff5b12d5daf540ed215e626a8a4ce
Adrian Webb <adrian@webb.sh>
2019-02-06 20:13:06 -0500

    Updating terraform firewall rule AWS manifest.

--- app/terraform/firewall_rule/aws.tf
+++ app/terraform/firewall_rule/aws.tf
@@ -2,11 +2,11 @@
 provider "aws" {
     access_key = "${var.access_key}"
     secret_key = "${var.secret_key}"
-    region = "${var.region}"
+    region = "${var.firewall.network.region}"
 }
 
 resource "aws_security_group_rule" "firewall" {
-    security_group_id = "${var.sgroup_id}"
+    security_group_id = "${var.firewall.security_group}"
     type = "${var.mode}"
     from_port = "${var.from_port}"
     to_port = "${var.to_port}"
