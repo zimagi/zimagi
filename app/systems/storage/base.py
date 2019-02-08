57288e24e42eb9f02c2ac3f344417804a69aad57
Adrian Webb <adrian@webb.sh>
2019-02-08 15:14:49 -0500

    Updating the base storage provider to work with the new provider architecture.

--- app/systems/storage/base.py
+++ app/systems/storage/base.py
@@ -2,146 +2,67 @@ from django.conf import settings
 
 from systems.command import providers
 
-import threading
-import pathlib
-import copy
+
+class StorageProvider(providers.TerraformProvider):
+    
+    def terraform_type(self):
+        return 'storage'
+
+    @property
+    def facade(self):
+        return self.command._storage
+
+    def create(self, name, network, fields, **relations):
+        fields['type'] = self.name
+        fields['network'] = network
+        return super().create(name, fields, **relations)
 
 
-class StorageResult(object):
+class StorageMountProvider(providers.TerraformProvider):
 
-    def __init__(self, type, name, network, config):
-        self.type = type
-        self.name = name
-        self.network = network
-        self.config = copy.deepcopy(config)
+    def provider_config(self, type = None):
+        self.option(str, 'mount_options', 'nfs4 rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 0 0', help = 'Mount options', config_name = 'manual_mount_options')
 
-    def __str__(self):
-        return "[{}:{}]> {}".format(
-            self.type,
-            self.network.name,
-            self.name         
-        )
+    def terraform_type(self):
+        return 'storage_mount'
 
+    @property
+    def facade(self):
+        return self.command._mount
 
-class StorageMountResult(object):
+    def create(self, name, storage, subnet, fields, **relations):
+        fields['type'] = self.name
+        fields['storage'] = storage
+        fields['subnet'] = subnet
+        return super().create(name, fields, **relations)
 
-    def __init__(self, subnet, firewalls, config,
-        name = None,
-        remote_host = None,
-        remote_path = None,
-        mount_options = None
-    ):
-        self.subnet = subnet
-        self.firewalls = firewalls
-        self.config = copy.deepcopy(config)
-        self.name = name
-        self.remote_host = remote_host
-        self.remote_path = remote_path
-        self.mount_options = mount_options
+    def initialize_terraform(self, instance, relations, created):
+        if 'firewalls' not in relations:
+            relations['firewalls'] = []
 
-    def __str__(self):
-        return "[{}]> {}{}".format(
-            self.name,
-            self.remote_host,
-            self.remote_path         
-        )
+        if 'nfs' not in relations['firewalls']:
+            firewall = self.command._firewall.retrieve('nfs')
+            if firewall:
+                relations['firewalls'].append('nfs')
+
+    def prepare_instance(self, instance, relations, created):
+        instance.mount_options = instance.config['mount_options']
+
+    def save_related(self, instance, relations, created):
+        if 'firewalls' in relations:
+            self.update_related(instance, 'firewalls',
+                self.command._firewall,
+                relations['firewalls']
+            )
 
 
-class BaseStorageProvider(providers.BaseCommandProvider):
+class BaseStorageProvider(providers.MetaCommandProvider):
 
-    def __init__(self, name, command, storage = None):
-        super().__init__(name, command)
-
-        self.storage = storage
-        self.thread_lock = threading.Lock()
-
+    def __init__(self, name, command, instance = None):
+        super().__init__(name, command, instance)
         self.provider_type = 'storage'
         self.provider_options = settings.STORAGE_PROVIDERS
-
-
-    def create_storage(self, name, network, config):
-        self.config = config
-        
-        self.provider_config('storage')
-        self.validate()
-
-        storage = StorageResult(self.name, name, network, config)
-        
-        for key, value in self.config.items():
-            if hasattr(storage, key) and key not in ('type', 'config', 'network'):
-                setattr(storage, key, value)
-
-        self.create_provider_storage(storage)
-        return storage
-
-    def create_provider_storage(self, storage):
-        # Override in subclass
-        pass
-
-    def destroy_storage(self, abort = False):
-        if not self.storage:
-            self.command.error("Destroying storage requires a valid storage instance given to provider on initialization")
-        try:
-            self.destroy_provider_storage()
-        
-        except Exception as e:
-            if abort:
-                raise e
-            else:
-                self.command.warning(str(e))
-
-    def destroy_provider_storage(self):
-        # Override in subclass
-        pass
-
-
-    def create_mount(self, subnet, config, firewalls = []):
-        self.config = config
-        
-        self.provider_config('mount')
-        self.validate()
-
-        mount = StorageMountResult(subnet, firewalls, config)
-
-        for key, value in self.config.items():
-            if hasattr(mount, key) and key not in ('config', 'subnet', 'firewalls'):
-                setattr(mount, key, value)
-
-        self.create_provider_mount(mount)
-        return mount
-
-    def create_provider_mount(self, mount):
-        # Override in subclass
-        pass
-
-    def update_mount_firewalls(self, mount, firewalls, abort = False):
-        if not self.storage:
-            self.command.error("Updating storage mount firewalls requires a valid storage instance given to provider on initialization")
-        try:
-            self.update_provider_mount_firewalls(mount, firewalls)
-        
-        except Exception as e:
-            if abort:
-                raise e
-            else:
-                self.command.warning(str(e))
-
-    def update_provider_mount_firewalls(self, mount, firewalls):
-        # Override in subclass
-        pass
-
-    def destroy_mount(self, mount, abort = False):
-        if not self.storage:
-            self.command.error("Destroying storage mount requires a valid storage instance given to provider on initialization")
-        try:
-            self.destroy_provider_mount(mount)
-        
-        except Exception as e:
-            if abort:
-                raise e
-            else:
-                self.command.warning(str(e))
-
-    def destroy_provider_mount(self, mount):
-        # Override in subclass
-        pass
+    
+    def register_types(self):
+        self.set('storage', StorageProvider)
+        self.set('mount', StorageMountProvider)
