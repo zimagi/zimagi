defaf90a96458962380ea170fffa08536490b8e1
Adrian Webb <adrian@webb.sh>
2019-02-07 19:18:03 -0500

    Readding some functionality to the AWS cloud utility mixin.

--- app/utility/cloud/aws.py
+++ app/utility/cloud/aws.py
@@ -11,3 +11,34 @@ class AWSServiceMixin(object):
             self.command.error("To use AWS provider you must have 'aws_access_key' and 'aws_secret_key' environment configurations; see: config set")
         
         return config
+
+
+    def _init_session(self):
+        if not getattr(self, 'session', None):
+            config = self.aws_credentials()
+            self.session = boto3.Session(
+                aws_access_key_id = config['access_key'],
+                aws_secret_access_key = config['secret_key']
+            )  
+
+    def ec2(self, network):
+        self._init_session()
+        return self.session.client('ec2', 
+            region_name = network.config['region']
+        )
+
+    def efs(self, network):
+        self._init_session()
+        return self.session.client('efs', 
+            region_name = network.config['region']
+        )
+
+
+    def get_security_groups(self, names):
+        firewalls = self.command.get_instances(self.command._firewall, names = names)
+        sgroups = []
+
+        for firewall in firewalls:
+            sgroups.append(firewall.variables['security_group'])                    
+                
+        return sgroups
