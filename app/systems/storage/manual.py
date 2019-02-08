4d628e93bb4deccab144d476f991b039e0bf0e61
Adrian Webb <adrian@webb.sh>
2019-02-08 15:15:24 -0500

    Updating the manual storage provider to use the new provider architecture.

--- app/systems/storage/manual.py
+++ app/systems/storage/manual.py
@@ -1,14 +1,16 @@
-from .base import BaseStorageProvider
+from .base import *
+
+
+class ManualStorageMountProvider(StorageMountProvider):
+
+    def provider_config(self, type = None):
+        super().provider_config(type)
+        self.requirement(str, 'remote_host', help = 'Remote host to connect storage mount')
+        self.option(str, 'remote_path', '/', help = 'Remote path to mount locally', config_name = 'manual_remote_path')
 
 
 class Manual(BaseStorageProvider):
-
-    def provider_config(self, type = None):
-        if type == 'storage':
-            pass
-
-        elif type == 'mount':
-            self.requirement(str, 'remote_host', help = 'Remote host to connect storage mount')
-        
-            self.option(str, 'remote_path', '/', help = 'Remote path to mount locally', config_name = 'manual_remote_path')
-            self.option(str, 'mount_options', 'nfs4 rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 0 0', help = 'Mount options', config_name = 'manual_mount_options')
+    
+    def register_types(self):
+        super().register_types()
+        self.set('mount', ManualStorageMountProvider)
