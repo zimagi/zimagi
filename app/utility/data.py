232ea2c37c802331588a251be8d4c3c1403d2461
Adrian Webb <adrian@webb.sh>
2019-02-13 15:22:00 -0500

    Adding ensure_list and number functions to the utility data library.

--- app/utility/data.py
+++ app/utility/data.py
@@ -5,6 +5,16 @@ import random
 def clean_dict(data):
     return {key: value for key, value in data.items() if value is not None}
 
+def ensure_list(data):
+    return list(data) if isinstance(data, (list, tuple)) else [data]
+
+
+def number(data):
+    try:
+        return int(data)
+    except TypeError:
+        return float(data)
+
 
 def create_token():
     chars = string.ascii_uppercase + string.digits
