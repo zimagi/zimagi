444475cd9657c64f0d8db2bf4d65773b9f17feab
Adrian Webb <adrian@webb.sh>
2019-02-15 05:41:13 -0500

    Adding a network peer model and updating network model.

--- app/data/network/models/__init__.py
+++ app/data/network/models/__init__.py
@@ -1,11 +1,13 @@
 
 from .network import *
+from .network_peer import *
 from .subnet import *
 from .firewall import *
 from .firewall_rule import *
 
 __all__ = [
     'network',
+    'network_peer',
     'subnet',
     'firewall',
     'firewall_rule'
--- app/data/network/models/network_peer.py
+++ app/data/network/models/network_peer.py
@@ -3,7 +3,7 @@ from data.environment import models as env
 from data.network import models as network
 
 
-class NetworkFacade(models.ProviderModelFacade):
+class NetworkPeerFacade(models.ProviderModelFacade):
 
     def get_packages(self):
         return super().get_packages() + ['network', 'server']
@@ -22,27 +22,20 @@ class NetworkFacade(models.ProviderModelFacade):
         return { 'environment_id': curr_env }
 
 
-class Network(models.AppProviderModel):
+class NetworkPeer(models.AppProviderModel):
 
     name = models.CharField(max_length=128)
     type = models.CharField(null=True, max_length=128)
-    cidr = models.CharField(null=True, max_length=128)
-
-    environment = models.ForeignKey(env.Environment, related_name='networks', on_delete=models.PROTECT)
+    
+    peers = models.ManyToManyField(network.Network)    
+    environment = models.ForeignKey(env.Environment, related_name='network_peers', on_delete=models.PROTECT)
 
     class Meta:
         unique_together = ('environment', 'name')
-        facade_class = NetworkFacade
+        facade_class = NetworkPeerFacade
 
     def __str__(self):
-        return "{} ({})".format(self.name, self.cidr)
-
+        return self.name
 
     def initialize(self, command):
-        network_peer, created = network.NetworkPeer.facade.store(self.name, type = self.type)
-
-        self.provider = command.get_provider('network:network', self.type, instance = self)
-        self.peer_provider = command.get_provider('network:network_peer', self.type, 
-            instance = network_peer
-        )
         return True
