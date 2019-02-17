3c4eb7fabf498f509472aded401d389b3e5e9b58
Adrian Webb <adrian@webb.sh>
2019-02-17 07:41:54 -0500

    Fixing property access issues and adding a zone_suffix subnet field in the AWS network provider.

--- app/systems/network/aws.py
+++ app/systems/network/aws.py
@@ -30,15 +30,23 @@ class AWSNetworkPeerProvider(AWSServiceMixin, NetworkPeerProvider):
         instance.config.setdefault(namespace, {})
         self.aws_credentials(instance.config[namespace])        
         
-        instance.config[namespace]['region'] = source.config['region']
-        instance.config[namespace]['vpc_id'] = source.variables['vpc_id']
-        instance.config[namespace]['peer_region'] = peer.config['region']
-        instance.config[namespace]['peer_vpc_id'] = peer.variables['vpc_id']
+        try:
+            instance.config[namespace]['region'] = source.config['region']
+            instance.config[namespace]['vpc_id'] = source.variables['vpc_id']
+            instance.config[namespace]['peer_region'] = peer.config['region']
+            instance.config[namespace]['peer_vpc_id'] = peer.variables['vpc_id']
+        
+        except KeyError as e:
+            self.command.warning("Could not access {} within {} peering connection".format(str(e), instance.name))
 
         super().initialize_terraform(instance, relations, created, pair)
 
     def finalize_terraform(self, instance, pair):
-        self.aws_credentials(instance.config[self._peer_namespace(pair)])
+        namespace = self._peer_namespace(pair)
+
+        instance.config.setdefault(namespace, {})
+        self.aws_credentials(instance.config[namespace])
+        
         super().finalize_terraform(instance, pair)
 
 
@@ -47,10 +55,17 @@ class AWSSubnetProvider(AWSServiceMixin, SubnetProvider):
     def provider_config(self, type = None):
         super().provider_config(type)
         self.option(str, 'zone', None, help = 'AWS availability zone (default random)', config_name = 'aws_zone')
+        self.option(str, 'zone_suffix', None, help = 'AWS availability zone suffix (appended to region)', config_name = 'aws_zone_suffix')
         self.option(bool, 'public_ip', True, help = 'Enable public IP addresses for instances in subnet', config_name = 'aws_public_ip')
 
     def initialize_terraform(self, instance, relations, created):
         self.aws_credentials(instance.config)
+
+        if instance.config['zone'] is None and instance.config['zone_suffix'] is not None:
+            instance.config['zone'] = "{}{}".format(
+                instance.network.config['region'], 
+                instance.config['zone_suffix']
+            )
         super().initialize_terraform(instance, relations, created)
 
     def finalize_terraform(self, instance):
