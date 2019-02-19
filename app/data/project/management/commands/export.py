2a01318f52464ce8d27c8ed839b2867af0739f41
Adrian Webb <adrian@webb.sh>
2019-02-18 19:15:09 -0500

    Adding an initial export command.

---
+++ app/data/project/management/commands/export.py
@@ -0,0 +1,48 @@
+from django.conf import settings
+
+from systems.command import types, mixins
+
+
+class Command(
+    mixins.data.NetworkMixin,
+    types.EnvironmentActionCommand
+):
+    def get_command_name(self):
+        return 'export'
+
+    def get_description(self, overview):
+        if overview:
+            return """export to a cluster profile
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam 
+pulvinar nisl ac magna ultricies dignissim. Praesent eu feugiat 
+elit. Cras porta magna vel blandit euismod.
+"""
+        else:
+            return """export to a cluster profile
+                      
+Etiam mattis iaculis felis eu pharetra. Nulla facilisi. 
+Duis placerat pulvinar urna et elementum. Mauris enim risus, 
+mattis vel risus quis, imperdiet convallis felis. Donec iaculis 
+tristique diam eget rutrum.
+
+Etiam sit amet mollis lacus. Nulla pretium, neque id porta feugiat, 
+erat sapien sollicitudin tellus, vel fermentum quam purus non sem. 
+Mauris venenatis eleifend nulla, ac facilisis nulla efficitur sed. 
+Etiam a ipsum odio. Curabitur magna mi, ornare sit amet nulla at, 
+scelerisque tristique leo. Curabitur ut faucibus leo, non tincidunt 
+velit. Aenean sit amet consequat mauris.
+"""
+    def server_enabled(self):
+        return True
+    
+    def parse(self):
+        self.parse_profile_components('--components')
+        self.parse_project_name()
+        self.parse_profile_name()
+
+    def exec(self):
+        self.project.provider.export_profile(
+            self.profile_name,
+            self.profile_component_names
+        )
