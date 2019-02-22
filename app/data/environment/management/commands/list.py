6c950b2c571adebeeecab21ad85b96333780fdad
Adrian Webb <adrian@webb.sh>
2019-02-22 02:54:43 -0500

    Adding runtime fields to list command display and removing description method.

--- app/data/environment/management/commands/list.py
+++ app/data/environment/management/commands/list.py
@@ -8,33 +8,12 @@ class Command(
     def get_command_name(self):
         return 'list'
 
-    def get_description(self, overview):
-        if overview:
-            return """list cluster environments
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam 
-pulvinar nisl ac magna ultricies dignissim. Praesent eu feugiat 
-elit. Cras porta magna vel blandit euismod.
-"""
-        else:
-            return """list cluster environments
-                      
-Etiam mattis iaculis felis eu pharetra. Nulla facilisi. 
-Duis placerat pulvinar urna et elementum. Mauris enim risus, 
-mattis vel risus quis, imperdiet convallis felis. Donec iaculis 
-tristique diam eget rutrum.
-
-Etiam sit amet mollis lacus. Nulla pretium, neque id porta feugiat, 
-erat sapien sollicitudin tellus, vel fermentum quam purus non sem. 
-Mauris venenatis eleifend nulla, ac facilisis nulla efficitur sed. 
-Etiam a ipsum odio. Curabitur magna mi, ornare sit amet nulla at, 
-scelerisque tristique leo. Curabitur ut faucibus leo, non tincidunt 
-velit. Aenean sit amet consequat mauris.
-"""
     def exec(self):
         self.exec_list(self._env,
             ('name', 'Name'),
             ('host', 'Host'),
             ('port', 'Port'),
-            ('token', 'Token')
+            ('token', 'Token'),
+            ('repo', 'Registry'),
+            ('image', 'Image')
         )
