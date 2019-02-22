dc3906c86cd93e0557ce8318f49f61cb80005adc
Adrian Webb <adrian@webb.sh>
2019-02-22 03:12:46 -0500

    Updating the network model to extend the base environment model and use the provider model mixin.

--- app/data/network/models/network.py
+++ app/data/network/models/network.py
@@ -1,41 +1,22 @@
 from systems import models
-from data.environment import models as env
+from systems.models import environment, provider
 
 
-class NetworkFacade(models.ProviderModelFacade):
-
-    def get_packages(self):
-        return super().get_packages() + ['network', 'server']
-
-    def key(self):
-        return 'name'
- 
-    def scope(self, fields = False):
-        if fields:
-            return ('environment',)
-        
-        curr_env = env.Environment.facade.get_env()
-        if not curr_env:
-            return False
-
-        return { 'environment_id': curr_env }
+class NetworkFacade(
+    provider.ProviderModelFacadeMixin,
+    environment.EnvironmentModelFacadeMixin
+):
+    pass
 
 
-class Network(models.AppProviderModel):
-
-    name = models.CharField(max_length=128)
-    type = models.CharField(null=True, max_length=128)
+class Network(
+    provider.ProviderMixin,
+    environment.EnvironmentModel
+):
     cidr = models.CharField(null=True, max_length=128)
 
-    environment = models.ForeignKey(env.Environment, related_name='networks', on_delete=models.PROTECT)
-
     class Meta:
-        unique_together = ('environment', 'name')
         facade_class = NetworkFacade
 
-    def __str__(self):
-        return "{} ({})".format(self.name, self.cidr)
-
-    def initialize(self, command):
-        self.provider = command.get_provider('network:network', self.type, instance = self)
-        return True
+    def get_provider_name(self):
+        return 'network:network'
