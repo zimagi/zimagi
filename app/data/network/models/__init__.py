071c1e98e352f455db4903f3af7c4d38ae0d11a7
Adrian Webb <adrian@webb.sh>
2019-02-22 03:11:51 -0500

    Removing the network peer model.

--- app/data/network/models/__init__.py
+++ app/data/network/models/__init__.py
@@ -1,13 +1,11 @@
 
 from .network import *
-from .network_peer import *
 from .subnet import *
 from .firewall import *
 from .firewall_rule import *
 
 __all__ = [
     'network',
-    'network_peer',
     'subnet',
     'firewall',
     'firewall_rule'
