92fbfdb153eadacdbc8db387d1cbee26ba610d7e
Adrian Webb <adrian@webb.sh>
2019-02-22 03:03:55 -0500

    Switching the config rm and clear commands to use config name instead of a group reference.

--- app/data/config/management/commands/_config/clear.py
+++ app/data/config/management/commands/_config/clear.py
@@ -4,36 +4,16 @@ from systems.command import types, mixins
 class ClearCommand(
     types.ConfigActionCommand
 ):
-    def get_description(self, overview):
-        if overview:
-            return """clear all existing environment configurations
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam 
-pulvinar nisl ac magna ultricies dignissim. Praesent eu feugiat 
-elit. Cras porta magna vel blandit euismod.
-"""
-        else:
-            return """clear all existing environment configurations
-                      
-Etiam mattis iaculis felis eu pharetra. Nulla facilisi. 
-Duis placerat pulvinar urna et elementum. Mauris enim risus, 
-mattis vel risus quis, imperdiet convallis felis. Donec iaculis 
-tristique diam eget rutrum.
-
-Etiam sit amet mollis lacus. Nulla pretium, neque id porta feugiat, 
-erat sapien sollicitudin tellus, vel fermentum quam purus non sem. 
-Mauris venenatis eleifend nulla, ac facilisis nulla efficitur sed. 
-Etiam a ipsum odio. Curabitur magna mi, ornare sit amet nulla at, 
-scelerisque tristique leo. Curabitur ut faucibus leo, non tincidunt 
-velit. Aenean sit amet consequat mauris.
-"""
+    def parse(self):
+        self.parse_force()
+    
     def confirm(self):
         self.confirmation()       
 
     def exec(self):
         def remove_config(config, state):
             self.exec_local('config rm', {
-                'config_reference': "name>{}".format(config.name)
+                'config_name': config.name,
+                'force': self.force
             })
-
         self.run_list(self.configs, remove_config)
--- app/data/config/management/commands/_config/rm.py
+++ app/data/config/management/commands/_config/rm.py
@@ -5,37 +5,12 @@ class RemoveCommand(
     mixins.op.RemoveMixin,
     types.ConfigActionCommand
 ):
-    def get_description(self, overview):
-        if overview:
-            return """remove an existing environment configuration
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam 
-pulvinar nisl ac magna ultricies dignissim. Praesent eu feugiat 
-elit. Cras porta magna vel blandit euismod.
-"""
-        else:
-            return """remove an existing environment configuration
-                      
-Etiam mattis iaculis felis eu pharetra. Nulla facilisi. 
-Duis placerat pulvinar urna et elementum. Mauris enim risus, 
-mattis vel risus quis, imperdiet convallis felis. Donec iaculis 
-tristique diam eget rutrum.
-
-Etiam sit amet mollis lacus. Nulla pretium, neque id porta feugiat, 
-erat sapien sollicitudin tellus, vel fermentum quam purus non sem. 
-Mauris venenatis eleifend nulla, ac facilisis nulla efficitur sed. 
-Etiam a ipsum odio. Curabitur magna mi, ornare sit amet nulla at, 
-scelerisque tristique leo. Curabitur ut faucibus leo, non tincidunt 
-velit. Aenean sit amet consequat mauris.
-"""
     def parse(self):
-        self.parse_config_reference()
+        self.parse_force()
+        self.parse_config_name()
 
     def confirm(self):
         self.confirmation()       
 
     def exec(self):
-        def remove_config(config, state):
-            self.exec_rm(self._config, config.name)
-
-        self.run_list(self.configs, remove_config)
+        self.exec_rm(self._config, self.config_name)
