da975e79ebde0dbfd430975580174060ebc3a59d
Adrian Webb <adrian@webb.sh>
2019-02-22 03:13:42 -0500

    Updating the subnet model to extend the base network model and use the provider model mixin.

--- app/data/network/models/subnet.py
+++ app/data/network/models/subnet.py
@@ -1,37 +1,25 @@
 from systems import models
-from data.network import models as network
+from systems.models import network, provider
 
 
-class SubnetFacade(models.ProviderModelFacade):
-
-    def get_packages(self):
-        return super().get_packages() + ['network', 'server']
-
-    def key(self):
-        return 'name'
-
-    def set_scope(self, network):
-        super().set_scope(network_id = network.id)
+class SubnetFacade(
+    provider.ProviderModelFacadeMixin,
+    network.NetworkModelFacadeMixin
+):
+    pass
 
 
-class Subnet(models.AppProviderModel):
-
-    name = models.CharField(max_length=128)
-    type = models.CharField(null=True, max_length=128)
+class Subnet(
+    provider.ProviderMixin,
+    network.NetworkModel
+):
     cidr = models.CharField(null=True, max_length=128)
-    
-    network = models.ForeignKey(network.Network, related_name='subnets', on_delete=models.PROTECT)
 
     class Meta:
-        unique_together = (
-            ('network', 'name')
-        )
         facade_class = SubnetFacade
 
     def __str__(self):
         return "{} ({})".format(self.name, self.cidr)
 
-
-    def initialize(self, command):
-        self.provider = command.get_provider('network:subnet', self.type, instance = self)
-        return True
+    def get_provider_name(self):
+        return 'network:subnet'
