b0678135b855895753e2e529e0d5c37e712cc7b2
Adrian Webb <adrian@webb.sh>
2019-02-22 03:30:04 -0500

    Removing the description method from various server commands.

--- app/data/server/management/commands/_server/group.py
+++ app/data/server/management/commands/_server/group.py
@@ -5,29 +5,6 @@ class GroupCommand(
     mixins.op.UpdateMixin,
     types.ServerActionCommand
 ):
-    def get_description(self, overview):
-        if overview:
-            return """add environment group children
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam 
-pulvinar nisl ac magna ultricies dignissim. Praesent eu feugiat 
-elit. Cras porta magna vel blandit euismod.
-"""
-        else:
-            return """add environment group children
-                      
-Etiam mattis iaculis felis eu pharetra. Nulla facilisi. 
-Duis placerat pulvinar urna et elementum. Mauris enim risus, 
-mattis vel risus quis, imperdiet convallis felis. Donec iaculis 
-tristique diam eget rutrum.
-
-Etiam sit amet mollis lacus. Nulla pretium, neque id porta feugiat, 
-erat sapien sollicitudin tellus, vel fermentum quam purus non sem. 
-Mauris venenatis eleifend nulla, ac facilisis nulla efficitur sed. 
-Etiam a ipsum odio. Curabitur magna mi, ornare sit amet nulla at, 
-scelerisque tristique leo. Curabitur ut faucibus leo, non tincidunt 
-velit. Aenean sit amet consequat mauris.
-"""
     def parse(self):
         self.parse_server_group(help_text = 'parent server group name')
         self.parse_server_groups(False, help_text = 'one or more child server group names')
