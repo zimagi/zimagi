f90b8627f14ca87c2b39e2eb196dc3c93293a024
Adrian Webb <adrian@webb.sh>
2019-02-22 03:09:48 -0500

    Removing the description method from various project commands.

--- app/data/project/management/commands/destroy.py
+++ app/data/project/management/commands/destroy.py
@@ -10,29 +10,6 @@ class Command(
     def get_command_name(self):
         return 'destroy'
 
-    def get_description(self, overview):
-        if overview:
-            return """destroy cluster profile resources
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam 
-pulvinar nisl ac magna ultricies dignissim. Praesent eu feugiat 
-elit. Cras porta magna vel blandit euismod.
-"""
-        else:
-            return """destroy cluster profile resources
-                      
-Etiam mattis iaculis felis eu pharetra. Nulla facilisi. 
-Duis placerat pulvinar urna et elementum. Mauris enim risus, 
-mattis vel risus quis, imperdiet convallis felis. Donec iaculis 
-tristique diam eget rutrum.
-
-Etiam sit amet mollis lacus. Nulla pretium, neque id porta feugiat, 
-erat sapien sollicitudin tellus, vel fermentum quam purus non sem. 
-Mauris venenatis eleifend nulla, ac facilisis nulla efficitur sed. 
-Etiam a ipsum odio. Curabitur magna mi, ornare sit amet nulla at, 
-scelerisque tristique leo. Curabitur ut faucibus leo, non tincidunt 
-velit. Aenean sit amet consequat mauris.
-"""
     def server_enabled(self):
         return True
     
--- app/data/project/management/commands/task.py
+++ app/data/project/management/commands/task.py
@@ -12,29 +12,6 @@ class Command(
     def get_command_name(self):
         return 'task'
 
-    def get_description(self, overview):
-        if overview:
-            return """execute a task on existing servers
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam 
-pulvinar nisl ac magna ultricies dignissim. Praesent eu feugiat 
-elit. Cras porta magna vel blandit euismod.
-"""
-        else:
-            return """execute a task on existing servers
-                      
-Etiam mattis iaculis felis eu pharetra. Nulla facilisi. 
-Duis placerat pulvinar urna et elementum. Mauris enim risus, 
-mattis vel risus quis, imperdiet convallis felis. Donec iaculis 
-tristique diam eget rutrum.
-
-Etiam sit amet mollis lacus. Nulla pretium, neque id porta feugiat,
-erat sapien sollicitudin tellus, vel fermentum quam purus non sem. 
-Mauris venenatis eleifend nulla, ac facilisis nulla efficitur sed. 
-Etiam a ipsum odio. Curabitur magna mi, ornare sit amet nulla at, 
-scelerisque tristique leo. Curabitur ut faucibus leo, non tincidunt 
-velit. Aenean sit amet consequat mauris.
-"""
     def parse(self):
         self.parse_project_name()
         self.parse_server_reference('--servers')
