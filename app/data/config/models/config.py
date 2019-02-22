5e5855968fe243d50eb6538833358e77c27d48a4
Adrian Webb <adrian@webb.sh>
2019-02-22 03:00:32 -0500

    Updating the config model to extend the base environment model and switching value to an encrypted field.

--- app/data/config/models/config.py
+++ app/data/config/models/config.py
@@ -1,103 +1,28 @@
-
 from systems import models
-from data.environment import models as env
+from systems.models import environment
 from data.config import models as config
-from utility.data import number, serialize, unserialize
-
-import json
 
 
-class ConfigFacade(models.ModelFacade):
-
-    def __init__(self, cls):
-        super().__init__(cls)
-        self.fields.append('value')
-
-    def get_packages(self):
-        return super().get_packages() + ['environment', 'config']
+class ConfigFacade(
+    environment.EnvironmentModelFacadeMixin,
+):
+    pass
 
 
-    def key(self):
-        return 'name'
- 
-    def scope(self, fields = False):
-        if fields:
-            return ('environment',)
-        
-        curr_env = env.Environment.facade.get_env()
-        if not curr_env:
-            return False
-
-        return { 'environment_id': curr_env }
-    
-
-    def render(self, fields, queryset_values):
-        fields = list(fields)
-        data = [fields]
-
-        for item in queryset_values:
-            record = []
-
-            for field in fields:
-                if field in ['created', 'updated'] and item[field]:
-                    value = item[field].strftime("%Y-%m-%d %H:%M:%S %Z")
-                elif field == '_value':
-                    value = json.dumps(unserialize(item[field]), indent=2)
-                else:
-                    value = item[field]
-
-                record.append(value)
-            data.append(record)
-
-        return data
-
-
-class Config(models.AppModel):
-    name = models.CharField(max_length=256)      
-    _value = models.TextField(db_column="value", null=True)
-    user = models.CharField(null=True, max_length=40)
-    
-    environment = models.ForeignKey(env.Environment, related_name='config', on_delete=models.PROTECT)
-    groups = models.ManyToManyField(config.ConfigGroup, related_name='config')
-
-    @property
-    def value(self):
-        if getattr(self, '_cached_value', None) is None:
-            self._cached_value = unserialize(self._value)
-        return self._cached_value
-
-    @value.setter
-    def value(self, data):
-        self._value = serialize(data)        
-        self._cached_value = None
+class Config(
+    environment.EnvironmentModel
+):
+    value = models.EncryptedTextField(null=True)
+    groups = models.ManyToManyField(config.ConfigGroup)
 
     class Meta:
-        unique_together = ('environment', 'name')
         facade_class = ConfigFacade
 
-    def  __str__(self):
-        return "{} ({})".format(self.name, self.value)
-
-
-    def save(self, *args, **kwargs):
-        self.value = self.value
-        return super().save(*args, **kwargs)
-
-
     def initialize(self, command):
-        groups = list(self.groups.values_list('name', flat = True))
-        
-        if groups and not command.check_access(groups):
+        if not super().initialize(command):
             return False
         
+        groups = list(self.groups.values_list('name', flat = True))
+        if groups and not command.check_access(groups):
+            return False
         return True
-
-    def add_groups(self, groups):
-        groups = [groups] if isinstance(groups, str) else groups
-        for group in groups:
-            group, created = config.ConfigGroup.objects.get_or_create(name = group)
-            self.groups.add(group)
-
-    def remove_groups(self, groups):
-        groups = [groups] if isinstance(groups, str) else groups
-        self.groups.filter(name__in = groups).delete()
