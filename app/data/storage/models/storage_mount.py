126d24d009e6f1557f4c6ec6dcd341a856a8282c
Adrian Webb <adrian@webb.sh>
2019-02-22 03:20:55 -0500

    Updating the storage mount model to extend the base environment model and use the provider and subnet model mixins.

--- app/data/storage/models/storage_mount.py
+++ app/data/storage/models/storage_mount.py
@@ -1,39 +1,40 @@
 from systems import models
+from systems.models import environment, subnet, provider
 from data.network import models as network
 from data.storage import models as storage
 
 
-class StorageMountFacade(models.ProviderModelFacade):
-
-    def get_packages(self):
-        return super().get_packages() + ['storage']
-
-    def key(self):
-        return 'name'
-
-    def set_scope(self, storage):
+class StorageMountFacade(
+    provider.ProviderModelFacadeMixin,
+    environment.EnvironmentModelFacadeMixin
+):
+    def set_storage_scope(self, storage):
         super().set_scope(storage_id = storage.id)
-
-
-class StorageMount(models.AppProviderModel):
     
-    name = models.CharField(max_length=128)
-    type = models.CharField(null=True, max_length=128)
+    def set_network_scope(self, network):
+        super().set_scope(subnet__network__id = network.id)
+
+    def set_subnet_scope(self, subnet):
+        super().set_scope(subnet_id = subnet.id)
+
+
+class StorageMount(
+    provider.ProviderMixin,
+    subnet.SubnetMixin,
+    environment.EnvironmentModel  
+):    
     remote_host = models.CharField(null=True, max_length=128)
     remote_path = models.CharField(null=True, max_length=256)
     mount_options = models.TextField(null=True)
  
-    storage = models.ForeignKey(storage.Storage, related_name='mounts', on_delete=models.PROTECT)
-    subnet = models.ForeignKey(network.Subnet, related_name='mounts', null=True, on_delete=models.PROTECT)
-    firewalls = models.ManyToManyField(network.Firewall, related_name='mounts')
+    storage = models.ForeignKey(storage.Storage, on_delete=models.PROTECT)
+    firewalls = models.ManyToManyField(network.Firewall)
 
     class Meta:
-        unique_together = ('storage', 'name')
         facade_class = StorageMountFacade
 
     def __str__(self):
         return "{} ({}:{})".format(self.name, self.remote_host, self.remote_path)
 
-    def initialize(self, command):
-        self.provider = command.get_provider('storage:mount', self.type, instance = self)
-        return True
+    def get_provider_name(self):
+        return 'storage:mount'
