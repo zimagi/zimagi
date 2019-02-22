5e9fa1849bd2787c3e46f12d8cb5f41db16fb2b9
Adrian Webb <adrian@webb.sh>
2019-02-22 03:22:45 -0500

    Replacing storage CRUD commands with a storage resource command factory.

--- app/data/storage/management/commands/storage.py
+++ app/data/storage/management/commands/storage.py
@@ -1,5 +1,4 @@
-from systems.command import types
-from data.storage.management.commands import _storage as storage
+from systems.command import types, factory
 
 
 class Command(types.StorageRouterCommand):
@@ -7,34 +6,17 @@ class Command(types.StorageRouterCommand):
     def get_command_name(self):
         return 'storage'
 
-    def get_description(self, overview):
-        if overview:
-            return """manage environment storage sources
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam 
-pulvinar nisl ac magna ultricies dignissim. Praesent eu feugiat 
-elit. Cras porta magna vel blandit euismod.
-"""
-        else:
-            return """manage environment storage sources
-                      
-Etiam mattis iaculis felis eu pharetra. Nulla facilisi. 
-Duis placerat pulvinar urna et elementum. Mauris enim risus, 
-mattis vel risus quis, imperdiet convallis felis. Donec iaculis 
-tristique diam eget rutrum.
-
-Etiam sit amet mollis lacus. Nulla pretium, neque id porta feugiat, 
-erat sapien sollicitudin tellus, vel fermentum quam purus non sem. 
-Mauris venenatis eleifend nulla, ac facilisis nulla efficitur sed. 
-Etiam a ipsum odio. Curabitur magna mi, ornare sit amet nulla at, 
-scelerisque tristique leo. Curabitur ut faucibus leo, non tincidunt 
-velit. Aenean sit amet consequat mauris.
-"""
     def get_subcommands(self):
-        return (
-            ('get', storage.GetCommand),
-            ('list', storage.ListCommand),
-            ('add', storage.AddCommand),
-            ('rm', storage.RemoveCommand),
-            ('clear', storage.ClearCommand)
+        return factory.ResourceCommands(
+            types.StorageActionCommand, 'storage',
+            provider_name = 'storage',
+            provider_subtype = 'storage',
+            list_fields = (
+                ('name', 'Name'),
+                ('type', 'Type')
+            ),
+            relations = ('mounts',),
+            scopes = {
+                'network': '--network'
+            }
         )
