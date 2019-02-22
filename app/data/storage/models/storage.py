ca1ce505ae01669dfc43a8445f78dd43997c8535
Adrian Webb <adrian@webb.sh>
2019-02-22 03:20:14 -0500

    Updating the storage model to extend the base network model and use the provider model mixin.

--- app/data/storage/models/storage.py
+++ app/data/storage/models/storage.py
@@ -1,46 +1,23 @@
-
 from systems import models
-from data.environment import models as env
-from data.network import models as network
+from systems.models import network, provider
 
 
-class StorageFacade(models.ProviderModelFacade):
-
-    def get_packages(self):
-        return super().get_packages() + ['storage']
-
-
-    def key(self):
-        return 'name'
- 
-    def scope(self, fields = False):
-        if fields:
-            return ('environment',)
-        
-        curr_env = env.Environment.facade.get_env()
-        if not curr_env:
-            return False
-
-        return { 'environment_id': curr_env }
-
-    def set_scope(self, network):
-        super().set_scope(network_id = network.id)
+class StorageFacade(
+    provider.ProviderModelFacadeMixin,
+    network.NetworkModelFacadeMixin
+):
+    pass
 
 
-class Storage(models.AppProviderModel):
-    name = models.CharField(max_length=128)
-    type = models.CharField(null=True, max_length=128)
-
-    network = models.ForeignKey(network.Network, related_name='storage', null=True, on_delete=models.PROTECT)
-    environment = models.ForeignKey(env.Environment, related_name='storage', on_delete=models.PROTECT)
-
+class Storage(
+    provider.ProviderMixin,
+    network.NetworkModel
+):
     class Meta:
-        unique_together = ('environment', 'name')
         facade_class = StorageFacade
 
     def __str__(self):
-        return "{} ({})".format(self.name, self.type)
+        return "{}".format(self.name)
 
-    def initialize(self, command):
-        self.provider = command.get_provider('storage:storage', self.type, instance = self)
-        return True
+    def get_provider_name(self):
+        return 'storage:storage'
