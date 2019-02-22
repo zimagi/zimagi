c6f0ed4c04a16742a2bfdcbbce041a684a45e3de
Adrian Webb <adrian@webb.sh>
2019-02-22 03:17:37 -0500

    Replacing subnet CRUD commands with a subnet resource command factory.

--- app/data/network/management/commands/subnet.py
+++ app/data/network/management/commands/subnet.py
@@ -1,5 +1,4 @@
-from systems.command import types
-from data.network.management.commands import _subnet as subnet
+from systems.command import types, factory
 
 
 class Command(types.NetworkRouterCommand):
@@ -7,34 +6,19 @@ class Command(types.NetworkRouterCommand):
     def get_command_name(self):
         return 'subnet'
 
-    def get_description(self, overview):
-        if overview:
-            return """manage network subnets
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam 
-pulvinar nisl ac magna ultricies dignissim. Praesent eu feugiat 
-elit. Cras porta magna vel blandit euismod.
-"""
-        else:
-            return """manage network subnets
-                      
-Etiam mattis iaculis felis eu pharetra. Nulla facilisi. 
-Duis placerat pulvinar urna et elementum. Mauris enim risus, 
-mattis vel risus quis, imperdiet convallis felis. Donec iaculis 
-tristique diam eget rutrum.
-
-Etiam sit amet mollis lacus. Nulla pretium, neque id porta feugiat, 
-erat sapien sollicitudin tellus, vel fermentum quam purus non sem. 
-Mauris venenatis eleifend nulla, ac facilisis nulla efficitur sed. 
-Etiam a ipsum odio. Curabitur magna mi, ornare sit amet nulla at, 
-scelerisque tristique leo. Curabitur ut faucibus leo, non tincidunt 
-velit. Aenean sit amet consequat mauris.
-"""
     def get_subcommands(self):
-        return (
-            ('list', subnet.ListCommand),
-            ('get', subnet.GetCommand),
-            ('save', subnet.SaveCommand),
-            ('rm', subnet.RemoveCommand),
-            ('clear', subnet.ClearCommand)
+        return factory.ResourceCommands(
+            types.NetworkSubnetActionCommand, 'subnet',
+            provider_name = 'network',
+            provider_subtype = 'subnet',
+            list_fields = (
+                ('name', 'Name'),
+                ('network__name', 'Network'),
+                ('network__type', 'Network type'),
+                ('cidr', 'CIDR')
+            ), 
+            relations = ('servers', 'mounts'),
+            scopes = {
+                'network': '--network'
+            }
         )
