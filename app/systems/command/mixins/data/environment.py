bb4b003501dd86839bd1d88815dfb970409d47fd
Adrian Webb <adrian@webb.sh>
2019-02-22 01:53:58 -0500

    Adding repo and image defaults, cleaning up the get_state method, and updating environment processing in the environment command data mixin.

--- app/systems/command/mixins/data/environment.py
+++ app/systems/command/mixins/data/environment.py
@@ -19,14 +19,14 @@ class EnvironmentMixin(DataMixin):
 
 
     def parse_env_repo(self, optional = False, help_text = 'environment runtime repository'):
-        self.parse_variable('repo', optional, str, help_text, 'HOST')
+        self.parse_variable('repo', optional, str, help_text, 'HOST', settings.DEFAULT_RUNTIME_REPO)
 
     @property
     def env_repo(self):
         return self.options.get('repo', None)
 
     def parse_env_image(self, optional = False, help_text = 'environment runtime image ({})'.format(settings.DEFAULT_RUNTIME_IMAGE)):
-        self.parse_variable('image', optional, str, help_text, 'REFERENCE')
+        self.parse_variable('image', optional, str, help_text, 'REFERENCE', settings.DEFAULT_RUNTIME_IMAGE)
 
     @property
     def env_image(self):
@@ -56,29 +56,22 @@ class EnvironmentMixin(DataMixin):
     def get_env(self, name = None):
         if not name:
             name = self._env.get_env()
-       
-        if name:
-            return self.get_instance(self._env, name, required = False)
-        
-        return None
+        return self.get_instance(self._env, name, required = False)
 
-    def set_env(self, name, repo = None, image = None):
+    def set_env(self, name = None, repo = None, image = None):
         self._env.set_env(name, repo, image)
-        self.success("Successfully updated environment state")
+        self.success("Successfully updated current environment")
 
-    def delete_env(self, name = None):
-        if self._env.delete_env(name):
-            self.success("Successfully removed environment")
-        else:
-            self.error("Environment state change failed")
+    def delete_env(self):
+        self._state.clear()
+        self._env.delete_env()
+        self.success("Successfully removed environment")
 
 
-    def get_state(self, name = None, default = None):
-        if name:
-            instance = self.get_instance(self._state, name, required = False)
-            if instance:
-                return instance.value
-
+    def get_state(self, name, default = None):
+        instance = self.get_instance(self._state, name, required = False)
+        if instance:
+            return instance.value
         return default
 
     def set_state(self, name, value = None):
