81c728f5721f1555d52bfb33f0668b74d0f9534f
Adrian Webb <adrian@webb.sh>
2019-02-22 02:55:45 -0500

    Adding dependency clearing and removing description method in the rm command.

--- app/data/environment/management/commands/rm.py
+++ app/data/environment/management/commands/rm.py
@@ -2,43 +2,26 @@ from systems.command import types, mixins
 
 
 class Command(
-    mixins.data.ServerMixin,
     mixins.op.RemoveMixin,
-    mixins.op.ClearMixin,
     types.EnvironmentActionCommand
 ):
     def get_command_name(self):
         return 'rm'
 
-    def get_description(self, overview):
-        if overview:
-            return """remove an existing cluster environment
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam 
-pulvinar nisl ac magna ultricies dignissim. Praesent eu feugiat 
-elit. Cras porta magna vel blandit euismod.
-"""
-        else:
-            return """remove an existing cluster environment
-                      
-Etiam mattis iaculis felis eu pharetra. Nulla facilisi. 
-Duis placerat pulvinar urna et elementum. Mauris enim risus, 
-mattis vel risus quis, imperdiet convallis felis. Donec iaculis 
-tristique diam eget rutrum.
-
-Etiam sit amet mollis lacus. Nulla pretium, neque id porta feugiat, 
-erat sapien sollicitudin tellus, vel fermentum quam purus non sem. 
-Mauris venenatis eleifend nulla, ac facilisis nulla efficitur sed. 
-Etiam a ipsum odio. Curabitur magna mi, ornare sit amet nulla at, 
-scelerisque tristique leo. Curabitur ut faucibus leo, non tincidunt 
-velit. Aenean sit amet consequat mauris.
-"""
     def parse(self):
-        self.parse_env_name()
+        self.parse_force()
 
     def confirm(self):
         self.confirmation()       
 
     def exec(self):
-        self.delete_env(self.env_name)
-        self.exec_rm(self._env, self.env_name, display_warning = False)
+        env = self.get_env()
+
+        def process(type, state):
+            self.exec_local("{} clear".format(type), {
+                'force': self.force
+            })
+        self.run_list(('federation', 'network', 'project', 'config'), process)
+        
+        self.exec_rm(self._env, env.name, display_warning = False)
+        self.delete_env()
