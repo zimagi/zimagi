1edaeb69a773e1f6143b57fdf44e8cd70307800e
Adrian Webb <adrian@webb.sh>
2019-02-22 03:14:28 -0500

    Updating the firewall rule model to extend the base firewall model and use the provider model mixin.

--- app/data/network/models/firewall_rule.py
+++ app/data/network/models/firewall_rule.py
@@ -1,65 +1,29 @@
 from systems import models
-from .firewall import Firewall
-
-import re
+from systems.models import firewall, provider
 
 
-class FirewallRuleFacade(models.ProviderModelFacade):
-
-    def __init__(self, cls):
-        super().__init__(cls)
-        self.fields.append('cidrs')
-
-    def get_packages(self):
-        return super().get_packages() + ['network', 'server']
-
-    def key(self):
-        return 'name'
-
-    def set_scope(self, firewall):
-        super().set_scope(firewall_id = firewall.id)
+class FirewallRuleFacade(
+    provider.ProviderModelFacadeMixin,
+    firewall.FirewallModelFacadeMixin
+):
+    pass
 
 
-class FirewallRule(models.AppProviderModel):
-    name = models.CharField(max_length=128)
-    type = models.CharField(null=True, max_length=128)
-
+class FirewallRule(
+    provider.ProviderMixin,
+    firewall.FirewallModel
+):
     mode = models.CharField(null=True, max_length=10, choices=[(i, i) for i in ('ingress', 'egress')])
     from_port = models.IntegerField(null=True)
     to_port = models.IntegerField(null=True)
     protocol = models.CharField(max_length=10, default='tcp', choices=[(i, i) for i in ('tcp', 'udp', 'icmp')])
-    _cidrs = models.TextField(db_column="cidrs", null=True)
-
-    firewall = models.ForeignKey(Firewall, related_name='rules', on_delete=models.PROTECT)
-    
-    @property
-    def cidrs(self):
-        if self._cidrs:
-            if getattr(self, '_cached_cidrs', None) is None:
-                self._cached_cidrs = re.sub(r'\s+', '', self._cidrs).split(',')
-            return self._cached_cidrs
-        return []
-
-    @cidrs.setter
-    def cidrs(self, data):
-        if isinstance(data, (list, tuple)):
-            data = ",".join([x.strip() for x in data])
-        
-        self._cidrs = data
-        self._cached_cidrs = None
-
+    cidrs = models.CSVField(null=True)
 
     class Meta:
-        unique_together = (
-            ('firewall', 'name')
-        )
         facade_class = FirewallRuleFacade
 
-
     def __str__(self):
-        return "{} ({})".format(self.name, self.type)
+        return "{}".format(self.name)
 
-
-    def initialize(self, command):
-        self.provider = command.get_provider('network:firewall_rule', self.type, instance = self)
-        return True
+    def get_provider_name(self):
+        return 'network:firewall_rule'
