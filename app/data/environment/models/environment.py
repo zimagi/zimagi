bb03ecc8e49374191f7a29408c807367cd2b5e19
Adrian Webb <adrian@webb.sh>
2019-02-22 02:50:44 -0500

    Updating runtime fields and facade processing and removing get_packages method in the environment model library.

--- app/data/environment/models/environment.py
+++ app/data/environment/models/environment.py
@@ -3,64 +3,36 @@ from django.conf import settings
 from systems import models
 from utility.runtime import Runtime
 
-import os
-
 
 class EnvironmentFacade(models.ModelFacade):
 
-    def __init__(self, cls):
-        super().__init__(cls)
-
-        self.fields.extend(['repo', 'image'])
-        self.optional.extend(['repo', 'image'])
-
-
-    def get_packages(self):
-        return super().get_packages() + ['environment', 'config', 'server']
-
-
     def ensure(self, env, user):
         curr_env = self.get_env()
-        
         if not self.retrieve(curr_env):
             self.store(curr_env)
-            
-        self.set_env(curr_env)
-
-
+  
     def get_env(self):
         return Runtime.get_env()
 
     def set_env(self, name = None, repo = None, image = None):
-        return Runtime.set_env(name, repo, image)
+        Runtime.set_env(name, repo, image)
 
-    def delete_env(self, name = None):
-        return Runtime.delete_env(name)
+    def delete_env(self):
+        Runtime.delete_env()
 
 
-    def render(self, fields, queryset_values):
+    def render(self, *fields, **filters):
+        data = super().render(*fields, **filters)
         env = self.get_env()
-        fields = list(fields)
-        data = [['Active'] + fields + ['Repo', 'Image']]
-        
-        fields.extend(['repo', 'image'])
 
-        for env_name in Runtime.get_env_index():
-            instance = self.retrieve(env_name)
-            record = []
+        data[0] = ['active'] + data[0]
 
-            if env and env_name == env:
-                record.append('******')
+        for index in range(1, len(data)):
+            record = data[index]
+            if env and record[0] == env.value:
+                data[index] = ['******'] + data[index]
             else:
-                record.append('')
-
-            for field in fields:
-                if field == 'name':
-                    record.append(env_name)
-                else:
-                    record.append(getattr(instance, field, ''))
-        
-            data.append(record)
+                data[index] = [''] + data[index]
 
         return data
 
@@ -69,34 +41,13 @@ class Environment(models.AppModel):
     
     name = models.CharField(primary_key=True, max_length=256)
     host = models.URLField(null=True)
-    port = models.IntegerField(null=True, default=5123)
-    
-    token = models.CharField(null=True, max_length=40, default=settings.DEFAULT_ADMIN_TOKEN)
-
-    @property
-    def repo(self):
-        return Runtime.get_repo_name(self.name)
-
-    @repo.setter
-    def repo(self, value):
-        Runtime.set_repo_name(self.name, value)
-
-    @property
-    def image(self):
-        return Runtime.get_image_name(self.name)
-
-    @image.setter
-    def image(self, value):
-        Runtime.set_image_name(self.name, value)
-
+    port = models.IntegerField(default=5123)
+    token = models.EncryptedCharField(max_length=40, default=settings.DEFAULT_ADMIN_TOKEN)
+    repo = models.CharField(max_length=1096, default=settings.DEFAULT_RUNTIME_REPO)
+    image = models.CharField(max_length=256, default=settings.DEFAULT_RUNTIME_IMAGE)
 
     class Meta:
         facade_class = EnvironmentFacade
 
     def  __str__(self):
         return "{}".format(self.name)
-
-
-    def save(self, *args, **kwargs):
-        Runtime.add_env_index(self.name)
-        return super().save(*args, **kwargs)
