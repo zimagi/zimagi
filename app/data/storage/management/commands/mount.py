77720ca36fa074f7454687d6b22a186e98930cc3
Adrian Webb <adrian@webb.sh>
2019-02-22 03:23:44 -0500

    Replacing storage mount CRUD commands with a storage mount resource command factory.

--- app/data/storage/management/commands/mount.py
+++ app/data/storage/management/commands/mount.py
@@ -1,5 +1,4 @@
-from systems.command import types
-from data.storage.management.commands import _mount as mount
+from systems.command import types, factory
 
 
 class Command(types.StorageRouterCommand):
@@ -7,36 +6,21 @@ class Command(types.StorageRouterCommand):
     def get_command_name(self):
         return 'mount'
 
-    def get_description(self, overview):
-        if overview:
-            return """manage environment storage mounts
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam 
-pulvinar nisl ac magna ultricies dignissim. Praesent eu feugiat 
-elit. Cras porta magna vel blandit euismod.
-"""
-        else:
-            return """manage environment storage mounts
-                      
-Etiam mattis iaculis felis eu pharetra. Nulla facilisi. 
-Duis placerat pulvinar urna et elementum. Mauris enim risus, 
-mattis vel risus quis, imperdiet convallis felis. Donec iaculis 
-tristique diam eget rutrum.
-
-Etiam sit amet mollis lacus. Nulla pretium, neque id porta feugiat, 
-erat sapien sollicitudin tellus, vel fermentum quam purus non sem. 
-Mauris venenatis eleifend nulla, ac facilisis nulla efficitur sed. 
-Etiam a ipsum odio. Curabitur magna mi, ornare sit amet nulla at, 
-scelerisque tristique leo. Curabitur ut faucibus leo, non tincidunt 
-velit. Aenean sit amet consequat mauris.
-"""
     def get_subcommands(self):
-        return (
-            ('get', mount.GetCommand),
-            ('list', mount.ListCommand),
-            ('add', mount.AddCommand),
-            ('update', mount.UpdateCommand),
-            ('attach', mount.AttachCommand),
-            ('rm', mount.RemoveCommand),
-            ('clear', mount.ClearCommand)
+        return factory.ResourceCommands(
+            types.StorageMountActionCommand, 'mount',
+            provider_name = 'storage',
+            provider_subtype = 'mount',
+            list_fields = (
+                ('name', 'Name'),
+                ('storage__name', 'Storage'),
+                ('storage__type', 'Storage type'),
+                ('remote_host', 'Remote host'),
+                ('remote_path', 'Remote path')
+            ),
+            relations = ('firewalls',),
+            scopes = {
+                'network': '--network',
+                'subnet': '--subnet'
+            }
         )
