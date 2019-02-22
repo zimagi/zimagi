5ea6e9e56145641396c21bc89eb486725bd2f240
Adrian Webb <adrian@webb.sh>
2019-02-22 03:14:06 -0500

    Updating the firewall model to extend the base network model and use the provider model mixin.

--- app/data/network/models/firewall.py
+++ app/data/network/models/firewall.py
@@ -1,40 +1,23 @@
 from systems import models
-from data.network import models as network
-
-import json
-import re
+from systems.models import network, provider
 
 
-class FirewallFacade(models.ProviderModelFacade):
-
-    def get_packages(self):
-        return super().get_packages() + ['network', 'server']
-
-    def key(self):
-        return 'name'
-
-    def set_scope(self, network):
-        super().set_scope(network_id = network.id)
+class FirewallFacade(
+    provider.ProviderModelFacadeMixin,
+    network.NetworkModelFacadeMixin
+):
+    pass
 
 
-class Firewall(models.AppProviderModel):
-
-    name = models.CharField(max_length=128)
-    type = models.CharField(null=True, max_length=128)
-    
-    network = models.ForeignKey(network.Network, related_name='firewalls', on_delete=models.PROTECT)
-   
+class Firewall(
+    provider.ProviderMixin,
+    network.NetworkModel
+):
     class Meta:
-        unique_together = (
-            ('network', 'name')
-        )
         facade_class = FirewallFacade
-
 
     def __str__(self):
         return "{}".format(self.name)
 
-
-    def initialize(self, command):
-        self.provider = command.get_provider('network:firewall', self.type, instance = self)
-        return True
+    def get_provider_name(self):
+        return 'network:firewall'
