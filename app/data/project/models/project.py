41c0fcc2bc6a182dae3aa3c5541fd252b7ea1b12
Adrian Webb <adrian@webb.sh>
2019-02-22 03:06:36 -0500

    Updating the project model to extend the base environment model and adding migration.

--- app/data/project/models/project.py
+++ app/data/project/models/project.py
@@ -1,56 +1,27 @@
 from django.conf import settings
 
 from systems import models
-from data.environment import models as env
-
-import json
+from systems.models import environment, provider
 
 
-class ProjectFacade(models.ProviderModelFacade):
-
-    def get_packages(self):
-        return super().get_packages() + ['project']
-
-
+class ProjectFacade(
+    provider.ProviderModelFacadeMixin,
+    environment.EnvironmentModelFacadeMixin
+):
     def ensure(self, env, user):
-        project = self.retrieve(settings.CORE_PROJECT)
-
-        if not project:
-            self.store(settings.CORE_PROJECT, 
-                type = 'internal'
-            )
+        if not self.retrieve(settings.CORE_PROJECT):
+            self.store(settings.CORE_PROJECT, type = 'internal')
 
 
-    def key(self):
-        return 'name'
- 
-    def scope(self, fields = False):
-        if fields:
-            return ('environment',)
-        
-        curr_env = env.Environment.facade.get_env()
-        if not curr_env:
-            return False
-
-        return { 'environment_id': curr_env }
-
-
-class Project(models.AppProviderModel):
-    name = models.CharField(max_length=128)
-    type = models.CharField(null=True, max_length=128)   
+class Project(
+    provider.ProviderMixin,
+    environment.EnvironmentModel
+):
     remote = models.CharField(null=True, max_length=256)
     reference = models.CharField(null=True, max_length=128)
- 
-    environment = models.ForeignKey(env.Environment, related_name='projects', on_delete=models.PROTECT)
 
     class Meta:
-        unique_together = ('environment', 'name')
         facade_class = ProjectFacade
 
-    def __str__(self):
-        return "{} ({})".format(self.name, self.type)
-
-
-    def initialize(self, command):
-        self.provider = command.get_provider('project', self.type, instance = self)
-        return True
+    def get_provider_name(self):
+        return 'project'
