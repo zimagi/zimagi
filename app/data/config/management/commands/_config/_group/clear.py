7a079cbc9be957fc587f0af5c1f68a9d81a97c89
Adrian Webb <adrian@webb.sh>
2019-02-22 03:04:34 -0500

    Removing the description method in various config commands.

--- app/data/config/management/commands/_config/_group/clear.py
+++ app/data/config/management/commands/_config/_group/clear.py
@@ -5,29 +5,6 @@ class ClearCommand(
     mixins.op.RemoveMixin,
     types.ConfigGroupActionCommand
 ):
-    def get_description(self, overview):
-        if overview:
-            return """clear all environment groups from configuration
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam 
-pulvinar nisl ac magna ultricies dignissim. Praesent eu feugiat 
-elit. Cras porta magna vel blandit euismod.
-"""
-        else:
-            return """clear all environment groups from configuration
-                      
-Etiam mattis iaculis felis eu pharetra. Nulla facilisi. 
-Duis placerat pulvinar urna et elementum. Mauris enim risus, 
-mattis vel risus quis, imperdiet convallis felis. Donec iaculis 
-tristique diam eget rutrum.
-
-Etiam sit amet mollis lacus. Nulla pretium, neque id porta feugiat, 
-erat sapien sollicitudin tellus, vel fermentum quam purus non sem. 
-Mauris venenatis eleifend nulla, ac facilisis nulla efficitur sed. 
-Etiam a ipsum odio. Curabitur magna mi, ornare sit amet nulla at, 
-scelerisque tristique leo. Curabitur ut faucibus leo, non tincidunt 
-velit. Aenean sit amet consequat mauris.
-"""
     def parse(self):
         self.parse_config_groups(True)
 
--- app/data/config/management/commands/_config/_group/list.py
+++ app/data/config/management/commands/_config/_group/list.py
@@ -9,29 +9,6 @@ class ListCommand(
     def groups_allowed(self):
         return False # Configuration access model
 
-    def get_description(self, overview):
-        if overview:
-            return """list environment groups for configuration
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam 
-pulvinar nisl ac magna ultricies dignissim. Praesent eu feugiat 
-elit. Cras porta magna vel blandit euismod.
-"""
-        else:
-            return """list environment groups for configuration
-                      
-Etiam mattis iaculis felis eu pharetra. Nulla facilisi. 
-Duis placerat pulvinar urna et elementum. Mauris enim risus, 
-mattis vel risus quis, imperdiet convallis felis. Donec iaculis 
-tristique diam eget rutrum.
-
-Etiam sit amet mollis lacus. Nulla pretium, neque id porta feugiat, 
-erat sapien sollicitudin tellus, vel fermentum quam purus non sem. 
-Mauris venenatis eleifend nulla, ac facilisis nulla efficitur sed. 
-Etiam a ipsum odio. Curabitur magna mi, ornare sit amet nulla at, 
-scelerisque tristique leo. Curabitur ut faucibus leo, non tincidunt 
-velit. Aenean sit amet consequat mauris.
-"""
     def parse(self):
         self.parse_config_groups(True)
 
