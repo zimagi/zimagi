e6dd19ac49807381ad69b5903091ef901215066d
Adrian Webb <adrian@webb.sh>
2019-02-22 02:33:23 -0500

    Adding DatabaseAccessError exception class, updating save method locking, and removing config and provider submodels from the base model library.

--- app/systems/models/base.py
+++ app/systems/models/base.py
@@ -5,11 +5,14 @@ from django.utils.timezone import now
 from .facade import ModelFacade
 
 import inspect
-import json
 import copy
 
 
 models.options.DEFAULT_NAMES += ('facade_class',)
+
+
+class DatabaseAccessError(Exception):
+    pass
 
 
 class AppMetaModel(ModelBase):
@@ -24,118 +27,25 @@ class AppMetaModel(ModelBase):
 
 class AppModel(models.Model, metaclass = AppMetaModel):
 
-    created = models.DateTimeField(null=True)
+    created = models.DateTimeField(null=True)    
     updated = models.DateTimeField(null=True)
     
     class Meta:
         abstract = True
         facade_class = ModelFacade
 
-
     def initialize(self, command):
         return True
 
     def save(self, *args, **kwargs):
-        with self.facade.thread_lock:
-            if not getattr(self, 'created', None):
-                self.created = now()
-            else:
-                self.updated = now()
+        if self.created is None:
+            self.created = now()
+        else:
+            self.updated = now()
         
+        with self.facade.thread_lock:
             super().save(*args, **kwargs)
-
 
     @property
     def facade(self):
         return copy.deepcopy(self.__class__.facade)
-
-
-class ConfigModelFacade(ModelFacade):
-
-    def __init__(self, cls):
-        super().__init__(cls)
-        self.fields.append('config')
-
-
-class AppConfigModel(AppModel):
-
-    _config = models.TextField(db_column="config", default='{}')
-    
-    @property
-    def config(self):
-        if getattr(self, '_cached_config', None) is None:        
-            self._cached_config = json.loads(self._config)
-        return self._cached_config
-
-    @config.setter
-    def config(self, data):
-        if not isinstance(data, str):
-            data = json.dumps(data)
-        
-        self._config = data
-        self._cached_config = None
-
-
-    class Meta:
-        abstract = True
-        facade_class = ConfigModelFacade
-    
-
-    def save(self, *args, **kwargs):
-        self.config = self.config
-        return super().save(*args, **kwargs)
-
-
-
-class ProviderModelFacade(ConfigModelFacade):
-
-    def __init__(self, cls):
-        super().__init__(cls)
-        self.fields.extend(['variables', 'state'])
-
-
-class AppProviderModel(AppConfigModel):
-
-    _variables = models.TextField(db_column="variables", default='{}')
-    _state_config = models.TextField(db_column="state", default='{}')
-
-
-    @property
-    def variables(self):
-        if getattr(self, '_cached_variables', None) is None:        
-            self._cached_variables = json.loads(self._variables)
-        return self._cached_variables
-
-    @variables.setter
-    def variables(self, data):
-        if not isinstance(data, str):
-            data = json.dumps(data)
-        
-        self._variables = data
-        self._cached_variables = None
-
-    
-    @property
-    def state(self):
-        if getattr(self, '_cached_state', None) is None:        
-            self._cached_state = json.loads(self._state_config)
-        return self._cached_state
-
-    @state.setter
-    def state(self, data):
-        if not isinstance(data, str):
-            data = json.dumps(data)
-        
-        self._state_config = data
-        self._cached_state = None
-
-
-    class Meta:
-        abstract = True
-        facade_class = ProviderModelFacade
-
-
-    def save(self, *args, **kwargs):
-        self.variables = self.variables
-        self.state = self.state
-        return super().save(*args, **kwargs)
