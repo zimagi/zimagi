bb2c32183a1e6bc969fd6ab5469a3c13506d6fcf
Adrian Webb <adrian@webb.sh>
2019-02-22 02:56:36 -0500

    Removing the description method in various environment commands.

--- app/data/environment/management/commands/set.py
+++ app/data/environment/management/commands/set.py
@@ -6,33 +6,10 @@ class Command(types.EnvironmentActionCommand):
     def get_command_name(self):
         return 'set'
 
-    def get_description(self, overview):
-        if overview:
-            return """set current cluster environment (for all operations)
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam 
-pulvinar nisl ac magna ultricies dignissim. Praesent eu feugiat 
-elit. Cras porta magna vel blandit euismod.
-"""
-        else:
-            return """set current cluster environment (for all operations)
-                      
-Etiam mattis iaculis felis eu pharetra. Nulla facilisi. 
-Duis placerat pulvinar urna et elementum. Mauris enim risus, 
-mattis vel risus quis, imperdiet convallis felis. Donec iaculis 
-tristique diam eget rutrum.
-
-Etiam sit amet mollis lacus. Nulla pretium, neque id porta feugiat, 
-erat sapien sollicitudin tellus, vel fermentum quam purus non sem. 
-Mauris venenatis eleifend nulla, ac facilisis nulla efficitur sed. 
-Etiam a ipsum odio. Curabitur magna mi, ornare sit amet nulla at, 
-scelerisque tristique leo. Curabitur ut faucibus leo, non tincidunt 
-velit. Aenean sit amet consequat mauris.
-"""
     def parse(self):
-        self.parse_env_name()
         self.parse_env_repo('--repo')
         self.parse_env_image('--image')
+        self.parse_env_name()
 
     def exec(self):
         self.set_env(self.env_name, self.env_repo, self.env_image)
