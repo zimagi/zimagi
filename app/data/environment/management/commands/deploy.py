5aa2a47db809e5becb1778aa3f3bd2f41a1c4b53
Adrian Webb <adrian@webb.sh>
2019-02-22 02:54:00 -0500

    Updating environment access in the deploy command.

--- app/data/environment/management/commands/deploy.py
+++ app/data/environment/management/commands/deploy.py
@@ -10,29 +10,6 @@ class Command(
     def get_command_name(self):
         return 'deploy'
 
-    def get_description(self, overview):
-        if overview:
-            return """deploy the cenv system to a standalone server
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam 
-pulvinar nisl ac magna ultricies dignissim. Praesent eu feugiat 
-elit. Cras porta magna vel blandit euismod.
-"""
-        else:
-            return """deploy the cenv system to a standalone server
-                      
-Etiam mattis iaculis felis eu pharetra. Nulla facilisi. 
-Duis placerat pulvinar urna et elementum. Mauris enim risus, 
-mattis vel risus quis, imperdiet convallis felis. Donec iaculis 
-tristique diam eget rutrum.
-
-Etiam sit amet mollis lacus. Nulla pretium, neque id porta feugiat, 
-erat sapien sollicitudin tellus, vel fermentum quam purus non sem. 
-Mauris venenatis eleifend nulla, ac facilisis nulla efficitur sed. 
-Etiam a ipsum odio. Curabitur magna mi, ornare sit amet nulla at, 
-scelerisque tristique leo. Curabitur ut faucibus leo, non tincidunt 
-velit. Aenean sit amet consequat mauris.
-"""
     def server_enabled(self):
         return True
     
@@ -44,7 +21,7 @@ velit. Aenean sit amet consequat mauris.
         self.set_server_scope()
         server = self.server
         
-        env = self.curr_env
+        env = self.get_env()
         env.host = server.ip
         env.token = settings.DEFAULT_ADMIN_TOKEN
         #env.save()
