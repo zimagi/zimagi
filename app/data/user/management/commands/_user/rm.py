b14a37f2c78779ab75f02930c20a96f5b3cf1dcb
Adrian Webb <adrian@webb.sh>
2019-02-22 02:47:44 -0500

    Removing description methods from the user namespace commands.

--- app/data/user/management/commands/_user/rm.py
+++ app/data/user/management/commands/_user/rm.py
@@ -5,29 +5,6 @@ class RemoveCommand(
     mixins.op.RemoveMixin,
     types.UserActionCommand
 ):
-    def get_description(self, overview):
-        if overview:
-            return """remove an existing user in current environment
-
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam 
-pulvinar nisl ac magna ultricies dignissim. Praesent eu feugiat 
-elit. Cras porta magna vel blandit euismod.
-"""
-        else:
-            return """remove an existing user in current environment
-                      
-Etiam mattis iaculis felis eu pharetra. Nulla facilisi. 
-Duis placerat pulvinar urna et elementum. Mauris enim risus, 
-mattis vel risus quis, imperdiet convallis felis. Donec iaculis 
-tristique diam eget rutrum.
-
-Etiam sit amet mollis lacus. Nulla pretium, neque id porta feugiat, 
-erat sapien sollicitudin tellus, vel fermentum quam purus non sem. 
-Mauris venenatis eleifend nulla, ac facilisis nulla efficitur sed. 
-Etiam a ipsum odio. Curabitur magna mi, ornare sit amet nulla at, 
-scelerisque tristique leo. Curabitur ut faucibus leo, non tincidunt 
-velit. Aenean sit amet consequat mauris.
-"""
     def parse(self):
         self.parse_user_name()
 
