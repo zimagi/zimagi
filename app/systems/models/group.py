b397884fc4252dd1c8bceed3adbba37138d1943c
Adrian Webb <adrian@webb.sh>
2019-02-24 01:58:26 -0500

    Adding an initial group model mixin.

---
+++ app/systems/models/group.py
@@ -0,0 +1,31 @@
+from settings import Roles
+from django.db import models as django
+
+from data.group.models import Group
+from .base import ModelFacade
+
+
+class GroupModelFacadeMixin(ModelFacade):
+    pass
+
+
+class GroupMixin(django.Model):
+
+    groups = django.ManyToManyField(Group)
+
+    class Meta:
+        abstract = True
+
+    def allowed_groups(self):
+        # Override in subclass
+        return []
+
+    def initialize(self, command):
+        if getattr(super(), 'initialize', None):
+            if not super().initialize(command):
+                return False
+               
+        groups = list(self.allowed_groups()) + list(self.groups.all().values_list('name', flat = True))
+        if groups and not command.check_access(groups):
+            return False
+        return True
