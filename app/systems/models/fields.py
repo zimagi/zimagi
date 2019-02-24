342c70493f170358276a8071e306edd240ccdddf
Adrian Webb <adrian@webb.sh>
2019-02-24 02:04:58 -0500

    Fixing issues with system model field definitions.

--- app/systems/models/fields.py
+++ app/systems/models/fields.py
@@ -7,38 +7,45 @@ import re
 
 
 class EncryptionMixin(object):
+    
+    def encrypt(self, value):
+        # Python data type
+        return Cipher.get('field').encrypt(value).decode()
+
+    def decrypt(self, value):
+        # Database cipher text
+        return Cipher.get('field').decrypt(str.encode(value))
+
+
+class EncryptedCharField(EncryptionMixin, models.CharField):
 
     def to_python(self, value):
-        if value is None:
-            return value
-        #value = Cipher.get('field').decrypt(value)
-        #return super().to_python(unserialize(value))
-        return super().to_python(value)
+        return super().to_python(self.decrypt(value))
 
     def from_db_value(self, value, expression, connection):
         return self.to_python(value)
 
-    def get_db_prep_save(self, value, connection):
-        value = super().get_db_prep_save(value, connection)
-        if value is None:
-            return value
-        #return Cipher.get('field').encrypt(serialize(value))
-        #return serialize(value)
-        return str(value)
+    def get_prep_value(self, value):
+        return self.encrypt(value)
 
 
-class EncryptedCharField(EncryptionMixin, models.CharField):
-    pass
+class EncryptedDataField(EncryptionMixin, models.TextField):
 
-class EncryptedTextField(EncryptionMixin, models.TextField):
-    pass
+    def to_python(self, value):
+        return super().to_python(unserialize(self.decrypt(value)))
+
+    def from_db_value(self, value, expression, connection):
+        return self.to_python(value)
+
+    def get_prep_value(self, value):
+        return self.encrypt(serialize(value))
 
 
 class CSVField(models.TextField):
 
     def to_python(self, value):
-        if value is None:
-            return value
+        if not value:
+            return []
         return super().to_python(
             [ x.strip() for x in value.split(',') ]
         )
@@ -46,8 +53,7 @@ class CSVField(models.TextField):
     def from_db_value(self, value, expression, connection):
         return self.to_python(value)
 
-    def get_db_prep_save(self, value, connection):
-        value = super().get_db_prep_save(value, connection)
-        if value is None:
+    def get_prep_value(self, value):
+        if not value:
             return value
         return ",".join([ x.strip() for x in value ])
