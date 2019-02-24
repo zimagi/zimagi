b7cbe7684e14d2519b8ea888a7392570518a3385
Adrian Webb <adrian@webb.sh>
2019-02-24 01:59:05 -0500

    Adding an initial storage model mixin.

---
+++ app/systems/models/storage.py
@@ -0,0 +1,38 @@
+from django.db import models as django
+
+from data.storage.models import Storage
+from .resource import ResourceModel, ResourceModelFacadeMixin
+
+
+class StorageModelFacadeMixin(ResourceModelFacadeMixin):
+
+    def set_scope(self, storage):
+        super().set_scope(storage_id = storage.id)
+
+
+class StorageMixin(django.Model):
+ 
+    storage = django.ForeignKey(Storage, on_delete=django.PROTECT)
+
+    class Meta:
+        abstract = True
+
+class StorageRelationMixin(django.Model):
+ 
+    storage = django.ManyToManyField(Storage)
+ 
+    class Meta:
+        abstract = True
+
+
+class StorageModel(StorageMixin, ResourceModel):
+
+    class Meta(ResourceModel.Meta):
+        abstract = True
+        unique_together = ('storage', 'name')
+
+    def __str__(self):
+        return "{}:{}".format(self.storage.name, self.name)
+
+    def get_id_fields(self):
+        return ('name', 'storage_id')
