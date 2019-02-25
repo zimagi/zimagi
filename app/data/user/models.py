e1f64ba918b7dd818fc4e3c6d0077097962258b6
Adrian Webb <adrian@webb.sh>
2019-02-25 13:58:43 -0500

    Adding provider support and more system related fields to the user model.

--- app/data/user/models.py
+++ app/data/user/models.py
@@ -1,9 +1,10 @@
 from django.conf import settings
 from django.db import models as django
 from django.contrib.auth.base_user import AbstractBaseUser
+from django.utils.timezone import now
 
 from settings import Roles
-from systems.models import base, facade, group
+from systems.models import base, facade, group, provider
 from data.environment.models import Environment
 
 import binascii
@@ -11,12 +12,26 @@ import os
 
 
 class UserFacade(
+    provider.ProviderModelFacadeMixin,
     group.GroupModelFacadeMixin,
     facade.ModelFacade
 ):
+    def key(self):
+        return 'name'
+
+    def ensure(self, command):
+        self._admin = self.retrieve(settings.ADMIN_USER)
+        if not self._admin:
+            self._admin = command.user_provider.create(
+                settings.ADMIN_USER, {}
+            )
+
+    def get_provider_name(self):
+        return 'user'
+
+
     @property
     def admin(self):
-        self.ensure()
         return self._admin
 
     @property
@@ -29,19 +44,12 @@ class UserFacade(
     def set_active_user(self, user):
         self._active_user = user
 
-    def ensure(self, env = None, user = None):
-        self._admin = self.retrieve(settings.ADMIN_USER)
-        if not self._admin:
-            self._admin, created = self.store(settings.ADMIN_USER)
-
-    def key(self):
-        return 'name'
-
     def generate_token(self):
         return binascii.hexlify(os.urandom(40)).decode()
 
 
 class User(
+    provider.ProviderMixin,
     group.GroupMixin,
     AbstractBaseUser, 
     metaclass = base.AppMetaModel
@@ -49,10 +57,13 @@ class User(
     USERNAME_FIELD = 'name'
 
     name = django.CharField(primary_key=True, max_length=150)
+    email = django.EmailField(null=True)
     first_name = django.CharField(max_length=30, null=True)
     last_name = django.CharField(max_length=150, null=True)
-    email = django.EmailField(null=True)
-    
+    is_active = django.BooleanField(default=True)
+    created = django.DateTimeField(null=True)    
+    updated = django.DateTimeField(null=True)
+        
     class Meta(AbstractBaseUser.Meta):
         facade_class = UserFacade
 
@@ -61,7 +72,12 @@ class User(
 
     def save(self, *args, **kwargs):
         if not self.password and self.name == settings.ADMIN_USER:
-            self.set_password(settings.DEFAULT_ADMIN_TOKEN)    
+            self.set_password(settings.DEFAULT_ADMIN_TOKEN)
+
+        if self.created is None:
+            self.created = now()
+        else:
+            self.updated = now()   
 
         with self.facade.thread_lock:
             super().save(*args, **kwargs)
