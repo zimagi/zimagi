a40f32cca891e211ea4eda4907bb8d36caa86c4b
Adrian Webb <adrian@webb.sh>
2019-02-25 13:59:18 -0500

    Adding provider support to the group model.

--- app/data/group/models.py
+++ app/data/group/models.py
@@ -1,21 +1,25 @@
 from django.db import models as django
 
 from settings import Roles
-from systems.models import environment
+from systems.models import environment, provider
 
 
 class GroupFacade(
+    provider.ProviderModelFacadeMixin,
     environment.EnvironmentModelFacadeMixin
 ):
-    def ensure(self, env, user):
+    def ensure(self, command):
         admin_group = self.retrieve(Roles.admin)
-
         if not admin_group:
-            (admin_group, created) = self.store(Roles.admin)
-            user.admin.groups.add(admin_group)
+            admin_group = command.group_provider.create(Roles.admin, {})
+            command._user.admin.groups.add(admin_group)
+
+    def get_provider_name(self):
+        return 'group'
 
 
 class Group(
+    provider.ProviderMixin,
     environment.EnvironmentModel
 ):
     parent = django.ForeignKey("Group", null=True, on_delete=django.SET_NULL)
