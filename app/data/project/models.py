f990a9513dd88e4945e9e2f17b51d6f1bab17a34
Adrian Webb <adrian@webb.sh>
2019-02-25 14:00:09 -0500

    Adding group support to the project model.

--- app/data/project/models.py
+++ app/data/project/models.py
@@ -1,20 +1,26 @@
 from django.conf import settings
 from django.db import models as django
 
-from systems.models import environment, provider
+from settings import Roles
+from systems.models import environment, group, provider
 
 
 class ProjectFacade(
     provider.ProviderModelFacadeMixin,
+    group.GroupModelFacadeMixin,
     environment.EnvironmentModelFacadeMixin
 ):
-    def ensure(self, env, user):
+    def ensure(self, command):
         if not self.retrieve(settings.CORE_PROJECT):
-            self.store(settings.CORE_PROJECT, type = 'internal')
+            command.project_provider.create(settings.CORE_PROJECT, {})
+
+    def get_provider_name(self):
+        return 'project'
 
 
 class Project(
     provider.ProviderMixin,
+    group.GroupMixin,
     environment.EnvironmentModel
 ):
     remote = django.CharField(null=True, max_length=256)
@@ -23,5 +29,5 @@ class Project(
     class Meta(environment.EnvironmentModel.Meta):
         facade_class = ProjectFacade
 
-    def get_provider_name(self):
-        return 'project'
+    def allowed_groups(self):
+        return [ Roles.admin ]
