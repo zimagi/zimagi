56780f8cca483bc38d646a5089fd008f99916118
Adrian Webb <adrian@webb.sh>
2019-02-25 14:24:39 -0500

    Updating project commands to use the new factory architecture.

--- app/interface/management/commands/project.py
+++ app/interface/management/commands/project.py
@@ -1,4 +1,53 @@
-from systems.command import types, factory
+from systems.command.base import command_list
+from systems.command import types, mixins, factory
+
+
+class ProvisionCommand(
+    types.ProjectActionCommand
+):
+    def parse(self):
+        self.parse_profile_components('--components')
+        self.parse_project_name()
+        self.parse_profile_name()
+        self.parse_profile_fields(True)
+
+    def exec(self):
+        self.project.provider.provision_profile(
+            self.profile_name,
+            self.profile_component_names, 
+            self.profile_fields
+        )
+
+
+class ExportCommand(
+    types.ProjectActionCommand
+):
+    def parse(self):
+        self.parse_profile_components('--components')
+        self.parse_project_name()
+        self.parse_profile_name()
+
+    def exec(self):
+        self.project.provider.export_profile(
+            self.profile_name,
+            self.profile_component_names
+        )
+
+
+class DestroyCommand(
+    types.ProjectActionCommand
+):
+    def parse(self):
+        self.parse_force()
+        self.parse_profile_components('--components')
+        self.parse_project_name()
+        self.parse_profile_name()
+
+    def exec(self):
+        self.project.provider.destroy_profile(
+            self.profile_name,
+            self.profile_component_names
+        )
 
 
 class Command(types.ProjectRouterCommand):
@@ -7,12 +56,20 @@ class Command(types.ProjectRouterCommand):
         return 'project'
 
     def get_subcommands(self):
-        return factory.ResourceCommands(
-            types.ProjectActionCommand, 'project',
-            list_fields = (
-                ('name', 'Name'),
-                ('type', 'Type'),
-                ('remote', 'Remote'),
-                ('reference', 'Reference')
-            )
+        return command_list(
+            factory.ResourceCommands(
+                types.ProjectActionCommand, 'project',
+                list_fields = (
+                    ('name', 'Name'),
+                    ('type', 'Type'),
+                    ('remote', 'Remote'),
+                    ('reference', 'Reference')
+                ),
+                relations = {
+                    'groups': ('group_names', '--groups')
+                }
+            ),
+            ('provision', ProvisionCommand),
+            ('export', ExportCommand),
+            ('destroy', DestroyCommand)
         )
