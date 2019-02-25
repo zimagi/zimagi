ddc02866185817503450da8eb28bd8fb74f714a4
Adrian Webb <adrian@webb.sh>
2019-02-25 13:36:36 -0500

    Adding provider related methods and removing excluded fields in the parse_config fields in the user command data mixin.

--- app/systems/command/mixins/data/user.py
+++ app/systems/command/mixins/data/user.py
@@ -1,8 +1,31 @@
 from . import DataMixin
 from data.user.models import User
+from utility import config
 
 
 class UserMixin(DataMixin):
+
+    def __init__(self, stdout = None, stderr = None, no_color = False):
+        super().__init__(stdout, stderr, no_color)
+        self.facade_index['00_user'] = self._user
+
+
+    def parse_user_provider_name(self, optional = False, help_text = 'system user provider (default @user_provider|internal)'):
+        self.parse_variable('user_provider_name', optional, str, help_text, 'NAME')
+
+    @property
+    def user_provider_name(self):
+        name = self.options.get('user_provider_name', None)
+        if not name:
+            name = self.get_config('user_provider', required = False)
+        if not name:
+            name = config.Config.string('USER_PROVIDER', 'internal')
+        return name
+
+    @property
+    def user_provider(self):
+        return self.get_provider('user', self.user_provider_name)
+
 
     def parse_user_name(self, optional = False, help_text = 'environment user name'):
         self.parse_variable('user', optional, str, help_text, 'NAME')
@@ -15,16 +38,9 @@ class UserMixin(DataMixin):
     def user(self):
         return self.get_instance(self._user, self.user_name)
 
-    def parse_user_fields(self, optional = False):
-        self.parse_fields(self._user, 'user_fields', optional, 
-            (
-                'password', 
-                'date_joined', 
-                'last_login', 
-                'is_superuser', 
-                'is_staff', 
-                'is_active'
-            )
+    def parse_user_fields(self, optional = False, help_callback = None):
+        self.parse_fields(self._user, 'user_fields', optional, [],
+            help_callback
         )
 
     @property
