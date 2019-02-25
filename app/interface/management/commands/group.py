780a5df673bc0c02fb390c1a1d3aac0ad8f8f363
Adrian Webb <adrian@webb.sh>
2019-02-25 14:21:52 -0500

    Updating group commands to use the new factory architecture.

--- app/interface/management/commands/group.py
+++ app/interface/management/commands/group.py
@@ -1,21 +1,38 @@
-from systems.command import types, mixins
+from systems.command.base import command_list
+from systems.command import types, mixins, factory
 
 
-class Command(
+class ChildrenCommand(
     mixins.op.UpdateMixin,
-    types.ServerActionCommand
+    types.GroupActionCommand
 ):
-    def get_command_name(self):
-        return 'group'
-
     def parse(self):
-        self.parse_group(help_text = 'parent group name')
-        self.parse_groups(False, help_text = 'one or more child group names')
+        self.parse_group_name(help_text = 'parent group name')
+        self.parse_group_names(False, help_text = 'one or more child group names')
 
     def exec(self):
         parent = self.exec_update(self._group, self.group_name)
-
         for group in self.group_names:
             self.exec_update(self._group, group, { 
                 'parent': parent
             })
+
+
+class Command(types.GroupRouterCommand):
+
+    def get_command_name(self):
+        return 'group'
+
+    def get_subcommands(self):
+        return command_list(
+            factory.ResourceCommands(
+                types.GroupActionCommand, 'group',
+                list_fields = (
+                    ('name', 'Group name'),
+                ),
+                relations = {
+                    'parent': ('group_parent_name', '--parent')
+                }
+            ),
+            ('children', ChildrenCommand)
+        )
