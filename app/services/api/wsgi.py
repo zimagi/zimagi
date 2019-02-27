1cf850f5f21a448ed18497d69c091bab2c3bf2df
Adrian Webb <adrian@webb.sh>
2019-02-27 10:40:32 -0500

    Removing the set of settings.API_EXEC in the WSGI API gateway.

--- app/services/api/wsgi.py
+++ app/services/api/wsgi.py
@@ -6,11 +6,9 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 """
-from django.conf import settings
 from django.core.wsgi import get_wsgi_application
 
 import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "services.api.settings")
-settings.API_EXEC = True
 application = get_wsgi_application()
