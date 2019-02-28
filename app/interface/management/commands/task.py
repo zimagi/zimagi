6c1612e3e75761bc71f911874b2c5b2e7889a45e
Adrian Webb <adrian@webb.sh>
2019-02-28 11:37:11 -0500

    Updates to the task command.

--- app/interface/management/commands/task.py
+++ app/interface/management/commands/task.py
@@ -1,5 +1,4 @@
-from systems.command.base import command_list
-from systems.command import types, mixins, factory
+from systems.command import types, mixins
 
 
 class Command(
@@ -13,14 +12,14 @@ class Command(
         return False # Access control via task definitions
 
     def parse(self):
-        self.parse_project_name()
-        self.parse_server_reference('--servers')
+        self.parse_server_search('--servers')
+        self.parse_project_name()        
         self.parse_task_name()        
         self.parse_task_params(True)
 
     def exec(self):
         self.project.provider.exec_task(
             self.task_name,
-            self.servers,
+            self.server_instances,
             self.task_params
         )
