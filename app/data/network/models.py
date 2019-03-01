4aba36a83dd10363fbaaa5e10377437314ae5b4e
Adrian Webb <adrian@webb.sh>
2019-03-01 05:50:43 -0500

    Adding group relation to the network model.

--- app/data/network/models.py
+++ app/data/network/models.py
@@ -1,18 +1,57 @@
 from django.db import models as django
 
-from systems.models import environment, provider
+from systems.models import environment, group, provider
 
 
 class NetworkFacade(
     provider.ProviderModelFacadeMixin,
+    group.GroupModelFacadeMixin,
     environment.EnvironmentModelFacadeMixin
 ):
     def get_provider_name(self):
         return 'network:network'
+    
+    def get_relations(self):
+        return {
+            'groups': ('group_names', '--groups'),
+            #'subnets': 'subnet_names', 
+            #'firewalls': 'firewall_names', 
+            #'storage': 'storage_names'
+        }
 
+    def default_order(self):
+        return 'cidr'
+
+    def get_list_fields(self):
+        return (
+            ('name', 'Name'),
+            ('type', 'Type'),
+            ('cidr', 'CIDR')
+        )
+    
+    def get_display_fields(self):
+        return (
+            ('name', 'Name'),
+            ('environment', 'Environment'),
+            ('type', 'Type'),
+            ('cidr', 'CIDR'),
+            '---',
+            ('config', 'Configuration'),
+            '---',
+            ('variables', 'Variables'),
+            ('state_config', 'State'),
+            '---',
+            ('created', 'Created'),
+            ('updated', 'Updated')
+        )
+    
+    def get_field_cidr_display(self, instance, value, short):
+        return value
+    
 
 class Network(
     provider.ProviderMixin,
+    group.GroupMixin,
     environment.EnvironmentModel
 ):
     cidr = django.CharField(null=True, max_length=128)
