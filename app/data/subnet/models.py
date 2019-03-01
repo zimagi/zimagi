b991c0db7c62482fc639972567208ab7470b561e
Adrian Webb <adrian@webb.sh>
2019-03-01 14:44:54 -0500

    Adding group support for network subnets in the subnet model and model facade.

--- app/data/subnet/models.py
+++ app/data/subnet/models.py
@@ -1,18 +1,62 @@
 from django.db import models as django
 
-from systems.models import network, provider
+from systems.models import network, group, provider
 
 
 class SubnetFacade(
     provider.ProviderModelFacadeMixin,
+    group.GroupModelFacadeMixin,
     network.NetworkModelFacadeMixin
 ):
     def get_provider_name(self):
         return 'network:subnet'
+    
+    def get_scopes(self):
+        return {
+            'network': ('network_name', '--network')
+        }
+    
+    def get_relations(self):
+        return {
+            'groups': ('group_names', '--groups'),
+            #'mounts': 'mount_names', 
+            #'servers': 'server_names'
+        }
+
+    def default_order(self):
+        return 'cidr'
+
+    def get_list_fields(self):
+        return (
+            ('name', 'Name'),
+            ('network', 'Network'),
+            ('type', 'Type'),            
+            ('cidr', 'CIDR')
+        )
+    
+    def get_display_fields(self):
+        return (
+            ('name', 'Name'),
+            ('network', 'Network'),
+            ('type', 'Type'),
+            ('cidr', 'CIDR'),
+            '---',
+            ('config', 'Configuration'),
+            '---',
+            ('variables', 'Variables'),
+            ('state_config', 'State'),
+            '---',
+            ('created', 'Created'),
+            ('updated', 'Updated')
+        )
+    
+    def get_field_cidr_display(self, instance, value, short):
+        return value
 
 
 class Subnet(
     provider.ProviderMixin,
+    group.GroupMixin,
     network.NetworkModel
 ):
     cidr = django.CharField(null=True, max_length=128)
