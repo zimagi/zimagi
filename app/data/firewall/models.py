fa1d8b4d18b813a07543e260484cd2f0c3de33e0
Adrian Webb <adrian@webb.sh>
2019-03-02 14:00:21 -0500

    Adding group support to the storage and firewall models.

--- app/data/firewall/models.py
+++ app/data/firewall/models.py
@@ -1,18 +1,25 @@
 from django.db import models as django
 
-from systems.models import network, provider
+from systems.models import group, network, provider
 
 
 class FirewallFacade(
     provider.ProviderModelFacadeMixin,
+    group.GroupModelFacadeMixin,
     network.NetworkModelFacadeMixin
 ):
     def get_provider_name(self):
         return 'network:firewall'
+    
+    def get_relations(self):
+        return {
+            'groups': ('group', 'group_names', '--groups')
+        }
 
 
 class Firewall(
     provider.ProviderMixin,
+    group.GroupMixin,
     network.NetworkModel
 ):
     class Meta(network.NetworkModel.Meta):
--- app/data/storage/models.py
+++ app/data/storage/models.py
@@ -1,18 +1,25 @@
 from django.db import models as django
 
-from systems.models import network, provider
+from systems.models import group, network, provider
 
 
 class StorageFacade(
     provider.ProviderModelFacadeMixin,
+    group.GroupModelFacadeMixin,
     network.NetworkModelFacadeMixin
 ):
     def get_provider_name(self):
         return 'storage:storage'
+    
+    def get_relations(self):
+        return {
+            'groups': ('group', 'group_names', '--groups')
+        }
 
 
 class Storage(
     provider.ProviderMixin,
+    group.GroupMixin,
     network.NetworkModel
 ):
     class Meta(network.NetworkModel.Meta):
