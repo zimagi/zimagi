f77383d47e03bf620ff61832e063d655be96d18a
Adrian Webb <adrian@webb.sh>
2019-03-06 11:13:27 -0500

    Adding config_value_type parse and accessor in the command config data mixin.

--- app/systems/command/mixins/data/config.py
+++ app/systems/command/mixins/data/config.py
@@ -9,14 +9,14 @@ class ConfigMixin(DataMixin):
             'full_name': 'configuration',
             'plural': 'configurations',
             'model': Config,
-            'provider': True,                       
+            'provider': True,
             'system_fields': (
                 'environment',
                 'type',
                 'config',
                 'variables',
                 'state_config',
-                'created', 
+                'created',
                 'updated'
             )
         }
@@ -28,7 +28,7 @@ class ConfigMixin(DataMixin):
 
 
     def parse_config_value(self, optional = False, help_text = 'environment configuration value'):
-        self.parse_variable('config_value', optional, str, help_text, 
+        self.parse_variable('config_value', optional, str, help_text,
             value_label = 'VALUE'
         )
 
@@ -37,12 +37,22 @@ class ConfigMixin(DataMixin):
         return self.options.get('config_value', None)
 
 
+    def parse_config_value_type(self, optional = '--type', help_text = 'environment configuration type (default str)'):
+        self.parse_variable('config_value_type', optional, str, help_text,
+            value_label = 'TYPE'
+        )
+
+    @property
+    def config_value_type(self):
+        return self.options.get('config_value_type', None)
+
+
     def get_config(self, name, default = None, required = False):
         if not name:
             return default
-        
+
         config = self.get_instance(self._config, name, required = required)
         if config is None:
             return default
-        
+
         return config.value
