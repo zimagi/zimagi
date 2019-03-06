7c3eaefeaa95605cbb4d9e60c185ee897d7f0f62
Adrian Webb <adrian@webb.sh>
2019-03-05 20:57:12 -0500

    Updates to the server model facade.

--- app/data/server/models.py
+++ app/data/server/models.py
@@ -12,10 +12,88 @@ class ServerFacade(
     def get_provider_name(self):
         return 'compute'
     
+    def get_scopes(self):
+        return (
+            'network',
+            'subnet'
+        )
+        
     def get_relations(self):
         return {
-            'groups': ('group', 'Groups', '--groups')
+            'groups': ('group', 'Groups', '--groups'),
+            'firewalls': ('firewall', 'Firewalls', '--firewalls')
         }
+
+    def default_order(self):
+        return 'name'
+
+    def get_list_fields(self):
+        return (
+            ('name', 'Name'),
+            ('subnet', 'Subnet'),
+            ('type', 'Type'),
+            ('status', 'Status'),
+            ('public_ip', 'Public IP'),
+            ('private_ip', 'Private IP'),
+            ('user', 'User'),
+            ('password', 'Password'),
+            ('private_key', 'Private key')
+        )
+    
+    def get_display_fields(self):
+        return (
+            ('name', 'Name'),
+            ('subnet', 'Subnet'),
+            ('type', 'Type'),
+            ('status', 'Status'),
+            '---',
+            ('public_ip', 'Public IP'),
+            ('private_ip', 'Private IP'),
+            ('user', 'User'),
+            ('password', 'Password'),
+            ('private_key', 'Private key'),
+            ('data_device', 'Data device'),
+            ('backup_device', 'Backup device'),
+            '---',
+            ('config', 'Configuration'),
+            '---',
+            ('variables', 'Variables'),
+            ('state_config', 'State'),
+            '---',
+            ('created', 'Created'),
+            ('updated', 'Updated')
+        )
+    
+    def get_field_public_ip_display(self, instance, value, short):
+        return value
+    
+    def get_field_private_ip_display(self, instance, value, short):
+        return value
+    
+    def get_field_user_display(self, instance, value, short):
+        return value
+    
+    def get_field_password_display(self, instance, value, short):
+        if short:
+            return '*****' if value else None
+        return value
+    
+    def get_field_private_key_display(self, instance, value, short):
+        if short:
+            return '*****' if value else None
+        return value
+    
+    def get_field_data_device_display(self, instance, value, short):
+        return value
+    
+    def get_field_backup_device_display(self, instance, value, short):
+        return value
+
+    def get_field_status_display(self, instance, value, short):
+        return self.model.STATUS_RUNNING if self.ping() else self.model.STATUS_UNREACHABLE
+
+    def ping(self, port = 22):
+        return self.model.provider.ping(port = port)
 
 
 class Server(
@@ -52,18 +130,7 @@ class Server(
     def allowed_groups(self):
         return [ Roles.admin, Roles.server_admin ]
 
-    def initialize(self, command):
-        if not super().initialize(command):
-            return False
-        
-        self.status = self.STATUS_RUNNING if self.ping() else self.STATUS_UNREACHABLE
-        return True
-
-
     def running(self):
         if self.status == self.STATUS_RUNNING:
             return True
         return False
-
-    def ping(self, port = 22):
-        return self.provider.ping(port = port)
