d08d0adb4684820068241b488e585fc23680ae81
Adrian Webb <adrian@webb.sh>
2019-03-06 11:52:17 -0500

    Updating the config profile provisioner to support recent updates to config command and model structure.

--- app/systems/project/provisioner/core/config.py
+++ app/systems/project/provisioner/core/config.py
@@ -1,17 +1,22 @@
+from utility import data
+
 
 class ConfigMixin(object):
 
     def get_configs(self):
         facade = self.command.facade(self.command._config)
-        return self.command.get_instances(facade)        
+        return self.command.get_instances(facade)
 
     def get_config(self, name):
         facade = self.command.facade(self.command._config)
-        return self.command.get_instance(facade, name, required = False)        
+        return self.command.get_instance(facade, name, required = False)
 
 
     def set_config(self, params):
         for name, value in params.items():
+            components = value.split(':')
+            if len(components) > 1:
+                value = data.format_value(components[0], components[1])
             self.ensure_config(name, value)
 
 
@@ -19,10 +24,11 @@ class ConfigMixin(object):
         if 'config' in self.data:
             for name, value in self.data['config'].items():
                 self.ensure_config(name, value)
-    
+
     def ensure_config(self, name, value):
-        self.command.exec_local('config set', {
-            'config': name,
+        self.command.exec_local('config save', {
+            'config_name': name,
+            'config_value_type': type(value).__name__,
             'config_value': value
         })
 
@@ -30,5 +36,5 @@ class ConfigMixin(object):
         index = {}
         for config in self.get_configs():
             index[config.name] = config.value
-        
+
         self.data['config'] = index
