cd6ecc86e5615a3ac4184e9b0bedba78c21f3502
Adrian Webb <adrian@webb.sh>
2019-03-05 21:03:17 -0500

    Updating the IP options in the internal compute provider.

--- app/systems/compute/internal.py
+++ app/systems/compute/internal.py
@@ -4,7 +4,8 @@ from .base import BaseComputeProvider
 class Internal(BaseComputeProvider):
 
     def provider_config(self, type = None):
-        self.requirement(str, 'ip', help = 'SSH capable IP of server')
+        self.requirement(str, 'public_ip', help = 'Public IP of server')
+        self.requirement(str, 'private_ip', help = 'Private IP of server')
         self.requirement(str, 'password', help = 'Password of server user')
 
         self.option(str, 'user', 'admin', help = 'Server SSH user', config_name = 'internal_user')
