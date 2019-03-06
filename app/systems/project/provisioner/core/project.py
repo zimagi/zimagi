992d49d23bd8787af61c397987aeb55eaffdc511
Adrian Webb <adrian@webb.sh>
2019-03-06 11:53:19 -0500

    Trimming trailing whitespace in the project profile provisioner library.

--- app/systems/project/provisioner/core/project.py
+++ app/systems/project/provisioner/core/project.py
@@ -7,7 +7,7 @@ class ProjectMixin(object):
         for instance in self.command.get_instances(facade):
             if instance.type != 'internal':
                 projects.append(instance)
-        
+
         return projects
 
     def get_project(self, name):
@@ -18,16 +18,16 @@ class ProjectMixin(object):
     def ensure_projects(self):
         def process(name, state):
             self.ensure_project(name, self.data['project'][name])
-        
+
         if 'project' in self.data:
             self.command.run_list(self.data['project'].keys(), process)
-    
+
     def ensure_project(self, name, config):
         provider = config.pop('provider', None)
 
         if provider is None:
             self.command.error("Project {} requires 'provider' field".format(name))
-        
+
         self.command.exec_local('project save', {
             'project_provider_name': provider,
             'project_name': name,
@@ -38,19 +38,19 @@ class ProjectMixin(object):
     def export_projects(self):
         def describe(project):
             return { 'provider': project.type }
-        
+
         self._export('project', self.get_projects(), describe)
 
 
     def destroy_projects(self):
         def process(name, state):
             self.destroy_project(name)
-        
+
         if 'project' in self.data:
             self.command.run_list(self.data['project'].keys(), process)
 
     def destroy_project(self, name):
-        self.command.exec_local('project rm', { 
+        self.command.exec_local('project rm', {
             'project_name': name,
             'force': True
         })
