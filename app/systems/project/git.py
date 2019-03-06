90fd972b3105ec6706d8a3b5d5453dbf0e3c990b
Adrian Webb <adrian@webb.sh>
2019-03-06 11:54:00 -0500

    Adding success messages for initialization/update of git repositories in the git project provider.

--- app/systems/project/git.py
+++ app/systems/project/git.py
@@ -12,7 +12,7 @@ class Git(BaseProjectProvider):
 
     def provider_config(self, type = None):
         self.requirement(str, 'remote', help = 'Git remote to clone and pull updates')
-        
+
         self.option(str, 'reference', 'master', help = 'Git branch, tag, or commit reference', config_name = 'git_reference')
 
 
@@ -30,15 +30,17 @@ class Git(BaseProjectProvider):
             self._pull(repository, branch_name = instance.reference)
         else:
             repository = pygit2.clone_repository(instance.remote, project_path, checkout_branch = instance.reference)
-        
+
         repository.update_submodules(init = True)
-    
+        self.command.success("Initialized repository from remote")
+
     def _update_repository(self, instance):
         repository = pygit2.Repository(self.project_path(instance.name))
         repository.remotes.set_url("origin", instance.remote)
 
         self._pull(repository, branch_name = instance.reference)
         repository.update_submodules(init = True)
+        self.command.success("Updated repository from remote")
 
 
     def finalize_instance(self, instance):
@@ -54,16 +56,16 @@ class Git(BaseProjectProvider):
 
         remote_reference = repository.lookup_reference('refs/remotes/{}/{}'.format(remote_name, branch_name)).target
         merge_result, _ = repository.merge_analysis(remote_reference)
-            
+
         if merge_result & pygit2.GIT_MERGE_ANALYSIS_UP_TO_DATE:
             return
-            
+
         elif merge_result & pygit2.GIT_MERGE_ANALYSIS_FASTFORWARD:
             repository.checkout_tree(repository.get(remote_reference))
             head_reference = repository.lookup_reference('refs/heads/{}'.format(branch_name))
             head_reference.set_target(remote_reference)
             repository.head.set_target(remote_reference)
-            
+
         elif merge_result & pygit2.GIT_MERGE_ANALYSIS_NORMAL:
             repository.merge(remote_reference)
             user = repository.default_signature
@@ -76,6 +78,6 @@ class Git(BaseProjectProvider):
                 [repository.head.target, remote_reference]
             )
             repository.state_cleanup()
-                
+
         else:
             self.command.error("Unable to pull remote changes from remote")
