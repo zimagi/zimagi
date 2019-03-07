f6aa2dc2da7b4109a2e9089d1f8e03f65ab3702e
Adrian Webb <adrian@webb.sh>
2019-03-07 12:07:04 -0500

    Moving group access check from model to facade behind thread lock in the base group model and facade.

--- app/systems/models/group.py
+++ app/systems/models/group.py
@@ -6,7 +6,14 @@ from .base import ModelFacade
 
 
 class GroupModelFacadeMixin(ModelFacade):
- 
+
+    def check_group_access(self, instance, command):
+        with self.thread_lock:
+            groups = list(instance.allowed_groups()) + list(instance.groups.all().values_list('name', flat = True))
+            if groups and not command.check_access(groups):
+                return False
+        return True
+
     def get_field_groups_display(self, instance, value, short):
         groups = [ str(x) for x in value.all() ]
         return "\n".join(groups)
@@ -14,7 +21,7 @@ class GroupModelFacadeMixin(ModelFacade):
 
 class GroupMixin(django.Model):
 
-    groups = django.ManyToManyField(Group, 
+    groups = django.ManyToManyField(Group,
         related_name = "%(class)s_relation"
     )
     class Meta:
@@ -27,8 +34,4 @@ class GroupMixin(django.Model):
         if getattr(super(), 'initialize', None):
             if not super().initialize(command):
                 return False
-               
-        groups = list(self.allowed_groups()) + list(self.groups.all().values_list('name', flat = True))
-        if groups and not command.check_access(groups):
-            return False
-        return True
+        return self.facade.check_group_access(self, command)
