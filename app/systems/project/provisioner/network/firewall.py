c052bbf81377fe98b9dc10e3cf926aa651fd0791
Adrian Webb <adrian@webb.sh>
2019-03-08 12:16:35 -0500

    Updates to the firewall project profile provisioner mixin.

--- app/systems/project/provisioner/network/firewall.py
+++ app/systems/project/provisioner/network/firewall.py
@@ -1,37 +1,17 @@
-from systems.command.base import AppOptions
-from utility.data import ensure_list
-
 
 class FirewallMixin(object):
 
-    def get_firewalls(self):
-        facade = self.command.facade(self.command._firewall)
-        return self.command.get_instances(facade)
-
-
-    def ensure_firewalls(self):
-        def process(name):
-            self.ensure_firewall(name, self.data['firewall'][name])
-
-        if self.include('firewall') and 'firewall' in self.data:
-            self.command.run_list(self.data['firewall'].keys(), process)
-
     def ensure_firewall(self, name, config):
-        collection = AppOptions(self.command)
-        network = config.pop('network', None)
-        rules = config.pop('rules', None)
+        networks = self.pop_values('network', config)
+        rules = self.pop_info('rules', config)
+        groups = self.pop_values('group_names', config)
 
-        if network is None:
-            self.command.error("Firewall {} requires 'network' field".format(name))
-
-        if rules is None:
-            self.command.error("Firewall {} requires 'rules' field defined".format(name))
-
-        def process_firewall(network):
+        def process(network):
             self.command.exec_local('firewall save', {
                 'network_name': network,
                 'firewall_name': name,
-                'firewall_fields': config
+                'firewall_fields': config,
+                'group_names': groups
             })
             def process_rule(rule):
                 self.command.exec_local('firewall rule save', {
@@ -42,56 +22,14 @@ class FirewallMixin(object):
                 })
             self.command.run_list(rules.keys(), process_rule)
 
-        self.command.run_list(ensure_list(collection.add('network', network)), process_firewall)
-
-
-    def export_firewalls(self):
-        def describe(firewall):
-            config = { 'network': firewall.network.name }
-            rules = {}
-
-            for rule in firewall.rules.all():
-                rules[rule.name] = self.get_variables(rule)
-
-            config['rules'] = rules
-            return config
-
-        if self.include('firewall'):
-            self._export('firewall', self.get_firewalls(), describe)
-
-
-    def destroy_firewalls(self):
-        def process(name):
-            self.destroy_firewall(name, self.data['firewall'][name])
-
-        if self.include('firewall') and 'firewall' in self.data:
-            self.command.run_list(self.data['firewall'].keys(), process)
-
-    def destroy_firewall(self, name, config):
-        collection = AppOptions(self.command)
-        network = config.pop('network', None)
-        rules = config.pop('rules', None)
-
-        if network is None:
+        if not networks:
             self.command.error("Firewall {} requires 'network' field".format(name))
 
-        if rules is None:
+        if not rules:
             self.command.error("Firewall {} requires 'rules' field defined".format(name))
 
-        def process_firewall(network):
-            def process_rule(rule):
-                self.command.exec_local('firewall rule rm', {
-                    'network_name': network,
-                    'firewall_name': name,
-                    'firewall_rule_name': rule,
-                    'force': True
-                })
+        self.command.run_list(networks, process)
 
-            self.command.run_list(rules.keys(), process_rule)
-            self.command.exec_local('firewall rm', {
-                'network_name': network,
-                'firewall_name': name,
-                'force': True
-            })
 
-        self.command.run_list(ensure_list(collection.add('network', network)), process_firewall)
+    def describe_firewall(self, firewall):
+        return { 'network': firewall.network.name }
