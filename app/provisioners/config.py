58dc44103ad84858235e99631486396e164811ab
Adrian Webb <adrian@webb.sh>
2019-03-18 09:08:56 -0400

    Updating the config provisioner to extend the command profile base provisioner.

--- app/provisioners/config.py
+++ app/provisioners/config.py
@@ -1,16 +1,21 @@
-from utility import data
+from systems.command import profile
 
 
-class ConfigMixin(object):
+class Provisioner(profile.BaseProvisioner):
 
-    def ensure_config(self, name, value):
+    def priority(self):
+        return 0
+
+
+    def ensure(self, name, value):
         self.command.exec_local('config save', {
             'config_name': name,
             'config_value_type': type(value).__name__,
             'config_value': value
         })
 
-    def destroy_config(self, name, value):
+
+    def destroy(self, name, value):
         self.command.exec_local('config rm', {
             'config_name': name,
             'force': True
