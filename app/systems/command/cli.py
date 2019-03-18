72ab19dc3a31080a116d696408e76a79db73eec3
Adrian Webb <adrian@webb.sh>
2019-03-18 17:36:33 -0400

    Some cleanup and formatting changes in the command CLI library.

--- app/systems/command/cli.py
+++ app/systems/command/cli.py
@@ -21,24 +21,26 @@ class CLI(TerminalMixin):
         self.registry = CommandRegistry()
 
 
-    def start_django(self):
-        try:
-            settings.INSTALLED_APPS
-        except ImproperlyConfigured as exc:
-            self.settings_exception = exc
-        except ImportError as exc:
-            self.settings_exception = exc
+    def handle_error(self, error):
+        if not isinstance(error, CommandError):
+            self.print()
+            self.print('** ' + self.error_color(str(error)), sys.stderr)
+            if Runtime.debug():
+                self.print()
+                self.print('> ' + self.traceback_color(
+                        "\n".join([ item.strip() for item in format_exception_info() ])
+                    ),
+                    stream = sys.stderr
+                )
 
-        if settings.configured:
-            django.setup()
-            call_command('migrate', interactive = False, verbosity = 0)
 
-    def initialize(self, argv):
-        self.start_django()
+    def initialize(self):
+        django.setup()
+        call_command('migrate', interactive = False, verbosity = 0)
 
         parser = CommandParser(add_help = False, allow_abbrev = False)
         parser.add_argument('args', nargs = '*')
-        namespace, extra = parser.parse_known_args(argv[1:])
+        namespace, extra = parser.parse_known_args(self.argv[1:])
         args = namespace.args
 
         if '--version' in extra:
@@ -56,32 +58,17 @@ class CLI(TerminalMixin):
         return args
 
     def execute(self):
-        command = self.registry.find_command(
-            self.initialize(self.argv),
-            main = True
-        )
-        self.print()
-        command.run_from_argv(self.argv)
+        try:
+            self.registry.find_command(
+                self.initialize(),
+                main = True
+            ).run_from_argv(self.argv)
+            self.exit(0)
 
-
-    def handle_error(self, error):
-        if not isinstance(error, CommandError):
-            self.print()
-            self.print(self.error_color(" ** {}".format(str(error))),
-                sys.stderr
-            )
-            if Runtime.debug():
-                exception = "\n".join([ '  ' + item.strip() for item in format_exception_info() ])
-                exception = re.sub(r'[\{\}]', '', exception)
-                self.print()
-                self.print(self.traceback_color(exception), stream = sys.stderr)
+        except Exception as e:
+            self.handle_error(e)
+            self.exit(1)
 
 
 def execute(argv):
-    cli = CLI(argv)
-    try:
-        cli.execute()
-        cli.exit(0)
-    except Exception as e:
-        cli.handle_error(e)
-        cli.exit(1)
+    CLI(argv).execute()
