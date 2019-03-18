f1c42c96250d8e3ff62f9a32f6260624a11ea659
Adrian Webb <adrian@webb.sh>
2019-03-17 23:18:46 -0400

    Fixing the save related implementation when no inputs given in the base model mixin.

--- app/systems/models/base.py
+++ app/systems/models/base.py
@@ -21,14 +21,14 @@ class AppMetaModel(ModelBase):
     def __init__(cls, name, bases, attr):
         if not cls._meta.abstract:
             facade_class = cls._meta.facade_class
-            
+
             if facade_class and inspect.isclass(facade_class):
                 cls.facade = facade_class(cls)
 
 
 class BaseModelMixin(django.Model):
 
-    created = django.DateTimeField(null=True)    
+    created = django.DateTimeField(null=True)
     updated = django.DateTimeField(null=True)
 
     class Meta:
@@ -42,30 +42,32 @@ class BaseModelMixin(django.Model):
             self.created = now()
         else:
             self.updated = now()
-        
+
         with self.facade.thread_lock:
             super().save(*args, **kwargs)
 
     def save_related(self, provider):
         relations = self.facade.get_all_relations()
         for field, value in self.facade.get_relation_names(provider.command).items():
-            facade = getattr(provider.command, "_{}".format(relations[field][0]))
-            field_value = getattr(self, field)
+            if value is not None:
+                facade = getattr(provider.command, "_{}".format(relations[field][0]))
+                field_value = getattr(self, field)
 
-            if isinstance(field_value, Manager):
-                provider.update_related(self, field, facade, value)
-            else:
-                provider.set_related(self, field, facade, value)
-            
+                if isinstance(field_value, Manager):
+                    provider.update_related(self, field, facade, value)
+                else:
+                    value = None if not value else value
+                    provider.set_related(self, field, facade, value)
+
     @property
     def facade(self):
         return copy.deepcopy(self.__class__.facade)
 
 
 class AppModel(
-    BaseModelMixin, 
+    BaseModelMixin,
     metaclass = AppMetaModel
-):   
+):
     class Meta:
         abstract = True
         facade_class = ModelFacade
