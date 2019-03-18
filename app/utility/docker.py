c16f2f1c1173fb690e100371d6c2df78c3de78b6
Adrian Webb <adrian@webb.sh>
2019-03-17 21:04:44 -0400

    Adding a scaffolding Docker run_registry method in the docker utility class (not finishedgit status).

--- app/utility/docker.py
+++ app/utility/docker.py
@@ -2,6 +2,7 @@ from utility import shell
 
 import datetime
 import docker
+import pathlib
 
 
 class MetaDocker(type):
@@ -19,6 +20,46 @@ class MetaDocker(type):
         client = docker.from_env()
         container = client.containers.get(id)
         container.commit(image_name)
+
+    def run_registry(self):
+        pathlib.Path('/var/lib/registry').mkdir(mode = 0o700, parents = True, exist_ok = True)
+
+        client = docker.from_env()
+        client.containers.run('registry:2',
+            name = 'registry',
+            network_mode = 'host',
+            detach = True,
+            restart_policy = {
+                "Name": "always",
+                "MaximumRetryCount": 10
+            },
+            mem_limit = '1g',
+            ports = {
+                5000: 443
+            },
+            volumes = {
+                '/var/lib/registry': {
+                    'bind': '/var/lib/registry',
+                    'mode': 'rw'
+                },
+                '-->/certs': {
+                    'bind': '/certs',
+                    'mode': 'rw'
+                },
+                '-->/auth': {
+                    'bind': '/auth',
+                    'mode': 'rw'
+                }
+            },
+            environment = {
+                'REGISTRY_HTTP_ADDR': '0.0.0.0:443',
+                'REGISTRY_HTTP_TLS_CERTIFICATE': '/certs/domain.crt',
+                'REGISTRY_HTTP_TLS_KEY': '/certs/domain.key',
+                'REGISTRY_AUTH': 'htpasswd',
+                'REGISTRY_AUTH_HTPASSWD_PATH': '/auth/htpasswd',
+                'REGISTRY_AUTH_HTPASSWD_REALM': 'Registry Realm'
+            }
+        )
 
 
 class Docker(object, metaclass = MetaDocker):
