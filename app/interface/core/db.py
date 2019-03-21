dbe25565b70f0168afc077f9c37a53b381b99c35
Adrian Webb <adrian@webb.sh>
2019-03-21 01:34:55 -0400

    Adding db start and stop commands to manage local PostgreSQL service.

--- app/interface/core/db.py
+++ app/interface/core/db.py
@@ -1,5 +1,31 @@
 from systems.command.base import command_list
 from systems.command.types import db
+from utility.docker import Docker
+
+
+class StartCommand(
+    db.DatabaseActionCommand
+):
+    def parse(self):
+        self.parse_variable('memory', '--memory', str,
+            'PostgreSQL database memory size in g(GB)/m(MB)',
+            value_label = 'NUM(g|m)',
+            default = '250m'
+        )
+
+    def exec(self):
+        Docker.start_postgres(self,
+            memory = self.options.get('memory')
+        )
+        self.success('Successfully started PostgreSQL database service')
+
+
+class StopCommand(
+    db.DatabaseActionCommand
+):
+    def exec(self):
+        Docker.stop_postgres(self)
+        self.success('Successfully stopped PostgreSQL database service')
 
 
 class PullCommand(
@@ -31,6 +57,8 @@ class Command(db.DatabaseRouterCommand):
 
     def get_subcommands(self):
         return (
+            ('start', StartCommand),
+            ('stop', StopCommand),
             ('pull', PullCommand),
             ('push', PushCommand)
         )
