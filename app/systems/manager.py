1288991431a70e80ee8c548d8e59af80479fd8a6
Adrian Webb <adrian@webb.sh>
2019-03-21 03:33:46 -0400

    Moving service management from docker utility class to manager class and deleting docker utility library.

--- app/systems/manager.py
+++ app/systems/manager.py
@@ -8,11 +8,14 @@ from utility.shell import Shell
 
 import os
 import sys
+import time
+import datetime
 import re
 import pathlib
 import importlib
 import json
 import yaml
+import docker
 
 
 class RequirementError(Exception):
@@ -52,6 +55,10 @@ class Manager(object):
         if content:
             content = yaml.load(content)
         return content
+
+    def load_env(self, env_name, variables = {}):
+        env_file = os.path.join(self.data_dir, "{}.env".format(env_name))
+        return Config.load(env_file, {})
 
 
     def module_config(self, path):
@@ -284,3 +291,93 @@ class Manager(object):
 
     def delete_service(self, name):
         os.remove(self.service_file(name))
+
+
+    @property
+    def container_id(self):
+        return Shell.capture(('cat', '/proc/1/cpuset')).split('/')[-1]
+
+
+    def generate_image_name(self, base_image):
+        repository = base_image.split(':')[0]
+        time = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        return "{}:{}".format(repository, time)
+
+    def create_image(self, id, image_name):
+        client = docker.from_env()
+        container = client.containers.get(id)
+        container.commit(image_name)
+
+
+    def create_volume(self, name):
+        return docker.from_env().volumes.create(name)
+
+
+    def start_service(self, command, name, image, ports,
+        docker_entrypoint = None,
+        docker_command = None,
+        environment = {},
+        volumes = {},
+        memory = '250m',
+        wait = 30
+    ):
+        client = docker.from_env()
+        success = True
+        service = self.get_service(name)
+        if service:
+            try:
+                client.containers.get(service['id'])
+                command.notice("Service {} is already running".format(name))
+                return
+
+            except docker.errors.NotFound:
+                pass
+
+        for local_path, remote_config in volumes.items():
+            if local_path[0] != '/':
+                self.create_volume(local_path)
+
+        container = client.containers.run(image,
+            entrypoint = docker_entrypoint,
+            command = docker_command,
+            name = name,
+            detach = True,
+            restart_policy = {
+                'Name': 'always',
+            },
+            mem_limit = memory,
+            ports = ports,
+            volumes = volumes,
+            environment = environment
+        )
+        for index in range(wait):
+            service = client.containers.get(container.id)
+            if service.status == 'restarting':
+                success = False
+                break
+            time.sleep(1)
+
+        service = client.containers.get(container.id)
+        self.save_service(name, container.id, {
+            'image': image,
+            'ports': service.attrs["NetworkSettings"]["Ports"],
+            'environment': environment,
+            'volumes': volumes,
+            'success': success
+        })
+        if not success:
+            command.info(command.notice_color(container.logs().decode("utf-8").strip()))
+            self.stop_service(command, name)
+            command.error("Service {} terminated with errors".format(name))
+
+    def stop_service(self, command, name):
+        service = self.get_service(name)
+        if service:
+            container = docker.from_env().containers.get(
+                service['id']
+            )
+            container.stop()
+            container.remove()
+            self.delete_service(name)
+        else:
+            command.notice("Service {} is not running".format(name))
