e3bd714edb42dcc5d573465a9493e7ce3a48a206
Adrian Webb <adrian@webb.sh>
2019-03-21 02:40:01 -0400

    Removing the uneeded AUTH_PASSWORD_VALIDATORS core setting since all passwords are autogenerated in the system.

--- app/settings/core.py
+++ app/settings/core.py
@@ -151,21 +151,6 @@ MIDDLEWARE = MANAGER.installed_middleware() + [
 #
 AUTH_USER_MODEL = 'user.User'
 
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
 #
 # Caching configuration
 #
