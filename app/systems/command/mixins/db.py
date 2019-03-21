9c7aea6bfe12c7477ced056c7ed10934d93638d3
Adrian Webb <adrian@webb.sh>
2019-03-21 01:33:08 -0400

    Cleaning up unneeded methods in the db command mixin.

--- app/systems/command/mixins/db.py
+++ app/systems/command/mixins/db.py
@@ -4,40 +4,6 @@ from .base import DataMixin
 
 class DatabaseMixin(DataMixin):
 
-    def parse_db_dir(self, optional = '--dir', help_text = 'database directory within module'):
-        self.parse_variable('db_dir', optional, str, help_text,
-            value_label = 'NAME',
-            default = 'db'
-        )
-
-    @property
-    def db_dir(self):
-        return self.options.get('db_dir', 'db')
-
-
-    def parse_file_name(self, optional = False, help_text = 'database file name within module'):
-        self.parse_variable('file_name', optional, str, help_text,
-            value_label = 'NAME'
-        )
-
-    @property
-    def file_name(self):
-        return self.options.get('file_name', None)
-
-    @property
-    def db_file_path(self, module):
-        file_location = os.path.join(self.db_dir, self.file_name)
-        return module.module_path(file_location)
-
-
-    def parse_no_encrypt(self, optional = '--no-encrypt', help_text = 'database file not encrypted'):
-        self.parse_flag('no_encrypt', optional, help_text)
-
-    @property
-    def no_encrypt(self):
-        return self.options.get('no_encrypt', None)
-
-
     @property
     def db(self):
         if not getattr(self, '_cached_db_manager', None):
