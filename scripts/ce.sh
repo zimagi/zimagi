62649a597918489d116259f8b9b39da7ccc1fd26
Adrian Webb <adrian@webb.sh>
2019-03-22 08:48:40 -0400

    Adding execution recovery when no image found in the ce script.

--- scripts/ce.sh
+++ scripts/ce.sh
@@ -2,24 +2,39 @@
 #-------------------------------------------------------------------------------
 set -e
 
+DEFAULT_CENV_IMAGE='cenv/cenv:latest'
+
 if [ -f /var/local/cenv/cenv.env ]
 then
     source /var/local/cenv/cenv.env
 else
     CENV_REPO=''
-    CENV_IMAGE='cenv/cenv:latest'
-fi
-if [ ! -z "${CENV_REPO}" ]
-then
-    CENV_REMOTE="${CENV_REPO}/${CENV_IMAGE}"
-else
-    CENV_REMOTE="${CENV_IMAGE}"
+    CENV_IMAGE="${DEFAULT_CENV_IMAGE}"
 fi
 
-if [ -z "${CENV_DEBUG}" -o ! -z "${CENV_NO_SYNC}" ]
+function sync_image() {
+    IMAGE="$1"
+
+    if [ ! -z "${CENV_REPO}" ]
+    then
+        CENV_REMOTE="${CENV_REPO}/${IMAGE}"
+    else
+        CENV_REMOTE="${IMAGE}"
+    fi
+
+    if [ -z "${CENV_DEBUG}" -o ! -z "${CENV_NO_SYNC}" ]
+    then
+        echo " ** synchronizing runtime..."
+        docker pull "${CENV_REMOTE}" >/dev/null 2>&1
+    fi
+    echo "$IMAGE"
+}
+
+CENV_IMAGE="$(sync_image ${CENV_IMAGE})"
+if ! docker inspect "${CENV_IMAGE}" >/dev/null 2>&1
 then
-    echo " ** synchronizing runtime..."
-    docker pull "${CENV_REMOTE}" >/dev/null 2>&1
+    rm -f /var/local/cenv/cenv.env
+    CENV_IMAGE="$(sync_image ${DEFAULT_CENV_IMAGE})"
 fi
 docker run --rm --interactive --tty \
     --env-file /var/local/cenv/django.env \
