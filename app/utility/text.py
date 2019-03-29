7b3695ced2b474e59c80f78473537ed6dadd7606
Adrian Webb <adrian@webb.sh>
2019-03-29 01:29:54 -0400

    removing extra whitespace in the text utility library.

--- app/utility/text.py
+++ app/utility/text.py
@@ -9,7 +9,7 @@ def split_lines(text):
 
 def split_paragraphs(text):
     para_edge = re.compile(r"(\n\s*\n)", re.MULTILINE)
-    return para_edge.split(text)    
+    return para_edge.split(text)
 
 
 def wrap(text, width, init_indent = '', init_style = None, indent = '', style = None):
@@ -21,7 +21,7 @@ def wrap(text, width, init_indent = '', init_style = None, indent = '', style = 
         header = True
         content = init_style(lines[0]) if init_style else lines[0]
         lines[0] = "{}{}".format(init_indent, content)
-        
+
         if count > 1:
             for index in range(1, len(lines)):
                 if header and lines[index] == '':
@@ -32,7 +32,7 @@ def wrap(text, width, init_indent = '', init_style = None, indent = '', style = 
                         content = init_style(lines[index]) if init_style else lines[index]
                     else:
                         content = style(lines[index]) if style else lines[index]
-                
+
                 lines[index] = "{}{}".format(indent, content)
 
         lines[-1] = "{}\n".format(lines[-1])
@@ -44,21 +44,21 @@ def wrap_page(text, init_indent = '', init_style = None, indent = '', style = No
 
 
 class TextWrapper(textwrap.TextWrapper):
-    
+
     def wrap(self, text):
         paragraphs = split_paragraphs(text)
         wrapped_lines = []
-        
+
         for para in paragraphs:
             if para.isspace():
                 if not self.replace_whitespace:
                     if self.expand_tabs:
                         para = para.expandtabs()
-                    
+
                     wrapped_lines.append(para[1:-1])
                 else:
                     wrapped_lines.append('')
             else:
                 wrapped_lines.extend(textwrap.TextWrapper.wrap(self, para))
-        
+
         return wrapped_lines
