3896ce4b8bb439b43a2bb722c92232481a9b732c
Adrian Webb <adrian@webb.sh>
2019-03-29 17:39:12 -0400

    Removing some whitespace in the config provisioner.

--- app/provisioners/config.py
+++ app/provisioners/config.py
@@ -6,14 +6,12 @@ class Provisioner(profile.BaseProvisioner):
     def priority(self):
         return 0
 
-
     def ensure(self, name, value):
         self.command.exec_local('config save', {
             'config_name': name,
             'config_value_type': type(value).__name__,
             'config_value': value
         })
-
 
     def destroy(self, name, value):
         self.command.exec_local('config rm', {
