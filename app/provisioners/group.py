eb71744d4ea31b792929ac53e4287ec2ec0e3c98
Adrian Webb <adrian@webb.sh>
2019-03-29 17:40:40 -0400

    Adding a describe method in the group provisioner.

--- app/provisioners/group.py
+++ app/provisioners/group.py
@@ -6,13 +6,14 @@ class Provisioner(profile.BaseProvisioner):
     def priority(self):
         return 1
 
-
     def ensure(self, name, children):
         self.command.exec_local('group children', {
             'group_name': name,
             'group_names': [] if not children else children
         })
 
+    def describe(self, instance):
+        return [ x.name for x in instance.group_relation.all() ]
 
     def destroy(self, name, children):
         self.command.exec_local('group rm', {
