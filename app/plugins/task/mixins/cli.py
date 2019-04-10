7fa7bf6ff88fdcd59d5bacd2365f783b7553ad75
Adrian Webb <adrian@webb.sh>
2019-04-10 11:06:30 -0400

    Adding an initial CLI task mixin.

---
+++ app/plugins/task/mixins/cli.py
@@ -0,0 +1,22 @@
+from systems.command.base import OptionsTemplate
+from utility.data import ensure_list
+
+
+class CLITaskMixin(object):
+
+    def _merge_options(self, options, overrides, lock = False):
+        if not lock and overrides:
+            return { **options, **overrides }
+        return options
+
+    def _interpolate(self, command, variables):
+        final_command = []
+
+        for component in ensure_list(command):
+            parser = OptionsTemplate(component)
+            try:
+                final_command.append(parser.substitute(**variables))
+            except KeyError as e:
+                self.command.error("Configuration {} does not exist, escape literal with @@".format(e))
+
+        return final_command
