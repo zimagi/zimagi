25521d05a877516e273a8531f353fd794d99d9f7
Adrian Webb <adrian@webb.sh>
2019-04-10 11:26:23 -0400

    Removing ensure and destroy methods in the config provisioner.

--- app/provisioners/config.py
+++ app/provisioners/config.py
@@ -5,16 +5,3 @@ class Provisioner(profile.BaseProvisioner):
 
     def priority(self):
         return 0
-
-    def ensure(self, name, value):
-        self.exec('config save',
-            config_name = name,
-            config_value_type = type(value).__name__,
-            config_value = value
-        )
-
-    def destroy(self, name, value):
-        self.exec('config rm',
-            config_name = name,
-            force = True
-        )
