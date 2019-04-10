7a023c045f035ecc2f72813519d740ad36f920c7
Adrian Webb <adrian@webb.sh>
2019-04-10 11:27:38 -0400

    Adding an initial destroy stage provisioner.

---
+++ app/provisioners/destroy.py
@@ -0,0 +1,24 @@
+from systems.command import profile
+
+
+class Provisioner(profile.BaseProvisioner):
+
+    def priority(self):
+        return 0
+
+    def destroy(self, name, config):
+        module = self.pop_value('module', config)
+        task = self.pop_value('task', config)
+        command = self.pop_value('command', config)
+
+        if not task and not command:
+            self.command.error("Provision {} requires 'task' or 'command' field".format(name))
+
+        if command:
+            self.exec(command, **config)
+        else:
+            self.exec('exec',
+                module_name = module,
+                task_name = task,
+                task_fields = config
+            )
