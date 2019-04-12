aca1104f77e70a9005169da22fe07a2fda90576a
Adrian Webb <adrian@webb.sh>
2019-04-12 12:39:28 -0400

    Adding optional scope processing in the provision and destroy provisioners.

--- app/provisioners/provision.py
+++ app/provisioners/provision.py
@@ -1,4 +1,5 @@
 from systems.command import profile
+from utility.data import get_dict_combinations
 
 
 class Provisioner(profile.BaseProvisioner):
@@ -7,6 +8,7 @@ class Provisioner(profile.BaseProvisioner):
         return 100
 
     def ensure(self, name, config):
+        scopes = self.pop_value('scopes', config)
         module = self.pop_value('module', config)
         task = self.pop_value('task', config)
         command = self.pop_value('command', config)
@@ -14,11 +16,17 @@ class Provisioner(profile.BaseProvisioner):
         if not task and not command:
             self.command.error("Provision {} requires 'task' or 'command' field".format(name))
 
-        if command:
-            self.exec(command, **config)
+        def _execute(data):
+            if command:
+                self.exec(command, **data)
+            else:
+                self.exec('exec',
+                    module_name = module,
+                    task_name = task,
+                    task_fields = data
+                )
+        if scopes:
+            for scope in get_dict_combinations(scopes):
+                _execute(self.interpolate(config, **scope))
         else:
-            self.exec('exec',
-                module_name = module,
-                task_name = task,
-                task_fields = config
-            )
+            _execute(config)
