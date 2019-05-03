e2d6858a153168ede7fed3d95254af9cb2e7f389
Adrian Webb <adrian@webb.sh>
2019-05-03 18:17:52 -0400

    Adding configuration override fields parameter to the module provision and destroy commands.

--- app/interface/core/module.py
+++ app/interface/core/module.py
@@ -68,10 +68,12 @@ class ProvisionCommand(
         self.parse_profile_components('--components')
         self.parse_module_name()
         self.parse_profile_name()
+        self.parse_profile_config_fields(True)
 
     def exec(self):
         self.module.provider.provision_profile(
             self.profile_name,
+            config = self.profile_config_fields,
             components = self.profile_component_names,
             display_only = self.display_only,
             plan = self.plan
@@ -87,6 +89,7 @@ class DestroyCommand(
         self.parse_profile_components('--components')
         self.parse_module_name()
         self.parse_profile_name()
+        self.parse_profile_config_fields(True)
 
     def confirm(self):
         self.confirmation()
@@ -94,6 +97,7 @@ class DestroyCommand(
     def exec(self):
         self.module.provider.destroy_profile(
             self.profile_name,
+            config = self.profile_config_fields,
             components = self.profile_component_names,
             display_only = self.display_only
         )
