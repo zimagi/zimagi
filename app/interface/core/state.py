bb0b0c92e19f8573a1fa8e37311ca38aa36597d4
Adrian Webb <adrian@webb.sh>
2019-05-19 14:54:08 -0400

    Adding an initial state command set.

---
+++ app/interface/core/state.py
@@ -0,0 +1,12 @@
+from systems.command.factory import resource
+from systems.command.types import state
+
+
+class Command(state.StateRouterCommand):
+
+    def get_subcommands(self):
+        return resource.ResourceCommandSet(
+            state.StateActionCommand, self.name,
+            allow_update = False,
+            allow_clear = False
+        )
