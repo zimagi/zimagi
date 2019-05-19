2834c356df9cbe88b4d8881d16d82837555bc31b
Adrian Webb <adrian@webb.sh>
2019-05-19 14:57:02 -0400

    Updating the log command set to use the resource command factory.

--- app/interface/core/log.py
+++ app/interface/core/log.py
@@ -6,8 +6,8 @@ class Command(
     log.LogRouterCommand
 ):
     def get_subcommands(self):
-        parent = log.LogActionCommand
-        return (
-            ('list', resource.ListCommand(parent, self.name)),
-            ('get', resource.GetCommand(parent, self.name))
+        return resource.ResourceCommandSet(
+            log.LogActionCommand, self.name,
+            allow_update = False,
+            allow_remove = False
         )
