454c0c4f0d18012b6dc5a0e1cd398bc3bb4b21d6
Adrian Webb <adrian@webb.sh>
2019-05-20 02:55:30 -0400

    Renaming provisioner methods to components in the system manager class.

--- app/systems/manager.py
+++ app/systems/manager.py
@@ -321,26 +321,26 @@ class Manager(object):
         return roles
 
 
-    def load_provisioner(self, profile, name):
-        provisioner_class = "provisioners.{}.Provisioner".format(name)
-        return import_string(provisioner_class)(name, profile)
+    def load_component(self, profile, name):
+        component_class = "components.{}.ProfileComponent".format(name)
+        return import_string(component_class)(name, profile)
 
-    def load_provisioners(self, profile):
-        provisioners = {}
-        for provisioner_dir in self.module_dirs('provisioners'):
-            for type in os.listdir(provisioner_dir):
+    def load_components(self, profile):
+        components = {}
+        for component_dir in self.module_dirs('components'):
+            for type in os.listdir(component_dir):
                 if type[0] != '_' and type.endswith('.py'):
                     name = type.replace('.py', '')
 
                     if name != 'config':
-                        instance = self.load_provisioner(profile, name)
+                        instance = self.load_component(profile, name)
                         priority = instance.priority()
 
-                        if priority not in provisioners:
-                            provisioners[priority] = [ instance ]
+                        if priority not in components:
+                            components[priority] = [ instance ]
                         else:
-                            provisioners[priority].append(instance)
-        return provisioners
+                            components[priority].append(instance)
+        return components
 
 
     def service_file(self, name):
