b88b1fe9eb15393485cb08eef799e88382c636ae
Adrian Webb <adrian@webb.sh>
2019-05-23 17:26:20 -0400

    Updating the temp utility class to extend the filesystem utility class.

--- app/utility/temp.py
+++ app/utility/temp.py
@@ -1,9 +1,7 @@
 from contextlib import contextmanager
 
-import os
-import threading
-import pathlib
-import shutil
+from .filesystem import FileSystem
+
 import string
 import random
 import datetime
@@ -18,15 +16,12 @@ def temp_dir(*args, **kwargs):
         temp.delete()
 
 
-class TempDir(object):
+class TempDir(FileSystem):
 
     def __init__(self):
-        self.temp_name = self._generate_name()
-        self.temp_path = "/tmp/{}".format(self.temp_name)
+        self.name = self._generate_name()
+        super().__init__("/tmp/{}".format(self.name))
 
-        self.thread_lock = threading.Lock()
-
-        pathlib.Path(self.temp_path).mkdir(mode = 0o700, parents = True, exist_ok = True)
 
     def _generate_name(self, length = 5):
         chars = string.ascii_lowercase + string.digits
@@ -34,62 +29,12 @@ class TempDir(object):
         return "{}-{}".format(datetime.datetime.now().strftime("%Y%m%d%H%M%S"), random_text)
 
 
-    def mkdir(self, directory):
-        temp_path = os.path.join(self.temp_path, directory)
-        pathlib.Path(temp_path).mkdir(mode = 0o700, parents = True, exist_ok = True)
-        return temp_path
-
-    def path(self, file_name, directory = None):
-        if file_name.startswith(self.temp_path):
-            return file_name
-
-        if directory:
-            temp_path = self.mkdir(directory)
-        else:
-            temp_path = self.temp_path
-
-        return os.path.join(temp_path, file_name)
-
-
-    def load(self, file_name, directory = None, binary = False):
-        tmp_path = self.path(file_name, directory = directory)
-        operation = 'rb' if binary else 'r'
-        content = None
-
-        with self.thread_lock:
-            if os.path.exists(tmp_path):
-                with open(tmp_path, operation) as file:
-                    content = file.read()
-
-        return content
-
     def save(self, content, name = None, directory = None, extension = None, binary = False):
         if name is None:
             name = self._generate_name()
-
-        tmp_path = self.path(name, directory = directory)
-        operation = 'wb' if binary else 'w'
-
-        if extension:
-            tmp_path = "{}.{}".format(tmp_path, extension)
-
-        with self.thread_lock:
-            with open(tmp_path, operation) as file:
-                file.write(content)
-
-            path = pathlib.Path(tmp_path)
-            path.chmod(0o700)
-
-        return tmp_path
+        return super().save(content, name, directory, extension, binary)
 
     def link(self, source_path, name = None, directory = None):
         if name is None:
             name = self._generate_name()
-
-        tmp_path = self.path(name, directory = directory)
-        os.symlink(source_path, tmp_path)
-        return tmp_path
-
-    def delete(self):
-        if self.temp_path.startswith('/tmp/'):
-            shutil.rmtree(self.temp_path, ignore_errors = True)
+        return super().link(source_path, name, directory)
