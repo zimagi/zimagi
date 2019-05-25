1c476107fa3046615162fde6583f8b53316bb7a9
Adrian Webb <adrian@webb.sh>
2019-05-25 15:09:04 -0400

    Adding host exec parameter for run and destroy methods in the profile component.

--- app/components/profile.py
+++ app/components/profile.py
@@ -10,6 +10,7 @@ class ProfileComponent(profile.BaseProfileComponent):
         return 90
 
     def run(self, name, config, display_only = False):
+        host = self.pop_value('host', config)
         profile = self.pop_value('profile', config)
         if not profile:
             self.command.error("Profile {} requires 'profile' field".format(name))
@@ -23,6 +24,7 @@ class ProfileComponent(profile.BaseProfileComponent):
 
             if display_only or not once or not self.command.get_state(state_name):
                 self.exec('run',
+                    host = host,
                     module_name = module,
                     profile_name = profile,
                     profile_config_fields = deep_merge(copy.deepcopy(self.profile.data['config']), config),
@@ -34,6 +36,7 @@ class ProfileComponent(profile.BaseProfileComponent):
 
 
     def destroy(self, name, config):
+        host = self.pop_value('host', config)
         profile = self.pop_value('profile', config)
         if not profile:
             self.command.error("Profile {} requires 'profile' field".format(name))
@@ -45,6 +48,7 @@ class ProfileComponent(profile.BaseProfileComponent):
             self.pop_value('once', config)
 
             self.exec('destroy',
+                host = host,
                 module_name = module,
                 profile_name = profile,
                 profile_config_fields = deep_merge(copy.deepcopy(self.profile.data['config']), config),
