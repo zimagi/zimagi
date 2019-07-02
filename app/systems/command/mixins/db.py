c3ad446ce7ec3a89689fc443aa87059fecb7154e
Adrian Webb <adrian@webb.sh>
2019-07-02 03:08:53 -0400

    Adding database packages parse and accessor methods in the db command data mixin.

--- app/systems/command/mixins/db.py
+++ app/systems/command/mixins/db.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+
 from systems.db import manager
 from .base import DataMixin
 
@@ -9,3 +11,13 @@ class DatabaseMixin(DataMixin):
         if not getattr(self, '_cached_db_manager', None):
             self._cached_db_manager = manager.DatabaseManager()
         return self._cached_db_manager
+
+
+    def parse_db_packages(self, optional = True, help_text = 'one or more database package names'):
+        self.parse_variables('db_packages', optional, str, help_text,
+            value_label = 'NAME'
+        )
+
+    @property
+    def db_packages(self):
+        return self.options.get('db_packages', [ settings.DB_PACKAGE_ALL_NAME ])
