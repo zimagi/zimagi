2d2b326c0a870286133e77b1766e966c167b3ade
Adrian Webb <adrian@webb.sh>
2019-07-03 14:47:21 -0400

    Splitting log messages from the command log model.

--- app/data/log/models.py
+++ app/data/log/models.py
@@ -13,27 +13,15 @@ class LogFacade(
         return [] # Do not export with db dumps!!
 
 
-    def create(self, key, **values):
-        instance = super().create(key, **values)
-        instance.messages = []
-        return instance
-
-
     def get_field_user_display(self, instance, value, short):
         return self.relation_color(str(value))
-
-    def get_field_command_display(self, instance, value, short):
-        return value
-
-    def get_field_status_display(self, instance, value, short):
-        return value
 
     def get_field_messages_display(self, instance, value, short):
         from systems.command import messages
 
         display = []
-        for data in value:
-            msg = messages.AppMessage.get(data, decrypt = False)
+        for record in instance.messages.all():
+            msg = messages.AppMessage.get(record.data, decrypt = False)
             display.append(self.raw_text(msg.format(True)))
 
         return "\n".join(display)
@@ -46,13 +34,13 @@ class Log(
     user = django.ForeignKey(User, null = True, on_delete = django.PROTECT, related_name = '+')
     command = django.CharField(max_length = 256, null = True)
     status = django.CharField(max_length = 64, null = True)
-    messages = fields.EncryptedDataField(default = [])
 
     class Meta(environment.EnvironmentModel.Meta):
         verbose_name = "log"
         verbose_name_plural = "logs"
         facade_class = LogFacade
         ordering = ['-created']
+        dynamic_fields = ['messages']
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -73,3 +61,11 @@ class Log(
 
     def set_status(self, success):
         self.status = self.STATUS_SUCCESS if success else self.STATUS_FAILED
+
+
+class LogMessage(django.Model):
+    log = django.ForeignKey(Log, related_name='messages', on_delete=django.CASCADE)
+    data = fields.EncryptedDataField(null = True)
+
+    def __str__(self):
+        return "{} ({})".format(self.log.command, self.data)
