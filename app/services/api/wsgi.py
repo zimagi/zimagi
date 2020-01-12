e885461842d9ca0c9249c201e37f922c74122238
Adrian Webb <adrian@webb.sh>
2020-01-12 15:59:28 -0500

    Updating the header comment in the API WSGI gateway.

--- app/services/api/wsgi.py
+++ app/services/api/wsgi.py
@@ -1,10 +1,10 @@
 """
-API WSGI config.
+MCMI WSGI config.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
 from django.core.wsgi import get_wsgi_application
 import os
