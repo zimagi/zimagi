d4411bdad8d5231d695e500d61dfe2b5a2d6aa23
Adrian Webb <adrian@webb.sh>
2020-01-13 17:38:32 -0500

    Adding a module_ensure state set in the module reset command to trigger a container rebuild.

--- app/interface/core/module.py
+++ app/interface/core/module.py
@@ -40,6 +40,7 @@ class ResetCommand(
         env = self.get_env()
         env.runtime_image = None
         env.save()
+        self.set_state('module_ensure', True)
         self.success("Successfully reset module runtime")
 
 
