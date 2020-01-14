a170ada89866868f0061d048ae38567a8d187227
Adrian Webb <adrian@webb.sh>
2020-01-14 13:17:13 -0500

    Adding an initial celery initialization library.

---
+++ app/services/tasks/celery.py
@@ -0,0 +1,11 @@
+from celery import Celery
+
+import os
+import django
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'services.tasks.settings')
+django.setup()
+
+app = Celery('MCMI')
+app.config_from_object('django.conf:settings', namespace = 'CELERY')
+app.autodiscover_tasks(force = True)
