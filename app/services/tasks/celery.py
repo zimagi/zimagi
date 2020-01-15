2201ce74822ca5f0cf4aae9f11b0900f0e84d5af
Adrian Webb <adrian@webb.sh>
2020-01-15 08:00:40 -0500

    Adding a call to set_default for the Celery app in the celery task service definition.

--- app/services/tasks/celery.py
+++ app/services/tasks/celery.py
@@ -7,5 +7,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'services.tasks.settings')
 django.setup()
 
 app = Celery('MCMI')
+app.set_default()
 app.config_from_object('django.conf:settings', namespace = 'CELERY')
 app.autodiscover_tasks(force = True)
