a742ffbcc7aa8253a8e2d2c7f3a01addfb8e3459
Adrian Webb <adrian@webb.sh>
2020-01-20 15:27:47 -0500

    Adding an initial Celery tasks definition in the core interface.

---
+++ app/interface/core/tasks.py
@@ -0,0 +1,24 @@
+from celery import shared_task
+
+from plugins.task.base import CeleryTask
+
+
+@shared_task(bind=True, base=CeleryTask, name='mcmi.task.exec')
+def task_exec(self, module_name, task_name, options = None, verbosity = 2):
+    if not options:
+        options = {}
+
+    self.exec_command('task', {
+        'module_name': module_name,
+        'task_name': task_name,
+        'task_fields': options,
+        'verbosity': verbosity
+    })
+
+
+@shared_task(bind=True, base=CeleryTask, name='mcmi.command.exec')
+def command_exec(self, name, options = None):
+    if not options:
+        options = {}
+
+    self.exec_command(name, options)
