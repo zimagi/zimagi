1ec71d1d28dbd85b925682251b3dd54d968e1d27
Adrian Webb <adrian@webb.sh>
2020-01-26 20:51:18 -0500

    Adding base task specification in the celery app in the celery tasks library.

--- app/services/tasks/celery.py
+++ app/services/tasks/celery.py
@@ -6,7 +6,7 @@ import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'services.tasks.settings')
 django.setup()
 
-app = Celery('MCMI')
+app = Celery('MCMI', task_cls='plugins.task.base:CeleryTask')
 app.set_default()
 app.config_from_object('django.conf:settings', namespace = 'CELERY')
 app.autodiscover_tasks(force = True)
