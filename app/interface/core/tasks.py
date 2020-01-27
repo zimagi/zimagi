e5ce4b85da718bdfa294fe42ac766af6aa1b7dfd
Adrian Webb <adrian@webb.sh>
2020-01-27 01:38:20 -0500

    Adding capturing output and return values for core celery tasks.

--- app/interface/core/tasks.py
+++ app/interface/core/tasks.py
@@ -1,17 +1,30 @@
 from celery import shared_task
 
+import sys
+import io
+
 
 @shared_task(bind=True, name='mcmi.task.exec')
-def task_exec(self, module_name, task_name, options = None, verbosity = 2):
-    if not options:
-        options = {}
+def task_exec(self, module_name, task_name, task_fields = None, verbosity = 2):
+    if not task_fields:
+        task_fields = {}
 
-    self.exec_command('task', {
+    task_options = {
         'module_name': module_name,
         'task_name': task_name,
-        'task_fields': options,
+        'task_fields': task_fields,
         'verbosity': verbosity
-    })
+    }
+
+    old_stdout = sys.stdout
+    sys.stdout = mystdout = io.StringIO()
+
+    self.exec_command('task', task_options)
+
+    sys.stdout = old_stdout
+
+    task_options['messages'] = mystdout.getvalue()
+    return task_options
 
 
 @shared_task(bind=True, name='mcmi.command.exec')
@@ -19,4 +32,15 @@ def command_exec(self, command, **options):
     options.pop('schedule', None)
     options.pop('schedule_begin', None)
     options.pop('schedule_end', None)
+
+    old_stdout = sys.stdout
+    sys.stdout = mystdout = io.StringIO()
+
     self.exec_command(command, options)
+
+    sys.stdout = old_stdout
+    return {
+        'command': command,
+        'options': options,
+        'messages': mystdout.getvalue()
+    }
