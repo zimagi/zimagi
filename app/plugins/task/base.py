0e9db6472166dd3d6f7aecd0d2d34d55772cf4b4
Adrian Webb <adrian@webb.sh>
2020-01-28 07:16:26 -0500

    Adding cleaning tasks and some updates to the after_return method in the base CeleryTask class in the base plugin library.

--- app/plugins/task/base.py
+++ app/plugins/task/base.py
@@ -3,6 +3,7 @@ from django.core.mail import send_mail
 from celery import Task
 from celery.exceptions import TaskError
 from celery.utils.log import get_task_logger
+from django_celery_beat.models import IntervalSchedule, CrontabSchedule, ClockedSchedule, PeriodicTask
 
 from settings.roles import Roles
 from systems.command.types.action import ActionCommand
@@ -50,18 +51,19 @@ class CeleryTask(Task):
 
 
     def after_return(self, status, retval, task_id, args, kwargs, einfo):
+        notifications = {}
+
         subject = "Status {} detected for task: {}".format(status, task_id)
         message = "Arguments: {}\nKeywords: {}\n\nMessages: {}".format(
             " ".join(args),
             json.dumps(kwargs, indent = 2),
-            retval['messages']
+            retval['messages'] if retval and 'messages' in retval else ''
         )
 
-        logger.debug("Active user: {}".format(self.command.active_user))
+        if self.command.active_user:
+            logger.debug("Active user: {}".format(self.command.active_user))
+            notifications[self.command.active_user.name] = self.command.active_user.email
 
-        notifications = {
-            self.command.active_user.name: self.command.active_user.email
-        }
         if 'notify' in kwargs and kwargs['notify']:
             for group in ensure_list(kwargs['notify']):
                 for user in self.command._user.filter(groups__name = group):
@@ -75,8 +77,71 @@ class CeleryTask(Task):
             logger.debug("\nTask: {}\n{}".format(task_id, message))
             logger.error("{}: {}".format(subject, str(einfo)))
 
-            if settings.EMAIL_HOST and settings.EMAIL_HOST_USER:
+            if settings.EMAIL_HOST and settings.EMAIL_HOST_USER and notifications:
                 send_mail(subject, message, settings.EMAIL_HOST_USER, list(notifications.values()))
+
+
+    def clean_interval_schedule(self):
+        old_stdout = sys.stdout
+        sys.stdout = mystdout = io.StringIO()
+
+        try:
+            interval_ids = list(PeriodicTask.objects.filter(interval_id__isnull=False).distinct().values_list('interval_id', flat=True))
+            logger.debug("Interval IDs: {}".format(interval_ids))
+
+            for record in IntervalSchedule.objects.exclude(id__in = interval_ids):
+                record.delete()
+                logger.info("Deleted unused interval schedule: {}".format(record.id))
+
+        except Exception as e:
+            raise TaskError(mystdout.getvalue())
+
+        finally:
+            sys.stdout = old_stdout
+
+        return mystdout.getvalue()
+
+
+    def clean_crontab_schedule(self):
+        old_stdout = sys.stdout
+        sys.stdout = mystdout = io.StringIO()
+
+        try:
+            crontab_ids = list(PeriodicTask.objects.filter(crontab_id__isnull=False).distinct().values_list('crontab_id', flat=True))
+            logger.debug("Crontab IDs: {}".format(crontab_ids))
+
+            for record in CrontabSchedule.objects.exclude(id__in = crontab_ids):
+                record.delete()
+                logger.info("Deleted unused crontab schedule: {}".format(record.id))
+
+        except Exception as e:
+            raise TaskError(mystdout.getvalue())
+
+        finally:
+            sys.stdout = old_stdout
+
+        return mystdout.getvalue()
+
+
+    def clean_datetime_schedule(self):
+        old_stdout = sys.stdout
+        sys.stdout = mystdout = io.StringIO()
+
+        try:
+            datetime_ids = list(PeriodicTask.objects.filter(clocked_id__isnull=False).distinct().values_list('clocked_id', flat=True))
+            logger.debug("Datetime IDs: {}".format(datetime_ids))
+
+            for record in ClockedSchedule.objects.exclude(id__in = datetime_ids):
+                record.delete()
+                logger.info("Deleted unused datetime schedule: {}".format(record.id))
+
+        except Exception as e:
+            raise TaskError(mystdout.getvalue())
+
+        finally:
+            sys.stdout = old_stdout
+
+        return mystdout.getvalue()
 
 
 class TaskResult(object):
