210a2ac8e23db6fd85cf7595ad0183f86fbad736
Adrian Webb <adrian@webb.sh>
2020-01-28 07:16:57 -0500

    Adding schedule cleanup Celery tasks in the core interface tasks.

--- app/interface/core/tasks.py
+++ app/interface/core/tasks.py
@@ -12,3 +12,16 @@ def command_exec(self, command, **options):
         'options': options,
         'messages': self.exec_command(command, options)
     }
+
+
+@shared_task(bind=True, name='mcmi.schedule.clean_interval')
+def clean_interval_schedule(self):
+    return { 'messages': self.clean_interval_schedule() }
+
+@shared_task(bind=True, name='mcmi.schedule.clean_crontab')
+def clean_crontab_schedule(self):
+    return { 'messages': self.clean_crontab_schedule() }
+
+@shared_task(bind=True, name='mcmi.schedule.clean_datetime')
+def clean_datetime_schedule(self):
+    return { 'messages': self.clean_datetime_schedule() }
