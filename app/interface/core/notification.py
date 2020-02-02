9e73d6e0580028678996f1ecfaa16d6d26d59118
Adrian Webb <adrian@webb.sh>
2020-02-02 05:10:47 -0500

    Including the command_set function in the notification command interface.

--- app/interface/core/notification.py
+++ app/interface/core/notification.py
@@ -1,11 +1,14 @@
 from systems.command.factory import resource
+from systems.command.base import command_set
 from systems.command.types import notification
 
 
 class Command(notification.NotificationRouterCommand):
 
     def get_subcommands(self):
-        return resource.ResourceCommandSet(
-            notification.NotificationActionCommand, self.name,
-            allow_update = False
+        return command_set(
+            resource.ResourceCommandSet(
+                notification.NotificationActionCommand, self.name,
+                allow_update = False
+            )
         )
