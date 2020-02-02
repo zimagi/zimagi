0555ac106f36c0b998b94f546f6adb98252f3f1d
Adrian Webb <adrian@webb.sh>
2020-02-02 07:56:10 -0500

    Adding initial notification save and rm command implementations.

--- app/interface/core/notification.py
+++ app/interface/core/notification.py
@@ -3,12 +3,68 @@ from systems.command.base import command_set
 from systems.command.types import notification
 
 
-class Command(notification.NotificationRouterCommand):
+class SaveCommand(
+    notification.NotificationActionCommand
+):
+    def parse(self):
+        self.parse_group_provider_name('--group-provider')
+        self.parse_notify_failure()
+        self.parse_notify_command(True)
+        self.parse_notify_groups(True)
 
+    def exec(self):
+        command = self.notify_command
+        instance, created = self._notification.store(command)
+
+        for group in self.notify_groups:
+            if self.notify_failure:
+                instance.failure_groups.get_or_create(group = group)
+                self.success("Group {} subscribed to {} failure notifications".format(
+                    group.name, command
+                ))
+            else:
+                instance.groups.get_or_create(group = group)
+                self.success("Group {} subscribed to {} notifications".format(
+                    group.name, command
+                ))
+
+class RemoveCommand(
+    notification.NotificationActionCommand
+):
+    def parse(self):
+        self.parse_group_provider_name('--group-provider')
+        self.parse_notify_failure()
+        self.parse_notify_command(True)
+        self.parse_notify_groups(True)
+
+    def exec(self):
+        command = self.notify_command
+        instance, created = self._notification.store(command)
+
+        for group in self.notify_groups:
+            if self.notify_failure:
+                instance.failure_groups.filter(group = group).delete()
+                self.success("Group {} unsubscribed from {} failure notifications".format(
+                    group.name, command
+                ))
+            else:
+                instance.groups.filter(group = group).delete()
+                self.success("Group {} unsubscribed from {} notifications".format(
+                    group.name, command
+                ))
+
+
+class Command(
+    notification.NotificationRouterCommand
+):
     def get_subcommands(self):
         return command_set(
             resource.ResourceCommandSet(
                 notification.NotificationActionCommand, self.name,
-                allow_update = False
-            )
+                allow_access = False,
+                allow_update = False,
+                allow_remove = False
+            ),
+            ('save', SaveCommand),
+            ('rm', RemoveCommand)
         )
