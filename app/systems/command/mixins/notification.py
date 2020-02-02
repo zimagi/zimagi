4c54232294c049ab6c2cc64f8e3eb5dd4761d36b
Adrian Webb <adrian@webb.sh>
2020-02-02 10:29:42 -0500

    Updating the message formatting and recipient generation in the notification command mixin.

--- app/systems/command/mixins/notification.py
+++ app/systems/command/mixins/notification.py
@@ -2,6 +2,7 @@ from django.conf import settings
 
 from .base import DataMixin
 from data.notification.models import Notification
+from utility.data import ensure_list
 
 import json
 import re
@@ -79,27 +80,61 @@ class NotificationMixin(DataMixin):
         return self.options.get('command_notify_failure', [])
 
 
-    @property
-    def recipients(self):
-        recipients = {}
+    def load_notification_users(self, success):
+        self.notification_users = {}
 
-        if self.active_user:
-            recipients[self.active_user.name] = self.active_user.email
-
-        if groups := self.notify():
+        def load_groups(groups):
             for group in ensure_list(groups):
                 for user in self._user.filter(groups__name = group):
-                    recipients[user.name] = user.email
+                    if user.email:
+                        self.notification_users[user.name] = user.email
 
-        return recipients
+        if self.active_user and self.active_user.email:
+            self.notification_users[self.active_user.name] = self.active_user.email
+
+        command = re.sub(r'\s+', ':', self.get_full_name())
+        notification = self._notification.retrieve(command)
+        notification_groups = list(notification.groups.values_list(
+            'group__name', flat = True
+        ))
+
+        if notification_groups:
+            load_groups(notification_groups)
+
+        if groups := self.command_notify:
+            load_groups(groups)
+
+        if not success:
+            notification_failure_groups = list(notification.failure_groups.values_list(
+                'group__name', flat = True
+            ))
+            if notification_failure_groups:
+                load_groups(notification_failure_groups)
+
+            if groups := self.command_notify_failure:
+                load_groups(groups)
+
+        return list(self.notification_users.values())
 
 
-    def format_subject(self, command, status):
-        return  "{}: {}".format(status, command)
+    def format_notification_subject(self, success):
+        status = 'SUCCESS' if success else 'FAILED'
+        return  "{}: {}".format(status, self.get_full_name())
 
-    def format_body(self, command, options, messages = None):
-        return "Command: {}\nOptions: {}\n\nMessages: {}".format(
-            command,
-            json.dumps(options, indent = 2),
-            messages if messages else ''
+    def format_notification_body(self):
+        option_lines = []
+        for key, val in self.options.export().items():
+            option_lines.append("> {}: {}".format(key, val))
+
+        return "Command: {}\n\nOptions:\n\n{}\n\nMessages:\n\n{}\n\nMore Information:\n\n{}".format(
+            self.get_full_name(),
+            "\n".join(option_lines),
+            "\n".join(self.notification_messages),
+            "mcmi log get {}".format(self.log_entry.id)
         )
+
+
+    def send_notifications(self, success):
+        recipients = self.load_notification_users(success)
+        subject = self.format_notification_subject(success)
+        body = self.format_notification_body()
