8cd59b74f8459b323f599c38d3dcb5595cb14490
Adrian Webb <adrian@webb.sh>
2020-02-02 11:01:51 -0500

    Adding a celery task definition in the core interface tasks library.

--- app/interface/core/tasks.py
+++ app/interface/core/tasks.py
@@ -25,3 +25,13 @@ def clean_crontab_schedule(self):
 @shared_task(bind=True, name='mcmi.schedule.clean_datetime')
 def clean_datetime_schedule(self):
     return { 'messages': self.clean_datetime_schedule() }
+
+
+@shared_task(bind=True,
+    name='mcmi.notification.send',
+    retry_backoff = True,
+    retry_backoff_max = 600,
+    retry_jitter = True
+)
+def send_notification(self, recipient, subject, body):
+    return { 'messages': self.send_notification(recipient, subject, body) }
