dd7916c3a075987b184d93cd0f25a0dc6b9a9ad6
Adrian Webb <adrian@webb.sh>
2020-02-02 01:52:15 -0500

    Pulling the user name from the command options again in the CeleryTask class in the base plugin library.

--- app/plugins/task/base.py
+++ app/plugins/task/base.py
@@ -39,11 +39,7 @@ class CeleryTask(Task):
         sys.stdout = mystdout = io.StringIO()
 
         try:
-            user_name = self.request.user
-            if not user_name:
-                user_name = settings.ADMIN_USER
-
-            user = self.command._user.retrieve(user_name)
+            user = self.command._user.retrieve(options.pop('_user', settings.ADMIN_USER))
             self.command._user.set_active_user(user)
 
             self.command.exec_local(name, options,
