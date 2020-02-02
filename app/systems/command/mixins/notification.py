5b7374276d66ffb586754614c235c2c02a64fde2
Adrian Webb <adrian@webb.sh>
2020-02-02 07:54:19 -0500

    Adding and renaming parsers and properties in the notification command mixin.

--- app/systems/command/mixins/notification.py
+++ app/systems/command/mixins/notification.py
@@ -4,6 +4,7 @@ from .base import DataMixin
 from data.notification.models import Notification
 
 import json
+import re
 
 
 class NotificationMixin(DataMixin):
@@ -19,24 +20,63 @@ class NotificationMixin(DataMixin):
         self.facade_index['01_notification'] = self._notification
 
 
-    def parse_notify(self, optional = '--notify', help_text = 'user group names to notify of command results'):
-        self.parse_variables('notify', optional, str, help_text,
-            value_label = 'GROUP_NAME'
-        )
-
-    @property
-    def notify(self):
-        return self.options.get('notify', [])
-
-
-    def parse_notify_failure(self, optional = '--notify-fail', help_text = 'user group names to notify of command failures'):
-        self.parse_variables('notify_failure', optional, str, help_text,
-            value_label = 'GROUP_NAME'
-        )
+    def parse_notify_failure(self):
+        self.parse_flag('notify_failure', '--failure', 'only notify groups on command failure')
 
     @property
     def notify_failure(self):
-        return self.options.get('notify_failure', [])
+        return self.options.get('notify_failure', False)
+
+    def parse_notify_command(self, optional = False, help_text = 'notification command with colons replacing spaces (ex: user:save)'):
+        self.parse_variable('notify_command', optional, str, help_text,
+            value_label = 'CMD[:SUBCMD[:...]]'
+        )
+
+    @property
+    def notify_command(self):
+        command = self.options.get('notify_command', None)
+        if command:
+            command = re.sub(r'\s+', ':', command)
+        return command
+
+    def parse_notify_groups(self, optional = '--groups', help_text = 'user group names to notify of command results'):
+        self.parse_variables('notify_groups', optional, str, help_text,
+            value_label = 'GROUP_NAME'
+        )
+
+    @property
+    def notify_groups(self):
+        groups = []
+
+        if group_names := self.options.get('notify_groups', None):
+            for name in group_names:
+                group = self._group.retrieve(name)
+                if not group:
+                    group = self.group_provider.store(name, {})
+                groups.append(group)
+
+        return groups
+
+
+
+    def parse_command_notify(self, optional = '--notify', help_text = 'user group names to notify of command results'):
+        self.parse_variables('command_notify', optional, str, help_text,
+            value_label = 'GROUP_NAME'
+        )
+
+    @property
+    def command_notify(self):
+        return self.options.get('command_notify', [])
+
+
+    def parse_command_notify_failure(self, optional = '--notify-fail', help_text = 'user group names to notify of command failures'):
+        self.parse_variables('command_notify_failure', optional, str, help_text,
+            value_label = 'GROUP_NAME'
+        )
+
+    @property
+    def command_notify_failure(self):
+        return self.options.get('command_notify_failure', [])
 
 
     @property
