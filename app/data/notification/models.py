ca3cff8a4da9c5f204aab9a0b280ad00e3f1e102
Adrian Webb <adrian@webb.sh>
2020-02-02 05:05:32 -0500

    Removing the notification success group model.

--- app/data/notification/models.py
+++ app/data/notification/models.py
@@ -23,35 +23,17 @@ class Notification(
         verbose_name = "notification"
         verbose_name_plural = "notifications"
         facade_class = NotificationFacade
-        dynamic_fields = ['all_groups', 'success_groups', 'failure_groups']
+        dynamic_fields = ['groups', 'failure_groups']
 
 
-class NotificationAllGroup(resource.ResourceModel):
+class NotificationGroup(resource.ResourceModel):
     name = None
-    notification = django.ForeignKey(Notification, related_name='all_groups', on_delete=django.CASCADE)
+    notification = django.ForeignKey(Notification, related_name='groups', on_delete=django.CASCADE)
     group = django.ForeignKey(Group, null = False, on_delete = django.PROTECT, related_name = '+')
 
     class Meta:
         verbose_name = "notification group"
         verbose_name_plural = "notification groups"
-        facade_class = NotificationGroupFacade
-        ordering = ('id',)
-
-    def __str__(self):
-        return "{} ({})".format(self.notification.name, self.group.name)
-
-    def get_id_fields(self):
-        return ('notification_id', 'group_id')
-
-
-class NotificationSuccessGroup(resource.ResourceModel):
-    name = None
-    notification = django.ForeignKey(Notification, related_name='success_groups', on_delete=django.CASCADE)
-    group = django.ForeignKey(Group, null = False, on_delete = django.PROTECT, related_name = '+')
-
-    class Meta:
-        verbose_name = "notification success group"
-        verbose_name_plural = "notification success groups"
         facade_class = NotificationGroupFacade
         ordering = ('id',)
 
