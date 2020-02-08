8ae41d6d8bc5a0ad17ca0beab829171f4d033caa
Adrian Webb <adrian@webb.sh>
2020-02-08 12:32:54 -0500

    Adding logging on authentication failure in the API authentication library.

--- app/systems/api/auth.py
+++ app/systems/api/auth.py
@@ -9,15 +9,20 @@ from rest_framework import permissions, authentication, exceptions
 from data.user.models import User
 from utility.encryption import Cipher
 
+import logging
+
+
+logger = logging.getLogger(__name__)
+
 
 class CommandPermission(permissions.BasePermission):
-    
+
     def has_permission(self, request, view):
         auth_method = getattr(view, 'groups_allowed', None)
 
         if auth_method and callable(auth_method):
             groups = view.groups_allowed()
-            
+
             if groups is False:
                 return True
 
@@ -27,7 +32,7 @@ class CommandPermission(permissions.BasePermission):
 
 
 class EncryptedAPITokenAuthentication(authentication.TokenAuthentication):
-    
+
     def authenticate(self, request):
         header = authentication.get_authorization_header(request)
 
@@ -36,6 +41,7 @@ class EncryptedAPITokenAuthentication(authentication.TokenAuthentication):
                 auth = Cipher.get('token').decrypt(header).split()
             except Exception as e:
                 msg = 'Invalid token header. Credentials can not be decrypted.'
+                logger.warning(msg)
                 raise exceptions.AuthenticationFailed(msg)
 
             if not auth or auth[0].lower() != self.keyword.lower():
@@ -43,16 +49,18 @@ class EncryptedAPITokenAuthentication(authentication.TokenAuthentication):
 
             if len(auth) == 1:
                 msg = 'Invalid token header. No credentials provided.'
+                logger.warning(msg)
                 raise exceptions.AuthenticationFailed(msg)
             elif len(auth) > 2:
                 msg = 'Invalid token header. Token string should not contain spaces.'
+                logger.warning(msg)
                 raise exceptions.AuthenticationFailed(msg)
 
             (user, token) = self.authenticate_credentials(auth[1])
         else:
             user = User.facade.retrieve(settings.ADMIN_USER)
-            token = None    
-        
+            token = None
+
         User.facade.set_active_user(user)
         return (user, token)
 
@@ -71,7 +79,7 @@ class EncryptedAPITokenAuthentication(authentication.TokenAuthentication):
             raise exceptions.AuthenticationFailed('User account is inactive. Contact administrator')
 
         if not user.check_password(token):
-            raise exceptions.AuthenticationFailed('Invalid token: User credentials are invalid')        
+            raise exceptions.AuthenticationFailed('Invalid token: User credentials are invalid')
 
         user.last_login = now()
         user.save()
@@ -84,11 +92,11 @@ class EncryptedClientTokenAuthentication(auth.TokenAuthentication):
     def __init__(self, user, token, scheme = None, domain = None):
         super().__init__(token, scheme, domain)
         self.user = user
-    
+
     def __call__(self, request):
         if not domain_matches(request, self.domain):
             return request
-        
+
         token = "{} {}++{}".format(self.scheme, self.user, self.token)
         request.headers['Authorization'] = Cipher.get('token').encrypt(token)
         return request
