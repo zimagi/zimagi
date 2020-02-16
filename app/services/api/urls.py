c9b4bc093a8c20ea70b1f6192bdfcbb4fecabe98
Adrian Webb <adrian@webb.sh>
2020-02-16 05:51:56 -0500

    Wrapping the API schema view in a cache_page invocation in the API service urls definition.

--- app/services/api/urls.py
+++ app/services/api/urls.py
@@ -1,5 +1,6 @@
 
 from django.conf.urls import include, url
+from django.views.decorators.cache import cache_page
 
 from rest_framework import renderers
 from rest_framework.schemas import get_schema_view
@@ -11,9 +12,9 @@ from systems.api.schema import generator
 urlpatterns = [
     url(r'^', include(routers.CommandAPIRouter().urls)),
     url(r'^status/?$', views.Status.as_view()),
-    url('^$', get_schema_view(
-        title = "MCMI API",
+    url('^$', cache_page(None, cache = 'api')(get_schema_view(
+        title = 'MCMI API',
         generator_class = generator.CommandSchemaGenerator,
         renderer_classes = [ renderers.CoreJSONRenderer ]
-    ))
+    )))
 ]
