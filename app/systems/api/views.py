fc38427ccf1457b0ed3d4b8bfc32fd02fbc422d4
Adrian Webb <adrian@webb.sh>
2020-05-03 11:05:12 -0400

    Adding an initial base model viewset in the API views library.

--- app/systems/api/views.py
+++ app/systems/api/views.py
@@ -1,3 +1,7 @@
+from collections import OrderedDict
+from datetime import datetime, date
+from urllib.parse import quote
+
 from django.conf import settings
 from django.core.management import call_command
 from django.http import StreamingHttpResponse
@@ -5,8 +9,13 @@ from django.http import StreamingHttpResponse
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.views import APIView
+from rest_framework.viewsets import ModelViewSet
 
+from rest_framework_filters.backends import RestFrameworkFilterBackend
+
+from systems.api import mixins, filters, pagination
 from systems.api.auth import CommandPermission
 from utility.encryption import Cipher
 
@@ -86,3 +95,172 @@ class Command(APIView):
         command.bootstrap(options)
         command.parse_base()
         return command
+
+
+class BaseModelViewSet(
+    mixins.FilterViewSetMixin,
+    mixins.PaginationViewSetMixin,
+    mixins.SerializerViewSetMixin,
+    ModelViewSet
+):
+    lookup_value_regex = '[^/]+'
+    action_filters = {
+        'list': (
+            filters.BaseComplexFilterBackend,
+            RestFrameworkFilterBackend,
+            SearchFilter,
+            OrderingFilter
+        ),
+        'values': 'list',
+        'count': 'list',
+        'meta': 'list',
+        'export_json': 'list',
+        'export_csv': 'list'
+    }
+    pagination_class = pagination.ResultSetPagination
+
+
+    def list(self, request, *args, **kwargs):
+        try:
+            return super(BaseModelViewSet, self).list(request, *args, **kwargs)
+        except AssertionError:
+            return Response({'count': 0, 'previous': None, 'next': None, 'results': []})
+
+    def full_list(self):
+        queryset = self.filter_queryset(self.get_queryset())
+        return self.get_serializer(queryset, many = True).data
+
+    def list_ids(self):
+        ids = []
+        for id in self.filter_queryset(self.get_queryset()).values_list(self.lookup_field, flat = True):
+            ids.append(id)
+
+        return ids
+
+
+    def retrieve(self, request, *args, **kwargs):
+        return super().retrieve(request, *args, **kwargs)
+
+    def values(self, request, *args, **kwargs):
+        field_lookup = kwargs['field_lookup']
+        queryset = self.filter_queryset(self.get_queryset().order_by(field_lookup))
+        values = []
+
+        for value in queryset.values_list(field_lookup, flat = True):
+            if value is not None:
+                if isinstance(value, (datetime, date)):
+                    value = value.isoformat()
+                    if value.endswith('+00:00'):
+                        value = value[:-6] + 'Z'
+
+                values.append(value)
+
+        return Response(OrderedDict([
+            ('count', len(values)),
+            ('results', values)
+        ]))
+
+    def count(self, request, *args, **kwargs):
+        field_lookup = kwargs['field_lookup']
+        queryset = self.filter_queryset(self.get_queryset().order_by(field_lookup))
+        return Response({'count': queryset.values_list(field_lookup, flat = True).count()})
+
+
+    def export(self, request, group_by, viewsets):
+        ids = self.list_ids()
+        data_series = []
+
+        if len(ids):
+            self._request_filters(request, ids)
+
+            for viewset_cls in viewsets:
+                data_series.append(self._view_data(viewset_cls, request))
+
+            return self._combine_data(data_series, group_by)
+        else:
+            raise AssertionError('Not found')
+
+
+    def _request_filters(self, request, ids):
+        request = request._request
+        filters = ['({}__{}__in={})'.format(self.base_entity, self.lookup_field, ",".join(ids))]
+
+        if not request.GET._mutable:
+            request.GET._mutable = True
+
+        request.GET['page'] = 0
+
+        for name, value in request.GET.items():
+            filters.append('({}={})'.format(name, value))
+
+        request.GET['filters'] = quote("&".join(filters))
+
+
+    def _view_data(self, viewset_cls, request, op = 'list'):
+        return self._normalize_data(getattr(viewset_cls, 'as_view')({'get': op})(request._request))
+
+
+    def _normalize_data(self, response):
+        results = {}
+
+        for record in response.data['results']:
+            url = record.pop('api_url')
+            date = record.pop('date')
+
+            id = record.pop('id')
+            related_id = record.pop(self.base_entity)[self.lookup_field]
+
+            if related_id not in results:
+                results[related_id] = {}
+
+            results[related_id][date] = {}
+            for field, value in record.items():
+                results[related_id][date][field] = value
+
+        return results
+
+    def _combine_data(self, data_sets, group_by):
+        results = OrderedDict()
+
+        instruments = []
+        dates = []
+
+        # Find all shared instruments
+        for index, data in enumerate(data_sets):
+            set_instruments = data.keys()
+            instruments = set_instruments if index == 0 else set(instruments).intersection(set_instruments)
+
+        instruments = sorted(instruments)
+
+        # Find all shared dates
+        for index, data in enumerate(data_sets):
+            for instrument in instruments:
+                set_dates = data[instrument].keys()
+                dates = set_dates if index == 0 else set(dates).intersection(set_dates)
+
+        dates = sorted(dates)
+
+        # Parse data
+        for index, data in enumerate(data_sets):
+            for instrument in instruments:
+                if group_by == 'instrument':
+                    if instrument not in results:
+                        results[instrument] = OrderedDict()
+
+                    for date in dates:
+                        if date not in results[instrument]:
+                            results[instrument][date] = {}
+
+                        for field, value in data[instrument][date].items():
+                            results[instrument][date][field] = value
+                else:
+                    for date in dates:
+                        if date not in results:
+                            results[date] = OrderedDict()
+
+                        if instrument not in results[date]:
+                            results[date][instrument] = {}
+
+                        for field, value in data[instrument][date].items():
+                            results[date][instrument][field] = value
+        return results
