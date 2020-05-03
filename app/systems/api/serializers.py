133a25ec43c0302a34ae0b69a6f15e1465f00ac8
Adrian Webb <adrian@webb.sh>
2020-05-03 17:06:35 -0400

    Restructuring the serializer factory functions in the API serializers library.

--- app/systems/api/serializers.py
+++ app/systems/api/serializers.py
@@ -12,21 +12,59 @@ class BaseUpdateSerializer(BaseSerializer):
     pass
 
 
+def get_field_map(facade):
+    field_map = {
+        'Meta': type('Meta', {
+            'model': facade.model,
+            'fields': []
+        })
+    }
+    # Initialize relation serializers
+
+    return field_map
+
+
 def MetaSerializer(facade):
-    return type('MetaSerializer', BaseSerializer, {})
+    field_map = get_field_map(facade)
+    field_map['Meta'].fields = [] # Meta fields (identity and dates)
+    return type('MetaSerializer', BaseSerializer, field_map)
 
 def TestSerializer(facade):
-    return type('TestSerializer', BaseSerializer, {})
+    field_map = get_field_map(facade)
+    field_map['Meta'].fields = [] # Base + Relations + URL
+    return type('TestSerializer', BaseSerializer, field_map)
 
 
 def SummarySerializer(facade):
-    return type('SummarySerializer', BaseSerializer, {})
+    field_map = get_field_map(facade)
+    field_map['Meta'].fields = [] # Base + URL
+    return type('SummarySerializer', BaseSerializer, field_map)
 
 def DetailSerializer(facade):
-    return type('DetailSerializer', BaseSerializer, {})
+    field_map = get_field_map(facade)
+    field_map['Meta'].fields = [] # Base + Relations
+    return type('DetailSerializer', BaseSerializer, field_map)
+
+
+def get_update_field_map(facade):
+    field_map = {
+        'Meta': type('Meta', {
+            'model': facade.model,
+            'fields': []
+        })
+    }
+    # Initialize relation ID serializers (char field)
+
+    return field_map
+
 
 def CreateSerializer(facade):
-    return type('CreateSerializer', BaseUpdateSerializer, {})
+    field_map = get_update_field_map(facade)
+    field_map['Meta'].fields = [] # Base + Relations
+    return type('CreateSerializer', BaseUpdateSerializer, field_map)
 
 def UpdateSerializer(facade):
-    return type('UpdateSerializer', BaseUpdateSerializer, {})
+    field_map = get_update_field_map(facade)
+    field_map['Meta'].fields = [] # Base + Relations
+    field_map['Meta'].read_only_fields = [ facade.pk ]
+    return type('UpdateSerializer', BaseUpdateSerializer, field_map)
