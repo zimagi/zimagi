16274d9a148d11b0c4f64125dbe17338ea1e034f
Adrian Webb <adrian@webb.sh>
2020-05-03 15:56:03 -0400

    Adding scaffolding serializer factory functions in the API serializers library.

--- app/systems/api/serializers.py
+++ app/systems/api/serializers.py
@@ -8,6 +8,25 @@ import re
 class BaseSerializer(HyperlinkedModelSerializer):
     pass
 
-
 class BaseUpdateSerializer(BaseSerializer):
     pass
+
+
+def MetaSerializer(facade):
+    return type('MetaSerializer', BaseSerializer, {})
+
+def TestSerializer(facade):
+    return type('TestSerializer', BaseSerializer, {})
+
+
+def SummarySerializer(facade):
+    return type('SummarySerializer', BaseSerializer, {})
+
+def DetailSerializer(facade):
+    return type('DetailSerializer', BaseSerializer, {})
+
+def CreateSerializer(facade):
+    return type('CreateSerializer', BaseUpdateSerializer, {})
+
+def UpdateSerializer(facade):
+    return type('UpdateSerializer', BaseUpdateSerializer, {})
