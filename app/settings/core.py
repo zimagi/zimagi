02531d31698a1b4d4771fdaca5236b55e684748b
Adrian Webb <adrian@webb.sh>
2020-05-03 09:13:28 -0400

    Removing the command permission from the REST configuration default permission classes in the core settings.

--- app/settings/core.py
+++ app/settings/core.py
@@ -268,8 +268,7 @@ REST_FRAMEWORK = {
         'systems.api.auth.EncryptedAPITokenAuthentication'
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-        'systems.api.auth.CommandPermission'
+        'rest_framework.permissions.IsAuthenticated'
     ],
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer'
