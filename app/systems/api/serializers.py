5907885ea1c194f0b78d29ca51131c9e6a3bbfd5
Adrian Webb <adrian@webb.sh>
2020-05-07 02:04:01 -0400

    Cleaning up and light restructuring of the dynamic field processing and the factory serializers in the API serializers library.

--- app/systems/api/serializers.py
+++ app/systems/api/serializers.py
@@ -14,23 +14,35 @@ class BaseItemSerializer(HyperlinkedModelSerializer):
     pass
 
 
-def get_field_map(facade, dynamic = True):
+def get_field_map(facade, fields = None, api_url = True, dynamic = True):
 
     def get_dynamic_field(field_name):
         def _dynamic_display(self, instance):
-            return getattr(instance, field_name, None)
+            value = getattr(instance, field_name, None)
+            if value is None or '<locals>.RelatedManager' in str(type(value)):
+                value = None
+                display_function = getattr(facade, "get_field_{}_display".format(field_name), None)
+                if display_function:
+                    value = facade.raw_text(display_function(instance, None, False))
+            return value
+
         return _dynamic_display
 
+    if fields is None:
+        fields = list(set(facade.atomic_fields) - set(facade.dynamic_fields))
+
     field_map = {
-        'api_url': HyperlinkedIdentityField(
-            view_name = "{}-detail".format(facade.name),
-            lookup_field = facade.pk
-        ),
         'Meta': type('Meta', (object,), {
             'model': facade.model,
-            'fields': list(set(facade.atomic_fields) - set(facade.dynamic_fields))
+            'fields': fields
         })
     }
+    if api_url or 'api_url' in fields:
+        field_map['api_url'] = HyperlinkedIdentityField(
+            view_name = "{}-detail".format(facade.name),
+            lookup_field = facade.pk
+        )
+        field_map['Meta'].fields.append('api_url')
     if dynamic:
         for field_name in facade.dynamic_fields:
             field_map[field_name] = SerializerMethodField()
@@ -39,10 +51,13 @@ def get_field_map(facade, dynamic = True):
 
     return field_map
 
-def get_related_field_map(facade, dynamic = True):
-    relations = facade.get_referenced_relations()
-    field_map = get_field_map(facade, dynamic)
-
+def get_related_field_map(facade, fields = None, api_url = True, dynamic = True):
+    relations = facade.get_all_relations()
+    field_map = get_field_map(facade,
+        fields = fields,
+        api_url = api_url,
+        dynamic = dynamic
+    )
     for field_name, info in relations.items():
         if getattr(info['model'], 'facade', None):
             field_map[field_name] = LinkSerializer(info['model'].facade)(many = info['multiple'])
@@ -57,14 +72,12 @@ def LinkSerializer(facade):
     if class_name in globals():
         return globals()[class_name]
 
-    field_map = get_field_map(facade, False)
-
-    if facade.pk != facade.key:
-        field_map['Meta'].fields = [ facade.pk, facade.key(), 'api_url' ]
-    else:
-        field_map['Meta'].fields = [ facade.pk, 'api_url' ]
-
-    serializer = type(class_name, (BaseItemSerializer,), field_map)
+    serializer = type(class_name, (BaseItemSerializer,), get_field_map(
+        facade,
+        fields = [ facade.pk, facade.key() ] if facade.pk != facade.key() else [ facade.pk ],
+        api_url = True,
+        dynamic = False
+    ))
     globals()[class_name] = serializer
     return serializer
 
@@ -74,10 +87,12 @@ def MetaSerializer(facade):
     if class_name in globals():
         return globals()[class_name]
 
-    field_map = get_field_map(facade)
-    field_map['Meta'].fields = facade.meta_fields + [ 'api_url' ]
-
-    serializer = type(class_name, (BaseItemSerializer,), field_map)
+    serializer = type(class_name, (BaseItemSerializer,), get_field_map(
+        facade,
+        fields = facade.meta_fields,
+        api_url = True,
+        dynamic = True
+    ))
     globals()[class_name] = serializer
     return serializer
 
@@ -87,10 +102,11 @@ def SummarySerializer(facade):
     if class_name in globals():
         return globals()[class_name]
 
-    field_map = get_field_map(facade)
-    field_map['Meta'].fields.append('api_url')
-
-    serializer = type(class_name, (BaseItemSerializer,), field_map)
+    serializer = type(class_name, (BaseItemSerializer,), get_field_map(
+        facade,
+        api_url = True,
+        dynamic = True
+    ))
     globals()[class_name] = serializer
     return serializer
 
@@ -100,10 +116,11 @@ def DetailSerializer(facade):
     if class_name in globals():
         return globals()[class_name]
 
-    field_map = get_related_field_map(facade)
-    field_map.pop('api_url')
-
-    serializer = type(class_name, (BaseItemSerializer,), field_map)
+    serializer = type(class_name, (BaseItemSerializer,), get_related_field_map(
+        facade,
+        api_url = False,
+        dynamic = True
+    ))
     globals()[class_name] = serializer
     return serializer
 
@@ -113,10 +130,11 @@ def TestSerializer(facade):
     if class_name in globals():
         return globals()[class_name]
 
-    field_map = get_related_field_map(facade)
-    field_map['Meta'].fields.append('api_url')
-
-    serializer = type(class_name, (BaseItemSerializer,), field_map)
+    serializer = type(class_name, (BaseItemSerializer,), get_related_field_map(
+        facade,
+        api_url = True,
+        dynamic = True
+    ))
     globals()[class_name] = serializer
     return serializer
 
