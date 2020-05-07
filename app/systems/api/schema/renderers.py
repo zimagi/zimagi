fc192e51faf943a9a9d101d45e226958eebc1f5c
Adrian Webb <adrian@webb.sh>
2020-05-06 20:09:33 -0400

    Adding a generic JSON renderer override and updating names of existing renderers to include schema in the name in the API schema renderers library.

--- app/systems/api/schema/renderers.py
+++ app/systems/api/schema/renderers.py
@@ -11,10 +11,14 @@ class SafeJSONEncoder(encoders.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, datetime.tzinfo):
             return str(obj)
-        return super(JSONEncoder, self).default(obj)
+        return super().default(obj)
 
 
-class CommandJSONRenderer(renderers.CoreJSONRenderer):
+class DataJSONRenderer(renderers.JSONRenderer):
+    encoder_class = SafeJSONEncoder
+
+
+class CommandSchemaJSONRenderer(renderers.CoreJSONRenderer):
 
     def render(self, data, media_type = None, renderer_context = None):
         indent = bool(renderer_context.get('indent', 2))
@@ -22,7 +26,7 @@ class CommandJSONRenderer(renderers.CoreJSONRenderer):
         return codec.dump(data, cls = SafeJSONEncoder, indent = indent)
 
 
-class DataJSONRenderer(renderers.JSONOpenAPIRenderer):
+class DataSchemaJSONRenderer(renderers.JSONOpenAPIRenderer):
 
     def render(self, data, media_type = None, renderer_context = None):
         indent = bool(renderer_context.get('indent', 2))
