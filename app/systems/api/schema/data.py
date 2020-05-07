ba21359cb3f4498363e1695509ac832f214cc7ba
Adrian Webb <adrian@webb.sh>
2020-05-06 20:10:40 -0400

    Fixing method names and implementations to match with openapi in the API data schema library.

--- app/systems/api/schema/data.py
+++ app/systems/api/schema/data.py
@@ -3,22 +3,22 @@ from rest_framework.schemas.openapi import AutoSchema
 
 class DataSchema(AutoSchema):
 
-    def get_filter_fields(self, path, method):
-        if not self._allows_filters(path, method):
+    def get_filter_parameters(self, path, method):
+        if not self.allows_filters(path, method):
             return []
 
-        fields = []
+        parameters = []
         id_map = {}
 
         for filter_backend in self.view.get_filter_classes():
-            for field in filter_backend().get_schema_fields(self.view):
-                if field.name not in id_map:
-                    fields.append(field)
+            for parameter in filter_backend().get_schema_fields(self.view):
+                if parameter.name not in id_map:
+                    parameters.append(parameter)
                     id_map[field.name] = True
 
-        return fields
+        return parameters
 
-    def _allows_filters(self, path, method):
+    def allows_filters(self, path, method):
         if getattr(self.view, 'filter_backends', None) is None:
             return False
 
