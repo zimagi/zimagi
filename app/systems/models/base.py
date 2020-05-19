f3052db6d207d9685142a5a71bfb4078143b830f
Adrian Webb <adrian@webb.sh>
2020-05-19 04:36:50 -0400

    Adding ModelBase __new__ override that converts non INSTALLED_APP models to abstract models for inheritance in the base model library.

--- app/systems/models/base.py
+++ app/systems/models/base.py
@@ -33,6 +33,28 @@ django.options.DEFAULT_NAMES += (
 )
 
 
+_base_model_new = ModelBase.__new__
+
+def _override_model_new(cls, name, bases, attrs, **kwargs):
+    orig_attrs = copy.copy(attrs)
+    try:
+        return _base_model_new(cls, name, bases, attrs, **kwargs)
+    except RuntimeError as e:
+        for key, value in orig_attrs.items():
+            attrs[key] = value
+
+        if 'Meta' in attrs:
+            attrs['Meta'].abstract = True
+        else:
+            attrs['Meta'] = type('Meta', (object,), {
+                'abstract': True
+            })
+        logger.info("Converting model {} to an abstract object because it is not in INSTALLED_APPS".format(name))
+        return _base_model_new(cls, name, bases, attrs, **kwargs)
+
+ModelBase.__new__ = _override_model_new
+
+
 def format_choices(*choices):
     return [ (choice, choice) for choice in choices ]
 
