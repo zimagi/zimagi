a94c16e389c3d9c3509f7483bad2fb7e7729e39d
Adrian Webb <adrian@webb.sh>
2020-05-27 12:23:06 -0400

    Readding the authenticated permission class for the REST API in the data service settings.

--- app/services/data/settings.py
+++ app/services/data/settings.py
@@ -28,7 +28,7 @@ REST_FRAMEWORK = {
         # 'systems.api.auth.EncryptedAPITokenAuthentication'
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        # 'rest_framework.permissions.IsAuthenticated'
+        'rest_framework.permissions.IsAuthenticated'
     ],
     'DEFAULT_RENDERER_CLASSES': [
         'systems.api.schema.renderers.DataJSONRenderer'
