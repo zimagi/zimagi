53f2231fdc714664bf099f6e542f837e27dd31ba
Adrian Webb <adrian@webb.sh>
2020-05-27 12:15:08 -0400

    Refactoring the system command router to work with command auto-generation.

--- app/systems/command/router.py
+++ app/systems/command/router.py
@@ -1,3 +1,4 @@
+from collections import OrderedDict
 
 from django.conf import settings
 from django.core.management.base import CommandError
@@ -7,24 +8,54 @@ from utility.text import wrap
 
 import sys
 import re
+import inspect
 
 
 class RouterCommand(base.BaseCommand):
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.init_subcommands()
+    def __init__(self, name, parent = None, priority = 1):
+        super().__init__(name, parent)
 
-    def init_subcommands(self):
-        self.subcommands = {}
+        self._priority = priority
+        self._subcommands = OrderedDict()
+        self.is_empty = True
 
-        for info in self.get_subcommands():
-            name, cls = list(info)
-            self.subcommands[name] = cls(name, self)
+
+    def get_priority(self):
+        return self._priority
+
 
     def get_subcommands(self):
-        # Populate in subclass
-        return []
+        command_index = {}
+        subcommands = []
+
+        for name, subcommand in self._subcommands.items():
+            priority = subcommand.get_priority()
+            command_index.setdefault(priority, [])
+            command_index[priority].append(subcommand)
+
+        for priority in sorted(command_index.keys()):
+            subcommands.extend(command_index[priority])
+
+        return subcommands
+
+    def exists(self, name):
+        return name in self._subcommands
+
+
+    def __getitem__(self, name):
+        return self._subcommands[name]
+
+    def get(self, name, default = None):
+        return self._subcommands.get(name, default)
+
+    def __setitem__(self, name, command):
+        self.is_empty = False
+
+        if inspect.isclass(command):
+            self._subcommands[name] = command(name, self)
+        else:
+            self._subcommands[name] = command
 
 
     def add_arguments(self, parser):
@@ -34,17 +65,24 @@ class RouterCommand(base.BaseCommand):
             "{} {}:".format(self.command_color(self.get_full_name()), self.notice_color('command to execute')),
             ""
         ]
-        for info in self.get_subcommands():
-            subcommand = info[0]
+        for subcommand in self.get_subcommands():
             subcommand_help.extend(wrap(
-                self.subcommands[subcommand].get_description(True),
+                subcommand.get_description(True),
                 settings.DISPLAY_WIDTH - 25,
-                init_indent = "{:2}{}  -  ".format(' ', self.command_color(subcommand)),
+                init_indent = "{:2}{}  -  ".format(' ', self.command_color(subcommand.name)),
                 init_style  = self.header_color(),
                 indent      = "".ljust(2)
             ))
-        parser.add_argument('subcommand', nargs=1, type=str, help="\n".join(subcommand_help))
+        parser.add_argument('subcommand',
+            nargs = 1,
+            type = str,
+            help = "\n".join(subcommand_help)
+        )
 
 
     def handle(self, options):
         self.print_help()
+
+
+    def __str__(self):
+        return "Router <{}>".format(self.name)
