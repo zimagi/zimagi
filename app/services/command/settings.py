5b9169552e17828fc29242963f6f2dee4670ea1d
Adrian Webb <adrian@webb.sh>
2020-06-02 16:59:15 -0400

    Removing the isauthenticated default permission class from the command service settings.

--- app/services/command/settings.py
+++ app/services/command/settings.py
@@ -28,7 +28,7 @@ REST_FRAMEWORK = {
         'systems.api.auth.CommandAPITokenAuthentication'
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated'
+        'systems.api.auth.CommandPermission'
     ],
     'DEFAULT_RENDERER_CLASSES': [
         'systems.api.schema.renderers.DataJSONRenderer'
