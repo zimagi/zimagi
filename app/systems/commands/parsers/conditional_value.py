089d433bb4ac06a85f1d967e2cdd2b4dcf0d10fb
Adrian Webb <adrian@webb.sh>
2020-06-12 06:06:44 -0400

    Adding an initial conditional value command option parser.

---
+++ app/systems/commands/parsers/conditional_value.py
@@ -0,0 +1,22 @@
+from .base import ParserBase
+
+import re
+
+
+class ConditionalValueParser(ParserBase):
+
+    conditional_pattern = r'^\?\s*\(([^\)]+)\)\s+([^\:]+)\|(.+)$'
+
+
+    def parse(self, value):
+        if not isinstance(value, str):
+            return value
+
+        conditional_match = re.search(self.conditional_pattern, value)
+        if conditional_match:
+            if eval(conditional_match.group(1).strip()):
+                value = eval(conditional_match.group(2).strip())
+            else:
+                value = eval(conditional_match.group(3).strip())
+
+        return value
