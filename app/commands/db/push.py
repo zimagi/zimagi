39762e6d464ced7b7664a2acac01f80a115712b6
Adrian Webb <adrian@webb.sh>
2020-06-25 11:53:33 -0400

    Updating the preprocess option assignment in the db push command.

--- app/commands/db/push.py
+++ app/commands/db/push.py
@@ -3,8 +3,8 @@ from systems.commands.index import Command
 
 class Push(Command('db.push')):
 
-    def preprocess(self, params):
-        params.data['db'] = self.db.save(self.db_packages, encrypted = False)
+    def preprocess(self, options):
+        options['db'] = self.db.save(self.db_packages, encrypted = False)
 
     def exec(self):
         self.db.load(self.options.get('db'), encrypted = False)
