f17b8e3ef50dcd368b448544f18edee169bc379d
Adrian Webb <adrian@webb.sh>
2020-08-23 12:10:08 -0400

    Fixing input related issues in the date_time formatter provider.

--- app/plugins/formatter/date_time.py
+++ app/plugins/formatter/date_time.py
@@ -6,8 +6,10 @@ import datetime
 class Provider(BaseProvider('formatter', 'date_time')):
 
     def format(self, value):
+        if isinstance(value, float):
+            value = int(value)
         try:
-            value = datetime.datetime.strptime(value, self.field_format)
+            value = datetime.datetime.strptime(str(value), self.field_format)
         except ValueError as e:
             self.error("Value {} is not a valid date time according to pattern: {}".format(value, self.field_format))
 
