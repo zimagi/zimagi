9f36b6b8e751d639c9d50b3d90b0c0f39498fe9e
Adrian Webb <adrian@webb.sh>
2020-08-23 12:10:53 -0400

    Fixing string array related issues in the joiner formatter provider.

--- app/plugins/formatter/joiner.py
+++ app/plugins/formatter/joiner.py
@@ -5,5 +5,5 @@ class Provider(BaseProvider('formatter', 'joiner')):
 
     def format(self, value):
         if isinstance(value, (list, tuple)):
-            value = self.field_join.join(value)
+            value = self.field_join.join([str(elem) for elem in value])
         return value
