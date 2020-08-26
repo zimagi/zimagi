47c033b1df832d93bcad64e2b7940af1bbcacd39
Adrian Webb <adrian@webb.sh>
2020-08-26 02:44:55 -0400

    Passing separator plugin option in the csv_file and csv_files source plugin providers.

--- app/plugins/source/csv_file.py
+++ app/plugins/source/csv_file.py
@@ -6,5 +6,6 @@ class Provider(BaseProvider('source', 'csv_file')):
     def load(self):
         return self.load_csv_data_from_file(
             self.field_file,
-            self.import_columns
+            self.import_columns,
+            separator = self.field_separator
         )
--- app/plugins/source/csv_files.py
+++ app/plugins/source/csv_files.py
@@ -6,5 +6,6 @@ class Provider(BaseProvider('source', 'csv_files')):
     def load(self):
         return self.load_csv_data_from_files(
             self.field_files,
-            self.import_columns
+            self.import_columns,
+            separator = self.field_separator
         )
