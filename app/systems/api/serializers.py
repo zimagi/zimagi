203249942c1f32000a1be416359787fb2718ad20
Adrian Webb <adrian@webb.sh>
2020-09-28 07:49:17 -0400

    Adding an initial CSV API serializer.

--- app/systems/api/serializers.py
+++ app/systems/api/serializers.py
@@ -65,6 +65,20 @@ def get_related_field_map(facade, fields = None, api_url = True, dynamic = True)
 
     return field_map
 
+def get_csv_field_map(facade, fields = None, dynamic = True):
+    relations = facade.get_all_relations()
+    field_map = get_field_map(facade,
+        fields = fields,
+        api_url = False,
+        dynamic = dynamic
+    )
+    for field_name, info in relations.items():
+        if getattr(info['model'], 'facade', None):
+            field_map[field_name] = SummarySerializer(info['model'].facade)(many = info['multiple'])
+            field_map['Meta'].fields.append(field_name)
+
+    return field_map
+
 
 def LinkSerializer(facade):
     class_name = "{}LinkSerializer".format(facade.name.title())
@@ -119,6 +133,19 @@ def DetailSerializer(facade):
     serializer = type(class_name, (BaseItemSerializer,), get_related_field_map(
         facade,
         api_url = False,
+        dynamic = True
+    ))
+    globals()[class_name] = serializer
+    return serializer
+
+def CSVSerializer(facade):
+    class_name = "{}CSVSerializer".format(facade.name.title())
+
+    if class_name in globals():
+        return globals()[class_name]
+
+    serializer = type(class_name, (BaseItemSerializer,), get_csv_field_map(
+        facade,
         dynamic = True
     ))
     globals()[class_name] = serializer
