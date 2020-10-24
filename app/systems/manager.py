7590bfb77549b2d3dc8a45eb037787bad4e125ce
Adrian Webb <adrian@webb.sh>
2020-10-24 06:51:47 -0400

    Adding a get_spec method to the system manager class.

--- app/systems/manager.py
+++ app/systems/manager.py
@@ -21,3 +21,21 @@ class Manager(
         self.index.register_core_module()
         self.index.update_search_path()
         self.index.collect_environment()
+
+
+    def get_spec(self, location = None, default = None):
+        spec = self.index.spec
+
+        if location is None:
+            return spec
+        if isinstance(location, str):
+            location = location.split('.')
+
+        if default is None:
+            default = {}
+
+        for index, element in enumerate(location):
+            inner_default = default if index == len(location) - 1 else {}
+            spec = spec.get(element, inner_default)
+
+        return spec
