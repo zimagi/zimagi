d481b707fdbbb11d57f6f2cdb53d9bfcf7c11374
Adrian Webb <adrian@webb.sh>
2020-10-24 17:58:13 -0400

    Adding optional prefix parameter in the keys function provider.

--- app/plugins/function/keys.py
+++ app/plugins/function/keys.py
@@ -3,5 +3,12 @@ from systems.plugins.index import BaseProvider
 
 class Provider(BaseProvider('function', 'keys')):
 
-    def exec(self, data):
-        return list(data.keys())
+    def exec(self, data, prefix = None):
+        keys = list(data.keys())
+        if prefix is None:
+            return keys
+
+        for index, key in enumerate(keys):
+            keys[index] = "{}{}".format(prefix, key)
+
+        return keys
