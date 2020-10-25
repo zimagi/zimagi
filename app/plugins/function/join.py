f394ca766ab79308270bcb02b27fc7c15fa85157
Adrian Webb <adrian@webb.sh>
2020-10-25 03:12:33 -0400

    Adding an initial join function provider and related specification.

---
+++ app/plugins/function/join.py
@@ -0,0 +1,13 @@
+from systems.plugins.index import BaseProvider
+from utility.data import ensure_list
+
+
+class Provider(BaseProvider('function', 'join')):
+
+    def exec(self, *elements):
+        values = []
+
+        for element in elements:
+            values.extend(ensure_list(element))
+
+        return values
