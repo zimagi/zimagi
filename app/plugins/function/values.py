d90c53f6c28ef482bb51bdcdfa72ee6d0810d4b2
Adrian Webb <adrian@webb.sh>
2020-10-25 01:12:40 -0400

    Updating the implementation of the values function provider to find values in complex data objects.

--- app/plugins/function/values.py
+++ app/plugins/function/values.py
@@ -3,5 +3,26 @@ from systems.plugins.index import BaseProvider
 
 class Provider(BaseProvider('function', 'values')):
 
-    def exec(self, data):
-        return list(data.values())
+    def exec(self, data, keys = None):
+        if keys is None:
+            return list(data.values())
+
+        def get_value(inner_data, inner_keys):
+            last_index = len(inner_keys) - 1
+            for index, inner_key in enumerate(inner_keys):
+                if index == last_index:
+                    inner_data = inner_data.get(inner_key, None)
+                else:
+                    inner_data = inner_data.get(inner_key, {})
+            return inner_data
+
+        keys = keys.split('.')
+        values = []
+        if isinstance(data, (list, tuple)):
+            for index, value in enumerate(data):
+                values.append(get_value(value, keys))
+        else:
+            for key, value in data.items():
+                values.append(get_value(value, keys))
+
+        return values
