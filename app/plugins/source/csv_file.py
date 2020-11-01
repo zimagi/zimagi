6b3674d573dbd18a3af01606af2518cd131b8a48
Adrian Webb <adrian@webb.sh>
2020-11-01 07:16:59 -0500

    Adding CSV loading related parameters in the CSV file source provider.

--- app/plugins/source/csv_file.py
+++ app/plugins/source/csv_file.py
@@ -7,6 +7,8 @@ class Provider(BaseProvider('source', 'csv_file')):
         return self.load_csv_data_from_file(
             self.field_file,
             self.import_columns,
+            archive_file = self.field_archive_file,
             separator = self.field_separator,
-            data_type = self.field_data_type
+            data_type = self.field_data_type,
+            header = self.field_header
         )
