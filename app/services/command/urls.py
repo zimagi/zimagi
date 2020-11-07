6fdfb12050349f30ae1309bfc77cfc4cdfd205f1
Adrian Webb <adrian@webb.sh>
2020-11-07 17:38:37 -0500

    Removing the cache_page call in the command urls service definition (moving to middleware).

--- app/services/command/urls.py
+++ app/services/command/urls.py
@@ -1,6 +1,5 @@
 
 from django.conf.urls import include, url
-from django.views.decorators.cache import cache_page
 
 from rest_framework.schemas import get_schema_view
 
@@ -11,9 +10,9 @@ from systems.api.schema import generators, renderers
 urlpatterns = [
     url(r'^status/?$', views.Status.as_view()),
     url(r'^', include(routers.CommandAPIRouter().urls)),
-    url('^$', cache_page(None, cache = 'api')(get_schema_view(
+    url('^$', get_schema_view(
         title = 'Zimagi Command API',
         generator_class = generators.CommandSchemaGenerator,
         renderer_classes = [ renderers.CommandSchemaJSONRenderer ]
-    )))
+    ))
 ]
