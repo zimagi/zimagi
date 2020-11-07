146ca19c4978f945e6274d537c6b53a230d3e7d1
Adrian Webb <adrian@webb.sh>
2020-11-07 17:39:06 -0500

    Removing the cache_page call in the data urls service definition (moving to middleware).

--- app/services/data/urls.py
+++ app/services/data/urls.py
@@ -1,6 +1,5 @@
 
 from django.conf.urls import include, url
-from django.views.decorators.cache import cache_page
 
 from rest_framework.schemas import get_schema_view
 
@@ -11,9 +10,9 @@ from systems.api.schema import generators, renderers
 urlpatterns = [
     url(r'^status/?$', views.Status.as_view()),
     url(r'^', include(routers.DataAPIRouter().urls)),
-    url('^$', cache_page(None, cache = 'api')(get_schema_view(
+    url('^$', get_schema_view(
         title = 'Zimagi Data API',
         generator_class = generators.DataSchemaGenerator,
         renderer_classes = [ renderers.DataSchemaJSONRenderer ]
-    )))
+    ))
 ]
