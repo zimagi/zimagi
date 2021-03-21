d6569894b6e9b90fb1fad15d22eb45bf14f7f5fb
Adrian Webb <adrian@webb.sh>
2021-03-21 12:00:14 -0400

    Fixing division calculation provider division by zero issue.

--- app/plugins/calculation/divide.py
+++ app/plugins/calculation/divide.py
@@ -1,7 +1,11 @@
 from systems.plugins.index import BaseProvider
 
+import math
+
 
 class Provider(BaseProvider('calculation', 'divide')):
 
     def calc(self, p):
-        return (p.a / p.b) if self.check(p.a, p.b) else None
+        if self.check(p.a, p.b):
+            return (p.a / p.b) if p.b != 0 else math.nan
+        return None
