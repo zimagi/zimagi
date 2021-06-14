0f95e54a82c605648a67cf2d9032c29203041ceb
Adrian Webb <adrian@webb.sh>
2021-06-14 10:09:45 -0400

    Refactoring the divide calculation provider.

--- app/plugins/calculation/divide.py
+++ app/plugins/calculation/divide.py
@@ -1,11 +1,7 @@
 from systems.plugins.index import BaseProvider
 
-import math
-
 
 class Provider(BaseProvider('calculation', 'divide')):
 
     def calc(self, p):
-        if self.check(p.a, p.b):
-            return (p.a / p.b) if p.b != 0 else math.nan
-        return None
+        return (p.a / p.b) if self.check(p.a, p.b) else None
