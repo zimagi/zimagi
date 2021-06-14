70c148a891ccab62fe451f1d417faa7398783ca4
Adrian Webb <adrian@webb.sh>
2021-06-14 10:08:48 -0400

    Refactoring the list calculation plugin mixin to eliminate the valid_list method.

--- app/plugins/mixins/list_calculation.py
+++ app/plugins/mixins/list_calculation.py
@@ -3,15 +3,13 @@ from systems.plugins.index import ProviderMixin
 
 class ListCalculationMixin(ProviderMixin('list_calculation')):
 
-    def valid_list(self, list_data):
-        if not isinstance(list_data, (list, tuple)) or len(list_data) == 0:
-            return False
-
-        if (self.field_min_values is not None and len(list_data) < self.field_min_values):
-            return False
-        return True
-
     def prepare_list(self, list_data):
-        if self.field_reverse:
-            list_data = list(reversed(list_data))
-        return list_data
+        if not isinstance(list_data, (list, tuple)):
+            self.abort("Calculation requires a list parameter")
+
+        list_data = [value for value in list(list_data) if value is not None]
+
+        if len(list_data) == 0 or (self.field_min_values is not None and len(list_data) < self.field_min_values):
+            self.set_null()
+
+        return reversed(list_data) if self.field_reverse else list_data
