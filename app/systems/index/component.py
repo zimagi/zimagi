3d83e84c553ef15418de91b4d1d3c24828935575
Adrian Webb <adrian@webb.sh>
2021-07-04 01:59:52 -0400

    Adding a load_component_names method to the system index component library.

--- app/systems/index/component.py
+++ app/systems/index/component.py
@@ -28,3 +28,14 @@ class IndexerComponentMixin(object):
                         else:
                             components[priority].append(instance)
         return components
+
+    def load_component_names(self, profile, filter_method = None):
+        component_map = self.load_components(profile)
+        component_names = []
+
+        for priority, component_list in sorted(component_map.items()):
+            for component in component_list:
+                if not filter_method or getattr(component, filter_method)():
+                    component_names.append(component.name)
+
+        return component_names
