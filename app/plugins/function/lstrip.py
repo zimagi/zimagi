3efb766d4ed3027c9934a6add00750e0aafc6338
Adrian Webb <adrian@webb.sh>
2021-07-06 02:42:12 -0400

    Adding initial lstrip and rstrip parser function providers.

---
+++ app/plugins/function/lstrip.py
@@ -0,0 +1,10 @@
+from systems.plugins.index import BaseProvider
+
+
+class Provider(BaseProvider('function', 'lstrip')):
+
+    def exec(self, value, prefix = None):
+        value = value.strip()
+        if prefix and value.startswith(prefix):
+            value = value[len(prefix):]
+        return value
---
+++ app/plugins/function/rstrip.py
@@ -0,0 +1,10 @@
+from systems.plugins.index import BaseProvider
+
+
+class Provider(BaseProvider('function', 'rstrip')):
+
+    def exec(self, value, suffix = None):
+        value = value.strip()
+        if suffix and value.endswith(suffix):
+            value = value[:-len(suffix)]
+        return value
