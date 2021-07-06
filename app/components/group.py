6078106b91528fc6f2e450336cb7f97238ae3c2b
Adrian Webb <adrian@webb.sh>
2021-07-06 02:22:55 -0400

    Adding recursive creation and destruction of group trees in the group profile component.

--- app/components/group.py
+++ app/components/group.py
@@ -1,4 +1,5 @@
 from systems.commands import profile
+from utility.data import ensure_list
 
 
 class ProfileComponent(profile.BaseProfileComponent):
@@ -8,15 +9,29 @@ class ProfileComponent(profile.BaseProfileComponent):
 
 
     def run(self, name, children):
+        if children and isinstance(children, dict):
+            for child, grandchildren in children.items():
+                self.run(child, grandchildren)
+
+            children = list(children.keys())
+
         self.exec('group children',
             group_name = name,
-            group_names = [] if not children else children
+            group_names = [] if not children else ensure_list(children)
         )
 
     def describe(self, instance):
         return self.get_names(instance.group_relation)
 
     def destroy(self, name, children):
+        if children:
+            if isinstance(children, dict):
+                for child, grandchildren in children.items():
+                    self.destroy(child, grandchildren)
+            elif isinstance(children, (list, tuple, str)):
+                for child in ensure_list(children):
+                    self.destroy(child, None)
+
         self.exec('group remove',
             group_name = name,
             force = True
