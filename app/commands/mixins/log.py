d3483fb1c8e0c507990e96f00c7acf2c114e718f
Adrian Webb <adrian@webb.sh>
2021-07-10 05:07:27 -0400

    Updating the log_init method to accept and return a log key for command log initialization in the log command mixin.

--- app/commands/mixins/log.py
+++ app/commands/mixins/log.py
@@ -3,21 +3,26 @@ from systems.commands.index import CommandMixin
 
 class LogMixin(CommandMixin('log')):
 
-    def log_init(self, options, task = None):
+    def log_init(self, options, task = None, log_key = None):
         if not options:
             options = {}
 
-        self.log_entry = self._log.create(None,
-            command = self.get_full_name()
-        )
+        if log_key is None:
+            self.log_entry = self._log.create(None,
+                command = self.get_full_name()
+            )
+        else:
+            self.log_entry = self._log.retrieve(log_key)
+
         self.log_entry.user = self.active_user
         self.log_entry.config = options
-
         if task:
             self.log_entry.scheduled = True
             self.log_entry.task_id = task.request.id
 
         self.log_entry.save()
+        return self.log_entry.name
+
 
     def log_message(self, data):
         def _create_log_message(command, data):
@@ -28,6 +33,7 @@ class LogMixin(CommandMixin('log')):
                 _create_log_message(command.exec_parent, data)
 
         _create_log_message(self, data)
+
 
     def log_status(self, status):
         self.log_entry.set_status(status)
