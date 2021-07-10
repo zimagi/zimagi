cbb9ae6e30a82914cb370898acab74702ab6a413
Adrian Webb <adrian@webb.sh>
2021-07-10 17:31:53 -0400

    Updating the naming convertions and colored output in the version command.

--- app/commands/version.py
+++ app/commands/version.py
@@ -10,7 +10,7 @@ class Version(Command('version')):
 
         if not settings.API_EXEC:
             version_info = [
-                [self.key_color("Client version"), self.get_version()]
+                [self.key_color("Client version"), self.value_color(self.get_version())]
             ]
 
             self.table(self.render_list(self._environment, filters = {
@@ -21,18 +21,18 @@ class Version(Command('version')):
                 result = self.exec_remote(env, 'version', display = False)
 
                 version_info.extend([
-                    [self.key_color("Server version"), result.named['server_version'].data],
-                    [self.key_color("Server environment"), result.named['server_env'].data],
-                    [self.key_color("Server runtime repository"), result.named['server_repo'].data],
-                    [self.key_color("Server runtime image"), result.named['server_image'].data]
+                    [self.key_color("Host version"), self.value_color(result.named['host_version'].data)],
+                    [self.key_color("Host environment"), result.named['host_env'].data],
+                    [self.key_color("Host runtime repository"), result.named['host_repo'].data],
+                    [self.key_color("Host runtime image"), result.named['host_image'].data]
                 ])
 
-                if env.name != result.named['server_env'].data:
+                if env.name != result.named['host_env'].data:
                     self.warning("Local and remote environment names do not match.  Use remote environment name locally to avoid sync issues.")
 
             self.table(version_info)
         else:
-            self.silent_data('server_version', self.get_version())
-            self.silent_data('server_env', env.name)
-            self.silent_data('server_repo', env.repo)
-            self.silent_data('server_image', env.base_image)
+            self.silent_data('host_version', self.get_version())
+            self.silent_data('host_env', env.name)
+            self.silent_data('host_repo', env.repo)
+            self.silent_data('host_image', env.base_image)
