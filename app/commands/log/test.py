b4a58326469c6b13fe78d8897503094de332085e
Adrian Webb <adrian@webb.sh>
2021-07-10 22:14:05 -0400

    Removing the temporary log test command and related specification.

--- app/commands/log/test.py
+++
@@ -1,9 +0,0 @@
-from systems.commands.index import Command
-
-
-class Test(Command('log.test')):
-
-    def exec(self):
-        for number in range(100):
-            self.data("Number", number)
-            self.sleep(2)
