d15c88cf2ae82053eb031f614224fd4a90afb0ca
Adrian Webb <adrian@webb.sh>
2021-07-12 00:29:24 -0400

    Adding validate_unique and save method overrides in the scheduled task data model implementation.

--- app/data/schedule/models.py
+++ app/data/schedule/models.py
@@ -83,3 +83,40 @@ class ScheduledTask(
     Model('scheduled_task')
 ):
     objects = celery_beat_managers.PeriodicTaskManager()
+
+    def validate_unique(self, *args, **kwargs):
+        super(celery_beat_models.PeriodicTask, self).validate_unique(*args, **kwargs)
+
+        schedule_types = ['interval', 'crontab', 'clocked']
+        selected_schedule_types = [s for s in schedule_types
+                                   if getattr(self, s)]
+
+        if len(selected_schedule_types) == 0:
+            raise ValidationError(
+                'One of clocked, interval, crontab, or solar '
+                'must be set.'
+            )
+
+        err_msg = 'Only one of clocked, interval, crontab, '\
+            'or solar must be set'
+        if len(selected_schedule_types) > 1:
+            error_info = {}
+            for selected_schedule_type in selected_schedule_types:
+                error_info[selected_schedule_type] = [err_msg]
+            raise ValidationError(error_info)
+
+        # clocked must be one off task
+        if self.clocked and not self.one_off:
+            err_msg = 'clocked must be one off, one_off must set True'
+            raise ValidationError(err_msg)
+
+    def save(self, *args, **kwargs):
+        self.exchange = self.exchange or None
+        self.routing_key = self.routing_key or None
+        self.queue = self.queue or None
+        self.headers = self.headers or None
+        if not self.enabled:
+            self.last_run_at = None
+        self._clean_expires()
+        self.validate_unique()
+        super(celery_beat_models.PeriodicTask, self).save(*args, **kwargs)
