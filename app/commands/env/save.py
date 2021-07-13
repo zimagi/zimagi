8cf1d14da00de09b1e7dbf87ede824cde39ac7b6
Adrian Webb <adrian@webb.sh>
2021-07-13 01:38:31 -0400

    Adding an initial env save command.

---
+++ app/commands/env/save.py
@@ -0,0 +1,10 @@
+from systems.commands.index import Command
+
+
+class Save(Command('env.save')):
+
+    def exec(self):
+        self.save_env(
+            name = self.environment_name,
+            **self.environment_fields
+        )
