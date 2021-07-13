5de68114de475475f95cfe5bf8edc3db51075a3c
Adrian Webb <adrian@webb.sh>
2021-07-13 01:29:41 -0400

    Updating the version command to use the host model instead of the integrated environment object.

--- app/commands/version.py
+++ app/commands/version.py
@@ -7,18 +7,25 @@ class Version(Command('version')):
 
     def exec(self):
         env = self.get_env()
+        host = self.get_host()
 
         if not settings.API_EXEC:
+            self.notice("Environment Information")
+            self.table([
+                [self.key_color("Name"), self.value_color(env.name)],
+                [self.key_color("Image Repository"), self.value_color(str(env.repo))],
+                [self.key_color("Base Image"), self.value_color(str(env.base_image))],
+                [self.key_color("Runtime Image"), self.value_color(str(env.runtime_image))],
+                [self.key_color("Created"), self.value_color(self.format_time(env.created))],
+                [self.key_color("Updated"), self.value_color(self.format_time(env.updated))]
+            ])
+            self.info('')
+
             version_info = [
                 [self.key_color("Client version"), self.value_color(self.get_version())]
             ]
-
-            self.table(self.render_list(self._environment, filters = {
-                'name': env.name
-            }))
-
-            if env.host and env.user and env.token:
-                result = self.exec_remote(env, 'version', display = False)
+            if host:
+                result = self.exec_remote(host, 'version', display = False)
 
                 version_info.extend([
                     [self.key_color("Host version"), self.value_color(result.named['host_version'].data)],
@@ -26,7 +33,6 @@ class Version(Command('version')):
                     [self.key_color("Host runtime repository"), result.named['host_repo'].data],
                     [self.key_color("Host runtime image"), result.named['host_image'].data]
                 ])
-
             self.table(version_info)
         else:
             self.silent_data('host_version', self.get_version())
