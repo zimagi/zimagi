f72bc50ace483a4e0b4a6e5652ace59689dc5fdc
Adrian Webb <adrian@webb.sh>
2021-07-13 01:39:03 -0400

    Adding an initial env remove command.

---
+++ app/commands/env/remove.py
@@ -0,0 +1,10 @@
+from systems.commands.index import Command
+
+
+class Remove(Command('env.remove')):
+
+    def exec(self):
+        env_name = self.environment_name if self.environment_name else self.curr_env_name
+
+        self.log_result = False
+        self.delete_env(env_name)
