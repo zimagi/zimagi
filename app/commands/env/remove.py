736b703f70157a93b4da3e215e5420c8ca7b5416
Adrian Webb <adrian@webb.sh>
2021-07-13 02:01:50 -0400

    Updating the env remove command to use the remove_module_path command parameter.

--- app/commands/env/remove.py
+++ app/commands/env/remove.py
@@ -7,4 +7,4 @@ class Remove(Command('env.remove')):
         env_name = self.environment_name if self.environment_name else self.curr_env_name
 
         self.log_result = False
-        self.delete_env(env_name)
+        self.delete_env(env_name, remove_module_path = self.remove_module_path)
