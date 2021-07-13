5471e620ece0d3a72f41c3deff0d0d57b5d297e7
Adrian Webb <adrian@webb.sh>
2021-07-13 01:35:40 -0400

    Updating the container image environment variables in the VM based Zimagi development executable.

--- scripts/zimagi.sh
+++ scripts/zimagi.sh
@@ -8,15 +8,20 @@ ZIMAGI_DEFAULT_RUNTIME_IMAGE="${ZIMAGI_DEFAULT_RUNTIME_IMAGE:-zimagi/zimagi:late
 if [ -f /var/local/zimagi/zimagi.env ]
 then
     source /var/local/zimagi/zimagi.env
+
+    if [ -z "${ZIMAGI_RUNTIME_IMAGE}" ]
+    then
+        ZIMAGI_RUNTIME_IMAGE="${ZIMAGI_BASE_IMAGE}"
+    fi
 else
     ZIMAGI_REPO="${ZIMAGI_DEFAULT_RUNTIME_REPO}"
-    ZIMAGI_IMAGE="${ZIMAGI_DEFAULT_RUNTIME_IMAGE}"
+    ZIMAGI_RUNTIME_IMAGE="${ZIMAGI_DEFAULT_RUNTIME_IMAGE}"
 fi
 
-if ! docker inspect "${ZIMAGI_IMAGE}" >/dev/null 2>&1
+if ! docker inspect "${ZIMAGI_RUNTIME_IMAGE}" >/dev/null 2>&1
 then
     rm -f /var/local/zimagi/zimagi.env
-    ZIMAGI_IMAGE="${ZIMAGI_DEFAULT_RUNTIME_IMAGE}"
+    ZIMAGI_RUNTIME_IMAGE="${ZIMAGI_DEFAULT_RUNTIME_IMAGE}"
 fi
 docker run --rm --interactive --tty \
     --env-file /var/local/zimagi/.env \
@@ -26,4 +31,4 @@ docker run --rm --interactive --tty \
     --volume /usr/local/share/zimagi:/usr/local/share/zimagi \
     --volume /var/local/zimagi:/var/local/zimagi \
     --volume /usr/local/lib/zimagi:/usr/local/lib/zimagi \
-    "${ZIMAGI_IMAGE}" "${@}"
+    "${ZIMAGI_RUNTIME_IMAGE}" "${@}"
