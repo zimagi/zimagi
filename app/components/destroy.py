ba3a0f2c5959c31034502f3c9900df775e43614e
Adrian Webb <adrian@webb.sh>
2021-07-21 19:57:25 -0400

    Adding config operation in the destroy profile component.

--- app/components/destroy.py
+++ app/components/destroy.py
@@ -1,4 +1,5 @@
 from systems.commands import profile
+from plugins.parser.config import Provider as ConfigParser
 from utility.data import get_dict_combinations
 
 
@@ -25,8 +26,8 @@ class ProfileComponent(profile.BaseProfileComponent):
         command = self.pop_value('command', config)
         host = self.pop_value('host', config)
 
-        if not task and not command:
-            self.command.error("Destroy {} requires 'task' or 'command' field".format(name))
+        if not task and not command and not self.get_value('config', config):
+            self.command.error("Destroy {} requires 'task', 'command', or 'config' field".format(name))
 
         def _execute(data):
             if command:
@@ -34,7 +35,7 @@ class ProfileComponent(profile.BaseProfileComponent):
                     data['environment_host'] = host
 
                 self.exec(command, **data)
-            else:
+            elif task:
                 options = {
                     'module_name': module,
                     'task_name': task,
@@ -44,6 +45,9 @@ class ProfileComponent(profile.BaseProfileComponent):
                     options['environment_host'] = host
 
                 self.exec('task', **options)
+            else:
+                ConfigParser.runtime_variables[data['config']] = data.get('value', None)
+
         if scopes:
             for scope in get_dict_combinations(scopes):
                 _execute(self.interpolate(config, **scope))
