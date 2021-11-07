47b6d8a5f890a6c84007a0a86457c84970fe4cf2
Adrian Webb <adrian@webb.sh>
2021-11-07 02:21:58 -0500

    Fixing issues with daylight savings time processing in the time utility class.

--- app/utility/time.py
+++ app/utility/time.py
@@ -1,7 +1,6 @@
-from django.utils.timezone import make_aware
+from django.utils.timezone import make_aware, get_current_timezone
 
 import datetime
-import time
 
 
 class TimeException(Exception):
@@ -25,72 +24,77 @@ class Time(object):
 
     @property
     def now(self):
-        time_components = time.localtime()
-        return make_aware(datetime.datetime.now(), is_dst = bool(time_components.tm_isdst))
+        return self.to_datetime(datetime.datetime.now())
 
     @property
     def now_string(self):
         return self.to_string(self.now)
 
 
-    def to_string(self, time):
-        if isinstance(time, str):
-            return time
+    def to_string(self, date_time):
+        if isinstance(date_time, str):
+            return date_time
 
-        if not time.hour and not time.minute and not time.second and not time.microsecond:
-            return time.strftime(self.date_format)
+        if not date_time.hour and not date_time.minute and not date_time.second and not date_time.microsecond:
+            return date_time.strftime(self.date_format)
         else:
-            return time.strftime(self.time_format)
+            return date_time.strftime(self.time_format)
 
 
-    def to_datetime(self, time):
-        if isinstance(time, str):
+    def to_datetime(self, date_time):
+        if isinstance(date_time, str):
             try:
-                time = datetime.datetime.strptime(time, self.time_format)
+                date_time = datetime.datetime.strptime(date_time, self.time_format)
             except ValueError:
-                time = datetime.datetime.strptime(time, self.date_format)
+                date_time = datetime.datetime.strptime(date_time, self.date_format)
 
-        if time.tzinfo is None:
-            time = make_aware(time)
+        if date_time.tzinfo is None:
+            date_time = make_aware(date_time, is_dst = self.is_dst(date_time))
 
-        return time
+        return date_time
 
 
-    def shift(self, time, units, unit_type = 'days', to_string = False):
-        new_time = self.to_datetime(time) + datetime.timedelta(**{
+    def shift(self, date_time, units, unit_type = 'days', to_string = False):
+        new_date_time = self.to_datetime(date_time) + datetime.timedelta(**{
             unit_type: units
         })
         if to_string:
-            return self.to_string(new_time)
-        return new_time
+            return self.to_string(new_date_time)
+        return new_date_time
 
-    def distance(self, time1, time2, unit_type = 'days', include_direction = False):
-        time1 = self.to_datetime(time1)
-        time2 = self.to_datetime(time2)
+    def distance(self, date_time1, date_time2, unit_type = 'days', include_direction = False):
+        date_time1 = self.to_datetime(date_time1)
+        date_time2 = self.to_datetime(date_time2)
 
-        time_span = time2 - time1
+        time_span = date_time2 - date_time1
         units = getattr(time_span, unit_type)
         return units if include_direction else abs(units)
 
 
-    def generate(self, start_time, end_time = None, unit_type = 'days', units = None):
-        start_time = self.to_datetime(start_time)
+    def generate(self, start_date_time, end_date_time = None, unit_type = 'days', units = None):
+        start_date_time = self.to_datetime(start_date_time)
         times = []
 
-        if end_time is None:
+        if end_date_time is None:
             if units is None:
-                end_time = self.now
+                end_date_time = self.now
             else:
-                end_time = self.shift(start_time, units,
+                end_date_time = self.shift(start_date_time, units,
                     unit_type = unit_type,
                     to_string = False
                 )
         else:
-            end_time = self.to_datetime(end_time)
+            end_date_time = self.to_datetime(end_date_time)
 
-        for index in range(self.distance(start_time, end_time, unit_type = unit_type) + 1):
-            times.append(self.shift(start_time, index,
+        for index in range(self.distance(start_date_time, end_date_time, unit_type = unit_type) + 1):
+            times.append(self.shift(start_date_time, index,
                 unit_type = unit_type,
                 to_string = True
             ))
         return times
+
+
+    def is_dst(self, date_time):
+        non_dst = datetime.datetime(year = date_time.year, month = 1, day = 1)
+        non_dst_tz_aware = get_current_timezone().localize(non_dst)
+        return not (non_dst_tz_aware.utcoffset() == date_time.utcoffset())
