e587d60fb9ad28b98043ad37608bf83abe9841e1
Adrian Webb <adrian@webb.sh>
2021-11-30 10:38:46 -0500

    Fixing various issues with the Concat query aggregate implementation.

--- app/systems/models/aggregates.py
+++ app/systems/models/aggregates.py
@@ -1,19 +1,29 @@
-from django.db.models import Aggregate, CharField, Value
+from django.db.models import Aggregate
 
 
 class Concat(Aggregate):
 
     function = 'GROUP_CONCAT'
-    template = '%(function)s(%(expressions)s)'
+    template = "%(function)s(%(distinct)s%(expressions)s)"
+    allow_distinct = True
 
 
-    def __init__(self, expression, delimiter = ',', **extra):
-        output_field = extra.pop('output_field', CharField())
-        delimiter = Value(delimiter)
-
-        super().__init__(expression, delimiter, output_field = output_field, **extra)
+    def __init__(self, expression, separator = ';', **extra):
+        super().__init__(expression,
+            separator = separator,
+            **extra
+        )
 
 
     def as_postgresql(self, compiler, connection):
         self.function = 'STRING_AGG'
+        self.template = "%(function)s(%(distinct)s%(expressions)s, '%(separator)s')"
+        return super().as_sql(compiler, connection)
+
+    def as_mysql(self, compiler, connection):
+        self.template = "%(function)s(%(distinct)s%(expressions)s SEPARATOR '%(separator)s')"
+        return super().as_sql(compiler, connection)
+
+    def as_sqlite(self, compiler, connection):
+        self.template = "%(function)s(REPLACE(%(distinct)s%(expressions)s, '', ''), '%(separator)s')"
         return super().as_sql(compiler, connection)
