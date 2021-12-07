eab7883b54b3ef5ae619c1dfd47517eacab4cdbc
Adrian Webb <adrian@webb.sh>
2021-12-07 17:45:48 -0500

    Removing the db, processor, and redis start and stop commands (no longer managing local services).

--- app/commands/db/stop.py
+++
@@ -1,9 +0,0 @@
-from systems.commands.index import Command
-
-
-class Stop(Command('db.stop')):
-
-    def exec(self):
-        self.log_result = False
-        self.manager.stop_service(self, 'zimagi-postgres', self.remove)
-        self.success('Successfully stopped PostgreSQL database service')
--- app/commands/redis/stop.py
+++
@@ -1,9 +0,0 @@
-from systems.commands.index import Command
-
-
-class Stop(Command('redis.stop')):
-
-    def exec(self):
-        self.manager.stop_service(self, 'zimagi-redis', self.remove)
-        self.set_state('config_ensure', True)
-        self.success('Successfully stopped Redis service')
