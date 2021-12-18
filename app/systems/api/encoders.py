30bef406c721595248dfeb44b799fe1193f3f24d
Adrian Webb <adrian@webb.sh>
2021-12-18 14:56:48 -0500

    Adding the SafeJSONEncoder to a top level systems API encoders library.

---
+++ app/systems/api/encoders.py
@@ -0,0 +1,10 @@
+from rest_framework.utils import encoders
+
+
+class SafeJSONEncoder(encoders.JSONEncoder):
+
+    def default(self, obj):
+        try:
+            return super().default(obj)
+        except Exception as e:
+            return str(obj)
