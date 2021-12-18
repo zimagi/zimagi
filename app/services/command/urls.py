49ae143a5038aa388e6d380be012b8ff1275c1e1
Adrian Webb <adrian@webb.sh>
2021-12-18 15:18:01 -0500

    Refactoring the class paths and implementation of the command service urls definition.

--- app/services/command/urls.py
+++ app/services/command/urls.py
@@ -1,18 +1,24 @@
 
 from django.conf.urls import include, url
-
 from rest_framework.schemas import get_schema_view
+from rest_framework import permissions
 
-from systems.api import routers, views
-from systems.api.schema import generators, renderers
+from systems.api import views as shared_views
+from systems.api.command import routers, schema, renderers
 
+
+status_view = shared_views.Status.as_view(
+    permission_classes = [ permissions.AllowAny ],
+    schema = schema.StatusSchema()
+)
 
 urlpatterns = [
-    url(r'^status/?$', views.Status.as_view()),
+    url(r'^status/?$', status_view),
     url(r'^', include(routers.CommandAPIRouter().urls)),
     url('^$', get_schema_view(
         title = 'Zimagi Command API',
-        generator_class = generators.CommandSchemaGenerator,
-        renderer_classes = [ renderers.CommandSchemaJSONRenderer ]
+        generator_class = schema.CommandSchemaGenerator,
+        renderer_classes = [ renderers.CommandSchemaJSONRenderer ],
+        permission_classes = [ permissions.AllowAny ]
     ))
 ]
