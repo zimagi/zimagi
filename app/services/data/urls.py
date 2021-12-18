3020af0124c2d4cc57d24de9d35bb41419713901
Adrian Webb <adrian@webb.sh>
2021-12-18 15:19:16 -0500

    Refactoring the class paths and implementation of the data service urls definition.

--- app/services/data/urls.py
+++ app/services/data/urls.py
@@ -1,19 +1,25 @@
 
 from django.conf.urls import include, url
-
 from rest_framework.schemas import get_schema_view
+from rest_framework import permissions
 
-from systems.api import routers, views
-from systems.api.schema import generators, renderers
+from systems.api import views as shared_views
+from systems.api.data import routers, views, schema, renderers
 
+
+status_view = shared_views.Status.as_view(
+    permission_classes = [ permissions.AllowAny ],
+    schema = schema.StatusSchema()
+)
 
 urlpatterns = [
-    url(r'^status/?$', views.Status.as_view()),
-    url(r'^dataset/(?P<name>[^\/]+)/?$', views.DataSet.as_view()),
+    url(r'^status/?$', status_view),
+    url(r'^download/(?P<name>[^\/]+)/?$', views.DataSet.as_view()),
     url(r'^', include(routers.DataAPIRouter().urls)),
     url('^$', get_schema_view(
         title = 'Zimagi Data API',
-        generator_class = generators.DataSchemaGenerator,
-        renderer_classes = [ renderers.DataSchemaJSONRenderer ]
+        generator_class = schema.DataSchemaGenerator,
+        renderer_classes = [ renderers.DataSchemaJSONRenderer ],
+        permission_classes = [ permissions.AllowAny ]
     ))
 ]
