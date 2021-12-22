1d3dcdbbab9a60bf7d6d9aa8d032ee00d765a417
Adrian Webb <adrian@webb.sh>
2021-12-22 08:00:46 -0500

    Adding an initial system command schema library (borrowed from the defunct coreapi project).

---
+++ app/systems/commands/schema.py
@@ -0,0 +1,146 @@
+from collections import OrderedDict
+
+
+def _key_sorting(item):
+    key, value = item
+    if isinstance(value, Link):
+        action_priority = {
+            'get': 0,
+            'post': 1
+        }.get(value.action, 2)
+        return (1, (value.url, action_priority))
+    return (0, key)
+
+
+class Field(object):
+
+    def __init__(self, name,
+        required = False,
+        location = None,
+        schema = None,
+        tags = None
+
+    ):
+        self.name = name
+        self.required = required
+        self.location = location
+        self.schema = schema
+        self.tags = [] if tags is None else tags
+
+
+class Document(dict):
+
+    def __init__(self,
+        url = None,
+        title = None,
+        description = None,
+        media_type = None,
+        content = None
+    ):
+        self.url = '' if url is None else url
+        self.title = '' if title is None else title
+        self.description = '' if description is None else description
+        self.media_type = '' if media_type is None else media_type
+        self._data = {} if content is None else content
+
+    def __iter__(self):
+        items = sorted(self._data.items(), key = _key_sorting)
+        return iter([ key for key, value in items ])
+
+
+    @property
+    def data(self):
+        return OrderedDict([
+            (key, value) for key, value in self.items()
+            if not isinstance(value, Link)
+        ])
+
+    @property
+    def links(self):
+        return OrderedDict([
+            (key, value) for key, value in self.items()
+            if isinstance(value, Link)
+        ])
+
+
+    def clone(self, data):
+        return self.__class__(
+            url = self.url,
+            title = self.title,
+            description = self.description,
+            media_type = self.media_type,
+            content = data
+        )
+
+
+class Object(dict):
+
+    def __init__(self, *args, **kwargs):
+        self._data = dict(*args, **kwargs)
+
+    def __iter__(self):
+        items = sorted(self._data.items(), key = _key_sorting)
+        return iter([ key for key, value in items ])
+
+
+    @property
+    def data(self):
+        return OrderedDict([
+            (key, value) for key, value in self.items()
+            if not isinstance(value, Link)
+        ])
+
+    @property
+    def links(self):
+        return OrderedDict([
+            (key, value) for key, value in self.items()
+            if isinstance(value, Link)
+        ])
+
+
+class Array(list):
+
+    def __init__(self, *args):
+        self._data = list(*args)
+
+
+class Link(object):
+
+    def __init__(self,
+        url = None,
+        action = None,
+        encoding = None,
+        title = None,
+        description = None,
+        fields = None
+    ):
+        self.url = '' if url is None else url
+        self.action = '' if action is None else action
+        self.encoding = '' if encoding is None else encoding
+        self.title = '' if title is None else title
+        self.description = '' if description is None else description
+        self.fields = () if fields is None else tuple([
+            item if isinstance(item, Field) else Field(item, required = False, location = '')
+            for item in fields
+        ])
+
+
+class Error(dict):
+
+    def __init__(self, title = None, content = None):
+        self.title = '' if title is None else title
+        self._data = {} if content is None else content
+
+    def __iter__(self):
+        items = sorted(self._data.items(), key = _key_sorting)
+        return iter([ key for key, value in items ])
+
+
+    def get_messages(self):
+        messages = []
+        for value in self.values():
+            if isinstance(value, Array):
+                messages += [
+                    item for item in value if isinstance(item, str)
+                ]
+        return messages
