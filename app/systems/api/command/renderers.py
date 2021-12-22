01343eb10e93486477b90d9d57523a184d95fd1b
Adrian Webb <adrian@webb.sh>
2021-12-22 08:12:04 -0500

    Updating the CoreJSON codec location and encode method in the systems API command renderers library.

--- app/systems/api/command/renderers.py
+++ app/systems/api/command/renderers.py
@@ -1,12 +1,12 @@
 from rest_framework import renderers
-from rest_framework.compat import coreapi
-
 from systems.api.encoders import SafeJSONEncoder
+from systems.api.command.codecs import CoreJSONCodec
 
 
 class CommandSchemaJSONRenderer(renderers.CoreJSONRenderer):
 
     def render(self, data, media_type = None, renderer_context = None):
-        indent = int(renderer_context.get('indent', 2))
-        codec = coreapi.codecs.CoreJSONCodec()
-        return codec.dump(data, cls = SafeJSONEncoder, indent = indent)
+        return CoreJSONCodec().encode(data,
+            cls = SafeJSONEncoder,
+            indent = int(renderer_context.get('indent', 2))
+        )
