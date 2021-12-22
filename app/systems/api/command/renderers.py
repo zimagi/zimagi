00c07a4558156336f97e9334b35fb4efc7cc072a
Adrian Webb <adrian@webb.sh>
2021-12-22 10:41:56 -0500

    Extending the BaseRenderer in the CommandSchemaJSONRenderer in the systems API command renderers library.

--- app/systems/api/command/renderers.py
+++ app/systems/api/command/renderers.py
@@ -3,7 +3,10 @@ from systems.api.encoders import SafeJSONEncoder
 from systems.api.command.codecs import CoreJSONCodec
 
 
-class CommandSchemaJSONRenderer(renderers.CoreJSONRenderer):
+class CommandSchemaJSONRenderer(renderers.BaseRenderer):
+
+    media_type = 'application/coreapi+json'
+
 
     def render(self, data, media_type = None, renderer_context = None):
         return CoreJSONCodec().encode(data,
