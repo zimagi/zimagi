acc09a640758ae50705a8ac6e11126c41691385a
Adrian Webb <adrian@webb.sh>
2021-12-30 05:11:55 -0500

    Refactoring the encryption utility library into encryption plugin and providers.

---
+++ app/plugins/encryption/aes256_user.py
@@ -0,0 +1,14 @@
+from systems.plugins.index import BaseProvider
+from systems.encryption.cipher import EncryptionError
+
+
+class Provider(BaseProvider('encryption', 'aes256_user')):
+
+    def initialize(self, options):
+        if options.get('user', None):
+            user_facade = self.manager.index.get_facade_index()['user']
+            user = user_facade.retrieve(options['user'])
+            if not user:
+                raise EncryptionError("User {} does not exist".format(options['user']))
+
+            options['key'] = user.encryption_key
---
+++ app/plugins/encryption/base.py
@@ -0,0 +1,76 @@
+from systems.plugins.index import BasePlugin
+
+import os
+import binascii
+
+
+class BaseProvider(BasePlugin('encryption')):
+
+    @classmethod
+    def generate_key(self):
+        size = 100
+        return binascii.hexlify(os.urandom(size)).decode()[:size]
+
+
+    def __init__(self, type, name, options = None, initialize = True):
+        super().__init__(type, name)
+
+        if not options:
+            options = {}
+
+        if initialize:
+            if options.get('key', None):
+                if os.path.isfile(options['key']):
+                    with open(options['key'], 'r') as file:
+                        options['key'] = file.read()
+
+            self.initialize(options)
+
+        self.import_config(options)
+
+    def initialize(self, options):
+        # Override in subclass if needed
+        pass
+
+
+    def encrypt(self, plain_text):
+        # Plain Text - str|bytes -> str
+        # Cipher Text - bytes
+        self.validate()
+        return self.encrypt_postprocess(self.encrypt_text(self.encrypt_preprocess(plain_text)))
+
+    def encrypt_preprocess(self, plain_text):
+        if isinstance(plain_text, bytes):
+            plain_text = self.field_binary_marker + plain_text.hex()
+        return plain_text
+
+    def encrypt_text(self, plain_text):
+        # Override in providers
+        return plain_text.encode()
+
+    def encrypt_postprocess(self, cipher_text):
+        # Override in providers if needed
+        return cipher_text
+
+
+    def decrypt(self, cipher_text, decode = True):
+        # Cipher Text - bytes
+        # Plain Text - str|bytes
+        self.validate()
+        return self.decrypt_postprocess(self.decrypt_text(self.decrypt_preprocess(cipher_text)), decode)
+
+    def decrypt_preprocess(self, cipher_text):
+        # Override in providers if needed
+        return cipher_text
+
+    def decrypt_text(self, cipher_text):
+        # Override in providers
+        return cipher_text
+
+    def decrypt_postprocess(self, plain_text, decode):
+        if decode and isinstance(plain_text, (bytes, bytearray)):
+            plain_text = plain_text.decode(self.field_decoder)
+
+        if isinstance(plain_text, str) and plain_text.startswith(self.field_binary_marker):
+            plain_text = bytes.fromhex(plain_text[len(self.field_binary_marker):])
+        return plain_text
