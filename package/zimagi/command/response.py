2ca4b0d0e672bef913a0f90f7f558898011f74eb
Adrian Webb <adrian@webb.sh>
2021-12-30 07:36:28 -0500

    Adding an initial command response library in the zimagi package.

---
+++ package/zimagi/command/response.py
@@ -0,0 +1,54 @@
+
+class CommandResponse(object):
+
+    def __init__(self):
+        self.messages = []
+        self.named = {}
+        self.errors = []
+
+
+    def __getitem__(self, name):
+        return self.get_named_data(name)
+
+
+    @property
+    def active_user(self):
+        return self.get_named_data('active_user')
+
+    @property
+    def log_key(self):
+        return self.get_named_data('log_key')
+
+
+    def add(self, messages):
+        if not isinstance(messages, (list, tuple)):
+            messages = [messages]
+
+        for message in messages:
+            self.messages.append(message)
+            if message.name:
+                self.named[message.name] = message
+            if message.is_error():
+                self.errors.append(message)
+
+
+    @property
+    def aborted(self):
+        return len(self.errors) > 0
+
+    def error_message(self):
+        messages = []
+        for message in self.errors:
+            messages.append(message.format())
+
+        return "\n\n".join(messages)
+
+
+    def get_named_data(self, name):
+        message = self.named.get(name, None)
+        if message:
+            try:
+                return message.data
+            except AttributeError:
+                return message.message
+        return None
