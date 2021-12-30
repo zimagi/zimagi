72c2746d8649282655a15cc5183ffae3366660a6
Adrian Webb <adrian@webb.sh>
2021-12-30 06:28:49 -0500

    Updating the API authentication implementation.

--- app/systems/api/auth.py
+++ app/systems/api/auth.py
@@ -2,7 +2,9 @@ from django.utils.timezone import now
 from rest_framework import authentication, exceptions
 
 from systems.models.index import Model
+from systems.encryption.cipher import Cipher
 
+import re
 import logging
 
 
@@ -12,6 +14,7 @@ logger = logging.getLogger(__name__)
 class APITokenAuthentication(authentication.TokenAuthentication):
 
     user_class = Model('user')
+    api_type = None
 
 
     def get_auth_header(self, request):
@@ -21,35 +24,39 @@ class APITokenAuthentication(authentication.TokenAuthentication):
         return header
 
 
-    def validate_token_header(self, auth):
-        if len(auth) == 1:
-            msg = 'Invalid token header. No credentials provided.'
-            logger.warning(msg)
-            raise exceptions.AuthenticationFailed(msg)
+    def parse_token(self, token_text):
+        auth_components = re.split(r'\s+', token_text)
 
-        elif len(auth) > 2:
-            msg = 'Invalid token header. Token string should not contain spaces.'
-            logger.warning(msg)
-            raise exceptions.AuthenticationFailed(msg)
+        if len(auth_components) != 3:
+            raise exceptions.AuthenticationFailed("Invalid token. Required format: {} <user_name> <token>".format(self.keyword))
+        if auth_components[0].lower() != self.keyword.lower():
+            raise exceptions.AuthenticationFailed("Authentication header required: 'Authorization: {} <user_name> <token>'".format(self.keyword))
 
-
-    def authenticate_credentials(self, auth):
-        self.validate_token_header(auth)
-
-        components = auth[1].split('++')
-
-        if len(components) != 2:
-            raise exceptions.AuthenticationFailed('Invalid token. Required format: Token user++token')
         try:
-            user = self.user_class.facade.retrieve(components[0])
-            token = components[1]
+            user = self.user_class.facade.retrieve(auth_components[1])
+            token = auth_components[2]
 
         except self.user_class.DoesNotExist:
             raise exceptions.AuthenticationFailed('Invalid token: User not found')
 
+        return (user, token)
+
+
+    def authenticate(self, request):
+        token_text = self.get_auth_header(request)
+        if not token_text and re.search(r'^(/|/status/?)$', request.path):
+            return None
+
+        user, token = self.parse_token(token_text)
+        try:
+            if self.api_type:
+                token = Cipher.get(self.api_type, user = user.name).decrypt(token)
+
+        except Exception as error:
+            raise exceptions.AuthenticationFailed('Invalid token header. Credentials can not be decrypted')
+
         if not user.is_active:
             raise exceptions.AuthenticationFailed('User account is inactive. Contact administrator')
-
         if not user.check_password(token):
             raise exceptions.AuthenticationFailed('Invalid token: User credentials are invalid')
 
