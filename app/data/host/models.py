5ab8043239fe63be4e3d42255e892ae1868adf8f
Adrian Webb <adrian@webb.sh>
2021-12-30 06:12:34 -0500

    Adding a field_encryption_key_display method to the host model facade.

--- app/data/host/models.py
+++ app/data/host/models.py
@@ -7,3 +7,8 @@ class HostFacade(ModelFacade('host')):
         if value and short:
             return super().get_field_token_display(instance, value[:10] + '...', short)
         return super().get_field_token_display(instance, value, short)
+
+    def get_field_encryption_key_display(self, instance, value, short):
+        if value and short:
+            return super().get_field_encryption_key_display(instance, value[:10] + '...', short)
+        return super().get_field_encryption_key_display(instance, value, short)
