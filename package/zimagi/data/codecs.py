c421e508b8e7c7ce68ead408f9527148eb323675
Adrian Webb <adrian@webb.sh>
2022-01-01 13:28:31 -0500

    Updating the error classes in the package data codecs library.

--- package/zimagi/data/codecs.py
+++ package/zimagi/data/codecs.py
@@ -7,18 +7,16 @@ import json
 
 class OpenAPIJSONCodec(object):
 
-    media_type = 'application/openapi+json'
     media_types = ['application/openapi+json', 'application/vnd.oai.openapi+json']
 
 
     def decode(self, bytestring, **options):
-        base_url = options.get('base_url')
         try:
             data = json.loads(bytestring.decode('utf-8'))
         except ValueError as exc:
-            raise exceptions.DataParseError("Malformed JSON: {}".format(exc))
+            raise exceptions.ParseError("Malformed JSON: {}".format(exc))
 
         try:
             return create_spec(data)
         except Exception as error:
-            raise exceptions.DataParseError(error)
+            raise exceptions.ParseError(error)
