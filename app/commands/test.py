2fc27653e49f608a2ed7203ad64ede2fba365d4c
Adrian Webb <adrian@webb.sh>
2022-01-02 16:05:53 -0500

    Updating the implementation of the test command.

--- app/commands/test.py
+++ app/commands/test.py
@@ -4,8 +4,6 @@ from systems.commands.index import Command
 class Test(Command('test')):
 
     def exec(self):
-        def run_profile(module):
-            module.provider.run_profile('test', ignore_missing = True)
-
         self.info("Running module tests...")
-        self.run_list(self.get_instances(self._module), run_profile)
+        for module in self.get_instances(self._module):
+            module.provider.run_profile('test', ignore_missing = True)
