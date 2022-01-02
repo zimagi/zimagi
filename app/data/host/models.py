98c5c37fd6c74a8c050330da0899c769c7fe2170
Adrian Webb <adrian@webb.sh>
2022-01-02 18:51:09 -0500

    Adding a Host model class override with api, command_api, and data_api methods.

--- app/data/host/models.py
+++ app/data/host/models.py
@@ -1,4 +1,6 @@
-from systems.models.index import ModelFacade
+from systems.models.index import Model, ModelFacade
+
+import zimagi
 
 
 class HostFacade(ModelFacade('host')):
@@ -12,3 +14,39 @@ class HostFacade(ModelFacade('host')):
         if value and short:
             return super().get_field_encryption_key_display(instance, value[:10] + '...', short)
         return super().get_field_encryption_key_display(instance, value, short)
+
+
+class Host(Model('host')):
+
+    def api(self, options_callback = None, message_callback = None):
+        return zimagi.Client(
+            user = self.user,
+            token = self.token,
+            encryption_key = self.encryption_key,
+            host = self.host,
+            command_port = self.command_port,
+            data_port = self.data_port,
+            options_callback = options_callback,
+            message_callback = message_callback
+        )
+
+    def command_api(self, options_callback = None, message_callback = None):
+        return zimagi.command.Client(
+            user = self.user,
+            token = self.token,
+            encryption_key = self.encryption_key,
+            host = self.host,
+            port = self.command_port,
+            options_callback = options_callback,
+            message_callback = message_callback
+        )
+
+    def data_api(self, options_callback = None):
+        return zimagi.data.Client(
+            user = self.user,
+            token = self.token,
+            encryption_key = self.encryption_key,
+            host = self.host,
+            port = self.data_port,
+            options_callback = options_callback
+        )
