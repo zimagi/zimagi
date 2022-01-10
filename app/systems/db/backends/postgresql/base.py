443a20ab88523ca55f77ba4dc5254d1c5c45d5f8
Adrian Webb <adrian@webb.sh>
2022-01-09 22:21:28 -0500

    Adding process locking (semaphore) in the get_new_connection method in the PostgreSQL database wrapper class.

--- app/systems/db/backends/postgresql/base.py
+++ app/systems/db/backends/postgresql/base.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import DEFAULT_DB_ALIAS
 from django.db.backends.postgresql import base as postgresql
 
@@ -6,3 +7,7 @@ class DatabaseWrapper(postgresql.DatabaseWrapper):
 
     def __init__(self, settings_dict, alias = DEFAULT_DB_ALIAS):
         super().__init__(settings_dict, alias)
+
+    def get_new_connection(self, conn_params):
+        with settings.DB_LOCK:
+            return super().get_new_connection(conn_params)
