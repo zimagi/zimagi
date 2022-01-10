84bb9cc8ebebcb834bb3763f2c85bcc58ebac2be
Adrian Webb <adrian@webb.sh>
2022-01-09 22:59:04 -0500

    Adding value normalization and interpolation of condition and outcomes in the conditional_value parser plugin provider.

--- app/plugins/parser/conditional_value.py
+++ app/plugins/parser/conditional_value.py
@@ -1,4 +1,5 @@
 from systems.plugins.index import BaseProvider
+from utility.data import normalize_value
 
 import re
 
@@ -14,9 +15,23 @@ class Provider(BaseProvider('parser', 'conditional_value')):
 
         conditional_match = re.search(self.conditional_pattern, value)
         if conditional_match:
-            if eval(conditional_match.group(1).strip()):
-                value = eval(conditional_match.group(2).strip())
+            try:
+                test_element = normalize_value(self.command.options.interpolate(conditional_match.group(1).strip(), **config.export()))
+                test = eval(test_element) if isinstance(test_element, str) else test_element
+            except NameError:
+                test = test_element
+
+            if test:
+                try:
+                    true_value = normalize_value(self.command.options.interpolate(conditional_match.group(2).strip(), **config.export()))
+                    value = eval(true_value) if isinstance(true_value, str) else true_value
+                except NameError:
+                    value = true_value
             else:
-                value = eval(conditional_match.group(3).strip())
+                try:
+                    false_value = normalize_value(self.command.options.interpolate(conditional_match.group(3).strip(), **config.export()))
+                    value = eval(false_value) if isinstance(false_value, str) else false_value
+                except NameError:
+                    value = false_value
 
         return value
