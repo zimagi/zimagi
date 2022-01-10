32c8472784f0abca8a59c3b7f6517d826de008b8
Adrian Webb <adrian@webb.sh>
2022-01-09 22:28:53 -0500

    Removing thread locking and updating file handling in the help system command library.

--- app/systems/commands/help.py
+++ app/systems/commands/help.py
@@ -1,16 +1,16 @@
 from django.conf import settings
 
+from utility.filesystem import load_yaml
 from utility.data import deep_merge
 
-import threading
 import os
-import yaml
 import re
 
 
 class CommandDescriptions(object):
 
     _instance = None
+
 
     def __new__(cls, *args, **kwargs):
         if not cls._instance:
@@ -19,7 +19,6 @@ class CommandDescriptions(object):
 
     def __init__(self):
         if not getattr(self, '_initialized', False):
-            self.thread_lock = threading.Lock()
             self.load()
             self._initialized = True
 
@@ -32,29 +31,26 @@ class CommandDescriptions(object):
                 path = os.path.join(help_path, name)
                 if os.path.isfile(path):
                     if path.endswith('.yml'):
-                        with open(path, 'r') as file:
-                            file_data = yaml.safe_load(file.read())
-                            data = deep_merge(data, file_data)
+                        file_data = load_yaml(path)
+                        data = deep_merge(data, file_data)
                 else:
                     load_inner(data, path)
 
-        with self.thread_lock:
-            for help_dir in settings.MANAGER.index.help_search_path():
-                load_inner(self.descriptions, help_dir)
+        for help_dir in settings.MANAGER.index.help_search_path():
+            load_inner(self.descriptions, help_dir)
 
     def get(self, full_name, overview = True):
-        with self.thread_lock:
-            components = re.split(r'\s+', full_name)
-            component_length = len(components)
-            scope = self.descriptions
+        components = re.split(r'\s+', full_name)
+        component_length = len(components)
+        scope = self.descriptions
 
-            for index, component in enumerate(components):
-                if component in scope:
-                    if index + 1 == component_length:  # last component
-                        text = scope[component].get('overview', ' ')
-                        if not overview:
-                            text += '\n' + scope[component].get('description', ' ')
-                        return text
-                    else:
-                        scope = scope[component]
+        for index, component in enumerate(components):
+            if component in scope:
+                if index + 1 == component_length:  # last component
+                    text = scope[component].get('overview', ' ')
+                    if not overview:
+                        text += '\n' + scope[component].get('description', ' ')
+                    return text
+                else:
+                    scope = scope[component]
         return ' '
