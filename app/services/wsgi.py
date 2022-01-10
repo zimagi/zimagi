298d7311b04d6a5a858cf8e138629aa2d5b46358
Adrian Webb <adrian@webb.sh>
2022-01-10 04:46:40 -0500

    Updating the Django version in guide links in various files.

--- app/services/wsgi.py
+++ app/services/wsgi.py
@@ -4,7 +4,7 @@ Zimagi WSGI config.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 from django.core.wsgi import get_wsgi_application
 import os
