611aec9d7e28b595c535265976bb0fa0b0dca1f2
Adrian Webb <adrian@webb.sh>
2022-01-11 23:54:28 -0500

    Adding remove_volumes, remove_image, and remove_network parameters in the destroy service command.

--- app/commands/service/destroy.py
+++ app/commands/service/destroy.py
@@ -8,7 +8,12 @@ class Destroy(Command('service.destroy')):
         service_names = self.service_names if self.service_names else self.manager.service_names
 
         def destroy_service(service_name):
-            self.manager.stop_service(service_name, remove = True)
+            self.manager.stop_service(service_name,
+                remove = True,
+                remove_volumes = self.remove_volumes,
+                remove_image = self.remove_image,
+                remove_network = not self.keep_network
+            )
             self.success("Successfully destroyed service: {}".format(service_name))
 
         self.run_list(service_names, destroy_service)
