c2d35b10558d973ac95cdc9f07036bdf6f0614c3
Adrian Webb <adrian@webb.sh>
2022-01-16 18:51:45 -0500

    Adding a process mutex lock in the log command mixin.

--- app/commands/mixins/log.py
+++ app/commands/mixins/log.py
@@ -1,27 +1,34 @@
 from systems.commands.index import CommandMixin
 
+import multiprocessing
+
 
 class LogMixin(CommandMixin('log')):
+
+    log_lock = multiprocessing.Lock()
+
 
     def log_init(self, options, task = None, log_key = None):
         if not options:
             options = {}
 
-        if log_key is None:
-            self.log_entry = self._log.create(None,
-                command = self.get_full_name()
-            )
-        else:
-            self.log_entry = self._log.retrieve(log_key)
+        with self.log_lock:
+            if log_key is None:
+                self.log_entry = self._log.create(None,
+                    command = self.get_full_name()
+                )
+            else:
+                self.log_entry = self._log.retrieve(log_key)
 
-        self.log_entry.user = self.active_user
-        self.log_entry.config = options
-        self.log_entry.status = None
-        if task:
-            self.log_entry.scheduled = True
-            self.log_entry.task_id = task.request.id
+            self.log_entry.user = self.active_user
+            self.log_entry.config = options
+            self.log_entry.status = None
+            if task:
+                self.log_entry.scheduled = True
+                self.log_entry.task_id = task.request.id
 
-        self.log_entry.save()
+            self.log_entry.save()
+
         return self.log_entry.name
 
 
@@ -33,9 +40,11 @@ class LogMixin(CommandMixin('log')):
             if command.exec_parent:
                 _create_log_message(command.exec_parent, data)
 
-        _create_log_message(self, data)
+        with self.log_lock:
+            _create_log_message(self, data)
 
 
     def log_status(self, status):
-        self.log_entry.set_status(status)
-        self.log_entry.save()
+        with self.log_lock:
+            self.log_entry.set_status(status)
+            self.log_entry.save()
