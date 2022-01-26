c39fae5f18ff13e6058c39629b58a6a5bcc0a0c1
Adrian Webb <adrian@webb.sh>
2022-01-26 01:47:18 -0500

    Updating the priority, adding better queue handling, including wait log keys, and returning log key in the run command profile component.

--- app/components/run.py
+++ app/components/run.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+
 from systems.commands import profile
 from utility.data import get_dict_combinations
 
@@ -5,14 +7,11 @@ from utility.data import get_dict_combinations
 class ProfileComponent(profile.BaseProfileComponent):
 
     def priority(self):
-        return 100
+        return 50
 
     def ensure_module_config(self):
         return True
 
-
-    def skip_destroy(self):
-        return True
 
     def run(self, name, config):
         scopes = self.pop_value('_scopes', config)
@@ -20,6 +19,11 @@ class ProfileComponent(profile.BaseProfileComponent):
         task = self.pop_value('_task', config)
         command = self.pop_value('_command', config)
         host = self.pop_value('_host', config)
+        queue = self.pop_value('_queue', config) if '_queue' in config else settings.QUEUE_COMMANDS
+        log_keys = []
+
+        if not settings.QUEUE_COMMANDS:
+            queue = False
 
         if not task and not command and not '_config' in config:
             self.command.error("Run {} requires '_task', '_command', or '_config' field".format(name))
@@ -28,8 +32,10 @@ class ProfileComponent(profile.BaseProfileComponent):
             if command:
                 if host:
                     data['environment_host'] = host
+                if queue:
+                    data['push_queue'] = queue
 
-                self.exec(command, **data)
+                log_keys.append(self.exec(command, **data))
             elif task:
                 options = {
                     'module_name': module,
@@ -38,15 +44,20 @@ class ProfileComponent(profile.BaseProfileComponent):
                 }
                 if host:
                     options['environment_host'] = host
+                if queue:
+                    options['push_queue'] = queue
 
-                self.exec('task', **options)
+                log_keys.append(self.exec('task', **options))
             else:
                 self.profile.config.set(
                     data.get('_name', name),
                     data.get('_config', None)
                 )
+
         if scopes:
             for scope in get_dict_combinations(scopes):
                 _execute(self.interpolate(config, **scope))
         else:
             _execute(config)
+
+        return log_keys if queue else []
