4771d36bcffdc702fef34606740746f5ef2a3a36
Adrian Webb <adrian@webb.sh>
2022-01-26 01:34:14 -0500

    Updating the thread log, message saving, and log status save and reporting in the log command mixin.

--- app/commands/mixins/log.py
+++ app/commands/mixins/log.py
@@ -1,11 +1,11 @@
 from systems.commands.index import CommandMixin
 
-import multiprocessing
+import threading
 
 
 class LogMixin(CommandMixin('log')):
 
-    log_lock = multiprocessing.Lock()
+    log_lock = threading.Lock()
 
 
     def log_init(self, options, task = None, log_key = None):
@@ -22,7 +22,7 @@ class LogMixin(CommandMixin('log')):
 
             self.log_entry.user = self.active_user
             self.log_entry.config = options
-            self.log_entry.status = None
+            self.log_entry.status = self._log.model.STATUS_RUNNING
             if task:
                 self.log_entry.scheduled = True
                 self.log_entry.task_id = task.request.id
@@ -32,19 +32,23 @@ class LogMixin(CommandMixin('log')):
         return self.log_entry.name
 
 
-    def log_message(self, data):
-        def _create_log_message(command, data):
-            if getattr(command, 'log_entry', None):
+    def log_message(self, data, log = True):
+        def _create_log_message(command, data, _log):
+            if getattr(command, 'log_entry', None) and _log:
                 command.log_entry.messages.create(data = data)
 
             if command.exec_parent:
-                _create_log_message(command.exec_parent, data)
+                _create_log_message(command.exec_parent, data, True)
 
         with self.log_lock:
-            _create_log_message(self, data)
+            _create_log_message(self, data, log)
 
 
-    def log_status(self, status):
-        with self.log_lock:
-            self.log_entry.set_status(status)
-            self.log_entry.save()
+    def log_status(self, status, check_log_result = False):
+        if not check_log_result or self.log_result:
+            with self.log_lock:
+                self.log_entry.set_status(status)
+                self.log_entry.save()
+
+    def get_status(self):
+        return self.log_entry.status
