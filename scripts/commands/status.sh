4c09a8f14c6b91e0099a2ff66a7f0b7a68de680c
Adrian Webb <adrian@webb.sh>
2022-01-28 16:42:29 -0500

    Adding an initial status command script library.

---
+++ scripts/commands/status.sh
@@ -0,0 +1,38 @@
+#
+#=========================================================================================
+# <Status> Command
+#
+
+function status_usage () {
+    cat <<EOF >&2
+
+Check status of nodes and pods in the Zimagi development environment.
+
+Usage:
+
+  reactor status [flags] [options]
+
+Flags:
+${__core_help_flags}
+
+EOF
+  exit 1
+}
+function status () {
+  while [[ $# -gt 0 ]]; do
+    case "$1" in
+      -h|--help)
+      status_usage
+      ;;
+      *)
+      error "Unknown argument: ${1}"
+      status_usage
+      ;;
+    esac
+    shift
+  done
+
+  debug "Command: status"
+
+  zimagi_status
+}
