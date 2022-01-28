cf04e6fcfd9a6e411e68a18365532f75d4e6a505
Adrian Webb <adrian@webb.sh>
2022-01-28 16:43:29 -0500

    Adding an initial reload command script library.

---
+++ scripts/commands/reload.sh
@@ -0,0 +1,38 @@
+#
+#=========================================================================================
+# <Reload> Command
+#
+
+function reload_usage () {
+    cat <<EOF >&2
+
+Reload Zimagi development environment services.
+
+Usage:
+
+  reactor reload [flags] [options]
+
+Flags:
+${__core_help_flags}
+
+EOF
+  exit 1
+}
+function reload () {
+  while [[ $# -gt 0 ]]; do
+    case "$1" in
+      -h|--help)
+      reload_usage
+      ;;
+      *)
+      error "Unknown argument: ${1}"
+      reload_usage
+      ;;
+    esac
+    shift
+  done
+
+  debug "Command: reload"
+
+  reload_zimagi
+}
