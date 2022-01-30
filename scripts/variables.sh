c8a12fcffa0514a674a1dfadf82679fcc1261c54
Adrian Webb <adrian@webb.sh>
2022-01-30 11:41:59 -0500

    Adding zimagi prefixes to all variables and standardizing on commenting string values in export assignment in the variables script.

--- scripts/variables.sh
+++ scripts/variables.sh
@@ -4,25 +4,26 @@
 #
 
 # Set magic variables for current file, directory, os, etc.
-export __skaffold_dir="${__dir}/.skaffold"
-export __binary_dir="${__skaffold_dir}/bin"
-export __docker_dir="${__dir}/docker"
-export __build_dir="${__dir}/build"
-export __charts_dir="${__dir}/charts"
-export __certs_dir="${__dir}/certs"
+export __zimagi_file="${__zimagi_dir}/${__zimagi_base}"
+export __zimagi_skaffold_dir="${__zimagi_dir}/.skaffold"
+export __zimagi_binary_dir="${__zimagi_skaffold_dir}/bin"
+export __zimagi_docker_dir="${__zimagi_dir}/docker"
+export __zimagi_build_dir="${__zimagi_dir}/build"
+export __zimagi_charts_dir="${__zimagi_dir}/charts"
+export __zimagi_certs_dir="${__zimagi_dir}/certs"
 
-export __app_dir="${__dir}/app"
-export __package_dir="${__dir}/package"
-export __data_dir="${__dir}/data"
-export __lib_dir="${__dir}/lib"
+export __zimagi_app_dir="${__zimagi_dir}/app"
+export __zimagi_package_dir="${__zimagi_dir}/package"
+export __zimagi_data_dir="${__zimagi_dir}/data"
+export __zimagi_lib_dir="${__zimagi_dir}/lib"
 
-export __env_file="${__dir}/.env"
-export __docker_env_file="${__dir}/.docker"
-export __cli_env_file="${__data_dir}/zimagi.env"
+export __zimagi_app_env_file="${__zimagi_dir}/.app.env"
+export __zimagi_runtime_env_file="${__zimagi_dir}/.runtime.env"
+export __zimagi_cli_env_file="${__zimagi_data_dir}/zimagi.env"
 
 # shellcheck disable=SC2034,SC2015
-export __invocation="$(printf %q "${__file}")$( (($#)) && printf ' %q' "$@" || true)"
-export __core_help_flags="
+export __zimagi_reactor_invocation="$(printf %q "${__zimagi_file}")$( (($#)) && printf ' %q' "$@" || true)"
+export __zimagi_reactor_core_flags="
     -v --verbose                      Enable verbose mode, print script as it is executed
     -d --debug                        Enables debug mode
     -n --no-color                     Disable color output
@@ -32,7 +33,7 @@ export __core_help_flags="
 export LOG_LEVEL="${LOG_LEVEL:-6}" # 7 = debug -> 0 = emergency
 export NO_COLOR="${NO_COLOR:-}"    # true = disable color. otherwise autodetected
 
-export DEFAULT_APP_NAME=zimagi
-export DEFAULT_DOCKER_RUNTIME=standard
-export DEFAULT_DOCKER_TAG=dev
-export DEFAULT_DATA_KEY="0123456789876543210"
+export DEFAULT_APP_NAME="zimagi"
+export DEFAULT_DOCKER_RUNTIME="standard"
+export DEFAULT_DOCKER_TAG="dev"
+export DEFAULT_DATA_KEY="b12e75f78n876543210H36j250162731"
