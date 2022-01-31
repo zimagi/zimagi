8eaa63566bb3e59aabd9d88fc40ba2068b5659f2
Adrian Webb <adrian@webb.sh>
2022-01-30 22:21:01 -0500

    Adding an initial session vm script.

---
+++ scripts/vm/session.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#
+#=========================================================================================
+# Initialize Bash session
+#
+
+cat > /etc/profile.d/zimagi.sh <<EOF
+cd /project
+
+source /project/reactor path
+
+export ZIMAGI_HOST_APP_DIR="/project/app"
+export ZIMAGI_HOST_DATA_DIR="/project/data"
+export ZIMAGI_HOST_LIB_DIR="/project/lib"
+EOF
