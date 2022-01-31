edd2908f34a0fe08b1afd11dcd9d5777198358aa
Adrian Webb <adrian@webb.sh>
2022-01-30 20:33:23 -0500

    Adding an initial Helm utility script with a install_helm function.

---
+++ scripts/utilities/helm.sh
@@ -0,0 +1,17 @@
+#
+#=========================================================================================
+# Helm Utilities
+#
+
+function install_helm () {
+  info "Installing Helm ..."
+
+  info "Downloading Helm installer ..."
+  curl -o /tmp/helm_install.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get 2>/dev/null
+  chmod 700 /tmp/helm_install.sh
+
+  info "Running Helm installer ..."
+  export HELM_INSTALL_DIR="${__zimagi_binary_dir}"
+  /tmp/helm_install.sh -v "v${HELM_VERSION}" --no-sudo >/dev/null 2>&1
+  rm -f /tmp/helm_install.sh
+}
