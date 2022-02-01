9d0248cfdcc54e4a8c4e4cf9727f53ce77374a9d
Adrian Webb <adrian@webb.sh>
2022-02-01 15:10:10 -0500

    Updating the header comment and adding some more options and flags to the test command script.

--- scripts/commands/test.sh
+++ scripts/commands/test.sh
@@ -10,20 +10,22 @@ Initialize and run Zimagi tests.
 
 Usage:
 
-  reactor test [<script:command>] [flags] [options]
+  reactor test [<script:str:${DEFAULT_TEST_SCRIPT_NAME}>] [flags] [options]
 
 Flags:
 ${__zimagi_reactor_core_flags}
 
-    -i --init                         Initialize the development environment before testing
-    -b --skip-build                   Skip Docker image build step (only applicable with --init option)
+    -i --init             Initialize the development environment before testing
+    -b --skip-build       Skip Docker image build step (requires --init)
+    -n --no-cache         Regenerate all intermediate images (requires --init)
 
 Options:
 
-    --name <zimagi>                   Application name or unique namespace on local machine (only applicable with --init option)
-    --runtime <standard>              Zimagi Docker runtime (only applicable with --init option)
-    --tag <dev>                       Zimagi Docker tag (only applicable with --init option)
-    --data-key <0123456789876543210>  Zimagi data encryption key built into Docker image (only applicable with --init option)
+    --name <str>          Application name (requires --init): ${DEFAULT_APP_NAME}
+    --runtime <str>       Zimagi Docker runtime (requires --init): ${DEFAULT_DOCKER_RUNTIME}
+    --tag <str>           Zimagi Docker tag (requires --init): ${DEFAULT_DOCKER_TAG}
+    --password <str>      Zimagi Docker user password (requires --init): ${DEFAULT_USER_PASSWORD}
+    --data-key <str>      Zimagi data encryption key (requires --init): ${DEFAULT_DATA_KEY}
 
 EOF
   exit 1
@@ -54,6 +56,13 @@ function test_command () {
       DOCKER_TAG="$2"
       shift
       ;;
+      --password=*)
+      USER_PASSWORD="${1#*=}"
+      ;;
+      --password)
+      USER_PASSWORD="$2"
+      shift
+      ;;
       --data-key=*)
       DATA_KEY="${1#*=}"
       ;;
@@ -66,6 +75,9 @@ function test_command () {
       ;;
       -b|--skip-build)
       SKIP_BUILD=1
+      ;;
+      -n|--no-cache)
+      NO_CACHE=1
       ;;
       -h|--help)
       test_usage
@@ -84,13 +96,18 @@ function test_command () {
   APP_NAME="${APP_NAME:-$DEFAULT_APP_NAME}"
   DOCKER_RUNTIME="${DOCKER_RUNTIME:-$DEFAULT_DOCKER_RUNTIME}"
   DOCKER_TAG="${DOCKER_TAG:-$DEFAULT_DOCKER_TAG}"
+  USER_PASSWORD="${USER_PASSWORD:-$DEFAULT_USER_PASSWORD}"
   DATA_KEY="${DATA_KEY:-$DEFAULT_DATA_KEY}"
   INITIALIZE=${INITIALIZE:-0}
   SKIP_BUILD=${SKIP_BUILD:-0}
+  NO_CACHE=${NO_CACHE:-0}
 
-  INIT_ARGS=("$APP_NAME" "--runtime=$DOCKER_RUNTIME" "--tag=$DOCKER_TAG" "--data-key=$DATA_KEY")
+  INIT_ARGS=("$APP_NAME" "--runtime=$DOCKER_RUNTIME" "--tag=$DOCKER_TAG" "--password=$USER_PASSWORD" "--data-key=$DATA_KEY")
   if [ $SKIP_BUILD -ne 0 ]; then
     INIT_ARGS=("${INIT_ARGS[@]}" "--skip-build")
+  fi
+  if [ $NO_CACHE -ne 0 ]; then
+    INIT_ARGS=("${INIT_ARGS[@]}" "--no-cache")
   fi
 
   debug "Command: test"
@@ -98,8 +115,10 @@ function test_command () {
   debug "> APP_NAME: ${APP_NAME}"
   debug "> DOCKER_RUNTIME: ${DOCKER_RUNTIME}"
   debug "> DOCKER_TAG: ${DOCKER_TAG}"
+  debug "> USER_PASSWORD: ${USER_PASSWORD}"
   debug "> DATA_KEY: ${DATA_KEY}"
   debug "> SKIP_BUILD: ${SKIP_BUILD}"
+  debug "> NO_CACHE: ${NO_CACHE}"
   debug "> INITIALIZE: ${INITIALIZE}"
   debug "> INIT ARGS: ${INIT_ARGS[@]}"
 
