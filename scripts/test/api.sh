11cc5d09376a5765e7ffb4a6b6581ee5406d25d4
Adrian Webb <adrian@webb.sh>
2022-02-01 15:23:10 -0500

    Adding simple header comments to the test scripts.

--- scripts/test/api.sh
+++ scripts/test/api.sh
@@ -1,4 +1,6 @@
-
+#
+# Run API tests
+#
 echo "Setting Zimagi ${DOCKER_RUNTIME} host"
 "${__zimagi_script_dir}"/zimagi host save default host=localhost encryption_key="$ZIMAGI_TEST_KEY"
 "${__zimagi_script_dir}"/zimagi env get
--- scripts/test/command.sh
+++ scripts/test/command.sh
@@ -1,4 +1,6 @@
-
+#
+# Run Command Orchestration tests
+#
 echo "Running Zimagi ${DOCKER_RUNTIME} command tests"
 "${__zimagi_script_dir}"/zimagi user list
 "${__zimagi_script_dir}"/zimagi test --types=command
