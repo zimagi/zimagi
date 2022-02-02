4b0ec230ce10707b32560c0372b832727d08e411
Adrian Webb <adrian@webb.sh>
2022-02-02 01:10:30 -0500

    Updating the formatting of the celery-flower Docker entrypoint script.

--- app/scripts/celery-flower.sh
+++ app/scripts/celery-flower.sh
@@ -4,12 +4,10 @@ set -e
 cd /usr/local/share/zimagi
 #-------------------------------------------------------------------------------
 
-if [ ! -z "$ZIMAGI_POSTGRES_HOST" -a ! -z "$ZIMAGI_POSTGRES_PORT" ]
-then
+if [ ! -z "$ZIMAGI_POSTGRES_HOST" -a ! -z "$ZIMAGI_POSTGRES_PORT" ]; then
   ./scripts/wait.sh --hosts="$ZIMAGI_POSTGRES_HOST" --port="$ZIMAGI_POSTGRES_PORT" --timeout=60
 fi
-if [ ! -z "$ZIMAGI_REDIS_HOST" -a ! -z "$ZIMAGI_REDIS_PORT" ]
-then
+if [ ! -z "$ZIMAGI_REDIS_HOST" -a ! -z "$ZIMAGI_REDIS_PORT" ]; then
   ./scripts/wait.sh --hosts="$ZIMAGI_REDIS_HOST" --port="$ZIMAGI_REDIS_PORT" --timeout=60
 fi
 
