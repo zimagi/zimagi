4331707a49e61ef69e15e618bac054f989df85d0
Adrian Webb <adrian@webb.sh>
2022-02-05 14:52:59 -0500

    Adding an initial worker configuration entrypoint script.

---
+++ app/scripts/config/worker.sh
@@ -0,0 +1,7 @@
+export ZIMAGI_SERVICE_PROCESS=(
+  "celery"
+  "--app=settings"
+  "worker"
+  "--loglevel=${ZIMAGI_LOG_LEVEL:-warning}"
+  "--autoscale=${ZIMAGI_WORKER_MAX_PROCESSES:-10},${ZIMAGI_WORKER_MIN_PROCESSES:-1}"
+)
