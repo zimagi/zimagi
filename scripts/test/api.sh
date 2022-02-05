fa697ff0a73e2e4ca4c52906d958afc415a36567
Adrian Webb <adrian@webb.sh>
2022-02-05 14:49:01 -0500

    Replacing the test key with the default admin API encryption key in the API test script.

--- scripts/test/api.sh
+++ scripts/test/api.sh
@@ -2,7 +2,7 @@
 # Run API tests
 #
 echo "Setting Zimagi ${DOCKER_RUNTIME} host"
-"${__zimagi_script_dir}"/zimagi host save default host=localhost encryption_key="$ZIMAGI_TEST_KEY"
+"${__zimagi_script_dir}"/zimagi host save default host=localhost encryption_key="$ZIMAGI_ADMIN_API_KEY"
 "${__zimagi_script_dir}"/zimagi env get
 
 echo "Running Zimagi ${DOCKER_RUNTIME} API tests"
