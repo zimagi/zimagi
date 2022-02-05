6392c491e884d43cc6d8fd91b5ee9fe8311dcba4
Adrian Webb <adrian@webb.sh>
2022-02-05 14:42:04 -0500

    Adding a reinit parameter to the ensure_resources call in the module init command implementation.

--- app/commands/module/init.py
+++ app/commands/module/init.py
@@ -10,7 +10,7 @@ class Init(Command('module.init')):
         def init_modules():
             initialized = self.get_state('startup_initialized')
             if self.reset or not initialized:
-                self.ensure_resources()
+                self.ensure_resources(reinit = True)
             self.set_state('startup_initialized', True)
 
         self.run_exclusive('module_init', init_modules, timeout = self.timeout)
