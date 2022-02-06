3da2ff274736d304357d07726c281f8e6a862fab
Adrian Webb <adrian@webb.sh>
2022-02-05 19:23:53 -0500

    Adding an exit trap and updating section echo statements in the gateway entrypoint script.

--- app/scripts/gateway.sh
+++ app/scripts/gateway.sh
@@ -5,6 +5,7 @@ cd /usr/local/share/zimagi
 
 SERVICE_TYPE="$1"
 
+
 if [[ -z "$SERVICE_TYPE" ]]; then
   echo "Service gateway requires a process type identifier"
   exit 1
@@ -18,15 +19,22 @@ export "ZIMAGI_${SERVICE_TYPE^^}_INIT"=True
 export ZIMAGI_NO_MIGRATE=True
 #-------------------------------------------------------------------------------
 
-trap 'kill --signal TERM "${PROCESS_PID}"; wait "${PROCESS_PID}"; cleanup' SIGTERM
+trap 'if [ $? -ne 0 ]; then kill -s TERM "$PPID"; cleanup; fi' EXIT
+trap 'kill -s TERM "${PROCESS_PID}"; wait "${PROCESS_PID}"; cleanup' SIGTERM
 
 function cleanup () {
-  echo "Service shut down: cleaning up"
+  echo ""
+  echo "================================================================================"
+  echo "> Service shut down: cleaning up"
+  echo ""
   rm -f "/var/local/zimagi/${SERVICE_TYPE}.pid"
 }
 
 #-------------------------------------------------------------------------------
-
+echo ""
+echo "================================================================================"
+echo "> Verifying data service connectivity"
+echo ""
 if [[ ! -z "$ZIMAGI_POSTGRES_HOST" ]] && [[ ! -z "$ZIMAGI_POSTGRES_PORT" ]]; then
   ./scripts/wait.sh --hosts="$ZIMAGI_POSTGRES_HOST" --port=$ZIMAGI_POSTGRES_PORT --timeout=60
 fi
@@ -34,15 +42,23 @@ if [[ ! -z "$ZIMAGI_REDIS_HOST" ]] && [[ ! -z "$ZIMAGI_REDIS_PORT" ]]; then
   ./scripts/wait.sh --hosts="$ZIMAGI_REDIS_HOST" --port=$ZIMAGI_REDIS_PORT --timeout=60
 fi
 
+echo ""
+echo "================================================================================"
 echo "> Initializing service runtime"
+echo ""
 zimagi migrate
-zimagi module init --reset --verbosity=3 --timeout=${ZIMAGI_INIT_TIMEOUT:-600}
+echo ""
+zimagi module init
 
 if [[ ! -z "$ZIMAGI_ADMIN_API_KEY" ]]; then
+  echo ""
   zimagi user save admin encryption_key="$ZIMAGI_ADMIN_API_KEY" --lock=admin_key_save --lock-timeout=0
 fi
 
+echo ""
+echo "================================================================================"
 echo "> Fetching command environment information"
+echo ""
 zimagi env get
 
 if [[ ! -z "${ZIMAGI_SERVICE_PROCESS[@]}" ]]; then
@@ -52,7 +68,10 @@ if [[ ! -z "${ZIMAGI_SERVICE_PROCESS[@]}" ]]; then
   export ZIMAGI_SERVICE_EXEC=True
   export ZIMAGI_BOOTSTRAP_DJANGO=True
 
+  echo ""
+  echo "================================================================================"
   echo "> Starting ${SERVICE_TYPE} service"
+  echo ""
   "${ZIMAGI_SERVICE_PROCESS[@]}" &
 
   PROCESS_PID="$!"
