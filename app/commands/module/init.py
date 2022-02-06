63a1e71f8e3297eb8bb34aa60bdd7a3095da9afa
Adrian Webb <adrian@webb.sh>
2022-02-05 19:23:01 -0500

    Eliminating the run_exclusive and the startup_initialized state modification in the module init command.

--- app/commands/module/init.py
+++ app/commands/module/init.py
@@ -7,10 +7,4 @@ class Init(Command('module.init')):
         return False
 
     def exec(self):
-        def init_modules():
-            initialized = self.get_state('startup_initialized')
-            if self.reset or not initialized:
-                self.ensure_resources(reinit = True)
-            self.set_state('startup_initialized', True)
-
-        self.run_exclusive('module_init', init_modules, timeout = self.timeout)
+        self.ensure_resources(reinit = True)
