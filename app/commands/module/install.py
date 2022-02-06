09623b158e5627d8777d87d700016d4203f71b6d
Adrian Webb <adrian@webb.sh>
2022-02-05 19:22:15 -0500

    Only removing old runtime images when CLI execution and Docker handle in the module install command implementation.

--- app/commands/module/install.py
+++ app/commands/module/install.py
@@ -7,11 +7,6 @@ class Install(Command('module.install')):
 
     def exec(self):
         self.info("Installing module requirements...")
-
-        old_runtime_image = self.delete_state('old_runtime_image')
-        if old_runtime_image:
-            self.manager.delete_image(old_runtime_image)
-
         self.manager.install_scripts(self, self.verbosity == 3)
         self.manager.install_requirements(self, self.verbosity == 3)
 
@@ -20,9 +15,12 @@ class Install(Command('module.install')):
             cid = self.manager.container_id
             image = self.manager.generate_image_name(env.base_image)
 
+            old_runtime_image = self.delete_state('old_runtime_image')
+            if old_runtime_image:
+                self.manager.delete_image(old_runtime_image)
+
             self.manager.create_image(cid, image)
             self.save_env(
                 runtime_image = image
             )
-
         self.success("Successfully installed module requirements")
