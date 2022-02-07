eacecf9cb6b35552ca563c6d28538ec183bff1b6
Adrian Webb <adrian@webb.sh>
2022-02-07 11:33:58 -0500

    Adding command_api and data_api connection accessors in the base test client library.

--- app/tests/client/base.py
+++ app/tests/client/base.py
@@ -13,7 +13,10 @@ class BaseTest(object):
     def __init__(self, command, host):
         self.command = command
         self.host = host
+
         self.api = host.api(message_callback = self.message_callback)
+        self.command_api = host.command_api(message_callback = self.message_callback)
+        self.data_api = host.data_api()
 
     def message_callback(self, message):
         message.display()
