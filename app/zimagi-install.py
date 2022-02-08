5c1f60fca94d58b0cd881387ee6b9b7606208bef
Adrian Webb <adrian@webb.sh>
2022-02-07 21:16:26 -0500

    Updating the zimagi-install gateway library to reference the install settings configuration.

--- app/zimagi-install.py
+++ app/zimagi-install.py
@@ -3,5 +3,5 @@ import os
 
 if __name__ == "__main__":
     from systems.commands import cli
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.core")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.install")
     cli.install()
