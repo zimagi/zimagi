d4694056f27d52629e6c1261d6bf475d51d01654
Adrian Webb <adrian@webb.sh>
2022-02-08 18:01:28 -0500

    Updating the Concat SQL aggregator model implementation in the system model aggregates library.

--- app/systems/models/aggregates.py
+++ app/systems/models/aggregates.py
@@ -3,26 +3,12 @@ from django.db.models import Aggregate
 
 class Concat(Aggregate):
 
-    function = 'GROUP_CONCAT'
-    template = "%(function)s(%(distinct)s%(expressions)s)"
+    function = 'STRING_AGG'
+    template = "%(function)s(%(distinct)s%(expressions)s, '%(separator)s')"
     allow_distinct = True
-
 
     def __init__(self, expression, separator = ',', **extra):
         super().__init__(expression,
             separator = separator,
             **extra
         )
-
-
-    def as_postgresql(self, compiler, connection):
-        self.function = 'STRING_AGG'
-        self.template = "%(function)s(%(distinct)s%(expressions)s, '%(separator)s')"
-        return super().as_sql(compiler, connection)
-
-    def as_mysql(self, compiler, connection):
-        self.template = "%(function)s(%(distinct)s%(expressions)s SEPARATOR '%(separator)s')"
-        return super().as_sql(compiler, connection)
-
-    def as_sqlite(self, compiler, connection):
-        return super().as_sql(compiler, connection)
