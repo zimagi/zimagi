2d130c133528c817840d7ebe1d40a070dceaf3af
Adrian Webb <adrian@webb.sh>
2022-02-07 20:34:52 -0500

    Adding a Zimagi install gateway library.

---
+++ app/zimagi-install.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+import os
+
+if __name__ == "__main__":
+    from systems.commands import cli
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.core")
+    cli.install()
