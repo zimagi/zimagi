2d2293ec96a08d632cc6f66f82a34c3a0da2f14f
Adrian Webb <adrian@webb.sh>
2022-02-11 17:55:04 -0500

    Updating the lib watch directory to modules only in the command, data, scheduler, and worker entrypoint scripts.

--- app/scripts/scheduler.sh
+++ app/scripts/scheduler.sh
@@ -4,7 +4,7 @@ if [[ "${ZIMAGI_AUTO_UPDATE^^}" == "TRUE" ]]; then
   echo "> Starting file watcher"
   watchmedo auto-restart \
     --directory=./ \
-    --directory=/usr/local/lib/zimagi \
+    --directory=/usr/local/lib/zimagi/modules \
     --pattern="*.py;*.sh" \
     --recursive \
     --signal SIGTERM \
