0c73e19941da7fe771555f22b6e02950343c2515
Adrian Webb <adrian@webb.sh>
2022-02-12 04:58:49 -0500

    Adding worker queues from an environment variable in the worker entrypoint configuration script.

--- app/scripts/config/worker.sh
+++ app/scripts/config/worker.sh
@@ -1,8 +1,15 @@
 export ZIMAGI_BOOTSTRAP_DJANGO=True
+
+WORKER_QUEUES="default"
+if [ ! -z "$ZIMAGI_WORKER_TYPE" ]; then
+  WORKER_QUEUES="${ZIMAGI_WORKER_TYPE},${WORKER_QUEUES}"
+fi
+
 export ZIMAGI_SERVICE_PROCESS=(
   "celery"
   "--app=settings"
   "worker"
   "--loglevel=${ZIMAGI_LOG_LEVEL:-warning}"
   "--autoscale=${ZIMAGI_WORKER_MAX_PROCESSES:-10},${ZIMAGI_WORKER_MIN_PROCESSES:-1}"
+  "--queues=${WORKER_QUEUES}"
 )
