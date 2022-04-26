db0eb2c19a120e8ac054673f07efd13ef666993a
Adrian Webb <adrian@webb.sh>
2022-04-26 18:20:20 -0400

    Adding an initial roles profile component.

---
+++ app/components/roles.py
@@ -0,0 +1,20 @@
+from systems.commands import profile
+
+
+class ProfileComponent(profile.BaseProfileComponent):
+
+    def priority(self):
+        return 5
+
+    def run(self, name, help):
+        if not help:
+            self.command.error("Role {} help string must be provided".format(name))
+
+        self.exec('template generate',
+            module_name = self.profile.module.instance.name,
+            module_template = 'user/role',
+            template_fields = {
+                'name': name.replace('_', '-'),
+                'help': help.capitalize()
+            }
+        )
