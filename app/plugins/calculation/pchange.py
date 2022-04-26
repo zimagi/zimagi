f8829c5fa72ab454e026969528d21ba65da201a9
Adrian Webb <adrian@webb.sh>
2022-04-26 18:17:42 -0400

    Adding an initial percentage change calculation plugin provider.

---
+++ app/plugins/calculation/pchange.py
@@ -0,0 +1,7 @@
+from systems.plugins.index import BaseProvider
+
+
+class Provider(BaseProvider('calculation', 'pchange')):
+
+    def calc(self, p):
+        return ((p.a - p.b) / p.b) if self.check(p.a, p.b) and p.b != 0 else None
