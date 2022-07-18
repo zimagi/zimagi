87ee60e1971eb0aa8c4545a23d136a8760da55d8
Adrian Webb <adrian@webb.sh>
2022-07-18 03:22:02 -0400

    Adding an initial base system model parser class.

---
+++ app/systems/models/parsers/base.py
@@ -0,0 +1,369 @@
+from types import MethodType
+
+from django.db.models import F
+
+from ..errors import ParseError
+
+import sys
+import operator
+import ply.lex as lex
+import ply.yacc as yacc
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+class BaseParser(object):
+    #
+    # Parser indexes
+    #
+    tokens = [
+        'FUNCTION_NAME',
+        'NAME',
+
+        'BOOL',
+        'INT',
+        'FLOAT',
+        'QUOTED_STRING',
+        'DOUBLE_QUOTED_STRING',
+
+        'ADD',
+        'SUBTRACT',
+        'MULTIPLY',
+        'DIVIDE',
+        'MODULO',
+        'POWER',
+
+        'LPAR',
+        'RPAR',
+        'EQUALS',
+        'SEP'
+    ]
+
+    precedence = (
+        ('left', 'ADD', 'SUBTRACT'),
+        ('left', 'MULTIPLY', 'DIVIDE', 'MODULO'),
+        ('left', 'POWER'),
+        ('left', 'LPAR', 'RPAR')
+    )
+
+    parser_index = {
+        'p_expression_boolean': (),
+        'p_expression_number': (),
+        'p_expression_string': (),
+        'p_expression_field': (),
+        'p_expression_function': (
+            'p_function',
+            'p_function_with_params'
+        ),
+        'p_expression_paren': (),
+        'p_expression_calculation': (),
+
+        'p_function': (),
+        'p_function_with_params': (
+            'p_parameters',
+            'p_parameters_single'
+        ),
+
+        'p_parameters': (
+            'p_parameter_argument_data',
+            'p_parameter_argument_field',
+            'p_parameter_option'
+        ),
+        'p_parameters_single': (
+            'p_parameter_argument_data',
+            'p_parameter_argument_field',
+            'p_parameter_option'
+        ),
+
+        'p_parameter_argument_data': (),
+        'p_parameter_argument_field': (
+            'p_function',
+            'p_function_with_params'
+        ),
+        'p_parameter_option': ()
+    }
+
+    operations = {
+        '+': operator.add,
+        '-': operator.sub,
+        '*': operator.mul,
+        '/': operator.truediv,
+        '%': operator.mod,
+        '^': operator.pow
+    }
+
+    #
+    # Parser initialization
+    #
+    def __init__(self, facade):
+        self.facade  = facade
+
+        self.generate()
+
+        self.lexer   = lex.lex(
+            module   = self,
+            optimize = False,
+            debug    = False
+        )
+        self.parser = yacc.yacc(
+            module       = self,
+            start        = 'statement',
+            optimize     = False,
+            debug        = False,
+            write_tables = False
+        )
+
+    #
+    # Parser evaluation
+    #
+    def generate(self):
+        def attach_parser(parser):
+            if getattr(self, parser, None) is None:
+                setattr(self, parser, MethodType(getattr(sys.modules[__name__], parser), self))
+
+                for dependency in self.parser_index[parser]:
+                    attach_parser(dependency)
+
+        for parser in self.base_parsers:
+            attach_parser(parser)
+
+
+    def evaluate(self, value):
+        logger.info("===== Query filter statement ===== ( {} )".format(value))
+        logger.info("  === Active base parsers === {}".format(self.base_parsers))
+        return self.parser.parse(value)
+
+    def process(self, statement):
+        # Override in subclass if needed
+        return statement
+
+    #
+    # Parser errors
+    #
+    def t_error(self, t):
+        raise ParseError("Illegal characters in query parser: {}".format(t.value[0]))
+
+    def p_error(self, p):
+        raise ParseError("Parse error: {}".format(p))
+
+
+    #
+    # Token definitions (in order)
+    #
+    def t_BOOL(self, t):
+        r'(TRUE|True|true|FALSE|False|false)'
+        if t.value.lower() == 'true':
+            t.value = True
+        else:
+            t.value = False
+        return t
+
+    def t_FUNCTION_NAME(self, t):
+        r'[a-zA-Z0-9]+[a-zA-Z\_\-]+[a-zA-Z0-9]+\:[A-Z0-9\_]+'
+        t.value = tuple(t.value.split(':'))
+        return t
+
+    def t_NAME(self, t):
+        r'[a-zA-Z0-9]+[a-zA-Z\_\-]+[a-zA-Z0-9]+'
+        return t
+
+    def t_FLOAT(self, t):
+        r'\-?\d*\.\d+'
+        t.value = float(t.value)
+        return t
+
+    def t_INT(self, t):
+        r'\-?\d+'
+        t.value = int(t.value)
+        return t
+
+    def t_QUOTED_STRING(self, t):
+        r'\'(?:[^\\]|(?:\\.))*\''
+        t.value = t.value[1:-1]
+        return t
+
+    def t_DOUBLE_QUOTED_STRING(self, t):
+        r'\"(?:[^\\]|(?:\\.))*\"'
+        t.value = t.value[1:-1]
+        return t
+
+    t_EQUALS = r'\='
+    t_SEP = r'\,'
+    t_LPAR = r'\('
+    t_RPAR = r'\)'
+
+    t_ADD = r'\+'
+    t_SUBTRACT = r'\-'
+    t_MULTIPLY = r'\*'
+    t_DIVIDE = r'\/'
+    t_POWER = r'\^'
+    t_MODULO = r'\%'
+
+    t_ignore = ' \t'
+
+    #
+    # Parser rules
+    #
+    base_parsers = ()
+
+
+    def p_statement(self, p):
+        '''
+        statement : expression
+        '''
+        logger.info("=== AST         : {}".format(p[1]))
+        p[0] = self.process(p[1])
+        logger.info("=== Result      : {}".format(p[0]))
+        logger.info("=== Annotations : {}".format(self.facade.get_annotations(False)))
+
+#
+# Optional parser rules (see cls.parsers variable)
+#
+def p_expression_boolean(self, p):
+    '''
+    expression : BOOL
+    '''
+    p[0] = p[1]
+    logger.debug("boolean: {}".format(p[0]))
+
+def p_expression_number(self, p):
+    '''
+    expression : FLOAT
+               | INT
+    '''
+    p[0] = p[1]
+    logger.debug("number: {}".format(p[0]))
+
+def p_expression_string(self, p):
+    '''
+    expression : QUOTED_STRING
+               | DOUBLE_QUOTED_STRING
+    '''
+    p[0] = p[1]
+    logger.debug("string: {}".format(p[0]))
+
+
+def p_expression_field(self, p):
+    '''
+    expression : NAME
+    '''
+    p[0] = F(p[1])
+    logger.debug("identifier: {}".format(p[0]))
+
+def p_expression_function(self, p):
+    '''
+    expression : function
+    '''
+    p[0] = p[1]
+    logger.debug("function: {}".format(p[0]))
+
+def p_expression_paren(self, p):
+    '''
+    expression : LPAR expression RPAR
+    '''
+    p[0] = p[2]
+    logger.debug("( expression ): {}".format(p[0]))
+
+def p_expression_calculation(self, p):
+    '''
+    expression : expression POWER expression
+               | expression MODULO expression
+               | expression MULTIPLY expression
+               | expression DIVIDE expression
+               | expression ADD expression
+               | expression SUBTRACT expression
+    '''
+    p[0] = self.operations[p[2]](p[1], p[3])
+    logger.debug("expression op expression: {}".format(p[0]))
+
+
+def p_function(self, p):
+    '''
+    function : FUNCTION_NAME
+             | FUNCTION_NAME LPAR RPAR
+    '''
+    field_name  = ":".join(p[1])
+    function    = p[1][1]
+    annotations = {
+        field_name: [ function, F(p[1][0]) ]
+    }
+
+    self.facade.add_annotations(**annotations)
+    p[0] = F(field_name)
+    logger.debug("function: {} {}".format(p[0], annotations))
+
+def p_function_with_params(self, p):
+    '''
+    function : FUNCTION_NAME LPAR parameters RPAR
+    '''
+    field_name = ":".join(p[1])
+    function   = p[1][1]
+    arguments  = [ F(p[1][0]) ]
+    options    = {}
+
+    for parameter in p[3]:
+        if parameter[0] == 'ARG':
+            arguments.append(parameter[1])
+        else:
+            options[parameter[1]] = parameter[2]
+
+    annotations = {
+        field_name: [ function, *arguments, options ]
+    }
+    self.facade.add_annotations(**annotations)
+    p[0] = F(field_name)
+    logger.debug("function: {} {}".format(p[0], annotations))
+
+
+def p_parameters(self, p):
+    '''
+    parameters : parameters SEP parameter
+    '''
+    if isinstance(p[1][0], tuple):
+        parameters = [ parameter for parameter in p[1] ]
+    else:
+        parameters = [ p[1] ]
+
+    parameters.append(p[3])
+    p[0] = tuple(parameters)
+    logger.debug("parameters: {}".format(p[0]))
+
+def p_parameters_single(self, p):
+    '''
+    parameters : parameter
+    '''
+    p[0] = (p[1],)
+    logger.debug("parameters: {}".format(p[0]))
+
+
+def p_parameter_argument_data(self, p):
+    '''
+    parameter : BOOL
+              | INT
+              | FLOAT
+              | QUOTED_STRING
+              | DOUBLE_QUOTED_STRING
+    '''
+    p[0] = ('ARG', p[1])
+    logger.debug("data argument: {}".format(p[0]))
+
+def p_parameter_argument_field(self, p):
+    '''
+    parameter : NAME
+              | function
+    '''
+    p[0] = ('ARG', F(p[1]))
+    logger.debug("field argument: {}".format(p[0]))
+
+def p_parameter_option(self, p):
+    '''
+    parameter : NAME EQUALS BOOL
+              | NAME EQUALS INT
+              | NAME EQUALS FLOAT
+              | NAME EQUALS QUOTED_STRING
+              | NAME EQUALS DOUBLE_QUOTED_STRING
+    '''
+    p[0] = ('OPTION', p[1], p[3])
+    logger.debug("option: {}".format(p[0]))
