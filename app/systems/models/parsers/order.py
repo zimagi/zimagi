ea3d4fb67e1331407701d4a5f25146287faab4fc
Adrian Webb <adrian@webb.sh>
2022-07-18 03:23:18 -0400

    Adding an initial order system model parser class.

---
+++ app/systems/models/parsers/order.py
@@ -0,0 +1,52 @@
+from django.db.models import F
+
+from .base import BaseParser
+
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+class OrderParser(BaseParser):
+    #
+    # Parser indexes
+    #
+    tokens = [
+        *BaseParser.tokens,
+
+        'DASH',
+        'TILDE'
+    ]
+
+    #
+    # Token definitions (in order)
+    #
+    t_DASH = r'\-'
+    t_TILDE = r'\~'
+
+    #
+    # Parser evaluation
+    #
+    def process(self, statement):
+        if isinstance(statement, F):
+            return statement.name
+        return statement
+
+    #
+    # Parser rules
+    #
+    base_parsers = (
+        'p_expression_field',
+        'p_expression_function'
+    )
+
+    def p_expression_field_negation(self, p):
+        '''
+        expression : DASH NAME
+                   | TILDE NAME
+                   | DASH function
+                   | TILDE function
+        '''
+        p[0] = "-{}".format(p[2].name)
+        logger.debug("[-|~]field: {}".format(p[0]))
