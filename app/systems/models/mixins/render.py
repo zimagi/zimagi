3fd73db24782f966d563f8d76b3ac4984239e19d
Adrian Webb <adrian@webb.sh>
2022-07-18 03:27:13 -0400

    Adding an initial render system model mixin.

---
+++ app/systems/models/mixins/render.py
@@ -0,0 +1,10 @@
+from django.utils.timezone import localtime
+
+
+class ModelFacadeRenderMixin(object):
+
+    def get_field_created_display(self, instance, value, short):
+        return localtime(value).strftime("%Y-%m-%d %H:%M:%S %Z")
+
+    def get_field_updated_display(self, instance, value, short):
+        return localtime(value).strftime("%Y-%m-%d %H:%M:%S %Z")
