76630fa0b5ab87fbda198bd792608b2c1a78fb1a
Adrian Webb <adrian@webb.sh>
2022-07-31 05:19:21 -0400

    Including the field processors parser in the field system model parser class parents.

--- app/systems/models/parsers/fields.py
+++ app/systems/models/parsers/fields.py
@@ -1,6 +1,5 @@
-from django.db.models import F
-
 from .base import BaseParser
+from .field_processors import FieldProcessor, FieldProcessorParser
 
 import logging
 
@@ -8,14 +7,14 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-class FieldParser(BaseParser):
-    #
-    # Parser rules
-    #
+class FieldParser(
+    FieldProcessorParser,
+    BaseParser
+):
     base_parsers = (
         'p_expression_number',
         'p_expression_field',
-        'p_expression_function',
+        'p_expression_db_function',
         'p_expression_paren',
         'p_expression_calculation'
     )
@@ -24,8 +23,15 @@ class FieldParser(BaseParser):
         '''
         expression : NAME EQUALS expression
         '''
-        annotations = { p[1].name: p[3] }
+        if isinstance(p[3], FieldProcessor):
+            p[3].name = p[1]
+            p[0] = p[3]
+            logger.debug("name = processor: {}".format(p[0]))
+        else:
+            annotations = { p[1]: p[3] }
 
-        self.facade.add_annotations(**annotations)
-        p[0] = p[1]
-        logger.debug("name = value: {} {}".format(p[0], annotations))
+            if self.facade:
+                self.facade.add_annotations(**annotations)
+
+            p[0] = p[1]
+            logger.debug("name = value: {} {}".format(p[0], annotations))
