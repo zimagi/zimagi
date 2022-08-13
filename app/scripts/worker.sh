3c72d203158f4686de57b00a086865c89281d44e
Adrian Webb <adrian@webb.sh>
2022-08-13 08:07:22 +0000

    Starting watchdog process as polling observer so it works with networked file systems in the platform entrypoint scripts.

--- app/scripts/worker.sh
+++ app/scripts/worker.sh
@@ -8,6 +8,8 @@ if [[ "${ZIMAGI_AUTO_UPDATE^^}" == "TRUE" ]]; then
     --pattern="*.py;*.sh" \
     --recursive \
     --signal SIGTERM \
+    --debug-force-polling \
+    --interval 1 \
     -- zimagi-gateway worker tasks
 else
   zimagi-gateway worker tasks
