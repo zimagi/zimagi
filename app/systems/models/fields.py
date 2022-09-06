c5ced4286ba3c2eb0a5159e2cc54562f689c7d11
Adrian Webb <adrian@webb.sh>
2022-09-06 02:46:24 +0000

    Adding dictionary and list field classes in the system model fields library.

--- app/systems/models/fields.py
+++ app/systems/models/fields.py
@@ -3,6 +3,12 @@ from django.db import models
 from systems.encryption.cipher import Cipher
 from utility.data import serialize, unserialize
 
+import json
+
+
+class FieldError(Exception):
+    pass
+
 
 class EncryptionMixin(object):
 
@@ -81,3 +87,19 @@ class CSVField(models.TextField):
 
     def value_to_string(self, obj):
         return self.value_from_object(obj)
+
+
+class ListField(models.JSONField):
+
+    def __init__(self, *args, **kwargs):
+        kwargs['default'] = list
+        kwargs['null'] = False
+        super().__init__(*args, **kwargs)
+
+
+class DictionaryField(models.JSONField):
+
+    def __init__(self, *args, **kwargs):
+        kwargs['default'] = dict
+        kwargs['null'] = False
+        super().__init__(*args, **kwargs)
