1cb01f6f82d1009985636cc1063eb7c1028324d7
Adrian Webb <adrian@webb.sh>
2022-09-12 06:59:50 +0000

    Removing the top level zimagi package facade and importing command and data clients directly in the zimagi package __init__.

--- package/zimagi/__init__.py
+++ package/zimagi/__init__.py
@@ -1,5 +1,6 @@
-from .facade import Client
-
 from .exceptions import *
 from .command.messages import *
 from .command.response import *
+
+from .command import Client as CommandClient
+from .data import Client as DataClient
