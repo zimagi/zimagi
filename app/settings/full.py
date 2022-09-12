f9bf1703fb091e2e7bd915bde8975dfba07bb94d
Adrian Webb <adrian@webb.sh>
2022-09-12 06:25:44 +0000

    Updating the PostgreSQL configuration variables in the full settings file.

--- app/settings/full.py
+++ app/settings/full.py
@@ -50,11 +50,16 @@ MIDDLEWARE = [
 DB_PACKAGE_ALL_NAME = Config.string('ZIMAGI_DB_PACKAGE_ALL_NAME', 'all')
 DATABASE_ROUTERS = ['systems.db.router.DatabaseRouter']
 
-postgres_service = MANAGER.get_service('pgbouncer')
+if RUN_TESTS:
+    postgres_service = MANAGER.get_service('postgresql')
+    postgres_service_port = postgres_service['ports']['5432/tcp'] if postgres_service else None
+else:
+    postgres_service = MANAGER.get_service('pgbouncer')
+    postgres_service_port = postgres_service['ports']['6432/tcp'] if postgres_service else None
 
 if postgres_service:
     postgres_host = '127.0.0.1'
-    postgres_port = postgres_service['ports']['6432/tcp']
+    postgres_port = postgres_service_port
 else:
     postgres_host = None
     postgres_port = None
