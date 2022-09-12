b1a3b9790bc20e1934d2b4265a86347380eedb65
Adrian Webb <adrian@webb.sh>
2022-09-12 06:58:44 +0000

    Refactoring the zimagi package data client library.

--- package/zimagi/data/client.py
+++ package/zimagi/data/client.py
@@ -1,4 +1,4 @@
-from .. import settings, utility, client
+from .. import settings, utility, client, exceptions
 from .. import codecs as shared_codecs
 from . import codecs, transports
 
@@ -24,22 +24,36 @@ class Client(client.BaseAPIClient):
             verify_cert = verify_cert,
             options_callback = options_callback
         )
+        self.schema = self.get_schema()
+        self._id_fields = {}
+        self._key_fields = {}
+
         if not self.get_status().encryption:
             self.cipher = None
 
 
-    def get_status(self):
-        if not getattr(self, '_status', None):
-            status_url = "/".join([ self.base_url.rstrip('/'), 'status' ])
+    def get_paths(self):
+        return self.schema['paths']
 
-            def processor():
-                return self._request(status_url)
+    def get_id_field(self, data_type):
+        if data_type not in self._id_fields:
+            for parameter in self.schema['paths']["/{}/".format(data_type)]['get']['parameters']:
+                if 'x-id' in parameter['schema']:
+                    self._id_fields[data_type] = parameter['name']
+                    break
+        return self._id_fields[data_type]
 
-            self._status = utility.wrap_api_call('data_status', status_url, processor)
-        return self._status
+    def get_key_field(self, data_type):
+        if data_type not in self._key_fields:
+            for parameter in self.schema['paths']["/{}/".format(data_type)]['get']['parameters']:
+                if 'x-key' in parameter['schema']:
+                    self._key_fields[data_type] = parameter['name']
+                    break
+        return self._key_fields[data_type]
 
 
-    def execute(self, path, options = None):
+
+    def _execute(self, method, path, options = None):
         url = "/".join([ self.base_url.rstrip('/'), path ])
 
         if not options:
@@ -47,45 +61,65 @@ class Client(client.BaseAPIClient):
         options = utility.format_options(options)
 
         def processor():
-            return self._request(url, options)
+            return self._request(method, url, options)
 
         return utility.wrap_api_call('data', path, processor, options)
 
 
-    def _execute_type_operation(self, data_type, op, options):
+    def _execute_type_operation(self, method, data_type, op, options):
         if op is None:
-            return self.execute(data_type, options)
-        return self.execute("{}/{}".format(data_type, op), options)
+            return self._execute(method, data_type, options)
+        return self._execute(method, "{}/{}".format(data_type, op), options)
 
-    def _execute_key_operation(self, data_type, op, key, options):
+    def _execute_key_operation(self, method, data_type, op, key, options):
         if op is None:
-            return self.execute("{}/{}".format(data_type, key), options)
-        return self.execute("{}/{}/{}".format(data_type, key, op), options)
+            return self._execute(method, "{}/{}".format(data_type, key), options)
+        return self._execute(method, "{}/{}/{}".format(data_type, key, op), options)
 
-    def _execute_field_operation(self, data_type, op, field_name, options):
-        return self.execute("{}/{}/{}".format(data_type, op, field_name), options)
-
-
-    def list(self, data_type, options = None):
-        return self._execute_type_operation(data_type, None, options)
-
-    def json(self, data_type, options = None):
-        return self._execute_type_operation(data_type, 'json', options)
-
-    def csv(self, data_type, options = None):
-        return self._execute_type_operation(data_type, 'csv', options)
+    def _execute_field_operation(self, method, data_type, op, field_name, options):
+        return self._execute(method, "{}/{}/{}".format(data_type, op, field_name), options)
 
 
-    def get(self, data_type, key, options = None):
-        return self._execute_key_operation(data_type, None, key, options)
+    def create(self, data_type, **fields):
+        return self._execute_type_operation('POST', data_type, None, fields)
+
+    def update(self, data_type, id, **fields):
+        return self._execute_key_operation('PUT', data_type, None, id, fields)
+
+    def delete(self, data_type, id):
+        return self._execute_key_operation('DELETE', data_type, None, id, {})
+
+    def get(self, data_type, id, **options):
+        return self._execute_key_operation('GET', data_type, None, id, options)
+
+    def get_by_key(self, data_type, key, **options):
+        options[self.get_key_field(data_type)] = key
+        results = self._execute_type_operation('GET', data_type, None, options)
+
+        if results.count == 0:
+            raise exceptions.ResponseError("Instance {} {}: not found".format(data_type, key))
+        elif results.count > 1:
+            raise exceptions.ResponseError("Instance {} {}: too many found".format(data_type, key))
+
+        return results.results[0]
 
 
-    def values(self, data_type, field_name, options = None):
-        return self._execute_field_operation(data_type, 'values', field_name, options)
+    def list(self, data_type, **options):
+        return self._execute_type_operation('GET', data_type, None, options)
 
-    def count(self, data_type, field_name, options = None):
-        return self._execute_field_operation(data_type, 'count', field_name, options)
+    def json(self, data_type, **options):
+        return self._execute_type_operation('GET', data_type, 'json', options)
+
+    def csv(self, data_type, **options):
+        return self._execute_type_operation('GET', data_type, 'csv', options)
+
+
+    def values(self, data_type, field_name, **options):
+        return self._execute_field_operation('GET', data_type, 'values', field_name, options)
+
+    def count(self, data_type, field_name, **options):
+        return self._execute_field_operation('GET', data_type, 'count', field_name, options)
 
 
     def download(self, dataset_name):
-        return self.execute("download/{}".format(dataset_name))
+        return self._execute('GET', "download/{}".format(dataset_name))
