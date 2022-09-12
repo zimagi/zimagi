820e63c958a4bb3f1919d9d5d9b89c2eb24b6fbe
Adrian Webb <adrian@webb.sh>
2022-09-12 07:03:14 +0000

    Updating the command and data client names and removing the facade client accessor in the host data model class.

--- app/data/host/models.py
+++ app/data/host/models.py
@@ -20,20 +20,8 @@ class HostFacade(ModelFacade('host')):
 
 class Host(Model('host')):
 
-    def api(self, options_callback = None, message_callback = None):
-        return zimagi.Client(
-            user = self.user,
-            token = self.token,
-            encryption_key = self.encryption_key if settings.ENCRYPT_COMMAND_API or settings.ENCRYPT_DATA_API else None,
-            host = self.host,
-            command_port = self.command_port,
-            data_port = self.data_port,
-            options_callback = options_callback,
-            message_callback = message_callback
-        )
-
     def command_api(self, options_callback = None, message_callback = None):
-        return zimagi.command.Client(
+        return zimagi.CommandClient(
             user = self.user,
             token = self.token,
             encryption_key = self.encryption_key if settings.ENCRYPT_COMMAND_API else None,
@@ -44,7 +32,7 @@ class Host(Model('host')):
         )
 
     def data_api(self, options_callback = None):
-        return zimagi.data.Client(
+        return zimagi.DataClient(
             user = self.user,
             token = self.token,
             encryption_key = self.encryption_key if settings.ENCRYPT_DATA_API else None,
