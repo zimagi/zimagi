605b73f2ba2cb9898330f724ec895f21f0e5e2cc
Adrian Webb <adrian@webb.sh>
2022-09-12 07:07:14 +0000

    Refactoring the api test library to use the Django test components.

--- app/tests/api.py
+++ app/tests/api.py
@@ -1,14 +1,32 @@
+from django.conf import settings
+from django.test.runner import get_max_test_processes
+from django.test.utils import TimeKeeper
+
 from .base import BaseTest
+from .sdk.runner import TestRunner
+
+import os
 
 
 class Test(BaseTest):
 
     def exec(self):
-        host = self.get_host()
+        time_keeper = TimeKeeper()
+        runner_options = {
+            'interactive': False,
+            'debug_sql': settings.DEBUG,
+            'timing': True,
+            'keepdb': True
+        }
+        if not self.command.no_parallel:
+            runner_options['parallel'] = get_max_test_processes()
 
-        def run_tests(module_name):
-            self.run_tests(module_name, host)
+        with time_keeper.timed('Total run'):
+            test_runner = TestRunner(**runner_options)
+            failures = test_runner.run_tests([
+                os.path.join(settings.BASE_TEST_DIR, 'sdk')
+            ])
+        time_keeper.print_results()
 
-        if host:
-            self.command.info("Running API tests...")
-            self.command.run_list(self.get_test_libs('client'), run_tests)
+        if failures:
+            self.command.error('Test suite api failed')
