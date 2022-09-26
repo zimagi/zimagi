b6e60f7ff91b4e3489d36054dc6a74412ed82ac6
Adrian Webb <adrian@webb.sh>
2022-09-26 10:03:37 +0000

    Adding an initial scaffolding create sdk test library.

---
+++ app/tests/sdk/test_create.py
@@ -0,0 +1,33 @@
+from tests.sdk.base import BaseTest
+
+
+class CreateTest(BaseTest):
+
+    def test_group_create(self):
+        group_data = {
+            'name': 'test3',
+            'provider_type': 'classification',
+            'parent': {
+                'name': 'test1',
+                'provider_type': 'classification'
+            },
+            'children': [
+                {
+                    'name': 'test2',
+                    'provider_type': 'classification',
+                    'hello': 3
+                },
+                {
+                    'name': 'test4',
+                    'provider_type': 'classification',
+                    'config': {
+                        'something': True
+                    }
+                }
+            ]
+        }
+        group = self.data_api.create('group', **group_data)
+        print(group)
+
+        for group_name in ['test1', 'test2', 'test3', 'test4']:
+            self.data_api.delete('group', group_name)
