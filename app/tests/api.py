c8b67b70edf34133eac67cf80abe57a4aa7ae47a
Adrian Webb <adrian@webb.sh>
2022-09-29 06:15:24 +0000

    Refactoring the api test implementation to iterate through test directories.

--- app/tests/api.py
+++ app/tests/api.py
@@ -21,12 +21,13 @@ class Test(BaseTest):
         if not self.command.no_parallel:
             runner_options['parallel'] = get_max_test_processes()
 
-        with time_keeper.timed('Total run'):
-            test_runner = TestRunner(**runner_options)
-            failures = test_runner.run_tests([
-                os.path.join(settings.BASE_TEST_DIR, 'sdk')
-            ])
-        time_keeper.print_results()
+        for test_dir in ('init', 'data'):
+            with time_keeper.timed("Total run: {}".format(test_dir)):
+                test_runner = TestRunner(**runner_options)
+                failures = test_runner.run_tests([
+                    os.path.join(settings.BASE_TEST_DIR, 'sdk', test_dir)
+                ])
+            time_keeper.print_results()
 
-        if failures:
-            self.command.error('Test suite api failed')
+            if failures:
+                self.command.error("Test suite api {} failed".format(test_dir))
