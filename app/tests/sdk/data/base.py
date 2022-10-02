bf446f669144716047920376a1781067dd24f5c5
Adrian Webb <adrian@webb.sh>
2022-10-02 09:14:45 +0000

    Refactoring error handling in the base ask data test library.

--- app/tests/sdk/data/base.py
+++ app/tests/sdk/data/base.py
@@ -1,4 +1,7 @@
+from zimagi.exceptions import ResponseError
+
 from tests.sdk.base import BaseTest
+from utility.data import dump_json
 
 
 class DataBaseTest(BaseTest):
@@ -8,6 +11,8 @@ class DataBaseTest(BaseTest):
 
     @classmethod
     def setup(cls):
+        errors = []
+
         for type in cls.load_types:
             key_field = cls.data_api.get_key_field(type)
             data = cls._load_data(type)
@@ -17,8 +22,20 @@ class DataBaseTest(BaseTest):
 
             for key, fields in data.items():
                 fields[key_field] = key
-                # cls.data_api.create(type, **fields)
-                cls.create_data(type, key, fields)
+                try:
+                    cls.data_api.create(type, **fields)
+                except ResponseError as e:
+                    errors.append("{}: {}\nprovided: {}".format(
+                        type,
+                        e,
+                        dump_json(fields, indent = 2)
+                    ))
+
+                if not errors:
+                    cls.create_data(type, key, fields)
+
+        if errors:
+            raise ResponseError("\n\n".join(errors))
 
     @classmethod
     def create_data(cls, type, key, fields):
@@ -28,15 +45,28 @@ class DataBaseTest(BaseTest):
 
     @classmethod
     def tear_down(cls):
+        errors = []
+
         for type in reversed(cls.load_types):
             id_field = cls.data_api.get_id_field(type)
             data = getattr(cls, "{}_data".format(type))
-            for key in reversed(list(data.keys())):
-                # instance = cls.data_api.get_by_key(type, key)
-                # id = instance[id_field]
 
-                # cls.data_api.delete(type, id)
-                cls.clear_data(type, id, key, data[key])
+            for key in reversed(list(data.keys())):
+                instance = cls.data_api.get_by_key(type, key, **data[key])
+                id = instance[id_field]
+
+                try:
+                    cls.data_api.delete(type, id)
+                except ResponseError as e:
+                    errors.append("{}: {}".format(
+                        type,
+                        e
+                    ))
+                if not errors:
+                    cls.clear_data(type, id, key, data[key])
+
+        if errors:
+            raise ResponseError("\n\n".join(errors))
 
     @classmethod
     def clear_data(cls, type, id, key, fields):
