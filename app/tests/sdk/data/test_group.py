4ebac4cbd1bee021989527ef91ded08933f1d7da
Adrian Webb <adrian@webb.sh>
2022-10-02 09:15:34 +0000

    Renaming and updating the equality assertions in the group sdk data test library.

---
+++ app/tests/sdk/data/test_group.py
@@ -0,0 +1,16 @@
+from tests.sdk.data.base import DataBaseTest
+
+
+DATA_TYPE = 'group'
+
+
+class GroupTest(DataBaseTest):
+
+    load_types = [ DATA_TYPE ]
+
+
+    def test_group_list(self):
+        count = 22
+        response = self.data_api.list(DATA_TYPE)
+        self.assertEqual(response.count, count)
+        self.assertEqual(len(response.results), count)
