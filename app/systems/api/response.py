8fd373f3407cde81581cc503abf2c271b078b354
Adrian Webb <adrian@webb.sh>
2022-10-09 12:19:42 +0000

    Removing the EncryptedCSVReponse class from the base, data, and command systems api response libraries.

--- app/systems/api/response.py
+++ app/systems/api/response.py
@@ -1,5 +1,4 @@
 from django.conf import settings
-from django.http import HttpResponse
 from rest_framework.response import Response
 
 from systems.encryption.cipher import Cipher
@@ -21,25 +20,3 @@ class EncryptedResponse(Response):
         if not self.api_type or not getattr(settings, "ENCRYPT_{}_API".format(self.api_type.replace('_api', '').upper()), True):
             return super().rendered_content
         return Cipher.get(self.api_type, user = self.user).encrypt(super().rendered_content)
-
-
-class EncryptedCSVResponse(HttpResponse):
-
-    api_type = None
-
-    def __init__(self, user = None, api_type = None, **kwargs):
-        super().__init__(**kwargs)
-        self.user = user
-
-        if api_type:
-            self.api_type = api_type
-
-    @property
-    def content(self):
-        if not self.api_type or not getattr(settings, "ENCRYPT_{}_API".format(self.api_type.replace('_api', '').upper()), True):
-            return super().content
-        return Cipher.get(self.api_type, user = self.user).encrypt(super().content)
-
-    @content.setter
-    def content(self, value):
-        super(EncryptedCSVResponse, self.__class__).content.fset(self, value)
