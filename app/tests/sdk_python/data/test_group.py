ecc0a5ec8c94347cd1e224ed90b34a5d9e65ecaf
Adrian Webb <adrian@webb.sh>
2022-10-09 05:54:03 +0000

    Adding a test_group_update method to the Python SDK group data test library.

--- app/tests/sdk_python/data/test_group.py
+++ app/tests/sdk_python/data/test_group.py
@@ -14,3 +14,31 @@ class GroupTest(DataBaseTest):
         response = self.data_api.list(DATA_TYPE)
         self.assertEqual(response.count, count)
         self.assertEqual(len(response.results), count)
+
+
+    def test_group_update(self):
+        config_data = {
+            'first': 1,
+            'second': 2
+        }
+        self.data_api.update(DATA_TYPE, 'test__1',
+            config = config_data
+        )
+        self.assertObjectEqual(
+            self.data_api.get(DATA_TYPE, 'test__1').config,
+            config_data
+        )
+
+        objects = self.data_api.json(DATA_TYPE,
+            fields = [
+                'name',
+                'first=config__first'
+            ],
+            config__first__isnull = False
+        )
+        self.assertEqual(len(objects), 1)
+        self.assertObjectEqual(objects[0], {
+            'name': 'test__1',
+            'first': 1
+        })
+
