b3405613c8f7ed274e0fc3244dc1d44d5e8d96f0
Adrian Webb <adrian@webb.sh>
2022-10-09 23:10:56 +0000

    Removing the encryption flag assertionsin the Python SDK init status test library.

--- app/tests/sdk_python/init/test_status.py
+++ app/tests/sdk_python/init/test_status.py
@@ -9,9 +9,7 @@ class StatusTest(BaseTest):
     def test_data_status(self):
         status_info = self.data_api.get_status()
         self.assertEqual(status_info['message'], SUCCESS_MESSAGE)
-        self.assertTrue(status_info['encryption'])
 
     def test_command_status(self):
         status_info = self.command_api.get_status()
         self.assertEqual(status_info['message'], SUCCESS_MESSAGE)
-        self.assertTrue(status_info['encryption'])
