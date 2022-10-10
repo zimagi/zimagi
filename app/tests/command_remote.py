5097fac4970b05ef114cdd2eee116ca2d3e63391
Adrian Webb <adrian@webb.sh>
2022-10-10 05:56:40 +0000

    Updating the command remote test library to use the inherited command instance for setting host instance.

--- app/tests/command_remote.py
+++ app/tests/command_remote.py
@@ -7,11 +7,9 @@ from tests.command.base import BaseCommandTest
 class Test(BaseCommandTest):
 
     def exec(self):
-        command = ActionCommand('command_test_case')
-
-        command._host.store(settings.DEFAULT_HOST_NAME, {
+        self.command._host.store(settings.DEFAULT_HOST_NAME, {
             'host': 'localhost',
             'encryption_key': settings.ADMIN_API_KEY
         })
         super().exec()
-        command._host.delete(settings.DEFAULT_HOST_NAME)
+        self.command._host.delete(settings.DEFAULT_HOST_NAME)
