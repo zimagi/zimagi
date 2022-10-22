f041b588a949e4c57972fc3a1fe1b9c83f487a59
Adrian Webb <adrian@webb.sh>
2022-10-22 19:29:46 -0400

    Updating the base plugin provider to support segregated secrets capabilities, adding a fiew field accessors, and some parameter cleanup.

--- app/plugins/base.py
+++ app/plugins/base.py
@@ -24,7 +24,7 @@ class ParamSchema(object):
             'options': []
         }
 
-    def require(self, type, name, help, config_name):
+    def require(self, type, name, help, config_name, secret):
         if name in self.schema['options']:
             return
         if name not in self.schema['requirements']:
@@ -32,16 +32,18 @@ class ParamSchema(object):
                 'type': type,
                 'name': name,
                 'help': help,
-                'config_name': config_name
+                'config_name': config_name,
+                'secret': secret
             })
 
-    def option(self, type, name, default, help, config_name):
+    def option(self, type, name, default, help, config_name, secret):
         self.schema['options'].append({
             'type': type,
             'name': name,
             'default': default,
             'help': help,
-            'config_name': config_name
+            'config_name': config_name,
+            'secret': secret
         })
 
     def export(self):
@@ -58,9 +60,17 @@ class BasePlugin(object):
                 return self.config.get(name, default)
             return property(config_accessor)
 
+        def get_secret(name, default):
+            def secret_accessor(self):
+                return self.secrets.get(name, default)
+            return property(secret_accessor)
+
         for config_type in ('requirement', 'option'):
             for name, info in plugin.meta.get(config_type, {}).items():
-                setattr(plugin, "field_{}".format(name), get_config(name, info.get('default', None)))
+                if 'secret' in info and info['secret']:
+                    setattr(plugin, "secret_{}".format(name), get_secret(name, info.get('default', None)))
+                else:
+                    setattr(plugin, "field_{}".format(name), get_config(name, info.get('default', None)))
 
         def check_system(cls):
             return generator.spec['system']
@@ -73,7 +83,9 @@ class BasePlugin(object):
         self.name = name
         self.command = command
         self.errors = []
+
         self.config = {}
+        self.secrets = {}
 
         self.schema = ParamSchema()
         self.provider_type = type
@@ -113,7 +125,7 @@ class BasePlugin(object):
             else:
                 return first == second
 
-        return is_equal(self.config, other.config)
+        return is_equal(self.config, other.config) and is_equal(self.secrets, other.secrets)
 
 
     @property
@@ -141,18 +153,27 @@ class BasePlugin(object):
                 else:
                     raise PluginError(error_message)
 
-            self.config[name] = config[name] if isinstance(config[name], requirement['type']) else requirement['type'](config[name])
+            value = config[name] if isinstance(config[name], requirement['type']) else requirement['type'](config[name])
+            if requirement.get('secret', False):
+                self.secrets[name] = value
+            else:
+                self.config[name] = value
 
         for option in schema['options']:
             name = option['name']
 
             if name not in config:
-                config[name] = copy.deepcopy(option['default'])
+                value = copy.deepcopy(option['default'])
             else:
-                self.config[name] = config[name] if isinstance(config[name], option['type']) else option['type'](config[name])
+                value = config[name] if isinstance(config[name], option['type']) else option['type'](config[name])
+
+            if option.get('secret', False):
+                self.secrets[name] = value
+            else:
+                self.config[name] = value
 
 
-    def provider_config(self, type = None):
+    def provider_config(self):
         config_names = []
 
         for name, info in self.meta.get('requirement', {}).items():
@@ -169,65 +190,99 @@ class BasePlugin(object):
             if name not in config_names:
                 self.config.pop(name)
 
+        for name in list(self.secrets.keys()):
+            if name not in config_names:
+                self.secrets.pop(name)
 
-    def provider_schema(self, type = None):
+
+    def provider_schema(self):
         self.schema.clear()
-        self.provider_config(type)
+        self.provider_config()
         return self.schema.export()
 
 
-    def get_fields(self, type = None):
-        schema = self.provider_schema(type)
+    def get_fields(self):
+        schema = self.provider_schema()
         fields = []
         for field_type, field_data in schema.items():
             for field_info in field_data:
                 fields.append(field_info['name'])
         return fields
 
+    def get_config_fields(self):
+        schema = self.provider_schema()
+        fields = []
+        for field_type, field_data in schema.items():
+            for field_info in field_data:
+                if not field_info.get('secret', False):
+                    fields.append(field_info['name'])
+        return fields
 
-    def requirement(self, type, name, callback = None, callback_args = None, help = None, config_name = None):
+    def get_secret_fields(self):
+        schema = self.provider_schema()
+        fields = []
+        for field_type, field_data in schema.items():
+            for field_info in field_data:
+                if field_info.get('secret', False):
+                    fields.append(field_info['name'])
+        return fields
+
+
+    def requirement(self, type, name, callback = None, callback_args = None, help = None, config_name = None, secret = False):
         if not callback_args:
             callback_args = []
 
         config_value = self.command.get_config(config_name) if self.command else None
 
-        self.schema.require(type, name, help, config_name)
+        def set_data(values):
+            if values.get(name, None) is None:
+                if config_value is not None:
+                    values[name] = config_value
+                elif self.create_op:
+                    self.errors.append("Field '{}' required when adding {} instances".format(name, self.name))
 
-        if self.config.get(name, None) is None:
-            if config_value is not None:
-                self.config[name] = config_value
-            elif self.create_op:
-                self.errors.append("Field '{}' required when adding {} instances".format(name, self.name))
+            if name in values and values[name] is not None:
+                values[name] = self.parse_value(type, values[name])
 
-        if name in self.config and self.config[name] is not None:
-            self.config[name] = self.parse_value(type, self.config[name])
+            if self.create_op and callback and callable(callback):
+                callback_args = [callback_args] if not isinstance(callback_args, (list, tuple)) else callback_args
+                callback(name, values[name], self.errors, *callback_args)
 
-        if self.create_op and callback and callable(callback):
-            callback_args = [callback_args] if not isinstance(callback_args, (list, tuple)) else callback_args
-            callback(name, self.config[name], self.errors, *callback_args)
+        self.schema.require(type, name, help, config_name, secret)
+        if secret:
+            set_data(self.secrets)
+        else:
+            set_data(self.config)
 
-    def option(self, type, name, default = None, callback = None, callback_args = None, help = None, config_name = None):
+
+    def option(self, type, name, default = None, callback = None, callback_args = None, help = None, config_name = None, secret = False):
         if not callback_args:
             callback_args = []
 
         config_value = self.command.get_config(config_name) if self.command else None
-        process = True
 
-        self.schema.option(type, name, default, help, config_name)
+        def set_data(values):
+            process = True
+            if values.get(name, None) is None:
+                if config_value is not None:
+                    values[name] = config_value
+                else:
+                    values[name] = default
+                    process = False
 
-        if self.config.get(name, None) is None:
-            if config_value is not None:
-                self.config[name] = config_value
-            else:
-                self.config[name] = default
-                process = False
+            if values[name] is not None:
+                values[name] = self.parse_value(type, values[name])
 
-        if self.config[name] is not None:
-            self.config[name] = self.parse_value(type, self.config[name])
+            if process and callback and callable(callback):
+                callback_args = [callback_args] if not isinstance(callback_args, (list, tuple)) else callback_args
+                callback(name, values[name], self.errors, *callback_args)
 
-        if process and callback and callable(callback):
-            callback_args = [callback_args] if not isinstance(callback_args, (list, tuple)) else callback_args
-            callback(name, self.config[name], self.errors, *callback_args)
+        self.schema.option(type, name, default, help, config_name, secret)
+        if secret:
+            set_data(self.secrets)
+        else:
+            set_data(self.config)
+
 
     def parse_value(self, type, value):
         if isinstance(value, type):
@@ -255,7 +310,7 @@ class BasePlugin(object):
                 raise PluginError(error_message)
 
 
-    def field_help(self, type = None, exclude_fields = None):
+    def field_help(self, type, exclude_fields = None):
         help = [' ']
 
         def render(messages = '', prefix = ''):
@@ -267,7 +322,7 @@ class BasePlugin(object):
 
         for name, provider_class in self.provider_options.items():
             provider = provider_class(self.provider_type, name, self.command)
-            schema = provider.provider_schema(type)
+            schema = provider.provider_schema()
 
             render("-" * 40)
             render(("provider: {}".format(self._color_text('relation', name)), ' '))
@@ -282,6 +337,9 @@ class BasePlugin(object):
 
                     if require['config_name']:
                         param_help += " (@{})".format(self._color_text('key', require['config_name']))
+
+                    if require.get('secret', False):
+                        param_help += " - {}".format(self._color_text('encrypted', 'secret'))
 
                     param_help += " - {}".format(require['help'])
                     render(param_help, '    ')
@@ -313,6 +371,9 @@ class BasePlugin(object):
 
                     elif option['default'] is not None:
                         param_help[0] += " ({})".format(self._color_text('value', option['default']))
+
+                    if option.get('secret', False):
+                        param_help[0] += " - {}".format(self._color_text('encrypted', 'secret'))
 
                     param_help.append("   - {}".format(option['help']))
                     render(param_help, '    ')
