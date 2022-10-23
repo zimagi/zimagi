abd4eed53d23c8500ab086cfa0232c64919f5d0e
Adrian Webb <adrian@webb.sh>
2022-10-23 04:33:24 -0400

    Adding an initial Git utility class (derived from Git module plugin provider).

---
+++ app/utility/git.py
@@ -0,0 +1,229 @@
+from django.conf import settings
+
+from .temp import temp_dir
+from .filesystem import FileSystem
+
+import pygit2
+
+
+class GitError(Exception):
+    pass
+
+
+class GitCredentials(pygit2.RemoteCallbacks):
+
+    def __init__(self, username, password, public_key_file, private_key_file):
+        self.username = username
+        self.password = password
+        self.public_key_file = public_key_file
+        self.private_key_file = private_key_file
+
+
+    def credentials(self, url, username_from_url, allowed_types):
+        username = username_from_url if username_from_url else self.username
+
+        if allowed_types & pygit2.credentials.GIT_CREDENTIAL_SSH_KEY:
+            return pygit2.Keypair(
+                username,
+                self.public_key_file,
+                self.private_key_file,
+                ''
+            )
+        elif allowed_types & pygit2.credentials.GIT_CREDENTIAL_USERPASS_PLAINTEXT:
+            return pygit2.UserPass(
+                username,
+                self.password
+            )
+        else:
+            return None
+
+
+class Git(object):
+
+    DEFAULT_USER = 'git'
+    DEFAULT_REMOTE = 'origin'
+    DEFAULT_BRANCH = 'main'
+
+
+    @classmethod
+    def clone(cls, remote_url, path,
+        reference = DEFAULT_BRANCH,
+        **auth_options
+    ):
+        with temp_dir() as temp:
+            repository = cls(
+                pygit2.clone_repository(remote_url, path,
+                    checkout_branch = reference,
+                    callbacks = cls._get_credentials(temp, **auth_options)
+                ),
+                auth_options = auth_options
+            )
+        return repository
+
+    @classmethod
+    def init(cls, path,
+        reference = DEFAULT_BRANCH,
+        remote = DEFAULT_REMOTE,
+        remote_url = None
+    ):
+        repository = pygit2.init_repository(
+            path,
+            bare = False,
+            initial_head = reference
+        )
+        if remote and remote_url:
+            repository.remotes.set_url(remote, remote_url)
+            repository.config.set_multivar(
+                "remote.{}.fetch".format(remote),
+                '',
+                "+refs/heads/{}:refs/remotes/{}/{}".format(
+                    reference,
+                    remote,
+                    reference
+                )
+            )
+        return cls(repository, reference = reference)
+
+
+    @classmethod
+    def _get_credentials(cls, temp,
+        username = DEFAULT_USER,
+        password = None,
+        public_key = None,
+        private_key = None
+    ):
+        return GitCredentials(
+            username,
+            password,
+            temp.save(public_key) if public_key else None,
+            temp.save(private_key) if private_key else None
+        )
+
+
+    def __init__(self, repo_reference, reference = DEFAULT_BRANCH, user = None, **auth_options):
+        if isinstance(repo_reference, pygit2.Repository):
+            self.repository = repo_reference
+        else:
+            self.repository = pygit2.Repository(repo_reference)
+
+        self.disk = FileSystem(self.repository.path)
+        self._default_reference = reference
+
+        self.user = None
+        if user:
+            self.set_user(user)
+
+        self.auth_options = {}
+        if auth_options:
+            self.set_auth(**auth_options)
+
+        self.repository.update_submodules(init = True)
+
+
+    @property
+    def reference(self):
+        try:
+            return self.repository.head.shorthand
+        except pygit2.errors.GitError:
+            return self._default_reference
+
+
+    def set_user(self, user):
+        self.user = pygit2.Signature(
+            "{} {}".format(user.first_name, user.last_name) if user.first_name and user.last_name else user.name,
+            user.email if user.email else "{}@{}".format(user.name, settings.APP_NAME)
+        )
+
+
+    def get_remote(self, remote = DEFAULT_REMOTE):
+        return self.repository.remotes[remote]
+
+    def set_remote(self, remote, remote_url):
+        self.repository.remotes.set_url(remote, remote_url)
+
+
+    def set_auth(self, **auth_options):
+        for name, value in auth_options.items():
+            self.auth_options[name] = value
+
+
+    def checkout(self, branch):
+        self.repository.checkout(
+            self.repository.lookup_branch(branch)
+        )
+
+
+    def pull(self, remote = DEFAULT_REMOTE, branch = None):
+        branch = self.reference if not branch else branch
+        if branch != self.reference:
+            self.checkout(branch)
+
+        remote_obj = self.get_remote(remote)
+        with temp_dir() as temp:
+            remote_obj.fetch(
+                callbacks = self._get_credentials(temp, **self.auth_options)
+            )
+
+        remote_reference = self.repository.lookup_reference(
+            'refs/remotes/{}/{}'.format(remote, branch)
+        ).target
+        merge_result, _ = self.repository.merge_analysis(remote_reference)
+
+        if merge_result & pygit2.GIT_MERGE_ANALYSIS_UP_TO_DATE:
+            return
+
+        elif merge_result & pygit2.GIT_MERGE_ANALYSIS_FASTFORWARD:
+            self.repository.checkout_tree(self.repository.get(remote_reference))
+            head_reference = self.repository.lookup_reference(
+                "refs/heads/{}".format(branch)
+            )
+            head_reference.set_target(remote_reference)
+            self.repository.head.set_target(remote_reference)
+
+        elif merge_result & pygit2.GIT_MERGE_ANALYSIS_NORMAL:
+            self.repository.merge(remote_reference)
+
+            tree = self.repository.index.write_tree()
+            self.repository.create_commit('HEAD',
+                self.user, self.user, 'Merge',
+                tree,
+                [ self.repository.head.target, remote_reference ]
+            )
+            self.repository.state_cleanup()
+            self.repository.update_submodules(init = True)
+        else:
+            raise GitError("Unable to pull {} changes from remote {}".format(branch, remote))
+
+
+    def commit(self, message, *files):
+        if files:
+            for file in files:
+                self.repository.index.add(file)
+        else:
+            self.repository.index.add_all()
+
+        self.repository.index.write()
+
+        try:
+            parents = [ self.repository.head.target ]
+        except pygit2.errors.GitError:
+            parents = []
+
+        self.repository.create_commit(
+            "refs/heads/{}".format(self.reference),
+            self.user, self.user, message,
+            self.repository.index.write_tree(),
+            parents
+        )
+
+
+    def push(self, remote = DEFAULT_REMOTE, branch = None):
+        branch = self.reference if not branch else branch
+        if branch != self.reference:
+            self.checkout(branch)
+
+        remote_obj = self.get_remote(remote)
+        with temp_dir() as temp:
+            remote_obj.push([ "refs/heads/{}".format(branch) ],
+                callbacks = self._get_credentials(temp, **self.auth_options)
+            )
