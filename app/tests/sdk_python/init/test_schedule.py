45e243bef3b06a89c156b953e33e4f8661e3d185
Adrian Webb <adrian@webb.sh>
2022-10-25 14:56:12 -0400

    Refactoring the Python SDK init schedule test library to support different commands per schedule type.

--- app/tests/sdk_python/init/test_schedule.py
+++ app/tests/sdk_python/init/test_schedule.py
@@ -11,36 +11,59 @@ class ScheduleTest(BaseTest):
 
 
     def test_interval_schedule(self):
-        self._test_schedule_exec('1M', 5, 4)
-
-    def test_crontab_schedule(self):
-        self._test_schedule_exec('*/1 * * * *', 5, 4)
-
-    def test_datetime_schedule(self):
-        event_time = zimagi.time.shift(zimagi.time.now,
-            units = 2,
-            unit_type = 'minutes',
-            to_string = True
-        )
-        self._test_schedule_exec(event_time, 5, 1)
-
-
-    def _test_schedule_exec(self, schedule_ref, wait_minutes, executions):
         start_time = zimagi.time.now_string
 
         self.command_api.run_task('core', 'echo',
             config = {
                 'text': 'Hello world!'
             },
-            schedule = schedule_ref
+            schedule = '1M'
         )
+        self._test_schedule_exec(5, 4,
+            command = 'task',
+            scheduled = True,
+            created__gt = start_time
+        )
+
+    def test_crontab_schedule(self):
+        start_time = zimagi.time.now_string
+
+        self.command_api.run_task('core', 'echo',
+            config = {
+                'text': 'Hello world!'
+            },
+            schedule = '*/1 * * * *'
+        )
+        self._test_schedule_exec(5, 4,
+            command = 'task',
+            scheduled = True,
+            created__gt = start_time
+        )
+
+    def test_datetime_schedule(self):
+        start_time = zimagi.time.now
+        event_time = zimagi.time.shift(start_time,
+            units = 2,
+            unit_type = 'minutes',
+            to_string = True
+        )
+
+        self.command_api.run_task('core', 'echo',
+            config = {
+                'text': 'Hello world!'
+            },
+            schedule = event_time
+        )
+        self._test_schedule_exec(5, 1,
+            command = 'task',
+            scheduled = True,
+            created__gt = zimagi.time.to_string(start_time)
+        )
+
+
+    def _test_schedule_exec(self, wait_minutes, executions, **filters):
         self.command.sleep(wait_minutes * 60)
 
-        filters = {
-            'command': 'task',
-            'scheduled': True,
-            'created__gt': start_time
-        }
         num_results = self.data_api.count('log', **filters)
         results = self.data_api.json('log', **{
             'fields': [
