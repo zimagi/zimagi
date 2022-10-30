dc6e95d19b62c6b0a1c6684bd056666c123c1352
Adrian Webb <adrian@webb.sh>
2022-10-30 03:36:53 -0400

    Updating the action command handling in the system data API view and serializer libraries.

--- app/systems/api/data/serializers.py
+++ app/systems/api/data/serializers.py
@@ -4,6 +4,7 @@ from rest_framework.relations import HyperlinkedIdentityField
 from rest_framework.serializers import Serializer, ModelSerializer, HyperlinkedModelSerializer, SerializerMethodField
 
 from systems.models import fields as zimagi_fields
+from systems.commands import action
 from .fields import HyperlinkedRelatedField, JSONDataField
 from utility.data import ensure_list, normalize_value
 
@@ -275,14 +276,25 @@ def save_relation(command, facade, field_name, data):
             facade.set_scope({ key: item for key, item in data.items() if key in scope_fields })
             instance = facade.retrieve(data[facade.key()])
 
-        serializer = UpdateSerializer(facade)(
-            instance = instance,
-            data = data,
-            partial = True,
-            command = command
-        )
-        serializer.is_valid(raise_exception = True)
-        instance = serializer.save()
+        sub_command = action.child(command, facade.name, data)
+        success = True
+        try:
+            serializer = UpdateSerializer(facade)(
+                instance = instance,
+                data = data,
+                partial = True,
+                command = sub_command
+            )
+            serializer.is_valid(raise_exception = True)
+            instance = serializer.save()
+
+        except Exception as e:
+            sub_command.error(e, terminate = False)
+            success = False
+            raise e
+        finally:
+            sub_command.log_status(success)
+
         data = getattr(instance, facade.pk)
 
     return data
