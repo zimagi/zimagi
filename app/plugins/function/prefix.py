c48fb9537900ccdd786de65aad53d343bfc12cc9
Adrian Webb <adrian@webb.sh>
2022-12-10 21:44:11 -0500

    Adding a keys parameter to the prefix function plugin provider.

--- app/plugins/function/prefix.py
+++ app/plugins/function/prefix.py
@@ -4,7 +4,7 @@ from utility.data import ensure_list
 
 class Provider(BaseProvider('function', 'prefix')):
 
-    def exec(self, values, prefix = None):
+    def exec(self, values, prefix = None, keys = None):
         if isinstance(values, dict):
             values = list(values.keys())
         else:
@@ -14,6 +14,12 @@ class Provider(BaseProvider('function', 'prefix')):
             return values
 
         for index, value in enumerate(values):
-            values[index] = "{}{}".format(prefix, value)
+            if keys and isinstance(value, (list, tuple, dict)):
+                for key in keys.split('.'):
+                    if isinstance(value, (list, tuple)):
+                        value = value[int(key)]
+                    else:
+                        value = value[key]
 
+            values[index] = "{}{}".format(prefix, value)
         return values
