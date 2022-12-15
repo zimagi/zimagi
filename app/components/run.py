c9d565870087c9cc63238c09104d875c0c29a811
Adrian Webb <adrian@webb.sh>
2022-12-14 21:33:12 -0500

    Adding parallel scope interpolation and thread locking for log keys in the run profile component.

--- app/components/run.py
+++ app/components/run.py
@@ -3,6 +3,8 @@ from django.conf import settings
 from systems.commands import profile
 from utility.data import get_dict_combinations
 
+import threading
+
 
 class ProfileComponent(profile.BaseProfileComponent):
 
@@ -20,6 +22,8 @@ class ProfileComponent(profile.BaseProfileComponent):
         command = self.pop_value('_command', config)
         host = self.pop_value('_host', config)
         queue = self.pop_value('_queue', config) if '_queue' in config else settings.QUEUE_COMMANDS
+
+        thread_lock = threading.Lock()
         log_keys = []
 
         if not task and not command and not '_config' in config:
@@ -32,7 +36,10 @@ class ProfileComponent(profile.BaseProfileComponent):
                 if settings.QUEUE_COMMANDS:
                     data['push_queue'] = queue
 
-                log_keys.append(self.exec(command, **data))
+                keys = self.exec(command, **data)
+                with thread_lock:
+                    log_keys.append(keys)
+
             elif task:
                 options = {
                     'module_key': module,
@@ -44,16 +51,22 @@ class ProfileComponent(profile.BaseProfileComponent):
                 if settings.QUEUE_COMMANDS:
                     options['push_queue'] = queue
 
-                log_keys.append(self.exec('task', **options))
+                keys = self.exec('task', **options)
+                with thread_lock:
+                    log_keys.append(keys)
+
             else:
-                self.profile.config.set(
-                    data.get('_name', name),
-                    data.get('_config', None)
-                )
+                with thread_lock:
+                    self.profile.config.set(
+                        data.get('_name', name),
+                        data.get('_config', None)
+                    )
 
         if scopes:
-            for scope in get_dict_combinations(scopes):
+            def _exec_scope(scope):
                 _execute(self.interpolate(config, **scope))
+
+            self.command.run_list(get_dict_combinations(scopes), _exec_scope)
         else:
             _execute(config)
 
