79df80a361399a9526cf1563278c6cc50d26abf2
Adrian Webb <adrian@webb.sh>
2023-02-17 23:39:38 -0500

    Adding version information in the env get command.

--- app/commands/env/get.py
+++ app/commands/env/get.py
@@ -13,6 +13,7 @@ class Get(Command('env.get')):
             [self.key_color("Image Repository"), self.value_color(str(env.repo))],
             [self.key_color("Base Image"), self.value_color(str(env.base_image))],
             [self.key_color("Runtime Image"), self.value_color(str(env.runtime_image))],
+            [self.key_color("Version"), self.value_color(self.get_version())],
             [self.key_color("Created"), self.value_color(self.format_time(env.created))],
             [self.key_color("Updated"), self.value_color(self.format_time(env.updated))]
         ], 'environment_info')
