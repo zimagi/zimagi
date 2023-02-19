c8dc8f5117d4dcec370b926b36bb0e7413df3bd1
Adrian Webb <adrian@webb.sh>
2023-02-19 18:26:24 -0500

    Readding task processing hooks and worker management initialization and updates in the celery service gateway library.

--- app/services/celery.py
+++ app/services/celery.py
@@ -1,9 +1,42 @@
+from django.conf import settings
+from celery.signals import celeryd_init, before_task_publish, worker_shutting_down
+from kombu import Queue
+
 from systems.celery.app import Celery
 from systems.models.overrides import *
 
 import os
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.full")
+
+
+@celeryd_init.connect
+def capture_service_name(sender, instance, **kwargs):
+    os.environ['ZIMAGI_CELERY_NAME'] = sender
+
+@before_task_publish.connect
+def task_sent_handler(sender, headers = None, body = None, **kwargs):
+    from systems.commands.action import ActionCommand
+    command = ActionCommand('worker')
+
+    queue = None
+    for entity in kwargs['declare']:
+        if isinstance(entity, Queue):
+            queue = entity.name
+            break
+    if queue:
+        worker = command.get_provider('worker', settings.WORKER_PROVIDER,
+            worker_type = queue,
+            command_name = body[0][0],
+            command_options = body[1]
+        )
+        worker.ensure()
+
+@worker_shutting_down.connect
+def cleanup_worker_manager(*args, **kwargs):
+    if manager:
+        manager.terminate()
+
 
 manager = None
 app = Celery('Zimagi',
@@ -16,3 +49,7 @@ if os.environ.get('ZIMAGI_BOOTSTRAP_DJANGO', None):
     import django
     django.setup()
     app.autodiscover_tasks(force = True)
+
+if os.environ.get('ZIMAGI_WORKER_EXEC', None):
+    from systems.celery.worker import start_worker_manager
+    manager = start_worker_manager(app)
