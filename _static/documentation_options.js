6fbad2cccdd2bb60328d854687b00dbf46a80c00
CircleCI ( adrianwebb ) <adrianwebb@main>
2023-02-27 15:54:33 +0000

    Building and publishing documentation updates

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.11.21',
+    VERSION: '0.11.22',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
