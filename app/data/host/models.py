74c5a737f3fdad75961861982863d2c1413277f0
Adrian Webb <adrian@webb.sh>
2023-03-05 01:01:19 -0500

    Updating the host model to fill in local runtime ports on client access instead of during save process.

--- app/data/host/models.py
+++ app/data/host/models.py
@@ -21,37 +21,36 @@ class HostFacade(ModelFacade('host')):
 class Host(Model('host')):
 
     def command_api(self, options_callback = None, message_callback = None):
+        port = self.command_port
         client = None
-        if self.command_port:
+
+        if self.host == 'localhost' and not port and settings.COMMAND_API_PORT:
+            port = settings.COMMAND_API_PORT
+        if port:
             client = zimagi.CommandClient(
                 user = self.user,
                 token = self.token,
                 encryption_key = self.encryption_key,
                 host = self.host,
-                port = self.command_port,
+                port = port,
                 options_callback = options_callback,
                 message_callback = message_callback
             )
         return client
 
     def data_api(self, options_callback = None):
+        port = self.data_port
         client = None
-        if self.data_port:
+
+        if self.host == 'localhost' and not port and settings.DATA_API_PORT:
+            port = settings.DATA_API_PORT
+        if port:
             client = zimagi.DataClient(
                 user = self.user,
                 token = self.token,
                 encryption_key = self.encryption_key,
                 host = self.host,
-                port = self.data_port,
+                port = port,
                 options_callback = options_callback
             )
         return client
-
-
-    def save(self, *args, **kwargs):
-        if self.host and self.host == 'localhost':
-            if settings.COMMAND_API_PORT and not self.command_port:
-                self.command_port = settings.COMMAND_API_PORT
-            if settings.DATA_API_PORT and not self.data_port:
-                self.data_port = settings.DATA_API_PORT
-        super().save(*args, **kwargs)
