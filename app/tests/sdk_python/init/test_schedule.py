e364f2b5c75299742d28d3c918190c30ff409378
Adrian Webb <adrian@webb.sh>
2023-03-12 23:55:34 -0400

    Refactoring the test case class structure of the Python SDK init test schedule library and replacing print statements with command messages.

--- app/tests/sdk_python/init/test_schedule.py
+++ app/tests/sdk_python/init/test_schedule.py
@@ -6,77 +6,10 @@ import time
 import zimagi
 
 
-@tag('init', 'schedule')
-class ScheduleTest(BaseTest):
-
-    @classmethod
-    def _message_callback(cls, message):
-        pass
-
-
-    @tag('schedule_interval')
-    def test_interval_schedule(self):
-        start_time = zimagi.time.now_string
-
-        print('starting interval: {}'.format(start_time))
-        self.command_api.run_task('core', 'echo',
-            config = {
-                'text': 'Hello interval!'
-            },
-            schedule = '1M'
-        )
-        self._test_schedule_exec(5,
-            command = 'task',
-            config__task_fields__text__icontains = 'interval',
-            schedule__isnull = False,
-            created__gt = start_time
-        )
-
-
-    @tag('schedule_crontab')
-    def test_crontab_schedule(self):
-        start_time = zimagi.time.now_string
-
-        print('starting crontab: {}'.format(start_time))
-        self.command_api.run_task('core', 'echo',
-            config = {
-                'text': 'Hello crontab!'
-            },
-            schedule = '*/1 * * * *'
-        )
-        self._test_schedule_exec(5,
-            command = 'task',
-            config__task_fields__text__icontains = 'crontab',
-            schedule__isnull = False,
-            created__gt = start_time
-        )
-
-
-    @tag('schedule_datetime')
-    def test_datetime_schedule(self):
-        start_time = zimagi.time.now
-        event_time = zimagi.time.shift(start_time,
-            units = 2,
-            unit_type = 'minutes',
-            to_string = True
-        )
-
-        print('starting datetime: {}'.format(start_time))
-        self.command_api.run_task('core', 'echo',
-            config = {
-                'text': 'Hello datetime!'
-            },
-            schedule = event_time
-        )
-        self._test_schedule_exec(5,
-            command = 'task',
-            config__task_fields__text__icontains = 'datetime',
-            schedule__isnull = False,
-            created__gt = zimagi.time.to_string(start_time)
-        )
-
+class ScheduleBaseTest(BaseTest):
 
     def _test_schedule_exec(self, wait_minutes, **filters):
+        reference_name = filters['config__task_fields__text__icontains']
         allowed_time = (wait_minutes * 60)
         start_time = time.time()
         current_time = start_time
@@ -84,12 +17,19 @@ class ScheduleTest(BaseTest):
         while (current_time - start_time) < allowed_time:
             num_results = self.data_api.count('log', **filters)
             if num_results:
-                print('num results {} at time {} with: {}'.format(num_results, current_time, filters))
+                self.command.success("Found {} results for {} schedule at {}".format(
+                    num_results,
+                    reference_name,
+                    zimagi.time.now_string
+                ))
                 break
 
             self.command.sleep(5)
             current_time = time.time()
-            print('checking time {} with: {}'.format(current_time, filters))
+            self.command.info("Checking results for {} schedule at {}".format(
+                reference_name,
+                zimagi.time.now_string
+            ))
 
         results = self.data_api.json('log', **{
             'fields': [
@@ -105,5 +45,77 @@ class ScheduleTest(BaseTest):
             ],
             **filters
         })
+        self.command.data("Results for {} schedule".format(reference_name), str(results))
         self.assertGreaterEqual(num_results, 1)
         self.assertGreaterEqual(len(results), num_results)
+
+
+@tag('init', 'schedule')
+class ScheduleIntervalTest(ScheduleBaseTest):
+
+    @tag('schedule_interval')
+    def test_interval_schedule(self):
+        start_time = zimagi.time.now_string
+
+        self.command.notice("Starting interval schedule at {}".format(start_time))
+        self.command_api.run_task('core', 'echo',
+            config = {
+                'text': 'Hello interval!'
+            },
+            schedule = '1M'
+        )
+        self._test_schedule_exec(5,
+            command = 'task',
+            config__task_fields__text__icontains = 'interval',
+            schedule__isnull = False,
+            created__gt = start_time
+        )
+
+
+@tag('init', 'schedule')
+class ScheduleCrontabTest(ScheduleBaseTest):
+
+    @tag('schedule_crontab')
+    def test_crontab_schedule(self):
+        start_time = zimagi.time.now_string
+
+        self.command.notice("Starting crontab schedule at {}".format(start_time))
+        self.command_api.run_task('core', 'echo',
+            config = {
+                'text': 'Hello crontab!'
+            },
+            schedule = '*/1 * * * *'
+        )
+        self._test_schedule_exec(5,
+            command = 'task',
+            config__task_fields__text__icontains = 'crontab',
+            schedule__isnull = False,
+            created__gt = start_time
+        )
+
+
+@tag('init', 'schedule')
+class ScheduleDatetimeTest(ScheduleBaseTest):
+
+    @tag('schedule_datetime')
+    def test_datetime_schedule(self):
+        start_time = zimagi.time.now
+        event_time = zimagi.time.shift(start_time,
+            units = 2,
+            unit_type = 'minutes',
+            to_string = True
+        )
+
+        self.command.notice("Starting datetime schedule at {}".format(start_time))
+        self.command_api.run_task('core', 'echo',
+            config = {
+                'text': 'Hello datetime!'
+            },
+            schedule = event_time
+        )
+        self._test_schedule_exec(5,
+            command = 'task',
+            config__task_fields__text__icontains = 'datetime',
+            schedule__isnull = False,
+            created__gt = zimagi.time.to_string(start_time)
+        )
