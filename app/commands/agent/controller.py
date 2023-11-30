275b78ae52c450ecf4444494053dac52a121edf0
Adrian Webb <adrian@webb.sh>
2023-11-30 13:58:44 -0500

    Refactoring the controller agent to use the system manager collect_agents method.

--- app/commands/agent/controller.py
+++ app/commands/agent/controller.py
@@ -7,32 +7,16 @@ from systems.commands.index import Agent
 class Controller(Agent('controller')):
 
     def exec(self):
-        def process_agents(spec, name = None, parents = None):
-            if parents is None:
-                parents = []
-            if name and name == 'controller' and not parents:
-                return
-
-            if name and 'base' in spec:
-                command = [ 'agent', *parents, name ]
-
-                worker = self.get_provider('worker', settings.WORKER_PROVIDER, app,
-                    worker_type = spec.get('worker_type', 'default'),
-                    command_name = " ".join(command),
-                    command_options = spec.get('options', {})
-                )
-
-                if self._check_agent_schedule(spec):
-                    worker.scale_agents(self.get_config("{}_count".format("_".join(command)), 0))
-                else:
-                    worker.scale_agents(0)
-            else:
-                sub_parents = [ *parents, name ] if name else parents
-
-                for key, value in spec.items():
-                    if isinstance(value, dict):
-                        process_agents(value, key, sub_parents)
-
         self.notice("Running agent manager")
         self.manager.reset_spec()
-        process_agents(self.manager.interpolate_spec('command.agent'))
+
+        for agent in self.manager.collect_agents():
+            worker = self.get_provider('worker', settings.WORKER_PROVIDER, app,
+                worker_type = agent.spec.get('worker_type', 'default'),
+                command_name = " ".join(agent.command),
+                command_options = agent.spec.get('options', {})
+            )
+            if self._check_agent_schedule(agent.spec):
+                worker.scale_agents(self.get_config(self.manager._get_agent_scale_config(agent.command), 0))
+            else:
+                worker.scale_agents(0)
