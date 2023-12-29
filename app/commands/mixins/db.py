4d9eed0a0267004d554bd5501ab2ffac1fae4919
Adrian Webb <adrian@webb.sh>
2023-12-29 00:24:19 -0500

    Removing the clean_logs call in the create_snapshot method of the database command mixin.

--- app/commands/mixins/db.py
+++ app/commands/mixins/db.py
@@ -69,7 +69,6 @@ class DatabaseMixin(CommandMixin('db')):
             module_disk.clone(temp.path('modules'))
 
             self.info('Backing up application database')
-            self.clean_logs()
             self.dump('db.tar', temp.base_path)
 
             self.info('Backing up application files')
