a25cf74216d8a82a52e3ef4243945572a4f28897
Adrian Webb <adrian@webb.sh>
2024-12-26 23:30:17 -0500

    Adding an initial zimagi reactor command.

---
+++ reactor/commands/zimagi.sh
@@ -0,0 +1,50 @@
+#
+#=========================================================================================
+# <Zimagi> Command
+#
+
+function zimagi_description () {
+  render "Execute a Zimagi operation within the reactor environment context"
+  export PASSTHROUGH="1"
+}
+
+function zimagi_command () {
+  kubernetes_environment
+
+  import_environment
+  docker_runtime_image
+
+  ZIMAGI_ARGS=(
+    "--rm"
+    "--interactive"
+    "--tty"
+    "--network" "host"
+    "--group-add" "${ZIMAGI_DOCKER_GROUP}"
+    "--volume" "/var/run/docker.sock:/var/run/docker.sock"
+    "--volume" "${__zimagi_app_dir}:/usr/local/share/zimagi"
+    "--volume" "${__zimagi_lib_dir}:/usr/local/lib/zimagi"
+    "--volume" "${__zimagi_data_dir}:/var/local/zimagi"
+    "--env" "ZIMAGI_CLI_EXEC=True"
+  )
+  if [ ! -z "${KUBECONFIG:-}" ]; then
+    ZIMAGI_ARGS=("${ZIMAGI_ARGS[@]}" "--volume" "${KUBECONFIG}:/home/zimagi/.kube/config")
+  fi
+  while IFS= read -r variable; do
+    ZIMAGI_ARGS=("${ZIMAGI_ARGS[@]}" "--env" "$variable")
+  done <<< "$(env | grep -o "KUBERNETES_[_A-Z0-9]*")"
+
+  while IFS= read -r variable; do
+    ZIMAGI_ARGS=("${ZIMAGI_ARGS[@]}" "--env" "$variable")
+  done <<< "$(env | grep -o "ZIMAGI_[_A-Z0-9]*")"
+
+  if [ "$ZIMAGI_DOCKER_RUNTIME" != "standard" ]; then
+    ZIMAGI_ARGS=("${ZIMAGI_ARGS[@]}" "--runtime" "$ZIMAGI_DOCKER_RUNTIME")
+  fi
+
+  if [[ ! "${@}" ]]; then
+    ZIMAGI_ARGS=("${ZIMAGI_ARGS[@]}" "$ZIMAGI_RUNTIME_IMAGE" "help")
+  else
+    ZIMAGI_ARGS=("${ZIMAGI_ARGS[@]}" "$ZIMAGI_RUNTIME_IMAGE" "${@}")
+  fi
+  docker run "${ZIMAGI_ARGS[@]}"
+}
