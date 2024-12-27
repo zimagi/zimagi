8e42abdf2cfcfe255afae3df75b2d5941f632b23
Adrian Webb <adrian@webb.sh>
2024-12-27 18:31:41 -0500

    Adding environment loading in the reactor build_image script.

--- reactor/build_image.sh
+++ reactor/build_image.sh
@@ -6,6 +6,10 @@ set -e
 NO_CACHE=$1
 #-------------------------------------------------------------------------------
 
+# Initialie Zimagi environment
+zimagi_environment
+
+# Clean directories
 cp -f "${__zimagi_app_dir}/VERSION" "${__zimagi_package_dir}/VERSION"
 
 find "${__zimagi_app_dir}" -name *.pyc -exec rm -f {} \;
@@ -26,6 +30,7 @@ if [ -d "${__zimagi_data_dir}/run" ]; then
   done
 fi
 
+# Build Docker Build Environment
 export ZIMAGI_CA_KEY="${APP_CA_KEY}"
 export ZIMAGI_CA_CERT="${APP_CA_CERT}"
 export ZIMAGI_KEY="${APP_KEY}"
@@ -34,11 +39,11 @@ export ZIMAGI_CERT="${APP_CERT}"
 export DOCKER_BUILD_VARS=(
   "ZIMAGI_PARENT_IMAGE"
   "ZIMAGI_USER_UID=$(id -u)"
-  "ZIMAGI_USER_PASSWORD=${ZIMAGI_USER_PASSWORD:-$DEFAULT_ZIMAGI_USER_PASSWORD}"
+  "ZIMAGI_USER_PASSWORD"
   "ZIMAGI_CA_KEY"
   "ZIMAGI_CA_CERT"
   "ZIMAGI_KEY"
   "ZIMAGI_CERT"
-  "ZIMAGI_DATA_KEY=${ZIMAGI_DATA_KEY:-$DEFAULT_ZIMAGI_DATA_KEY}"
+  "ZIMAGI_DATA_KEY"
   "ZIMAGI_DEFAULT_MODULES"
 )
