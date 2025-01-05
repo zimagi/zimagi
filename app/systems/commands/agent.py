69c05ced9a631942ba9ab104e061834d1b753552
Adrian Webb <adrian@webb.sh>
2025-01-05 03:45:50 -0500

    Adding some state environment variables and updating the queues to use threads in the kubernetes agent class and agent base command.

--- app/systems/commands/agent.py
+++ app/systems/commands/agent.py
@@ -6,7 +6,7 @@ from utility.data import dump_json, load_json
 from utility.parallel import Parallel
 from utility.display import format_exception_info
 
-import multiprocessing
+import queue
 import time
 import re
 import logging
@@ -17,10 +17,9 @@ logger = logging.getLogger(__name__)
 
 class AgentCommand(exec.ExecCommand):
 
-    process_queues = [ 'default' ]
+    process_queues = ["default"]
     processes = ()
     process_map = {}
-
 
     def signal_shutdown(self):
         try:
@@ -29,17 +28,19 @@ class AgentCommand(exec.ExecCommand):
                 process.join()
 
         except Exception as error:
-            logger.info("Signal shutdown for base executable command errored with: {}".format(error))
+            logger.info(
+                "Signal shutdown for base executable command errored with: {}".format(
+                    error
+                )
+            )
 
         super().signal_shutdown()
 
+    def run_from_argv(self, argv, options=None):
+        return super().run_from_argv(argv, self.spec.get("options", {}))
 
-    def run_from_argv(self, argv, options = None):
-        return super().run_from_argv(argv, self.spec.get('options', {}))
-
-
-    def _exec_local_handler(self, log_key, primary = True):
-        profiler_name = 'exec.agent.local.primary' if primary else 'exec.agent.local'
+    def _exec_local_handler(self, log_key, primary=True):
+        profiler_name = "exec.agent.local.primary" if primary else "exec.agent.local"
         notify = False
 
         if (primary and settings.WORKER_EXEC) or not self.set_queue_task(log_key):
@@ -47,11 +48,13 @@ class AgentCommand(exec.ExecCommand):
                 self.preprocess_handler(self.options, primary)
                 try:
                     self.start_profiler(profiler_name)
-                    self.run_exclusive(self.lock_id, self.repeat_exec,
-                        error_on_locked = self.lock_error,
-                        timeout = self.lock_timeout,
-                        interval = self.lock_interval,
-                        run_once = self.run_once
+                    self.run_exclusive(
+                        self.lock_id,
+                        self.repeat_exec,
+                        error_on_locked=self.lock_error,
+                        timeout=self.lock_timeout,
+                        interval=self.lock_interval,
+                        run_once=self.run_once,
                     )
                 finally:
                     self.stop_profiler(profiler_name)
@@ -61,9 +64,8 @@ class AgentCommand(exec.ExecCommand):
                 self.postprocess_handler(self.exec_result, primary)
         return notify
 
-
     def _exec_api_handler(self, log_key):
-        profiler_name = 'exec.agent.api'
+        profiler_name = "exec.agent.api"
         try:
             self.start_profiler(profiler_name)
             self.set_queue_task(log_key, True)
@@ -71,44 +73,29 @@ class AgentCommand(exec.ExecCommand):
             self.stop_profiler(profiler_name)
         return False
 
-
     def repeat_exec(self):
-        process_manager = multiprocessing.Manager()
-        process_queues = process_manager.dict()
-        for queue in self.process_queues:
-            process_queues[queue] = process_manager.Queue()
+        process_queues = {}
+        for process_queue in self.process_queues:
+            process_queues[process_queue] = queue.Queue()
 
         def exec_process(name):
-            def run_loop(queues):
-                self._process_queues = queues
+            self.info("Starting process {}".format(name))
+            self._process_queues = process_queues
 
-                self.exec()
-                self.exec_loop(name, getattr(self, name))
+            self.exec()
+            self.exec_loop(name, getattr(self, name))
 
-            process = multiprocessing.Process(
-                name = name,
-                daemon = True,
-                target = run_loop,
-                args = [ process_queues ]
-            )
-            self.process_map[name] = process
-
-            process.start()
-            process.join()
-
-            if process.exitcode != 0:
-                self.error("Process {} failed".format(name))
+            self.info("Finished process {}".format(name))
 
         if self.processes:
             Parallel.list(
                 self.processes,
                 exec_process,
-                thread_count = len(self.processes),
-                disable_parallel = False
+                thread_count=len(self.processes),
+                disable_parallel=False,
             )
         else:
-            self.exec_loop('main', self.exec)
-
+            self.exec_loop("main", self.exec)
 
     def exec_init(self, name):
         # Implement in subclass if needed
@@ -122,17 +109,20 @@ class AgentCommand(exec.ExecCommand):
         while True:
             try:
                 self.exec_init(name)
-                self.run_exec_loop(name, exec_callback,
-                    terminate_callback = self.terminate_agent,
-                    pause = self.pause
+                self.run_exec_loop(
+                    name,
+                    exec_callback,
+                    terminate_callback=self.terminate_agent,
+                    pause=self.pause,
                 )
                 self.exec_exit(name, True, None)
 
             except Exception as e:
-                self.error(str(e),
-                    prefix = name,
-                    traceback = format_exception_info(),
-                    terminate = False
+                self.error(
+                    str(e),
+                    prefix=name,
+                    traceback=format_exception_info(),
+                    terminate=False,
                 )
                 self.exec_exit(name, False, e)
 
@@ -142,22 +132,18 @@ class AgentCommand(exec.ExecCommand):
     def terminate_agent(self):
         return False
 
-
-    def push(self, data, name = 'default', block = True, timeout = None):
+    def push(self, data, name="default", block=True, timeout=None):
         queue = self._process_queues.get(name, None)
         if not queue:
             self.error("Process queue {} not defined".format(name))
 
         try:
-            queue.put(dump_json(data),
-                block = block,
-                timeout = timeout
-            )
+            queue.put(dump_json(data), block=block, timeout=timeout)
             return True
         except Full:
             return False
 
-    def pull(self, name = 'default', timeout = 0, block_sec = 10, terminate_callback = None):
+    def pull(self, name="default", timeout=0, block_sec=10, terminate_callback=None):
         queue = self._process_queues.get(name, None)
         if not queue:
             self.error("Process queue {} not defined".format(name))
@@ -173,10 +159,7 @@ class AgentCommand(exec.ExecCommand):
 
         while not terminate_callback(name):
             try:
-                data = load_json(queue.get(
-                    block = True,
-                    timeout = block_sec
-                ))
+                data = load_json(queue.get(block=True, timeout=block_sec))
                 start_time = time.time()
 
             except Empty:
@@ -190,33 +173,59 @@ class AgentCommand(exec.ExecCommand):
             if timeout and ((current_time - start_time) > timeout):
                 break
 
-
     def _check_agent_schedule(self, spec):
         current_time = self.time.components()
 
-        if 'weeks' in spec and spec['weeks'].strip() != '*' and current_time.week not in self._expand_time_values(spec['weeks']):
+        if (
+            "weeks" in spec
+            and spec["weeks"].strip() != "*"
+            and current_time.week not in self._expand_time_values(spec["weeks"])
+        ):
             return False
-        if 'weekdays' in spec and spec['weekdays'].strip() != '*' and current_time.weekday not in self._expand_time_values(spec['weekdays']):
+        if (
+            "weekdays" in spec
+            and spec["weekdays"].strip() != "*"
+            and current_time.weekday not in self._expand_time_values(spec["weekdays"])
+        ):
             return False
-        if 'months' in spec and spec['months'].strip() != '*' and current_time.month not in self._expand_time_values(spec['months']):
+        if (
+            "months" in spec
+            and spec["months"].strip() != "*"
+            and current_time.month not in self._expand_time_values(spec["months"])
+        ):
             return False
-        if 'days' in spec and spec['days'].strip() != '*' and current_time.day not in self._expand_time_values(spec['days']):
+        if (
+            "days" in spec
+            and spec["days"].strip() != "*"
+            and current_time.day not in self._expand_time_values(spec["days"])
+        ):
             return False
-        if 'hours' in spec and spec['hours'].strip() != '*' and current_time.hour not in self._expand_time_values(spec['hours']):
+        if (
+            "hours" in spec
+            and spec["hours"].strip() != "*"
+            and current_time.hour not in self._expand_time_values(spec["hours"])
+        ):
             return False
-        if 'minutes' in spec and spec['minutes'].strip() != '*' and current_time.minute not in self._expand_time_values(spec['minutes']):
+        if (
+            "minutes" in spec
+            and spec["minutes"].strip() != "*"
+            and current_time.minute not in self._expand_time_values(spec["minutes"])
+        ):
             return False
         return True
 
     def _expand_time_values(self, pattern):
         values = []
-        for value in re.sub(r'\s+', '', pattern).split(','):
-            value_range = re.match(r'^(\d+)\-(\d+)$', value)
+        for value in re.sub(r"\s+", "", pattern).split(","):
+            value_range = re.match(r"^(\d+)\-(\d+)$", value)
             if value_range:
-                values.extend(list(range(
-                    int(value_range.group(1)),
-                    (int(value_range.group(2)) + 1)
-                )))
+                values.extend(
+                    list(
+                        range(
+                            int(value_range.group(1)), (int(value_range.group(2)) + 1)
+                        )
+                    )
+                )
             else:
                 values.append(int(value))
         return values
--- app/systems/kubernetes/agent.py
+++ app/systems/kubernetes/agent.py
@@ -14,35 +14,37 @@ logger = logging.getLogger(__name__)
 class KubeAgent(KubeBase):
 
     def get_spec(self, name, command):
-        type = 'agent'
+        type = "agent"
         labels = self._get_labels(name, type)
 
         if not command:
             command = []
 
         return client.V1Deployment(
-            api_version = 'apps/v1',
-            kind = 'Deployment',
-            metadata = self._get_metadata_spec(labels, name),
-            spec = client.V1DeploymentSpec(
-                replicas = 1,
-                revision_history_limit = 2,
-                selector = client.V1LabelSelector(
-                    match_labels = self._get_selector_labels(name, type)
+            api_version="apps/v1",
+            kind="Deployment",
+            metadata=self._get_metadata_spec(labels, name),
+            spec=client.V1DeploymentSpec(
+                replicas=1,
+                revision_history_limit=2,
+                selector=client.V1LabelSelector(
+                    match_labels=self._get_selector_labels(name, type)
                 ),
-                template = self._get_pod_spec(
+                template=self._get_pod_spec(
                     name,
                     labels,
-                    [ 'zimagi', *ensure_list(command) ],
-                    restart_policy = 'Always'
-                )
-            )
+                    ["zimagi", *ensure_list(command)],
+                    env={"ZIMAGI_CLI_EXEC": "True", "ZIMAGI_WORKER_EXEC": "True"},
+                    restart_policy="Always",
+                ),
+            ),
         )
-
 
     def check(self, name):
         try:
-            self.cluster.apps_api.read_namespaced_deployment(name, self.cluster.namespace)
+            self.cluster.apps_api.read_namespaced_deployment(
+                name, self.cluster.namespace
+            )
 
         except client.ApiException as e:
             if e.status == 404:
@@ -53,9 +55,10 @@ class KubeAgent(KubeBase):
 
     def create(self, name, command):
         def create_agent(cluster):
-            cluster.apps_api.create_namespaced_deployment(cluster.namespace, self.get_spec(name, command),
-                pretty = False
+            cluster.apps_api.create_namespaced_deployment(
+                cluster.namespace, self.get_spec(name, command), pretty=False
             )
+
         return self.cluster.exec("create {} agent".format(self.type), create_agent)
 
     def destroy(self, name):
