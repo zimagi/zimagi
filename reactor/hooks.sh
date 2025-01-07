f33c0238e228a6d70be222482121d00cfd5f5864
Adrian Webb <adrian@webb.sh>
2025-01-07 00:19:39 -0500

    Adding Zimagi related post update hook commands.

--- reactor/hooks.sh
+++ reactor/hooks.sh
@@ -12,4 +12,16 @@ function hook_modified () {
 function hook_update () {
   info "Initializing Zimagi CLI ..."
   run_subcommand zimagi env get
+  run_subcommand zimagi host save "${__environment}" \
+    host="cmd.${ZIMAGI_DOMAIN}" \
+    command_port="443" \
+    user="${ZIMAGI_ADMIN_USER}" \
+    token="${ZIMAGI_DEFAULT_ADMIN_TOKEN}" \
+    encryption_key="${ZIMAGI_ADMIN_API_KEY}"
+}
+
+function hook_update_host () {
+  info "Checking Zimagi CLI ${__environment} connection ..."
+  run_subcommand zimagi user list --host="${__environment}"
+  run_subcommand zimagi config save option_environment_host "${__environment}"
 }
