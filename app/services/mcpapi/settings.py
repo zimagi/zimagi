e5c287206205bb25fe901ed9b0cf9a30b85b8888
Adrian Webb <adrian@webb.sh>
2025-04-27 07:03:46 -0400

    Adding an initial MCP service settings file.

---
+++ app/services/mcpapi/settings.py
@@ -0,0 +1,18 @@
+"""
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/4.1/ref/settings/
+"""
+
+# -------------------------------------------------------------------------------
+# Global settings
+
+#
+# Display configurations
+#
+DISPLAY_COLOR = False
+
+# -------------------------------------------------------------------------------
+# Core Django settings
+
+# -------------------------------------------------------------------------------
+# Django Addons
