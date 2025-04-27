1f41358b472593fca83c28baeac90a92379b4892
Adrian Webb <adrian@webb.sh>
2025-04-27 06:45:31 -0400

    Adding an initial MCP gateway configuration script.

---
+++ app/scripts/config/mcp.sh
@@ -0,0 +1,14 @@
+export ZIMAGI_SERVICE_PROCESS=(
+  "uvicorn"
+  "services.mcp:application"
+  "$([ "${ZIMAGI_DISABLE_HTTPS,,}" == "true" ] && echo "" || echo "--ssl-cert-reqs=1")"
+  "$([ "${ZIMAGI_DISABLE_HTTPS,,}" == "true" ] && echo "" || echo "--ssl-certfile=/etc/ssl/certs/zimagi.crt")"
+  "$([ "${ZIMAGI_DISABLE_HTTPS,,}" == "true" ] && echo "" || echo "--ssl-keyfile=/etc/ssl/private/zimagi.key")"
+  "--log-level=${ZIMAGI_LOG_LEVEL:-info}"
+  "--timeout-keep-alive=${ZIMAGI_SERVER_TIMEOUT:-14400}"
+  "--workers=1"
+  "--limit-concurrency=${ZIMAGI_SERVER_CONNECTIONS:-100}"
+  "--backlog=${ZIMAGI_SERVER_MAX_PENDING_CONNECTIONS:-3000}"
+  "--host=0.0.0.0"
+  "--port=5000"
+)
