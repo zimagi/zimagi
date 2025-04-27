dcc75d15733b027a81ba7c58515dfe474e7080d4
Adrian Webb <adrian@webb.sh>
2025-04-27 07:07:22 -0400

    Adding an initial MCP application gateway.

---
+++ app/services/mcp.py
@@ -0,0 +1,55 @@
+"""
+Zimagi ASGI config.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
+"""
+
+import os
+import django
+
+from starlette.applications import Starlette
+from starlette.routing import Mount, Route
+from starlette.middleware import Middleware
+from starlette.middleware.authentication import AuthenticationMiddleware
+from mcp.server.sse import SseServerTransport
+
+from systems.models.overrides import *  # noqa: F401, F403
+from systems.api.mcp.auth import TokenAuthBackend
+
+from systems.api.mcp.session import MCPSessions
+from systems.api.mcp.routes import create_connection_handler, create_message_handler, handle_status
+from utility.mutex import Mutex
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.full")
+os.environ.setdefault("DJANGO_ALLOW_ASYNC_UNSAFE", "true")
+
+django.setup()
+
+sse = SseServerTransport("/messages/")
+sessions = MCPSessions()
+
+
+def get_asgi_application():
+    from django.conf import settings
+
+    return Starlette(
+        debug=settings.DEBUG,
+        routes=[
+            Route("/", endpoint=create_connection_handler(sse, sessions)),
+            Mount("/messages/", app=create_message_handler(sse, sessions)),
+            Route("/status", endpoint=handle_status, methods=["GET"]),
+        ],
+        middleware=[Middleware(AuthenticationMiddleware, backend=TokenAuthBackend())],
+    )
+
+
+#
+# Application Initialization
+#
+application = get_asgi_application()
+
+Mutex.set("startup_{}".format(os.environ["ZIMAGI_SERVICE"]))
