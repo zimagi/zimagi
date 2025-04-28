3365c41d9dd8a292ab7931c465e10b8ff53c8928
Adrian Webb <adrian@webb.sh>
2025-04-28 18:34:03 -0400

    Adding conditional checks around store-cert and store-key implementation in the application script library.

--- app/scripts/store-key.py
+++ app/scripts/store-key.py
@@ -7,15 +7,17 @@ import sys
 # -------------------------------------------------------------------------------
 
 key_path = sys.argv[1]
-components = re.search(r"^(\-+[^\-]+\-+)\s+(.+)\s+(\-+[^\-]+\-+)$", sys.argv[2], re.DOTALL)
 
-if components:
-    key_prefix = components.group(1)
-    key_material = "\n".join(re.split(r"\s+", components.group(2)))
-    key_suffix = components.group(3)
-else:
-    raise Exception(f"Key {key_path} entered is not correct format: {sys.argv[2]}")
+if sys.argv[2]:
+    components = re.search(r"^(\-+[^\-]+\-+)\s+(.+)\s+(\-+[^\-]+\-+)$", sys.argv[2], re.DOTALL)
 
-with open(key_path, "w") as file:
-    file.write(f"{key_prefix}\n{key_material}\n{key_suffix}")
-    os.chmod(key_path, 0o660)
+    if components:
+        key_prefix = components.group(1)
+        key_material = "\n".join(re.split(r"\s+", components.group(2)))
+        key_suffix = components.group(3)
+    else:
+        raise Exception(f"Key {key_path} entered is not correct format: {sys.argv[2]}")
+
+    with open(key_path, "w") as file:
+        file.write(f"{key_prefix}\n{key_material}\n{key_suffix}")
+        os.chmod(key_path, 0o660)
