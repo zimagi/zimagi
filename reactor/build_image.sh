18708983293aebfc8db9d76ff6a4f8a86561aebf
Adrian Webb <adrian@webb.sh>
2025-04-30 16:33:32 -0400

    Removing some deprecated operations in the Reactor build_image script.

--- reactor/build_image.sh
+++ reactor/build_image.sh
@@ -6,22 +6,15 @@ set -e
 NO_CACHE=$1
 #-------------------------------------------------------------------------------
 
-# Initialie Zimagi environment
+# Initialize Zimagi environment
 cert_environment
 zimagi_environment
 
-# Clean directories
-cp -f "${__zimagi_app_dir}/VERSION" "${__zimagi_package_dir}/VERSION"
-
-find "${__zimagi_app_dir}" -name *.pyc -exec rm -f {} \;
-find "${__zimagi_package_dir}" -name *.pyc -exec rm -f {} \;
-find "${__zimagi_lib_dir}" -name *.pyc -exec rm -f {} \;
-
 # Build Docker Build Environment
-export ZIMAGI_CA_KEY="${APP_CA_KEY}"
-export ZIMAGI_CA_CERT="${APP_CA_CERT}"
-export ZIMAGI_KEY="${APP_KEY}"
-export ZIMAGI_CERT="${APP_CERT}"
+export ZIMAGI_CA_KEY="${APP_CA_KEY:-}"
+export ZIMAGI_CA_CERT="${APP_CA_CERT:-}"
+export ZIMAGI_KEY="${APP_KEY:-}"
+export ZIMAGI_CERT="${APP_CERT:-}"
 
 export DOCKER_BUILD_VARS=(
   "ZIMAGI_PARENT_IMAGE"
