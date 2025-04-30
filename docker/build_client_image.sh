4e7eebef5a5ba80df3fe33369e4c5d60d512cee7
Adrian Webb <adrian@webb.sh>
2025-04-30 16:32:34 -0400

    Updating the version specification and removing no cache and force rebuild flags for the docker build command in the Docker build_client_image script.

--- docker/build_client_image.sh
+++ docker/build_client_image.sh
@@ -8,10 +8,10 @@ export __zimagi_dir="$(dirname "${__zimagi_docker_dir}")"
 source "${__zimagi_dir}/.env"
 #-------------------------------------------------------------------------------
 
-ZIMAGI_BUILD_IMAGE="zimagi/client:$(cat "${__zimagi_dir}/app/VERSION")"
+ZIMAGI_BUILD_IMAGE="zimagi/client:${ZIMAGI_VERSION}"
 
 echo "Building Zimagi Client Docker image: ${ZIMAGI_BUILD_IMAGE}"
-docker build --force-rm --no-cache \
+docker build \
     --file "${__zimagi_docker_dir}/Dockerfile.client" \
     --tag "$ZIMAGI_BUILD_IMAGE" \
     --build-arg ZIMAGI_USER_UID \
