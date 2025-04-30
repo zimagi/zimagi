f96dc89ab4c307417575eb9cd0610e70a61b05f7
Adrian Webb <adrian@webb.sh>
2025-04-30 12:26:00 -0400

    Adding an initial system CLI client errors library.

---
+++ app/systems/client/cli/errors.py
@@ -0,0 +1,13 @@
+from django.core.management.base import CommandError
+
+
+class CommandNetworkError(CommandError):
+    pass
+
+
+class CommandNotFoundError(CommandError):
+    pass
+
+
+class CommandMessageError(CommandError):
+    pass
