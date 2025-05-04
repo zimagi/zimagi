66952ebe66adbdc0d04cac7694be7ea74ebb87f1
Adrian Webb <adrian@webb.sh>
2025-05-03 23:32:05 -0400

    Refactoring the system API command schema library.

--- app/systems/api/command/schema.py
+++ app/systems/api/command/schema.py
@@ -1,32 +1,46 @@
 import urllib
 
+from django.conf import settings
 from rest_framework.schemas.generators import BaseSchemaGenerator
 from rest_framework.schemas.inspectors import ViewInspector
 from systems.api.command.views import Command
-from systems.commands.schema import Document, Link, Object
+from systems.commands import help, schema
 
 
 class CommandSchemaGenerator(BaseSchemaGenerator):
     def get_schema(self, request=None, public=False):
         self._initialise_endpoints()
 
-        links = self.get_links(None if public else request)
-        if not links:
+        commands = self.get_commands(None if public else request)
+        if not commands:
             return None
 
         url = self.url
         if not url and request is not None:
             url = request.build_absolute_uri()
 
-        return Document(url=url, title=self.title, description=self.description, content=links)
+        return schema.Root(commands, url=url, title=self.title, description=self.description)
 
-    def get_links(self, request=None):
-        links = Object()
+    def get_commands(self, request=None):
+        commands = schema.Router()
+        descriptions = help.CommandDescriptions()
 
-        def insert_link(target, keys, value):
+        def insert_action(target, keys, value):
+            router = []
             for key in keys[:-1]:
+                router.append(key)
+
                 if key not in target:
-                    target[key] = Object()
+                    full_name = " ".join(router)
+                    spec = settings.MANAGER.get_spec("command." + ".".join(router))
+
+                    target[key] = schema.Router(
+                        name=full_name,
+                        overview=descriptions.get(full_name, True),
+                        description=descriptions.get(full_name, False),
+                        priority=spec.get("priority", 1),
+                        resource=spec.get("resource", None),
+                    )
                 target = target[key]
 
             target[keys[-1]] = value
@@ -42,10 +56,11 @@ class CommandSchemaGenerator(BaseSchemaGenerator):
                 continue
 
             resource = view.get_resource() if isinstance(view, Command) else None
-
             keys = [component for component in path[len(prefix) :].strip("/").split("/")]
-            insert_link(links, keys, view.schema.get_link(path, method, base_url=self.url, resource=resource))
-        return links
+
+            insert_action(commands, keys, view.schema.get_action(path, method, base_url=self.url, resource=resource))
+
+        return commands
 
     def _determine_path_prefix(self, paths):
         prefixes = []
@@ -76,38 +91,53 @@ class CommandSchemaGenerator(BaseSchemaGenerator):
         return "/" + "/".join(common)
 
 
-class BaseSchema(ViewInspector):
-    def __init__(self, fields=None, description="", encoding=None):
+class StatusSchema(ViewInspector):
+
+    def __init__(self, encoding=None):
         super().__init__()
-        self._fields = fields
-        self._description = description
         self._encoding = encoding
 
-    def get_link(self, path, method, base_url, resource):
+    def get_action(self, path, method, base_url, resource):
         if base_url and path.startswith("/"):
             path = path[1:]
 
-        return Link(
+        return schema.Action(
             url=urllib.parse.urljoin(base_url, path),
-            action=method.lower(),
+            method=method.lower(),
             encoding=self._encoding,
-            fields=self._fields,
-            description=self._description,
-            resource=resource,
         )
 
 
-class StatusSchema(BaseSchema):
-    pass
+class CommandSchema(ViewInspector):
 
+    def __init__(self, name="", overview="", description="", priority=1, encoding=None, fields=None):
+        super().__init__()
+        self._name = name
+        self._overview = overview
+        self._description = description
+        self._priority = priority
+        self._encoding = encoding
+        self._fields = fields
+        self._field_map = {}
 
-class CommandSchema(BaseSchema):
-    def __init__(self, fields, description="", encoding=None):
-        super().__init__(fields, description, encoding)
-        self.field_map = {}
+    def get_action(self, path, method, base_url, resource):
+        if base_url and path.startswith("/"):
+            path = path[1:]
+
+        return schema.Action(
+            url=urllib.parse.urljoin(base_url, path),
+            method=method.lower(),
+            encoding=self._encoding,
+            name=self._name,
+            overview=self._overview,
+            description=self._description,
+            priority=self._priority,
+            resource=resource,
+            fields=self._fields,
+        )
 
     def get_fields(self):
-        if not self.field_map:
+        if not self._field_map:
             for field in self._fields:
-                self.field_map[field.name] = field
-        return self.field_map
+                self._field_map[field.name] = field
+        return self._field_map
