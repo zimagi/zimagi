85576e85df496a93c40a46538ffceffe84ff77ce
Adrian Webb <adrian@webb.sh>
2025-05-03 23:48:35 -0400

    Updating the zimagi package command codecs library to reflect changes in the command schema.

--- package/zimagi/command/codecs.py
+++ package/zimagi/command/codecs.py
@@ -38,6 +38,13 @@ def get_string(item, key):
     return ""
 
 
+def get_number(item, key, default=None):
+    value = item.get(key)
+    if isinstance(value, (int, float)):
+        return value
+    return default
+
+
 def get_list(item, key):
     value = item.get(key)
     if isinstance(value, list):
@@ -58,6 +65,10 @@ def unescape_key(string):
     return string
 
 
+class CommandParseError(Exception):
+    pass
+
+
 class ZimagiJSONCodec:
     media_types = ["application/zimagi+json", "application/vnd.zimagi+json"]
 
@@ -69,36 +80,46 @@ class ZimagiJSONCodec:
         except ValueError as exc:
             raise exceptions.ParseError(f"Malformed JSON. {exc}")
 
-        document = self._convert_to_document(data, base_url)
-        if isinstance(document, schema.Object):
-            document = schema.Document(content=dict(document))
+        document = self._convert_to_schema(data, base_url)
 
-        elif not (isinstance(document, schema.Document) or isinstance(document, schema.Error)):
-            raise exceptions.ParseError("Top level node should be a document or error.")
+        if not (isinstance(document, schema.Root) or isinstance(document, schema.Error)):
+            raise CommandParseError("Top level node should be a root or error.")
 
         return document
 
-    def _convert_to_document(self, data, base_url=None):
-        if isinstance(data, dict) and data.get("_type") == "document":
+    def _convert_to_schema(self, data, base_url=None):
+        if isinstance(data, dict) and data.get("_type") == "root":
             meta = get_dict(data, "_meta")
             url = urllib.parse.urljoin(base_url, get_string(meta, "url"))
-            return schema.Document(
+            return schema.Root(
+                commands=self._get_document_content(data, base_url=url),
                 url=url,
                 title=get_string(meta, "title"),
                 description=get_string(meta, "description"),
                 media_type="application/zimagi+json",
-                content=self._get_document_content(data, base_url=url),
             )
         if isinstance(data, dict) and data.get("_type") == "error":
             meta = get_dict(data, "_meta")
             return schema.Error(title=get_string(meta, "title"), content=self._get_document_content(data, base_url=base_url))
-        elif isinstance(data, dict) and data.get("_type") == "link":
-            return schema.Link(
+        elif isinstance(data, dict) and data.get("_type") == "router":
+            meta = get_dict(data, "_meta")
+            return schema.Router(
+                commands=self._get_document_content(data, base_url=base_url),
+                name=get_string(meta, "name"),
+                overview=get_string(meta, "overview"),
+                description=get_string(meta, "description"),
+                priority=get_number(meta, "priority", 1),
+                resource=get_string(meta, "resource"),
+            )
+        elif isinstance(data, dict) and data.get("_type") == "action":
+            return schema.Action(
                 url=urllib.parse.urljoin(base_url, get_string(data, "url")),
-                action=get_string(data, "action"),
+                method=get_string(data, "method"),
                 encoding=get_string(data, "encoding"),
-                title=get_string(data, "title"),
+                name=get_string(data, "name"),
+                overview=get_string(data, "overview"),
                 description=get_string(data, "description"),
+                priority=get_number(data, "priority", 1),
                 resource=get_string(data, "resource"),
                 fields=[
                     schema.Field(
@@ -117,12 +138,12 @@ class ZimagiJSONCodec:
         elif isinstance(data, dict):
             return schema.Object(self._get_document_content(data, base_url=base_url))
         elif isinstance(data, list):
-            return schema.Array([self._convert_to_document(item, base_url) for item in data])
+            return schema.Array([self._convert_to_schema(item, base_url) for item in data])
         return data
 
     def _get_document_content(self, item, base_url=None):
         return {
-            unescape_key(key): self._convert_to_document(value, base_url)
+            unescape_key(key): self._convert_to_schema(value, base_url)
             for key, value in item.items()
             if key not in ("_type", "_meta")
         }
