60f476cf284aa9683fc00c5cc9058c3ed9239a22
Adrian Webb <adrian@webb.sh>
2025-05-07 01:25:35 -0400

    Completing the initial implementation of the system CLI client help command library.

--- app/systems/client/cli/commands/help.py
+++ app/systems/client/cli/commands/help.py
@@ -1,15 +1,59 @@
+from django.conf import settings
+from utility.text import wrap
+
+from zimagi.command.schema import Router
+
 from .base import BaseExecutable, SubCommandMixin
+from .router import RouterCommand
 
 
 class HelpCommand(SubCommandMixin, BaseExecutable):
 
-    def __init__(self, client, schema):
-        super().__init__(client, schema)
+    def __init__(self, index, schema):
+        super().__init__(index, schema)
         self.title = schema.title
         self.description = schema.description
 
-    def exec(self, argv):
-        print(self.commands)
-        print(self.title)
-        print(self.description)
-        print(argv)
+    def get_arg_boundary(self):
+        return super().get_arg_boundary() + 1
+
+    def exec(self):
+        if not self.args:
+            self.render_overview()
+        else:
+            self.render_command()
+
+    def render_overview(self):
+        usage = [
+            "",
+            "Type '{}' for help on a specific subcommand.".format(
+                self.command_color(f"{settings.APP_NAME} help <subcommand> ...")
+            ),
+            "",
+            "Available subcommands:",
+            "",
+        ]
+
+        def render_command(command, width, init_indent, indent):
+            usage.extend(
+                wrap(
+                    command.overview,
+                    width,
+                    init_indent="{:{width}}{}  -  ".format(" ", self.command_color(command.name), width=init_indent),
+                    init_style=self.header_color,
+                    indent="".ljust(indent),
+                )
+            )
+            if isinstance(command, Router):
+                for subcommand in RouterCommand(self.index, command).get_subcommands():
+                    render_command(subcommand, width - 5, init_indent + 5, indent + 5)
+
+        for subcommand in self.get_subcommands():
+            if subcommand.name not in ["help", "agent"]:
+                render_command(subcommand, settings.DISPLAY_WIDTH, 1, 5)
+
+        self.print("\n".join(usage))
+
+    def render_command(self):
+        command = self.index.find(self.args)
+        command.print_help()
