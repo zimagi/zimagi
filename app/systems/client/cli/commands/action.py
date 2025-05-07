61057f15df5f57b2bf5377115fd4fafde3677963
Adrian Webb <adrian@webb.sh>
2025-05-07 01:28:57 -0400

    Completing the initial implementation of the system CLI client action command library.

--- app/systems/client/cli/commands/action.py
+++ app/systems/client/cli/commands/action.py
@@ -1,17 +1,49 @@
+from systems.commands.args import get_type
+
 from .base import BaseCommand
 
 
 class ActionCommand(BaseCommand):
 
-    def __init__(self, client, schema):
-        super().__init__(client, schema)
+    def __init__(self, index, schema):
+        super().__init__(index, schema)
         self.fields = schema.fields
 
-    def exec(self, argv):
-        print(self.name)
-        print(self.overview)
-        print(self.description)
-        print(self.priority)
-        print(self.resource)
-        print(self.fields)
-        print(argv)
+    def parse(self):
+        for field in self.fields:
+            if field.name not in ["json_options"]:
+                if field.method == "flag":
+                    self.parse_flag(
+                        field.name, field.argument, field.description, config_name=field.config, default=field.default
+                    )
+                elif field.method == "variable":
+                    self.parse_variable(
+                        field.name,
+                        field.argument if field.argument else not field.required,
+                        get_type(field.type),
+                        field.description,
+                        value_label=field.value_label,
+                        config_name=field.config,
+                        default=field.default,
+                        choices=field.choices,
+                    )
+                elif field.method == "variables":
+                    self.parse_variables(
+                        field.name,
+                        field.argument if field.argument else not field.required,
+                        get_type(field.type),
+                        field.description,
+                        value_label=field.value_label,
+                        config_name=field.config,
+                        default=field.default,
+                    )
+                elif field.method == "fields":
+                    self.parse_fields(
+                        field.name,
+                        field.description,
+                        value_label=field.value_label,
+                        optional=not field.required,
+                    )
+
+    def exec(self):
+        self.client.execute(self.name, **self.options)
