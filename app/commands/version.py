f625a803d340ca64784917272d5474116499eb74
Adrian Webb <adrian@webb.sh>
2025-05-08 15:53:45 -0400

    Updating the implementation of the version command to only retrieve the server version.

--- app/commands/version.py
+++ app/commands/version.py
@@ -1,20 +1,6 @@
-from django.conf import settings
 from systems.commands.index import Command
 
 
 class Version(Command("version")):
     def exec(self):
-        if not settings.API_EXEC:
-            host = self.get_host()
-            version_info = [[self.key_color("Client version"), self.value_color(self.get_version())]]
-            if host:
-                response = self.exec_remote(host, "version", display=False)
-
-                version_info.extend(
-                    [
-                        [self.key_color("Host version"), self.value_color(response["host_version"])],
-                    ]
-                )
-            self.table(version_info, "version_info")
-        else:
-            self.silent_data("host_version", self.get_version())
+        self.table([["Server version", self.get_version()]], "version_info")
