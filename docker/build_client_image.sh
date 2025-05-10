8dca4a8e80adac871484b35c0b5dd283d41083df
Adrian Webb <adrian@webb.sh>
2025-05-10 15:42:37 -0400

    Fixing Docker build arguments and version handling in the docker build_client_image script.

--- docker/build_client_image.sh
+++ docker/build_client_image.sh
@@ -4,17 +4,15 @@ set -e
 
 export __zimagi_docker_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 export __zimagi_dir="$(dirname "${__zimagi_docker_dir}")"
-
-source "${__zimagi_dir}/.env"
 #-------------------------------------------------------------------------------
 
+ZIMAGI_VERSION=$(cat "${__zimagi_dir}/app/VERSION")
 ZIMAGI_BUILD_IMAGE="zimagi/client:${ZIMAGI_VERSION}"
 
 echo "Building Zimagi Client Docker image: ${ZIMAGI_BUILD_IMAGE}"
 docker build \
     --file "${__zimagi_docker_dir}/Dockerfile.client" \
     --tag "$ZIMAGI_BUILD_IMAGE" \
-    --build-arg ZIMAGI_USER_UID \
-    --build-arg ZIMAGI_USER_PASSWORD \
+    --build-arg ZIMAGI_USER_UID="$(id -u)" \
     --build-arg ZIMAGI_ENVIRONMENT=local \
     "${__zimagi_dir}"
