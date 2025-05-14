73a1dd7ecd814b20c67b3012c7c73b2433421d56
Adrian Webb <adrian@webb.sh>
2025-05-14 02:15:31 -0400

    Updating the Docker image name in the Docker build_client_image script.

--- docker/build_client_image.sh
+++ docker/build_client_image.sh
@@ -7,11 +7,11 @@ export __zimagi_dir="$(dirname "${__zimagi_docker_dir}")"
 #-------------------------------------------------------------------------------
 
 ZIMAGI_VERSION=$(cat "${__zimagi_dir}/app/VERSION")
-ZIMAGI_BUILD_IMAGE="zimagi/client:${ZIMAGI_VERSION}"
+ZIMAGI_BUILD_IMAGE="zimagi/cli:${ZIMAGI_VERSION}"
 
 echo "Building Zimagi Client Docker image: ${ZIMAGI_BUILD_IMAGE}"
 docker build \
-    --file "${__zimagi_docker_dir}/Dockerfile.client" \
+    --file "${__zimagi_docker_dir}/Dockerfile.cli" \
     --tag "$ZIMAGI_BUILD_IMAGE" \
     --build-arg ZIMAGI_USER_UID="$(id -u)" \
     --build-arg ZIMAGI_ENVIRONMENT=local \
