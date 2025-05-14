e2c0c264ab2fbc6ccc96e499668575590622f0fb
Adrian Webb <adrian@webb.sh>
2025-05-14 02:17:24 -0400

    Removing the SSL variable handling and the data encryption key variable in various reactor scripts.

--- reactor/build/shared.sh
+++ reactor/build/shared.sh
@@ -1,15 +1,5 @@
 
-function init_certs() {
-  cert_environment
-
-  export ZIMAGI_CA_KEY="${APP_CA_KEY:-}"
-  export ZIMAGI_CA_CERT="${APP_CA_CERT:-}"
-  export ZIMAGI_KEY="${APP_KEY:-}"
-  export ZIMAGI_CERT="${APP_CERT:-}"
-}
-
 function server_build_args() {
-  init_certs
   zimagi_environment
 
   export DOCKER_BUILD_VARS=(
@@ -17,11 +7,6 @@ function server_build_args() {
     "ZIMAGI_ENVIRONMENT=${__environment}"
     "ZIMAGI_USER_UID=$(id -u)"
     "ZIMAGI_USER_PASSWORD"
-    "ZIMAGI_CA_KEY"
-    "ZIMAGI_CA_CERT"
-    "ZIMAGI_KEY"
-    "ZIMAGI_CERT"
-    "ZIMAGI_DATA_KEY"
     "ZIMAGI_DEFAULT_MODULES"
   )
 }
