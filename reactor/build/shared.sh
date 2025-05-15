591d932f69e0e0d74fd0283282dd92ef3e6555ae
Adrian Webb <adrian@webb.sh>
2025-05-15 07:47:56 -0400

    Readding the ZIMAGI_USER_UID build argumentsin the shared reactor build script.

--- reactor/build/shared.sh
+++ reactor/build/shared.sh
@@ -5,6 +5,7 @@ function server_build_args() {
   export DOCKER_BUILD_VARS=(
     "ZIMAGI_PARENT_IMAGE"
     "ZIMAGI_ENVIRONMENT=${__environment}"
+    "ZIMAGI_USER_UID=$(id -u)"
     "ZIMAGI_USER_PASSWORD"
     "ZIMAGI_DEFAULT_MODULES"
   )
@@ -16,5 +17,6 @@ function client_build_args() {
   export DOCKER_BUILD_VARS=(
     "ZIMAGI_PARENT_IMAGE"
     "ZIMAGI_ENVIRONMENT=${__environment}"
+    "ZIMAGI_USER_UID=$(id -u)"
   )
 }
