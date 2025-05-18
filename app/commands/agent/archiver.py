e106e81482d10a83da4a1caa24eac6c0c24b8416
Adrian Webb <adrian@webb.sh>
2025-05-18 01:12:19 -0400

    Updating the archiver agent implementation with message collection and updated scaling_event fields.

--- app/commands/agent/archiver.py
+++ app/commands/agent/archiver.py
@@ -1,4 +1,5 @@
 from systems.commands.index import Agent
+from utility.data import Collection
 
 
 class Archiver(Agent("archiver")):
@@ -6,13 +7,14 @@ class Archiver(Agent("archiver")):
 
     def record_scaling_event(self):
         for package in self.listen("worker:scaling", state_key="core_archiver"):
-            message = package.message
+            message = Collection(**package.message)
             self.save_instance(
                 self._scaling_event,
                 None,
                 fields={
                     "created": package.time,
-                    "log_id": message.log_key,
+                    "command": message.command,
+                    "worker_type": message.worker_type,
                     "worker_max_count": message.worker_max_count,
                     "worker_count": message.worker_count,
                     "task_count": message.task_count,
